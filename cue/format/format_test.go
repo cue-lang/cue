b001da55dbd277ef02111a6c28009d078f108cbc
Marcel van Lohuizen <mpvl@golang.org>
2018-12-10 15:34:30 +0100

    cue/format: add package
    Change-Id: I09091c053009fc29d1426e1037f67933c805d158

---
+++ cue/format/format_test.go
@@ -0,0 +1,415 @@
+// Copyright 2018 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package format
+
+// TODO: port more of the tests of go/printer
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"cuelang.org/go/cue/ast"
+	"cuelang.org/go/cue/parser"
+	"cuelang.org/go/cue/token"
+)
+
+var (
+	defaultConfig = newConfig([]Option{FileSet(fset)})
+	Fprint        = defaultConfig.fprint
+)
+
+const (
+	dataDir  = "testdata"
+	tabwidth = 8
+)
+
+var update = flag.Bool("update", false, "update golden files")
+
+var fset = token.NewFileSet()
+
+type checkMode uint
+
+const (
+	export checkMode = 1 << iota
+	rawFormat
+	idempotent
+	simplify
+)
+
+// format parses src, prints the corresponding AST, verifies the resulting
+// src is syntactically correct, and returns the resulting src or an error
+// if any.
+func format(src []byte, mode checkMode) ([]byte, error) {
+	// parse src
+	var opts []Option
+	if mode&simplify != 0 {
+		opts = append(opts, Simplify())
+	}
+
+	res, err := Source(src, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	// make sure formatted output is syntactically correct
+	if _, err := parser.ParseFile(fset, "", res,
+		parser.ParseLambdas, parser.AllErrors); err != nil {
+		return nil, fmt.Errorf("re-parse: %s\n%s", err, res)
+	}
+
+	return res, nil
+}
+
+// lineAt returns the line in text starting at offset offs.
+func lineAt(text []byte, offs int) []byte {
+	i := offs
+	for i < len(text) && text[i] != '\n' {
+		i++
+	}
+	return text[offs:i]
+}
+
+// diff compares a and b.
+func diff(aname, bname string, a, b []byte) error {
+	var buf bytes.Buffer // holding long error message
+
+	// compare lengths
+	if len(a) != len(b) {
+		fmt.Fprintf(&buf, "\nlength changed: len(%s) = %d, len(%s) = %d", aname, len(a), bname, len(b))
+	}
+
+	// compare contents
+	line := 1
+	offs := 1
+	for i := 0; i < len(a) && i < len(b); i++ {
+		ch := a[i]
+		if ch != b[i] {
+			fmt.Fprintf(&buf, "\n%s:%d:%d: %s", aname, line, i-offs+1, lineAt(a, offs))
+			fmt.Fprintf(&buf, "\n%s:%d:%d: %s", bname, line, i-offs+1, lineAt(b, offs))
+			fmt.Fprintf(&buf, "\n\n")
+			break
+		}
+		if ch == '\n' {
+			line++
+			offs = i + 1
+		}
+	}
+
+	if buf.Len() > 0 {
+		return errors.New(buf.String())
+	}
+	return nil
+}
+
+func runcheck(t *testing.T, source, golden string, mode checkMode) {
+	src, err := ioutil.ReadFile(source)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	res, err := format(src, mode)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// update golden files if necessary
+	if *update {
+		if err := ioutil.WriteFile(golden, res, 0644); err != nil {
+			t.Error(err)
+		}
+		return
+	}
+
+	// get golden
+	gld, err := ioutil.ReadFile(golden)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	// formatted source and golden must be the same
+	if err := diff(source, golden, res, gld); err != nil {
+		t.Error(err)
+		return
+	}
+
+	if mode&idempotent != 0 {
+		// formatting golden must be idempotent
+		// (This is very difficult to achieve in general and for now
+		// it is only checked for files explicitly marked as such.)
+		res, err = format(gld, mode)
+		if err := diff(golden, fmt.Sprintf("format(%s)", golden), gld, res); err != nil {
+			t.Errorf("golden is not idempotent: %s", err)
+		}
+	}
+}
+
+func check(t *testing.T, source, golden string, mode checkMode) {
+	// run the test
+	cc := make(chan int)
+	go func() {
+		runcheck(t, source, golden, mode)
+		cc <- 0
+	}()
+
+	// wait with timeout
+	select {
+	case <-time.After(100000 * time.Second): // plenty of a safety margin, even for very slow machines
+		// test running past time out
+		t.Errorf("%s: running too slowly", source)
+	case <-cc:
+		// test finished within allotted time margin
+	}
+}
+
+type entry struct {
+	source, golden string
+	mode           checkMode
+}
+
+// Use go test -update to create/update the respective golden files.
+var data = []entry{
+	{"comments.input", "comments.golden", 0},
+	{"simplify.input", "simplify.golden", simplify},
+	{"expressions.input", "expressions.golden", 0},
+}
+
+func TestFiles(t *testing.T) {
+	t.Parallel()
+	for _, e := range data {
+		source := filepath.Join(dataDir, e.source)
+		golden := filepath.Join(dataDir, e.golden)
+		mode := e.mode
+		t.Run(e.source, func(t *testing.T) {
+			t.Parallel()
+			check(t, source, golden, mode)
+			// TODO(gri) check that golden is idempotent
+			//check(t, golden, golden, e.mode)
+		})
+	}
+}
+
+// Verify that the printer can be invoked during initialization.
+func init() {
+	const name = "foobar"
+	var buf bytes.Buffer
+	if err := Fprint(&buf, &ast.Ident{Name: name}); err != nil {
+		panic(err) // error in test
+	}
+	// in debug mode, the result contains additional information;
+	// ignore it
+	if s := buf.String(); !debug && s != name {
+		panic("got " + s + ", want " + name)
+	}
+}
+
+// Verify that the printer doesn't crash if the AST contains BadXXX nodes.
+func TestBadNodes(t *testing.T) {
+	const src = "package p\n("
+	const res = "package p\n\n(BadExpr)\n"
+	f, err := parser.ParseFile(fset, "", src, parser.ParseComments)
+	if err == nil {
+		t.Error("expected illegal program") // error in test
+	}
+	var buf bytes.Buffer
+	Fprint(&buf, f)
+	if buf.String() != res {
+		t.Errorf("got %q, expected %q", buf.String(), res)
+	}
+}
+
+// idents is an iterator that returns all idents in f via the result channel.
+func idents(f *ast.File) <-chan *ast.Ident {
+	v := make(chan *ast.Ident)
+	go func() {
+		ast.Walk(f, func(n ast.Node) bool {
+			if ident, ok := n.(*ast.Ident); ok {
+				v <- ident
+			}
+			return true
+		}, nil)
+		close(v)
+	}()
+	return v
+}
+
+// identCount returns the number of identifiers found in f.
+func identCount(f *ast.File) int {
+	n := 0
+	for range idents(f) {
+		n++
+	}
+	return n
+}
+
+// Verify that the SourcePos mode emits correct //line comments
+// by testing that position information for matching identifiers
+// is maintained.
+func TestSourcePos(t *testing.T) {
+	const src = `package p
+
+import (
+	"go/printer"
+	"math"
+	"regexp"
+)
+
+pi = 3.14  // TODO: allow on same line
+xx = 0
+t: {
+	x: int
+	y: int
+	z: int
+	u: number
+	v: number
+	w: number
+}
+e: a*t.x + b*t.y
+
+// two extra lines here // ...
+e2: c*t.z
+`
+
+	// parse original
+	f1, err := parser.ParseFile(fset, "src", src, parser.ParseComments)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// pretty-print original
+	var buf bytes.Buffer
+	err = (&config{UseSpaces: true, Tabwidth: 8}).fprint(&buf, f1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// parse pretty printed original
+	// (//line comments must be interpreted even w/o syntax.ParseComments set)
+	f2, err := parser.ParseFile(fset, "", buf.Bytes(),
+		parser.AllErrors, parser.ParseLambdas, parser.ParseComments)
+	if err != nil {
+		t.Fatalf("%s\n%s", err, buf.Bytes())
+	}
+
+	// At this point the position information of identifiers in f2 should
+	// match the position information of corresponding identifiers in f1.
+
+	// number of identifiers must be > 0 (test should run) and must match
+	n1 := identCount(f1)
+	n2 := identCount(f2)
+	if n1 == 0 {
+		t.Fatal("got no idents")
+	}
+	if n2 != n1 {
+		t.Errorf("got %d idents; want %d", n2, n1)
+	}
+
+	// verify that all identifiers have correct line information
+	i2range := idents(f2)
+	for i1 := range idents(f1) {
+		i2 := <-i2range
+
+		if i2 == nil || i1 == nil {
+			t.Fatal("non nil identifiers")
+		}
+		if i2.Name != i1.Name {
+			t.Errorf("got ident %s; want %s", i2.Name, i1.Name)
+		}
+
+		l1 := fset.Position(i1.Pos()).Line
+		l2 := fset.Position(i2.Pos()).Line
+		if l2 != l1 {
+			t.Errorf("got line %d; want %d for %s", l2, l1, i1.Name)
+		}
+	}
+
+	if t.Failed() {
+		t.Logf("\n%s", buf.Bytes())
+	}
+}
+
+var decls = []string{
+	`import "fmt"`,
+	"pi = 3.1415\ne = 2.71828\n\nx = pi",
+}
+
+func TestDeclLists(t *testing.T) {
+	for _, src := range decls {
+		file, err := parser.ParseFile(fset, "", "package p\n"+src, parser.ParseComments)
+		if err != nil {
+			panic(err) // error in test
+		}
+
+		var buf bytes.Buffer
+		err = Fprint(&buf, file.Decls) // only print declarations
+		if err != nil {
+			panic(err) // error in test
+		}
+
+		out := buf.String()
+
+		if out != src {
+			t.Errorf("\ngot : %q\nwant: %q\n", out, src)
+		}
+	}
+}
+
+var stmts = []string{
+	"i := 0",
+	"select {}\nvar a, b = 1, 2\nreturn a + b",
+	"go f()\ndefer func() {}()",
+}
+
+type limitWriter struct {
+	remaining int
+	errCount  int
+}
+
+func (l *limitWriter) Write(buf []byte) (n int, err error) {
+	n = len(buf)
+	if n >= l.remaining {
+		n = l.remaining
+		err = io.EOF
+		l.errCount++
+	}
+	l.remaining -= n
+	return n, err
+}
+
+// TextX is a skeleton test that can be filled in for debugging one-off cases.
+// Do not remove.
+func TestX(t *testing.T) {
+	const src = `
+	{ e: k <-
+	for a, v in s}
+	a: b
+
+`
+	b, err := format([]byte(src), 0)
+	if err != nil {
+		t.Error(err)
+	}
+	_ = b
+	// t.Error("\n", string(b))
+}
---
+++ cue/format/node.go
@@ -0,0 +1,688 @@
+// Copyright 2018 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package format
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"cuelang.org/go/cue/ast"
+	"cuelang.org/go/cue/parser"
+	"cuelang.org/go/cue/token"
+)
+
+func printNode(node interface{}, f *printer) error {
+	s := newFormatter(f)
+
+	// format node
+	f.allowed = nooverride // gobble initial whitespace.
+	switch x := node.(type) {
+	case *ast.File:
+		s.file(x)
+	case ast.Expr:
+		s.expr(x)
+	case ast.Decl:
+		s.decl(x)
+	// case ast.Node: // TODO: do we need this?
+	// 	s.walk(x)
+	case []ast.Decl:
+		s.walkDeclList(x)
+	default:
+		goto unsupported
+	}
+
+	return nil
+
+unsupported:
+	return fmt.Errorf("cue/format: unsupported node type %T", node)
+}
+
+// Helper functions for common node lists. They may be empty.
+
+func (f *formatter) walkDeclList(list []ast.Decl) {
+	f.before(nil)
+	for i, x := range list {
+		if i > 0 {
+			f.print(declcomma)
+		}
+		f.decl(x)
+		f.print(f.current.parentSep)
+	}
+	f.after(nil)
+}
+
+func (f *formatter) walkSpecList(list []*ast.ImportSpec) {
+	f.before(nil)
+	for _, x := range list {
+		f.importSpec(x)
+	}
+	f.after(nil)
+}
+
+func (f *formatter) walkClauseList(list []ast.Clause) {
+	f.before(nil)
+	for _, x := range list {
+		f.clause(x)
+	}
+	f.after(nil)
+}
+
+func (f *formatter) walkExprList(list []ast.Expr, depth int) {
+	f.before(nil)
+	for _, x := range list {
+		f.before(x)
+		f.exprRaw(x, token.LowestPrec, depth)
+		f.print(comma, blank)
+		f.after(x)
+	}
+	f.after(nil)
+}
+
+func (f *formatter) file(file *ast.File) {
+	f.before(file)
+	if file.Name != nil {
+		f.print(file.Package, "package")
+		f.print(blank, file.Name, newline, newsection)
+	}
+	f.current.pos = 3
+	f.visitComments(3)
+	f.walkDeclList(file.Decls)
+	f.after(file)
+	f.print(token.EOF)
+}
+func (f *formatter) decl(decl ast.Decl) {
+	if decl == nil {
+		return
+	}
+	if !f.before(decl) {
+		goto after
+	}
+	switch n := decl.(type) {
+	case *ast.Field:
+		// shortcut single-element structs.
+		lastSize := len(f.labelBuf)
+		f.labelBuf = f.labelBuf[:0]
+		first := n.Label
+		for {
+			obj, ok := n.Value.(*ast.StructLit)
+			if !ok || len(obj.Elts) != 1 || (obj.Lbrace.IsValid() && !f.printer.cfg.simplify) {
+				break
+			}
+
+			// Verify that struct doesn't have inside comments and that
+			// element doesn't have doc comments.
+			hasComments := len(obj.Elts[0].Comments()) > 0
+			for _, c := range obj.Comments() {
+				if c.Position == 1 || c.Position == 2 {
+					hasComments = true
+				}
+			}
+			if hasComments {
+				break
+			}
+
+			mem, ok := obj.Elts[0].(*ast.Field)
+			if !ok {
+				break
+			}
+			f.labelBuf = append(f.labelBuf, mem.Label)
+			n = mem
+		}
+
+		if lastSize != len(f.labelBuf) {
+			f.print(formfeed)
+		}
+
+		f.before(nil)
+		f.label(first)
+		for _, x := range f.labelBuf {
+			f.print(blank, nooverride)
+			f.label(x)
+		}
+		f.after(nil)
+
+		nextFF := f.nextNeedsFormfeed(n.Value)
+		tab := vtab
+		if nextFF || f.nestExpr >= 1 {
+			tab = blank
+		}
+
+		f.print(n.Colon, token.COLON, tab)
+		if n.Value != nil {
+			f.expr(n.Value)
+		} else {
+			f.current.pos++
+			f.visitComments(f.current.pos)
+		}
+
+		if nextFF {
+			f.print(formfeed)
+		}
+
+	case *ast.ComprehensionDecl:
+		f.decl(n.Field)
+		f.print(blank)
+		if n.Select != token.NoPos {
+			f.print(n.Select, token.ARROW, blank)
+		}
+		f.print(indent)
+		f.walkClauseList(n.Clauses)
+		f.print(unindent)
+		f.print("") // force whitespace to be written
+
+	case *ast.BadDecl:
+		f.print(n.From, "*bad decl*", declcomma)
+
+	case *ast.ImportDecl:
+		f.print(n.Import, "import")
+		if len(n.Specs) == 0 {
+			f.print(blank, n.Lparen, token.LPAREN, n.Rparen, token.RPAREN, newline)
+		}
+		switch {
+		case len(n.Specs) == 1:
+			if !n.Lparen.IsValid() {
+				f.print(blank)
+				f.walkSpecList(n.Specs)
+				break
+			}
+			fallthrough
+		default:
+			f.print(blank, n.Lparen, token.LPAREN, newline, indent)
+			f.walkSpecList(n.Specs)
+			f.print(unindent, newline, n.Rparen, token.RPAREN, newline)
+		}
+		f.print(newsection)
+
+	case *ast.EmitDecl:
+		f.expr(n.Expr)
+		f.print(newline, newsection) // force newline
+
+	case *ast.Alias:
+		f.expr(n.Ident)
+		f.print(blank, n.Equal, token.BIND, blank)
+		f.expr(n.Expr)
+		f.print(declcomma, newline) // implied
+	}
+after:
+	f.after(decl)
+}
+
+func (f *formatter) nextNeedsFormfeed(n ast.Expr) bool {
+	switch x := n.(type) {
+	case *ast.StructLit:
+		return true
+	case *ast.BasicLit:
+		return strings.IndexByte(x.Value, '\n') >= 0
+	case *ast.ListLit:
+		return true
+	}
+	return false
+}
+
+func (f *formatter) importSpec(x *ast.ImportSpec) {
+	if x.Name != nil {
+		f.label(x.Name)
+		f.print(blank)
+	} else {
+		f.current.pos++
+		f.visitComments(f.current.pos)
+	}
+	f.expr(x.Path)
+	f.print(newline)
+}
+
+func (f *formatter) label(l ast.Label) {
+	switch n := l.(type) {
+	case *ast.Ident:
+		f.print(n.NamePos, n)
+
+	case *ast.BasicLit:
+		if f.cfg.simplify && n.Kind == token.STRING && len(n.Value) > 2 {
+			s := n.Value
+			unquoted, err := strconv.Unquote(s)
+			fset := token.NewFileSet()
+			if err == nil {
+				e, _ := parser.ParseExpr(fset, "check", unquoted)
+				if _, ok := e.(*ast.Ident); ok {
+					f.print(n.ValuePos, unquoted)
+					break
+				}
+			}
+		}
+		f.print(n.ValuePos, n.Value)
+
+	case *ast.TemplateLabel:
+		f.print(n.Langle, token.LSS, indent)
+		f.label(n.Ident)
+		f.print(unindent, n.Rangle, token.GTR)
+
+	case *ast.Interpolation:
+		f.expr(n)
+
+	case *ast.ExprLabel:
+		f.print(n.Lbrack, token.LBRACK, indent)
+		f.expr(n.Label)
+		f.print(unindent, n.Rbrack, token.RBRACK)
+
+	default:
+		panic(fmt.Sprintf("unknown label type %T", n))
+	}
+}
+
+func (f *formatter) expr(x ast.Expr) {
+	const depth = 1
+	f.expr1(x, token.LowestPrec, depth)
+}
+
+func (f *formatter) expr0(x ast.Expr, depth int) {
+	f.expr1(x, token.LowestPrec, depth)
+}
+
+func (f *formatter) expr1(expr ast.Expr, prec1, depth int) {
+	if f.before(expr) {
+		f.exprRaw(expr, prec1, depth)
+	}
+	f.after(expr)
+}
+
+func (f *formatter) exprRaw(expr ast.Expr, prec1, depth int) {
+
+	switch x := expr.(type) {
+	case *ast.BadExpr:
+		f.print(x.From, "BadExpr")
+
+	case *ast.BottomLit:
+		f.print(x.Bottom, token.BOTTOM)
+
+	case *ast.Ident:
+		f.print(x.NamePos, x)
+
+	case *ast.BinaryExpr:
+		if depth < 1 {
+			f.internalError("depth < 1:", depth)
+			depth = 1
+		}
+		if prec1 == 8 { // ..
+			prec1 = 9 // always parentheses for values of ranges
+		}
+		f.binaryExpr(x, prec1, cutoff(x, depth), depth)
+
+	case *ast.UnaryExpr:
+		const prec = token.UnaryPrec
+		if prec < prec1 {
+			// parenthesis needed
+			f.print(token.LPAREN, nooverride)
+			f.expr(x)
+			f.print(token.RPAREN)
+		} else {
+			// no parenthesis needed
+			f.print(x.OpPos, x.Op, nooverride)
+			f.expr1(x.X, prec, depth)
+		}
+
+	case *ast.BasicLit:
+		f.print(x.ValuePos, x)
+
+	case *ast.Interpolation:
+		f.before(nil)
+		for _, x := range x.Elts {
+			f.expr0(x, depth+1)
+		}
+		f.after(nil)
+
+	case *ast.ParenExpr:
+		if _, hasParens := x.X.(*ast.ParenExpr); hasParens {
+			// don't print parentheses around an already parenthesized expression
+			// TODO: consider making this more general and incorporate precedence levels
+			f.expr0(x.X, depth)
+		} else {
+			f.print(x.Lparen, token.LPAREN)
+			f.expr0(x.X, reduceDepth(depth)) // parentheses undo one level of depth
+			f.print(x.Rparen, token.RPAREN)
+		}
+
+	case *ast.SelectorExpr:
+		f.selectorExpr(x, depth)
+
+	case *ast.IndexExpr:
+		f.expr1(x.X, token.HighestPrec, 1)
+		f.print(x.Lbrack, token.LBRACK)
+		f.expr0(x.Index, depth+1)
+		f.print(x.Rbrack, token.RBRACK)
+
+	case *ast.SliceExpr:
+		f.expr1(x.X, token.HighestPrec, 1)
+		f.print(x.Lbrack, token.LBRACK)
+		indices := []ast.Expr{x.Low, x.High}
+		for i, y := range indices {
+			if i > 0 {
+				// blanks around ":" if both sides exist and either side is a binary expression
+				x := indices[i-1]
+				if depth <= 1 && x != nil && y != nil && (isBinary(x) || isBinary(y)) {
+					f.print(blank, token.COLON, blank)
+				} else {
+					f.print(token.COLON)
+				}
+			}
+			if y != nil {
+				f.expr0(y, depth+1)
+			}
+		}
+		f.print(x.Rbrack, token.RBRACK)
+
+	case *ast.CallExpr:
+		if len(x.Args) > 1 {
+			depth++
+		}
+		wasIndented := f.possibleSelectorExpr(x.Fun, token.HighestPrec, depth)
+		f.print(x.Lparen, token.LPAREN)
+		f.walkExprList(x.Args, depth)
+		f.print(trailcomma, noblank, x.Rparen, token.RPAREN)
+		if wasIndented {
+			f.print(unindent)
+		}
+
+	case *ast.Ellipsis:
+		f.print(x.Ellipsis, token.ELLIPSIS)
+		if x.Elt != nil {
+			f.expr(x.Elt) // TODO
+		}
+
+	case *ast.StructLit:
+		f.print(x.Lbrace, token.LBRACE, noblank, f.formfeed(), indent)
+		f.walkDeclList(x.Elts)
+		f.matchUnindent()
+		f.print(noblank, x.Rbrace, token.RBRACE)
+
+	case *ast.ListLit:
+		f.print(x.Lbrack, token.LBRACK, indent)
+		f.walkExprList(x.Elts, 1)
+		if x.Ellipsis != token.NoPos || x.Type != nil {
+			f.print(x.Ellipsis, token.ELLIPSIS)
+			if x.Type != nil && !isTop(x.Type) {
+				f.expr(x.Type)
+			}
+		} else {
+			f.print(trailcomma, noblank)
+			f.current.pos += 2
+			f.visitComments(f.current.pos)
+		}
+		f.matchUnindent()
+		f.print(noblank, x.Rbrack, token.RBRACK)
+
+	case *ast.ListComprehension:
+		f.print(x.Lbrack, token.LBRACK, blank, indent)
+		f.expr(x.Expr)
+		f.print(blank)
+		f.walkClauseList(x.Clauses)
+		f.print(unindent, f.wsOverride(blank), x.Rbrack, token.RBRACK)
+
+	case *ast.LambdaExpr:
+		f.print(x.Lparen, token.LPAREN, indent, noblank)
+
+		f.before(nil)
+		for _, x := range x.Params {
+			f.label(x.Label)
+			if x.Colon.IsValid() {
+				f.print(x.Colon, token.COLON, blank)
+				f.expr(x.Value)
+			}
+			f.print(comma, blank)
+		}
+		f.print(trailcomma, noblank)
+		f.after(nil)
+
+		f.print(trailcomma, noblank, unindent)
+		f.print(x.Rparen, token.RPAREN, blank)
+		f.print(token.LAMBDA, blank)
+		f.expr(x.Expr)
+
+	default:
+		panic(fmt.Sprintf("unimplemented type %T", x))
+	}
+	return
+}
+
+func (f *formatter) clause(clause ast.Clause) {
+	switch n := clause.(type) {
+	case *ast.ForClause:
+		f.print(blank, n.For, "for", blank)
+		if n.Key != nil {
+			f.label(n.Key)
+			f.print(n.Colon, token.COMMA, blank)
+		} else {
+			f.current.pos++
+			f.visitComments(f.current.pos)
+		}
+		f.label(n.Value)
+		f.print(blank, n.In, "in", blank)
+		f.expr(n.Source)
+
+	case *ast.IfClause:
+		f.print(blank, n.If, "if", blank)
+		f.expr(n.Condition)
+
+	default:
+		panic("unknown clause type")
+	}
+}
+
+func walkBinary(e *ast.BinaryExpr) (has6, has7, has8 bool, maxProblem int) {
+	switch e.Op.Precedence() {
+	case 6:
+		has6 = true
+	case 7:
+		has7 = true
+	case 8:
+		has8 = true
+	}
+
+	switch l := e.X.(type) {
+	case *ast.BinaryExpr:
+		if l.Op.Precedence() < e.Op.Precedence() {
+			// parens will be inserted.
+			// pretend this is an *syntax.ParenExpr and do nothing.
+			break
+		}
+		h6, h7, h8, mp := walkBinary(l)
+		has6 = has6 || h6
+		has7 = has7 || h7
+		has8 = has8 || h8
+		if maxProblem < mp {
+			maxProblem = mp
+		}
+	}
+
+	switch r := e.Y.(type) {
+	case *ast.BinaryExpr:
+		if r.Op.Precedence() <= e.Op.Precedence() {
+			// parens will be inserted.
+			// pretend this is an *syntax.ParenExpr and do nothing.
+			break
+		}
+		h6, h7, h8, mp := walkBinary(r)
+		has6 = has6 || h6
+		has7 = has7 || h7
+		has8 = has8 || h8
+		if maxProblem < mp {
+			maxProblem = mp
+		}
+
+	case *ast.UnaryExpr:
+		switch e.Op.String() + r.Op.String() {
+		case "/*":
+			maxProblem = 8
+		case "++", "--":
+			if maxProblem < 6 {
+				maxProblem = 6
+			}
+		}
+	}
+	return
+}
+
+func cutoff(e *ast.BinaryExpr, depth int) int {
+	has6, has7, has8, maxProblem := walkBinary(e)
+	if maxProblem > 0 {
+		return maxProblem + 1
+	}
+	if (has6 || has7) && has8 {
+		if depth == 1 {
+			return 8
+		}
+		if has7 {
+			return 7
+		}
+		return 6
+	}
+	if has6 && has7 {
+		if depth == 1 {
+			return 7
+		}
+		return 6
+	}
+	if depth == 1 {
+		return 8
+	}
+	return 6
+}
+
+func diffPrec(expr ast.Expr, prec int) int {
+	x, ok := expr.(*ast.BinaryExpr)
+	if !ok || prec != x.Op.Precedence() {
+		return 1
+	}
+	return 0
+}
+
+func reduceDepth(depth int) int {
+	depth--
+	if depth < 1 {
+		depth = 1
+	}
+	return depth
+}
+
+// Format the binary expression: decide the cutoff and then format.
+// Let's call depth == 1 Normal mode, and depth > 1 Compact mode.
+// (Algorithm suggestion by Russ Cox.)
+//
+// The precedences are:
+//  8             ..
+//	7             *  /  % quo rem div mod
+//	6             +  -
+//	5             ==  !=  <  <=  >  >=
+//	4             &&
+//	3             ||
+//	2             &
+//	1             |
+//
+// The only decision is whether there will be spaces around levels 6 and 7.
+// There are never spaces at level 8 (unary), and always spaces at levels 5 and below.
+//
+// To choose the cutoff, look at the whole expression but excluding primary
+// expressions (function calls, parenthesized exprs), and apply these rules:
+//
+//	1) If there is a binary operator with a right side unary operand
+//	   that would clash without a space, the cutoff must be (in order):
+//
+//		/*	8
+//		++	7 // not necessary, but to avoid confusion
+//		--	7
+//
+//         (Comparison operators always have spaces around them.)
+//
+//	2) If there is a mix of level 7 and level 6 operators, then the cutoff
+//	   is 7 (use spaces to distinguish precedence) in Normal mode
+//	   and 6 (never use spaces) in Compact mode.
+//
+//	3) If there are no level 6 operators or no level 7 operators, then the
+//	   cutoff is 8 (always use spaces) in Normal mode
+//	   and 6 (never use spaces) in Compact mode.
+//
+func (f *formatter) binaryExpr(x *ast.BinaryExpr, prec1, cutoff, depth int) {
+	f.nestExpr++
+	defer func() { f.nestExpr-- }()
+
+	prec := x.Op.Precedence()
+	if prec < prec1 {
+		// parenthesis needed
+		// Note: The parser inserts an syntax.ParenExpr node; thus this case
+		//       can only occur if the AST is created in a different way.
+		// defer p.pushComment(nil).pop()
+		f.print(token.LPAREN, nooverride)
+		f.expr0(x, reduceDepth(depth)) // parentheses undo one level of depth
+		f.print(token.RPAREN)
+		return
+	}
+
+	printBlank := prec < cutoff
+
+	ws := indent
+	f.expr1(x.X, prec, depth+diffPrec(x.X, prec))
+	f.print(nooverride)
+	if printBlank {
+		f.print(blank)
+	}
+	f.print(x.OpPos, x.Op)
+	if x.Y.Pos().IsNewline() {
+		// at least one line break, but respect an extra empty line
+		// in the source
+		f.print(formfeed)
+		printBlank = false // no blank after line break
+	} else {
+		f.print(nooverride)
+	}
+	if printBlank {
+		f.print(blank)
+	}
+	f.expr1(x.Y, prec+1, depth+1)
+	if ws == ignore {
+		f.print(unindent)
+	}
+}
+
+func isBinary(expr ast.Expr) bool {
+	_, ok := expr.(*ast.BinaryExpr)
+	return ok
+}
+
+func (f *formatter) possibleSelectorExpr(expr ast.Expr, prec1, depth int) bool {
+	if x, ok := expr.(*ast.SelectorExpr); ok {
+		return f.selectorExpr(x, depth)
+	}
+	f.expr1(expr, prec1, depth)
+	return false
+}
+
+// selectorExpr handles an *syntax.SelectorExpr node and returns whether x spans
+// multiple lines.
+func (f *formatter) selectorExpr(x *ast.SelectorExpr, depth int) bool {
+	f.expr1(x.X, token.HighestPrec, depth)
+	f.print(token.PERIOD)
+	if x.Sel.Pos().IsNewline() {
+		f.print(indent, formfeed, x.Sel.Pos(), x.Sel)
+		f.print(unindent)
+		return true
+	}
+	f.print(x.Sel.Pos(), x.Sel)
+	return false
+}
+
+func isTop(e ast.Expr) bool {
+	ident, ok := e.(*ast.Ident)
+	return ok && ident.Name == "_"
+}

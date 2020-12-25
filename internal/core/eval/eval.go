0ef6caa86e9599307f86436d7820e84099f65c7c
Marcel van Lohuizen <mpvl@golang.org>
2020-12-25 17:12:52 +0100

    internal/core/eval: make arcMap a list
    Measurement shows that the number of conjuncts hardly
    exceeds 12. As dealing with maps is hard in upcomping
    changes, change the data structure to a list, which should
    be faster at these order of magnitudes anyway.
    
    Benchmarks indeed show a slight improvement.
    
    Change-Id: I6289247bce1245edca0f0fe0e4af15efafd72309
    Reviewed-on: https://cue-review.googlesource.com/c/cue/+/8041
    Reviewed-by: CUE cueckoo <cueckoo@gmail.com>
    Reviewed-by: Marcel van Lohuizen <mpvl@golang.org>

--- internal/core/eval/eval.go
+++ internal/core/eval/eval.go
@@ -725,7 +725,7 @@ type nodeContext struct {
 	// should already ensure a quick-fail for struct disjunctions with
 	// discriminators.
 
-	arcMap map[arcKey]bool
+	arcMap []arcKey
 
 	// Current value (may be under construction)
 	scalar   adt.Value // TODO: use Value in node.
@@ -770,13 +770,11 @@ func (e *Evaluator) newNodeContext(shared *nodeShared) *nodeContext {
 		e.freeListNode = n.nextFree
 
 		*n = nodeContext{
-			// TODO(perf): use another technique that doesn't require allocation.
-			// arcMap: map[arcKey]bool{},
-
 			kind:       adt.TopKind,
 			nodeShared: shared,
 			isFinal:    true,
 
+			arcMap:          n.arcMap[:0],
 			checks:          n.checks[:0],
 			dynamicFields:   n.dynamicFields[:0],
 			ifClauses:       n.ifClauses[:0],
@@ -793,7 +791,6 @@ func (e *Evaluator) newNodeContext(shared *nodeShared) *nodeContext {
 	e.stats.Allocs++
 
 	return &nodeContext{
-		// arcMap:     map[arcKey]bool{},
 		kind:       adt.TopKind,
 		nodeShared: shared,
 
@@ -1138,15 +1135,13 @@ func (n *nodeContext) addVertexConjuncts(env *adt.Environment, closeInfo adt.Clo
 	// (pointer can probably be shared). Aside from being more performant,
 	// this is probably the best way to guarantee that conjunctions are
 	// linear in this case.
-	if n.arcMap == nil {
-		n.arcMap = map[arcKey]bool{}
+	key := arcKey{arc, closeInfo}
+	for _, k := range n.arcMap {
+		if key == k {
+			return
+		}
 	}
-
-	id := closeInfo
-	if n.arcMap[arcKey{arc, id}] {
-		return
-	}
-	n.arcMap[arcKey{arc, id}] = true
+	n.arcMap = append(n.arcMap, key)
 
 	// Pass detection of structural cycles from parent to children.
 	cyclic := false

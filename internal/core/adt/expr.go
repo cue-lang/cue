9b263eb385b128ab87b37cde0072f7ed09468fdb
Marcel van Lohuizen <mpvl@golang.org>
2021-03-28 12:38:56 +0200

    internal/core/adt: track more incomplete errors
    This was sometimes lost in computation,
    for instance across package boundaries.
    
    This results in less nice errors for certain cycle cases.
    This should be handled in an evaluator rework.
    
    Fixes #854
    
    Change-Id: Ieb927dbb8d11d2ad2651b85a375d11913a8612b2
    Reviewed-on: https://cue-review.googlesource.com/c/cue/+/9181
    Reviewed-by: Marcel van Lohuizen <mpvl@golang.org>

--- internal/core/adt/expr.go
+++ internal/core/adt/expr.go
@@ -864,6 +864,12 @@ func (x *SelectorExpr) resolve(c *OpContext, state VertexStatus) *Vertex {
 	if n == emptyNode {
 		return n
 	}
+	if n.status == Partial {
+		if b := n.state.incompleteErrors(); b != nil && b.Code < CycleError {
+			n.BaseValue = b
+			return n
+		}
+	}
 	return c.lookup(n, x.Src.Sel.Pos(), x.Sel, state)
 }
 
@@ -890,6 +896,12 @@ func (x *IndexExpr) resolve(ctx *OpContext, state VertexStatus) *Vertex {
 	i := ctx.value(x.Index)
 	if n == emptyNode {
 		return n
+	}
+	if n.status == Partial {
+		if b := n.state.incompleteErrors(); b != nil && b.Code < CycleError {
+			n.BaseValue = b
+			return n
+		}
 	}
 	f := ctx.Label(x.Index, i)
 	return ctx.lookup(n, x.Src.Index.Pos(), f, state)

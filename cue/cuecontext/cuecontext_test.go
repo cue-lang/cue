cca13d49eb831f38bc687904df5cb2eb8037e052
Marcel van Lohuizen <mpvl@golang.org>
2021-11-23 18:24:41 +0100

    internal/core/export: fix bug with API-generated top-level close
    Two problems:
    - node context not cleanly removed from Vertices computed by `close`
    - top-level closed structs not properly exported.
    
    Fixes #1204
    
    Signed-off-by: Marcel van Lohuizen <mpvl@golang.org>
    
    Change-Id: I1e8d08cf5ad4e0270ebcbdcc140bcb09527805e1
    Signed-off-by: Marcel van Lohuizen <mpvl@golang.org>

---
+++ cue/cuecontext/cuecontext_test.go
@@ -0,0 +1,47 @@
+// Copyright 2021 CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cuecontext
+
+import (
+	"fmt"
+	"testing"
+
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/ast"
+)
+
+func TestAPI(t *testing.T) {
+	testCases := []struct {
+		name string
+		fun  func() cue.Value
+		want string
+	}{{
+		name: "issue1204",
+		fun: func() cue.Value {
+			ctx := New()
+			expr := ast.NewCall(ast.NewIdent("close"), ast.NewStruct())
+			return ctx.BuildExpr(expr)
+		},
+		want: `close({})`,
+	}}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := fmt.Sprintf("%#v", tc.fun())
+			if got != tc.want {
+				t.Errorf("got:\n%v;\nwant:\n%v", got, tc.want)
+			}
+		})
+	}
+}

242538c58aa806b81a726de1ea88338dea0b8ab7
Marcel van Lohuizen <mpvl@golang.org>
2022-02-02 12:02:42 +0100

    internal/core/adt: store Ellipsis instead of Expr in Additional
    This keeps more history.
    It is also necessary for the upcoming comprehension change.
    
    Change-Id: I540981aeed97706a6c1f7ec07a9cbe47259efe98
    Signed-off-by: Marcel van Lohuizen <mpvl@golang.org>

--- internal/core/adt/composite.go
+++ internal/core/adt/composite.go
@@ -814,10 +814,10 @@ func (c *Conjunct) Field() Node {
 // If it is a Field, it will return the field value.
 func (c *Conjunct) Elem() Elem {
 	switch x := c.x.(type) {
-	case Elem:
-		return x
 	case interface{ expr() Expr }:
 		return x.expr()
+	case Elem:
+		return x
 	default:
 		panic("unreachable")
 	}

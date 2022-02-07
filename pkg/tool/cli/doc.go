491a71541d8e4589de9caf162dd6892c86b09d75
Scott Lewis-Kelly <slewiskelly@slewiskel.ly>
2022-02-07 14:22:39 +1100

    pkg/tool/cli: Fix Ask task
    At the moment the task is not being resolved as the `kind` field is used to
    resolve legacy kinds. It is the only task that has only this field, and not an
    `$id` field.
    
    Signed-off-by: Scott Lewis-Kelly <slewiskelly@slewiskel.ly>
    Change-Id: I42d7d0676dbbd0544a8356da3cb34a11642b1dbd

--- pkg/tool/cli/doc.go
+++ pkg/tool/cli/doc.go
@@ -20,7 +20,7 @@
 //     //         repsonse: bool
 //     //     })
 //     Ask: {
-//     	kind: "tool/cli.Ask"
+//     	$id: "tool/cli.Ask"
 //
 //     	// prompt sends this message to the output.
 //     	prompt: string

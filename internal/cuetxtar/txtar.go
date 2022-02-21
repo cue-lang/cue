0c83833a67c6ecefcfff79cd780b1c0498172d2d
Paul Jolly <paul@myitcv.io>
2022-02-21 05:30:02 +0000

    internal/cuetxtar: enforce that .cue files are formatted
    Enforce that .cue files in txtar archives are well formatted, raising an
    error if not.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Ia10eb0c818656bd4242fb65f68ef18200330a37d

--- internal/cuetxtar/txtar.go
+++ internal/cuetxtar/txtar.go
@@ -300,6 +300,8 @@ func (x *TxTarTest) Run(t *testing.T, f func(tc *Test)) {
 					if cuetest.FormatTxtar {
 						update = true
 						a.Files[i].Data = ff
+					} else {
+						t.Errorf("%s::%s is not formatted", filepath.Join(root, fullpath), f.Name)
 					}
 				}
 			}

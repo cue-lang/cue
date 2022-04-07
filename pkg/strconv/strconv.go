4e33d24004888a9c4a57b92a3143997484db36c5
Marcel van Lohuizen <mpvl@golang.org>
2022-04-07 16:38:29 +0200

    pkg: remove usage of qgo in go generate
    We no longer update by default.
    
    This also improves tests.
    
    Signed-off-by: Marcel van Lohuizen <mpvl@golang.org>
    Change-Id: I8f1d1cfb9cadbf63234701c19c315eff77b307cb

--- pkg/strconv/strconv.go
+++ pkg/strconv/strconv.go
@@ -16,7 +16,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:generate go run cuelang.org/go/internal/cmd/qgo -exclude=Append,Unquote,Itoa,CanBackquote,FormatComplex extract strconv
+// Generated with go run cuelang.org/go/internal/cmd/qgo -exclude=Append,Unquote,Itoa,CanBackquote,FormatComplex extract strconv
 
 package strconv
 

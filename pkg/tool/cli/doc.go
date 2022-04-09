a7b08f5cd121b64f00e59c15ab44148c07e061c3
Daniel Martí <mvdan@mvdan.cc>
2022-04-09 17:03:59 +0900

    internal/ci: add Go 1.18 and bump action and Go versions
    Add Go 1.18 as the latest stable version,
    as all of our tests are nearly passing with it at this point.
    
    We also stop pinning the exact Go versions for running the tests,
    because they should always be running on the latest bugfix release.
    We can drop the exact version pinning from code generation too,
    now that qgo is no longer used as part of `go generate`.
    We do keep the pinning for the releases, for reproducibility.
    
    There have been a few bugfix Go releases recently.
    The official GitHub Actions for setup-go, checkout, and cache
    all have jumped to v3 now.
    
    Beyond a number of fixes, the only notable change is that setup-go now
    works with Go pre-release tags without needing `stable: false`.
    
    While here, I noticed that the cue.mod/module.cue file in the github
    test was buggy: it was missing the cue extension and had bad syntax.
    
    Fixes #1619.
    
    Change-Id: Ib12004d7856ac3702cd2a1e370e7f66211ecccdb
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>

--- pkg/tool/cli/doc.go
+++ pkg/tool/cli/doc.go
@@ -4,30 +4,29 @@
 //
 // These are the supported tasks:
 //
-//     // Print sends text to the stdout of the current process.
-//     Print: {
-//     	$id: *"tool/cli.Print" | "print" // for backwards compatibility
+//	// Print sends text to the stdout of the current process.
+//	Print: {
+//		$id: *"tool/cli.Print" | "print" // for backwards compatibility
 //
-//     	// text is the text to be printed.
-//     	text: string
-//     }
+//		// text is the text to be printed.
+//		text: string
+//	}
 //
-//     // Ask prompts the current console with a message and waits for input.
-//     //
-//     // Example:
-//     //     task: ask: cli.Ask({
-//     //         prompt:   "Are you okay?"
-//     //         repsonse: bool
-//     //     })
-//     Ask: {
-//     	$id: "tool/cli.Ask"
+//	// Ask prompts the current console with a message and waits for input.
+//	//
+//	// Example:
+//	//     task: ask: cli.Ask({
+//	//         prompt:   "Are you okay?"
+//	//         repsonse: bool
+//	//     })
+//	Ask: {
+//		$id: "tool/cli.Ask"
 //
-//     	// prompt sends this message to the output.
-//     	prompt: string
+//		// prompt sends this message to the output.
+//		prompt: string
 //
-//     	// response holds the user's response. If it is a boolean expression it
-//     	// will interpret the answer using textual yes/ no.
-//     	response: string | bool
-//     }
-//
+//		// response holds the user's response. If it is a boolean expression it
+//		// will interpret the answer using textual yes/ no.
+//		response: string | bool
+//	}
 package cli

965c66deaf56266ffd58644f47d78b40ec887efe
Roger Peppe <rogpeppe@gmail.com>
2022-04-27 15:55:26 +0100

    cue/scanner: allow leading double-quote in #-quoted string
    Fixes #1649.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I2343130f714d841d376689117689c8540d0894bd

--- cue/scanner/scanner.go
+++ cue/scanner/scanner.go
@@ -568,13 +568,19 @@ func (s *Scanner) consumeStringClose(ch rune, quote quoteInfo) (next rune, atEnd
 }
 
 func (s *Scanner) checkHashCount(offs int, quote quoteInfo) {
-	for i := 0; i < quote.numHash; i++ {
+	if n := s.scanHashes(quote.numHash); n < quote.numHash {
+		s.errf(offs, "string literal not terminated")
+	}
+}
+
+func (s *Scanner) scanHashes(maxHash int) int {
+	for i := 0; i < maxHash; i++ {
 		if s.ch != '#' {
-			s.errf(offs, "string literal not terminated")
-			return
+			return i
 		}
 		s.next()
 	}
+	return maxHash
 }
 
 func stripCR(b []byte) []byte {
@@ -830,8 +836,22 @@ scanAgain:
 				quote.numChar = 1
 				tok, lit = s.scanString(offs, quote)
 			case 1:
-				s.checkHashCount(offs, quote)
-				tok, lit = token.STRING, string(s.src[offs:s.offset])
+				if quote.numHash > 0 {
+					// The string is surrounded by hashes, which means that
+					// the single leading quote is OK (and part of the string)
+					// e.g. #""hello""#
+					// unless it's succeeded by the correct number of terminating
+					// hash characters
+					// e.g. ##""##
+					if n := s.scanHashes(quote.numHash); n == quote.numHash {
+						tok, lit = token.STRING, string(s.src[offs:s.offset])
+					} else {
+						tok, lit = s.scanString(offs, quote)
+					}
+				} else {
+					s.checkHashCount(offs, quote)
+					tok, lit = token.STRING, string(s.src[offs:s.offset])
+				}
 			case 2:
 				quote.numChar = 3
 				switch s.ch {

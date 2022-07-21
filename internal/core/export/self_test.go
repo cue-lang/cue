c71f6745235d5d2dc48ad044d93efc50485fdd7a
Marcel van Lohuizen <mpvl@golang.org>
2022-07-21 14:53:27 +0900

    internal/core/export: implement self-contained algo for export
    The ensures  that all references pointing outside an exported
    value (in Def) are either inlined or included as let, the
    latter to avoid a possible exponential blowup.
    
    This does not yet handle the case where a reference points to
    an ancestor node of the exported value.
    
    Issue #867
    
    Signed-off-by: Marcel van Lohuizen <mpvl@golang.org>
    Change-Id: Ie5a93949a68565ea60e954417bb2573fd36b0284
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/541464
    Reviewed-by: Paul Jolly <paul@myitcv.io>
    Unity-Result: CUEcueckoo <cueckoo@cuelang.org>
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>

---
+++ internal/core/export/self_test.go
@@ -0,0 +1,197 @@
+// Copyright 2022 CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package export_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"strings"
+	"testing"
+
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/ast"
+	"cuelang.org/go/cue/build"
+	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/cue/errors"
+	"cuelang.org/go/cue/format"
+	"cuelang.org/go/internal/core/adt"
+	"cuelang.org/go/internal/core/export"
+	"cuelang.org/go/internal/cuetest"
+	"cuelang.org/go/internal/cuetxtar"
+	"cuelang.org/go/internal/diff"
+	"cuelang.org/go/internal/types"
+	"github.com/rogpeppe/go-internal/txtar"
+)
+
+func TestSelfContained(t *testing.T) {
+	test := cuetxtar.TxTarTest{
+		Root:   "./testdata/selfcontained",
+		Update: cuetest.UpdateGoldenFiles,
+	}
+
+	r := cuecontext.New()
+
+	test.Run(t, func(t *cuetxtar.Test) {
+		a := t.ValidInstances()
+
+		v := buildFile(t.T, r, a[0])
+
+		p, ok := t.Value("path")
+		if ok {
+			v = v.LookupPath(cue.ParsePath(p))
+		}
+
+		var tValue types.Value
+		v.Core(&tValue)
+
+		self := *export.All
+		self.SelfContained = true
+
+		w := t.Writer("default")
+
+		test := func() {
+			file, errs := self.Def(tValue.R, "", tValue.V)
+
+			errors.Print(w, errs, nil)
+			_, _ = w.Write(formatNode(t.T, file))
+
+			vf := patch(t.T, r, t.Archive, file)
+			doDiff(t.T, v, vf)
+
+			v = v.Unify(vf)
+			doDiff(t.T, v, vf)
+		}
+		test()
+
+		if _, ok := t.Value("inlineImports"); ok {
+			w = t.Writer("expand_imports")
+			self.InlineImports = true
+			test()
+		}
+	})
+}
+
+func buildFile(t *testing.T, r *cue.Context, b *build.Instance) cue.Value {
+	v := r.BuildInstance(b)
+	if err := v.Err(); err != nil {
+		t.Fatal(errors.Details(err, nil))
+	}
+	return v
+}
+
+// patch replaces the package at the root of the Archive with the given CUE
+// file.
+func patch(t *testing.T, r *cue.Context, orig *txtar.Archive, f *ast.File) cue.Value {
+	a := *orig
+	a.Files = make([]txtar.File, len(a.Files))
+	copy(a.Files, orig.Files)
+
+	k := 0
+	for _, f := range a.Files {
+		if strings.HasSuffix(f.Name, ".cue") && !strings.ContainsRune(f.Name, '/') {
+			continue
+		}
+		a.Files[k] = f
+		k++
+	}
+	b, err := format.Node(f)
+	if err != nil {
+		t.Error(err)
+	}
+
+	a.Files = append(a.Files, txtar.File{
+		Name: "in.cue",
+		Data: b,
+	})
+
+	dir, err := ioutil.TempDir("", "test")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	instance := cuetxtar.Load(&a, dir)[0]
+	if instance.Err != nil {
+		t.Fatal(instance.Err)
+	}
+
+	vn := buildFile(t, r, instance)
+	if err := vn.Err(); err != nil {
+		t.Fatal(err)
+	}
+	return vn
+}
+
+func doDiff(t *testing.T, v, w cue.Value) {
+	var bb bytes.Buffer
+	p := diff.Schema
+	p.SkipHidden = true
+	d, script := p.Diff(v, w)
+	if d != diff.Identity {
+		diff.Print(&bb, script)
+		t.Error(bb.String())
+	}
+}
+
+// TestSC is for debugging purposes. Do not delete.
+func TestSC(t *testing.T) {
+	in := `
+-- cue.mod/module.cue --
+module: "example.com/a"
+
+-- in.cue --
+	`
+	if strings.HasSuffix(strings.TrimSpace(in), ".cue --") {
+		t.Skip()
+	}
+
+	dir, err := ioutil.TempDir("", "test")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a := txtar.Parse([]byte(in))
+	instance := cuetxtar.Load(a, dir)[0]
+	if instance.Err != nil {
+		t.Fatal(instance.Err)
+	}
+
+	r := cuecontext.New()
+
+	v := buildFile(t, r, instance)
+
+	// v = v.LookupPath(cue.ParsePath("a.b"))
+
+	var tValue types.Value
+	v.Core(&tValue)
+	self := export.All
+	self.SelfContained = true
+	self.InlineImports = true
+
+	adt.Verbosity = 1
+
+	file, errs := self.Def(tValue.R, "", tValue.V)
+	if errs != nil {
+		t.Fatal(errs)
+	}
+
+	adt.Verbosity = 0
+
+	b, _ := format.Node(file)
+	t.Error(string(b))
+
+	vf := patch(t, r, a, file)
+	doDiff(t, v, vf)
+}

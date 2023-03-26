812f3f38ac73135f26f2771db9b4fbb3ecc83fad
Daniel Martí <mvdan@mvdan.cc>
2023-03-26 23:59:00 +0100

    cmd/cue: avoid loading the current package in `cue help`
    Loading a CUE package is necessary for `cue help cmd`,
    as the inline comment now explains.
    
    However, it was done for any help invocation,
    even for those which did not need to list available `cue cmd` commands
    such as `cue help` or `cue -h`.
    
    There's already a regression test for `cue help cmd` showing available
    commands in _tools.cue files, which continues to pass.
    Other help commands are unaffected in behavior, but are cheaper now.
    For example, in our ./internal/ci package which contains CUE commands:
    
    	$ time cue help >/dev/null
    	real  0m1.972s
    	user  0m3.519s
    	sys   0m0.083s
    
    and after the change:
    
    	$ time cue help >/dev/null
    	real  0m0.006s
    	user  0m0.000s
    	sys   0m0.006s
    
    Fixes #2161.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I5cd4942e6874550b9f7b3e51f80cc7ffd54a1cfd

--- cmd/cue/cmd/root.go
+++ cmd/cue/cmd/root.go
@@ -21,6 +21,7 @@ import (
 	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/cuecontext"
@@ -279,10 +280,19 @@ func New(args []string) (cmd *Command, err error) {
 		// "fix":   {"fix", nil},
 	}
 
-	// handle help, --help and -h on root 'cue' command
-	if args[0] == "help" || args[0] == "--help" || args[0] == "-h" {
-		// Allow errors.
-		_ = addSubcommands(cmd, sub, args[1:], true)
+	if args[0] == "help" {
+		if len(args) >= 2 && sub[args[1]] != nil {
+			// addSubcommands adds helpful information to each of the help docs,
+			// for example `cue help cmd` shows the available commands by loading
+			// `*_tools.cue` files.
+			//
+			// Ignore errors, since this is a help command and the extra content
+			// is an extra that shouldn't prevent help text from being shown.
+			//
+			// TODO: `cue cmd -h` does not trigger this, and maybe it should for
+			// consistency with `cue help cmd`.
+			_ = addSubcommands(cmd, sub, args[1:], true)
+		}
 		return cmd, nil
 	}
 
@@ -294,6 +304,9 @@ func New(args []string) (cmd *Command, err error) {
 	// it.
 	err = cmd.cmd.ParseFlags(args)
 	if err != nil {
+		if errors.Is(err, pflag.ErrHelp) {
+			return cmd, nil
+		}
 		return nil, err
 	}
 

aa3d23c3f4e6df9f7bf3d7ba64181c70dab38cb7
Marcel van Lohuizen <mpvl@gmail.com>
2023-06-22 12:50:01 +0200

    internal/core/validate: allow required fields in definitions
    Fixes #2336
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I962073e909e2919cfbd1307b68e973a51872f06c

--- internal/core/validate/validate_test.go
+++ internal/core/validate/validate_test.go
@@ -25,18 +25,21 @@ import (
 	"cuelang.org/go/internal/core/compile"
 	"cuelang.org/go/internal/core/eval"
 	"cuelang.org/go/internal/core/runtime"
+	"cuelang.org/go/internal/cuetest"
+	"cuelang.org/go/internal/tdtest"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestValidate(t *testing.T) {
-	testCases := []struct {
-		desc   string
+	type testCase struct {
+		name   string
 		in     string
 		out    string
 		lookup string
 		cfg    *Config
-	}{{
-		desc: "no error, but not concrete, even with definition label",
+	}
+	testCases := []testCase{{
+		name: "no error, but not concrete, even with definition label",
 		cfg:  &Config{Concrete: true},
 		in: `
 		#foo: { use: string }
@@ -44,43 +47,43 @@ func TestValidate(t *testing.T) {
 		lookup: "#foo",
 		out:    "incomplete\n#foo.use: incomplete value string:\n    test:2:16",
 	}, {
-		desc: "definitions not considered for completeness",
+		name: "definitions not considered for completeness",
 		cfg:  &Config{Concrete: true},
 		in: `
 		#foo: { use: string }
 		`,
 	}, {
-		desc: "hidden fields not considered for completeness",
+		name: "hidden fields not considered for completeness",
 		cfg:  &Config{Concrete: true},
 		in: `
 		_foo: { use: string }
 		`,
 	}, {
-		desc: "hidden fields not considered for completeness",
+		name: "hidden fields not considered for completeness",
 		in: `
 		_foo: { use: string }
 		`,
 	}, {
-		desc: "evaluation error at top",
+		name: "evaluation error at top",
 		in: `
 		1 & 2
 		`,
 		out: "eval\nconflicting values 2 and 1:\n    test:2:3\n    test:2:7",
 	}, {
-		desc: "evaluation error in field",
+		name: "evaluation error in field",
 		in: `
 		x: 1 & 2
 		`,
 		out: "eval\nx: conflicting values 2 and 1:\n    test:2:6\n    test:2:10",
 	}, {
-		desc: "first error",
+		name: "first error",
 		in: `
 		x: 1 & 2
 		y: 2 & 4
 		`,
 		out: "eval\nx: conflicting values 2 and 1:\n    test:2:6\n    test:2:10",
 	}, {
-		desc: "all errors",
+		name: "all errors",
 		cfg:  &Config{AllErrors: true},
 		in: `
 		x: 1 & 2
@@ -94,7 +97,7 @@ y: conflicting values 4 and 2:
     test:3:6
     test:3:10`,
 	}, {
-		desc: "incomplete",
+		name: "incomplete",
 		cfg:  &Config{Concrete: true},
 		in: `
 		y: 2 + x
@@ -102,20 +105,20 @@ y: conflicting values 4 and 2:
 		`,
 		out: "incomplete\ny: non-concrete value string in operand to +:\n    test:2:6\n    test:3:6",
 	}, {
-		desc: "allowed incomplete cycle",
+		name: "allowed incomplete cycle",
 		in: `
 		y: x
 		x: y
 		`,
 	}, {
-		desc: "allowed incomplete when disallowing cycles",
+		name: "allowed incomplete when disallowing cycles",
 		cfg:  &Config{DisallowCycles: true},
 		in: `
 		y: string
 		x: y
 		`,
 	}, {
-		desc: "disallow cycle",
+		name: "disallow cycle",
 		cfg:  &Config{DisallowCycles: true},
 		in: `
 		y: x + 1
@@ -123,7 +126,7 @@ y: conflicting values 4 and 2:
 		`,
 		out: "cycle\ncycle error:\n    test:2:6",
 	}, {
-		desc: "disallow cycle",
+		name: "disallow cycle",
 		cfg:  &Config{DisallowCycles: true},
 		in: `
 		a: b - 100
@@ -131,7 +134,7 @@ y: conflicting values 4 and 2:
 		c: [c[1], c[0]]		`,
 		out: "cycle\ncycle error:\n    test:2:6",
 	}, {
-		desc: "treat cycles as incomplete when not disallowing",
+		name: "treat cycles as incomplete when not disallowing",
 		cfg:  &Config{},
 		in: `
 		y: x + 1
@@ -140,7 +143,7 @@ y: conflicting values 4 and 2:
 	}, {
 		// Note: this is already handled by evaluation, as terminal errors
 		// are percolated up.
-		desc: "catch most serious error",
+		name: "catch most serious error",
 		cfg:  &Config{Concrete: true},
 		in: `
 		y: string
@@ -148,13 +151,13 @@ y: conflicting values 4 and 2:
 		`,
 		out: "eval\nx: conflicting values 2 and 1:\n    test:3:6\n    test:3:10",
 	}, {
-		desc: "consider defaults for concreteness",
+		name: "consider defaults for concreteness",
 		cfg:  &Config{Concrete: true},
 		in: `
 		x: *1 | 2
 		`,
 	}, {
-		desc: "allow non-concrete in definitions in concrete mode",
+		name: "allow non-concrete in definitions in concrete mode",
 		cfg:  &Config{Concrete: true},
 		in: `
 		x: 2
@@ -164,7 +167,7 @@ y: conflicting values 4 and 2:
 		}
 		`,
 	}, {
-		desc: "pick up non-concrete value in default",
+		name: "pick up non-concrete value in default",
 		cfg:  &Config{Concrete: true},
 		in: `
 		x: null | *{
@@ -173,7 +176,7 @@ y: conflicting values 4 and 2:
 		`,
 		out: "incomplete\nx.a: incomplete value int:\n    test:3:7",
 	}, {
-		desc: "pick up non-concrete value in default",
+		name: "pick up non-concrete value in default",
 		cfg:  &Config{Concrete: true},
 		in: `
 			x: null | *{
@@ -181,38 +184,66 @@ y: conflicting values 4 and 2:
 			}
 			`,
 		out: "incomplete\nx.a: incomplete value 1 | 2",
+	}, {
+		name: "required field not present",
+		cfg:  &Config{Concrete: true},
+		in: `
+			Person: {
+				name!:  string
+				age?:   int
+				height: 1.80
+			}
+			`,
+		out: "incomplete\nPerson.name: field is required but not present",
+	}, {
+		name: "allow required fields in definitions",
+		cfg:  &Config{Concrete: true},
+		in: `
+		#Person: {
+			name!: string
+			age?:  int
+		}
+		`,
+		out: "",
+	}, {
+		name: "allow required fields when not concrete",
+		in: `
+		Person: {
+			name!: string
+			age?:  int
+		}
+		`,
+		out: "",
 	}}
 
 	r := runtime.New()
 	ctx := eval.NewContext(r, nil)
 
-	for _, tc := range testCases {
-		t.Run(tc.desc, func(t *testing.T) {
-			f, err := parser.ParseFile("test", tc.in)
-			if err != nil {
-				t.Fatal(err)
-			}
-			v, err := compile.Files(nil, r, "", f)
-			if err != nil {
-				t.Fatal(err)
-			}
-			v.Finalize(ctx)
-			if tc.lookup != "" {
-				v = v.Lookup(adt.MakeIdentLabel(r, tc.lookup, "main"))
-			}
+	tdtest.Run(t, testCases, func(t *cuetest.T, tc *testCase) {
+		f, err := parser.ParseFile("test", tc.in)
+		if err != nil {
+			t.Fatal(err)
+		}
+		v, err := compile.Files(nil, r, "", f)
+		if err != nil {
+			t.Fatal(err)
+		}
+		v.Finalize(ctx)
+		if tc.lookup != "" {
+			v = v.Lookup(adt.MakeIdentLabel(r, tc.lookup, "main"))
+		}
 
-			b := Validate(ctx, v, tc.cfg)
+		b := Validate(ctx, v, tc.cfg)
 
-			w := &strings.Builder{}
-			if b != nil {
-				fmt.Fprintln(w, b.Code)
-				errors.Print(w, b.Err, nil)
-			}
+		w := &strings.Builder{}
+		if b != nil {
+			fmt.Fprintln(w, b.Code)
+			errors.Print(w, b.Err, nil)
+		}
 
-			got := strings.TrimSpace(w.String())
-			if tc.out != got {
-				t.Error(cmp.Diff(tc.out, got))
-			}
-		})
-	}
+		got := strings.TrimSpace(w.String())
+		if tc.out != got {
+			t.Error(cmp.Diff(tc.out, got))
+		}
+	})
 }

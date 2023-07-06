ea99825afbe85d26e89920bfb86bb441bf36ddbf
Roger Peppe <rogpeppe@gmail.com>
2023-07-06 17:10:56 +0100

    cue/load: update for OCI modules
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I7b6d71993c253e6525fc3a69216e87d814ba6b3e

--- internal/mod/modfile/modfile.go
+++ internal/mod/modfile/modfile.go
@@ -30,6 +30,10 @@ type Dep struct {
 	Default bool   `json:"default,omitempty"`
 }
 
+type noDepsFile struct {
+	Module string `json:"module"`
+}
+
 var moduleSchema = func() cue.Value {
 	ctx := cuecontext.New()
 	schemav := ctx.CompileBytes(moduleSchemaData, cue.Filename("cuelang.org/go/internal/mod/modfile/schema.cue"))
@@ -56,6 +60,23 @@ func Parse(modfile []byte, filename string) (*File, error) {
 	return parse(modfile, filename, true)
 }
 
+// ParseLegacy parses the legacy version of the module file
+// that only supports the single field "module" and ignores all other
+// fields.
+func ParseLegacy(modfile []byte, filename string) (*File, error) {
+	v := moduleSchema.Context().CompileBytes(modfile, cue.Filename(filename))
+	if err := v.Err(); err != nil {
+		return nil, errors.Wrapf(err, token.NoPos, "invalid module.cue file")
+	}
+	var f noDepsFile
+	if err := v.Decode(&f); err != nil {
+		return nil, newCUEError(err)
+	}
+	return &File{
+		Module: f.Module,
+	}, nil
+}
+
 // ParseNonStrict is like Parse but allows some laxity in the parsing:
 //   - if a module path lacks a version, it's taken from the version.
 //   - if a non-canonical version is used, it will be canonicalized.
@@ -78,17 +99,7 @@ func parse(modfile []byte, filename string, strict bool) (*File, error) {
 	}
 	v = v.Unify(moduleSchema)
 	if err := v.Validate(); err != nil {
-		// The default CUE errors are not great currently, particularly when the recipient doesn't
-		// necessarily have access to the original schema, so just include a list of
-		// all the fields where there are errors.
-		if paths := badPaths(err); len(paths) > 0 {
-			f := "fields"
-			if len(paths) == 1 {
-				f = "field"
-			}
-			return nil, fmt.Errorf("invalid module.cue file: errors in the following %s: %s", f, strings.Join(paths, ", "))
-		}
-		return nil, fmt.Errorf("%v", errors.Details(err, nil)) // errors.Wrapf(err, token.NoPos, "invalid module.cue file")
+		return nil, newCUEError(err)
 	}
 	var mf File
 	if err := v.Decode(&mf); err != nil {
@@ -97,10 +108,10 @@ func parse(modfile []byte, filename string, strict bool) (*File, error) {
 	if strict {
 		_, v, ok := module.SplitPathVersion(mf.Module)
 		if !ok {
-			return nil, fmt.Errorf("module path %q does not contain major version", mf.Module)
+			return nil, fmt.Errorf("module path %q in %q does not contain major version", mf.Module, filename)
 		}
 		if semver.Major(v) != v {
-			return nil, fmt.Errorf("module path %q should contain the major version only", mf.Module)
+			return nil, fmt.Errorf("module path %q in %q should contain the major version only", mf.Module, filename)
 		}
 	}
 	var versions []module.Version
@@ -108,16 +119,30 @@ func parse(modfile []byte, filename string, strict bool) (*File, error) {
 	for m, dep := range mf.Deps {
 		v, err := module.NewVersion(m, dep.Version)
 		if err != nil {
-			return nil, fmt.Errorf("invalid module.cue file: cannot make version from module %q, version %q: %v", m, dep.Version, err)
+			return nil, fmt.Errorf("invalid module.cue file %q: cannot make version from module %q, version %q: %v", filename, m, dep.Version, err)
 		}
 		versions = append(versions, v)
 		if strict && v.Path() != m {
-			return nil, fmt.Errorf("invalid module.cue file: no major version in %q", m)
+			return nil, fmt.Errorf("invalid module.cue file %q: no major version in %q", filename, m)
 		}
 	}
 
 	mf.versions = versions[:len(versions):len(versions)]
 	return &mf, nil
+}
+
+// The default CUE errors are not great currently, particularly when the recipient doesn't
+// necessarily have access to the original schema, so just include a list of
+// all the fields where there are errors.
+func newCUEError(err error) error {
+	if paths := badPaths(err); len(paths) > 0 {
+		f := "fields"
+		if len(paths) == 1 {
+			f = "field"
+		}
+		return fmt.Errorf("invalid module.cue file: errors in the following %s: %s", f, strings.Join(paths, ", "))
+	}
+	return fmt.Errorf("%v", errors.Details(err, nil)) // errors.Wrapf(err, token.NoPos, "invalid module.cue file")
 }
 
 func (f *File) Versions() []module.Version {

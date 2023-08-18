b072d9b68662a1fce77680b52a771dce82781785
Daniel Martí <mvdan@mvdan.cc>
2023-08-18 13:42:05 +0200

    all: clean up some TODOs for old Go versions
    Now that we require Go 1.20 or later, we can simplify some of our code.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Iede54cc9b67806a5c4de8cffc88903e838106186

--- cue/ast/astutil/sanitize.go
+++ cue/ast/astutil/sanitize.go
@@ -332,9 +332,8 @@ func (z *sanitizer) uniqueName(base string, hidden bool) string {
 		}
 	}
 
-	// TODO(go1.13): const mask = 0xff_ffff_ffff_ffff
-	const mask = 0xffffffffffffff // max bits; stay clear of int64 overflow
-	const shift = 4               // rate of growth
+	const mask = 0xff_ffff_ffff_ffff // max bits; stay clear of int64 overflow
+	const shift = 4                  // rate of growth
 	for n := int64(0x10); ; n = int64(mask&((n<<shift)-1)) + 1 {
 		num := z.rand.Intn(int(n))
 		name := fmt.Sprintf("%s_%01X", base, num)

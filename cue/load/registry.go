d6948814bb13f977bc9f645c3a3bda53732f251f
Roger Peppe <rogpeppe@gmail.com>
2023-09-08 11:29:31 +0100

    cue/load: update for OCI modules
    Note: This is an experimental feature and is intended for internal-only
    use for now.
    
    Summary of changes:
    - adapt cue/load/internal/registrytest to use OCI registry API;
    - change cue/load to use new internal/mod/modfile package;
    - change cue/load.TestLoad to use tdtest package for easier test
    updating;
    - change cue/load to support inferring major module version from
    module.cue deps.
    - remove now-unused packages inside cue/load/internal.
    
    For #2330.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I7b6d71993c253e6525fc3a69216e87d814ba6b3e

--- cue/load/registry.go
+++ cue/load/registry.go
@@ -1,29 +1,25 @@
 package load
 
 import (
+	"context"
 	"fmt"
 	"io"
-	"net/http"
 	"os"
 	"path"
 	"path/filepath"
 
-	"golang.org/x/mod/module"
-	"golang.org/x/mod/zip"
-
-	"cuelang.org/go/cue"
-	"cuelang.org/go/cue/errors"
-	"cuelang.org/go/cue/parser"
-	"cuelang.org/go/cue/token"
-	"cuelang.org/go/internal/core/runtime"
+	"cuelang.org/go/internal/mod/modfile"
+	"cuelang.org/go/internal/mod/modregistry"
+	"cuelang.org/go/internal/mod/module"
+	"cuelang.org/go/internal/mod/zip"
 )
 
 // registryClient implements the protocol for talking to
 // the registry server.
 type registryClient struct {
 	// TODO caching
-	registryURL string
-	cacheDir    string
+	client   *modregistry.Client
+	cacheDir string
 }
 
 // newRegistryClient returns a registry client that talks to
@@ -31,21 +27,25 @@ type registryClient struct {
 // in the given cache directory. It assumes that information
 // in the registry is immutable, so if it's in the cache, a module
 // will not be downloaded again.
-func newRegistryClient(registryURL, cacheDir string) *registryClient {
-	return &registryClient{
-		registryURL: registryURL,
-		cacheDir:    cacheDir,
+func newRegistryClient(registryHost string, cacheDir string) (*registryClient, error) {
+	client, err := modregistry.NewClient(registryHost, "cue/") // TODO configurable prefix
+	if err != nil {
+		return nil, err
 	}
+	return &registryClient{
+		client:   client,
+		cacheDir: cacheDir,
+	}, nil
 }
 
 // fetchModFile returns the parsed contents of the cue.mod/module.cue file
 // for the given module.
-func (c *registryClient) fetchModFile(m module.Version) (*modFile, error) {
-	data, err := c.fetchRawModFile(m)
+func (c *registryClient) fetchModFile(ctx context.Context, m module.Version) (*modfile.File, error) {
+	data, err := c.fetchRawModFile(ctx, m)
 	if err != nil {
 		return nil, err
 	}
-	mf, err := parseModuleFile(data, path.Join(m.Path, "cue.mod/module.cue"))
+	mf, err := modfile.Parse(data, path.Join(m.Path(), "cue.mod/module.cue"))
 	if err != nil {
 		return nil, err
 	}
@@ -54,40 +54,31 @@ func (c *registryClient) fetchModFile(m module.Version) (*modFile, error) {
 
 // fetchModFile returns the contents of the cue.mod/module.cue file
 // for the given module without parsing it.
-func (c *registryClient) fetchRawModFile(m module.Version) ([]byte, error) {
-	resp, err := http.Get(c.registryURL + "/" + m.Path + "/@v/" + m.Version + ".mod")
+func (c *registryClient) fetchRawModFile(ctx context.Context, mv module.Version) ([]byte, error) {
+	m, err := c.client.GetModule(ctx, mv)
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("cannot get HTTP response body: %v", err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("module.cue HTTP GET request failed: %s", body)
-	}
-	return body, nil
+	return m.ModuleFile(ctx)
 }
 
 // getModContents downloads the module with the given version
 // and returns the directory where it's stored.
-func (c *registryClient) getModContents(m module.Version) (string, error) {
-	modPath := filepath.Join(c.cacheDir, fmt.Sprintf("%s@%s", m.Path, m.Version))
+func (c *registryClient) getModContents(ctx context.Context, mv module.Version) (string, error) {
+	modPath := filepath.Join(c.cacheDir, mv.String())
 	if _, err := os.Stat(modPath); err == nil {
 		return modPath, nil
 	}
-	// TODO synchronize parallel invocations
-	resp, err := http.Get(c.registryURL + "/" + m.Path + "/@v/" + m.Version + ".zip")
+	m, err := c.client.GetModule(ctx, mv)
 	if err != nil {
 		return "", err
 	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		body, _ := io.ReadAll(resp.Body)
-		return "", fmt.Errorf("module.cue HTTP GET request failed: %s", body)
+	r, err := m.GetZip(ctx)
+	if err != nil {
+		return "", err
 	}
-	zipfile := filepath.Join(c.cacheDir, m.String()+".zip")
+	defer r.Close()
+	zipfile := filepath.Join(c.cacheDir, mv.String()+".zip")
 	if err := os.MkdirAll(filepath.Dir(zipfile), 0o777); err != nil {
 		return "", fmt.Errorf("cannot create parent directory for zip file: %v", err)
 	}
@@ -97,40 +88,11 @@ func (c *registryClient) getModContents(m module.Version) (string, error) {
 	}
 
 	defer f.Close() // TODO check error on close
-	if _, err := io.Copy(f, resp.Body); err != nil {
+	if _, err := io.Copy(f, r); err != nil {
 		return "", fmt.Errorf("cannot copy data to zip file %q: %v", zipfile, err)
 	}
-	if err := zip.Unzip(modPath, m, zipfile); err != nil {
-		return "", fmt.Errorf("cannot unzip %v: %v", m, err)
+	if err := zip.Unzip(modPath, mv, zipfile); err != nil {
+		return "", fmt.Errorf("cannot unzip %v: %v", mv, err)
 	}
 	return modPath, nil
-}
-
-// parseModuleFile parses a cue.mod/module.cue file.
-// TODO move this to be closer to the modFile type definition.
-func parseModuleFile(data []byte, filename string) (*modFile, error) {
-	file, err := parser.ParseFile(filename, data)
-	if err != nil {
-		return nil, errors.Wrapf(err, token.NoPos, "invalid module.cue file %q", data)
-	}
-	// TODO disallow non-data-mode CUE.
-
-	ctx := (*cue.Context)(runtime.New())
-	schemav := ctx.CompileBytes(moduleSchema, cue.Filename("$cueroot/cue/load/moduleschema.cue"))
-	if err := schemav.Validate(); err != nil {
-		return nil, errors.Wrapf(err, token.NoPos, "internal error: invalid CUE module.cue schema")
-	}
-	v := ctx.BuildFile(file)
-	if err := v.Validate(cue.Concrete(true)); err != nil {
-		return nil, errors.Wrapf(err, token.NoPos, "invalid module.cue file")
-	}
-	v = v.Unify(schemav)
-	if err := v.Validate(); err != nil {
-		return nil, errors.Wrapf(err, token.NoPos, "invalid module.cue file")
-	}
-	var mf modFile
-	if err := v.Decode(&mf); err != nil {
-		return nil, errors.Wrapf(err, token.NoPos, "internal error: cannot decode into modFile struct (\nfile %q\ncontents %q\nvalue %#v\n)", filename, data, v)
-	}
-	return &mf, nil
 }

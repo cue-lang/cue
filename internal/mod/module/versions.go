d66453a5b6680c9934a693718ad1572304a021e9
Roger Peppe <rogpeppe@gmail.com>
2023-09-11 14:06:22 +0100

    internal/mod/mvs: adapt to CUE semantics
    Note: experimental feature.
    
    The mvs tests rely on using module.Version in a way that
    doesn't conform to the new requirements for that type.
    Given that mvs doesn't actually care that it's specifically
    dealing with module.Version per se, we make it generic
    across any version-like value.
    This allows us to avoid rewriting all the (fairly extensive) tests
    and seems a reasonably natural fit for the generic nature of
    the MVS algorithm itself.
    
    For #2330.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: Ib1b16c3267ac98fde30347f118f0eb6b2764a473

---
+++ internal/mod/module/versions.go
@@ -0,0 +1,42 @@
+package module
+
+import (
+	"golang.org/x/mod/semver"
+)
+
+// Versions implements mvs.Versions[Version].
+type Versions struct{}
+
+// New implements mvs.Versions[Version].Version.
+func (Versions) Version(v Version) string {
+	return v.Version()
+}
+
+// New implements mvs.Versions[Version].Path.
+func (Versions) Path(v Version) string {
+	return v.Path()
+}
+
+// New implements mvs.Versions[Version].New.
+func (Versions) New(p, v string) (Version, error) {
+	return NewVersion(p, v)
+}
+
+// Max implements mvs.Reqs.Max.
+//
+// Note that the empty
+// string is allowed because it's a potential version of
+// a build target where there is no known version.
+// See [mvs.Reqs] for more detail.
+func (Versions) Max(v1, v2 string) string {
+	if v1 == "none" || v2 == "" {
+		return v2
+	}
+	if v2 == "none" || v1 == "" {
+		return v1
+	}
+	if semver.Compare(v1, v2) > 0 {
+		return v1
+	}
+	return v2
+}

782f95225c692f82a8624e7d4ba3442d318fd063
Daniel Martí <mvdan@mvdan.cc>
2023-09-20 10:37:32 +0100

    cmd/cue: do not ignore Encoder.Close errors on export
    Typically, this is not a big deal: "cue export" writes to stdout,
    so the output is streamed to stdout directly, which should not fail.
    
    However, when using "cue export -o", we buffer the entire output
    and Encoder.Close actually writes it to the filename via os.WriteFile.
    Ignoring the error from os.WriteFile means we are ignoring any error
    related to creating the file, writing to it, or flushing/closing it.
    
    Stop ignoring the error by doing what other commands like def do.
    Note that they don't defer closing the encoder, which seems okay;
    all that Close does today is the equivalent of os.WriteFile,
    so there aren't any resources like a file descriptor to release.
    
    This fixes the first test case in the previous commit,
    inverting the expected outcome to match our expectations.
    
    While here, I also noticed a related issue with the writer func,
    which is what constructs the only non-nil Encoder.Close function value.
    The output "-o" flag comes with a "-f" flag to force overwriting
    existing files, which was implemented via an upfront call to os.Stat.
    
    This was racy: if I make a call to "cue export -o out.json",
    and evaluating and exporting my CUE takes around one second,
    it could be that a different process has created the file
    after the initial os.Stat call but before the final os.WriteFile call.
    "cue export -o" thus overwrites the file even without the "-f" flag.
    
    Instead, implement the logic to allow or disallow overwriting a file
    by using different os.FileModes and calling os.OpenFile directly,
    rather than using the simpler and higher level os.WriteFile.
    Without "-f", we allow creating a file with the mode bit os.O_EXCL,
    so we get the underlying error fs.ErrExist if the file already exists.
    With "-f", we use os.O_TRUNC instead, so we allow replacing an existing
    file and truncate it to zero bytes before writing - like os.WriteFile.
    
    This fixes the second test case in the previous commit,
    uncommenting the relevant lines since the behavior is no longer racy.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Ia78ebf5569d5bbf1dac4d660bcb2d520d97be680

--- internal/encoding/encoder.go
+++ internal/encoding/encoder.go
@@ -19,6 +19,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
 
@@ -300,17 +301,27 @@ func writer(f *build.File, cfg *Config) (_ io.Writer, close func() error, err er
 		}
 		return cfg.Stdout, nil, nil
 	}
-	if !cfg.Force {
-		if _, err := os.Stat(path); err == nil {
-			return nil, nil, errors.Wrapf(os.ErrExist, token.NoPos,
-				"error writing %q", path)
-		}
-	}
-	// Delay opening the file until we can write it to completion. This will
-	// prevent clobbering the file in case of a crash.
+	// Delay opening the file until we can write it to completion.
+	// This prevents clobbering the file in case of a crash.
 	b := &bytes.Buffer{}
 	fn := func() error {
-		return os.WriteFile(path, b.Bytes(), 0644)
+		mode := os.O_WRONLY | os.O_CREATE | os.O_EXCL
+		if cfg.Force {
+			// Swap O_EXCL for O_TRUNC to allow replacing an entire existing file.
+			mode = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+		}
+		f, err := os.OpenFile(path, mode, 0o644)
+		if err != nil {
+			if errors.Is(err, fs.ErrExist) {
+				return errors.Wrapf(fs.ErrExist, token.NoPos, "error writing %q", path)
+			}
+			return err
+		}
+		_, err = f.Write(b.Bytes())
+		if err1 := f.Close(); err1 != nil && err == nil {
+			err = err1
+		}
+		return err
 	}
 	return b, fn, nil
 }

c276e817c588d4f933d286318064f6a70b8f97c4
Marcel van Lohuizen <mpvl@gmail.com>
2023-10-05 16:47:30 +0200

    internal/core/adt: change semantics of unary_op(basictype)
    This seems wrong. New evaluator handles this correctly based on
    semantics of old evaluator, which means it ends up silently ignoring
    incomplete errors. The old evaluator was inconsistent here.
    
    Rather than mimicing the old behavior, it seems better to adjust
    the semantics.
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: Iab11975e1ea62a15b0a3f9619b9f0b1a84d7a824

--- internal/core/adt/expr.go
+++ internal/core/adt/expr.go
@@ -548,7 +548,11 @@ func (x *BoundExpr) evaluate(ctx *OpContext, state vertexStatus) Value {
 	case *BasicType:
 		switch x.Op {
 		case LessEqualOp, LessThanOp, GreaterEqualOp, GreaterThanOp:
-			return y
+			// TODO: this does not seem correct and results in some weird
+			// behavior for bounds.
+			ctx.addErrf(IncompleteError, token.NoPos,
+				"non-concrete value %s for bound %s", x.Expr, x.Op)
+			return nil
 		}
 	}
 	if v.Concreteness() > Concrete {

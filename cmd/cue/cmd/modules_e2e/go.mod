641be181ef544da1e6555f5773a1eaba715a97e1
Daniel Martí <mvdan@mvdan.cc>
2023-10-30 20:53:03 +0000

    cmd/cue: first version of the end-to-end module tests
    GITHUB_TOKEN is required with the documented permissions for GITHUB_ORG.
    The test should otherwise work without any other manual steps.
    
    A cleanup go-build-ignored program is also included
    which removes repositories left behind by GITHUB_KEEP=true or failures,
    which also follows the GITHUB_TOKEN and GITHUB_ORG env vars.
    
    We use a separate Go module since these tests use google/go-github,
    and we don't want the root module to gain extra module dependencies.
    The test module requires the root module via a replace directive;
    we avoid using a Go workspace because it's unnecessary for two modules,
    and because the test module should always use the git repo's CUE module.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Id65235a058acb78b544265e508c9462856b06b1e

---
+++ cmd/cue/cmd/modules_e2e/go.mod
@@ -0,0 +1,34 @@
+module test/e2e
+
+go 1.20
+
+require (
+	cuelang.org/go v0.0.0-00010101000000-000000000000
+	github.com/google/go-github/v56 v56.0.0
+	github.com/rogpeppe/go-internal v1.11.1-0.20230926105539-32ae33786ecc
+)
+
+require (
+	cuelabs.dev/go/oci/ociregistry v0.0.0-20231004130125-2c3ad8a6ecd3 // indirect
+	github.com/cockroachdb/apd/v3 v3.2.1 // indirect
+	github.com/emicklei/proto v1.10.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/mpvl/unique v0.0.0-20150818121801-cbe035fff7de // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0-rc4 // indirect
+	github.com/protocolbuffers/txtpbfmt v0.0.0-20230328191034-3462fbc510c0 // indirect
+	github.com/spf13/cobra v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tetratelabs/wazero v1.0.2 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+replace cuelang.org/go => ../../../../
---
+++ cmd/cue/cmd/modules_e2e/script_test.go
@@ -0,0 +1,174 @@
+// Copyright 2023 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package e2e_test
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/google/go-github/v56/github"
+	"github.com/rogpeppe/go-internal/testscript"
+)
+
+func TestMain(m *testing.M) {
+	cachedGobin := os.Getenv("CUE_CACHED_GOBIN")
+	if cachedGobin == "" {
+		// Install the cmd/cue version into a cached GOBIN so we can reuse it.
+		// TODO: use "go tool cue" once we can rely on Go 1.22's tool dependency tracking in go.mod.
+		// See: https://go.dev/issue/48429
+		cacheDir, err := os.UserCacheDir()
+		if err != nil {
+			panic(err)
+		}
+		cachedGobin = filepath.Join(cacheDir, "cue-e2e-gobin")
+		cmd := exec.Command("go", "install", "cuelang.org/go/cmd/cue")
+		cmd.Env = append(cmd.Environ(), "GOBIN="+cachedGobin)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			panic(fmt.Errorf("%v: %s", err, out))
+		}
+		os.Setenv("CUE_CACHED_GOBIN", cachedGobin)
+	}
+
+	os.Exit(testscript.RunMain(m, map[string]func() int{
+		"cue": func() int {
+			// Note that we could avoid this wrapper entirely by setting PATH,
+			// since TestMain sets up a single cue binary in a GOBIN directory,
+			// but that may change at any point, or we might just switch to "go tool cue".
+			cmd := exec.Command(filepath.Join(cachedGobin, "cue"), os.Args[1:]...)
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err := cmd.Run(); err != nil {
+				if err, ok := err.(*exec.ExitError); ok {
+					return err.ExitCode()
+				}
+				fmt.Fprintln(os.Stderr, err)
+				return 1
+			}
+			return 0
+		},
+	}))
+}
+
+func envOr(name, fallback string) string {
+	if s := os.Getenv(name); s != "" {
+		return s
+	}
+	return fallback
+}
+
+func envMust(name string) string {
+	if s := os.Getenv(name); s != "" {
+		return s
+	}
+	panic(fmt.Sprintf("%s must be set", name))
+}
+
+func tsExpand(ts *testscript.TestScript, s string) string {
+	return os.Expand(s, func(key string) string {
+		return ts.Getenv(key)
+	})
+}
+
+var (
+	// githubOrg is a GitHub organization where the "CUE Module Publisher"
+	// GitHub App has been installed on all repositories.
+	// This is necessary since we will create a new repository per test,
+	// and there's no way to easily install the app on each repo via the API.
+	githubOrg = envOr("GITHUB_ORG", "cue-labs-modules-testing")
+	// githubToken should have read and write access to repository
+	// administration and contents within githubOrg,
+	// to be able to create repositories under the org and git push to them.
+	githubToken = envMust("GITHUB_TOKEN")
+	// githubKeep leaves the newly created repo around when set to true.
+	githubKeep = envOr("GITHUB_KEEP", "false")
+)
+
+func TestScript(t *testing.T) {
+	p := testscript.Params{
+		Dir:                 filepath.Join("testdata", "script"),
+		RequireExplicitExec: true,
+		Setup: func(env *testscript.Env) error {
+			env.Setenv("CUE_EXPERIMENT", "modules")
+			env.Setenv("CUE_REGISTRY", "registry.cue.works")
+			env.Setenv("CUE_CACHED_GOBIN", os.Getenv("CUE_CACHED_GOBIN"))
+			env.Setenv("GITHUB_TOKEN", githubToken) // needed for "git push"
+			return nil
+		},
+		Cmds: map[string]func(ts *testscript.TestScript, neg bool, args []string){
+			// create-github-repo creates a unique repository under githubOrg
+			// and sets $MODULE to its resulting module path.
+			"create-github-repo": func(ts *testscript.TestScript, neg bool, args []string) {
+				// TODO: name the repo after ts.Name once the API lands
+				// TODO: add a short random suffix to prevent time collisions
+				repoName := fmt.Sprintf("%d", time.Now().Unix())
+
+				client := github.NewClient(nil).WithAuthToken(githubToken)
+				ctx := context.TODO()
+
+				repo := &github.Repository{
+					Name: github.String(repoName),
+				}
+				_, _, err := client.Repositories.Create(ctx, githubOrg, repo)
+				ts.Check(err)
+
+				// Unless GITHUB_KEEP=true is set, delete the repo when we finish.
+				//
+				// TODO: It might be useful to leave the repo around when the test fails.
+				// We would need testscript.TestScript to expose T.Failed for this.
+				ts.Defer(func() {
+					if githubKeep == "true" {
+						return
+					}
+					_, err := client.Repositories.Delete(ctx, githubOrg, repoName)
+					ts.Check(err)
+				})
+
+				ts.Setenv("MODULE", fmt.Sprintf("github.com/%s/%s", githubOrg, repoName))
+			},
+			// env-fill expands variables in a file like cmpenv, replacing the file contents with the result.
+			"env-fill": func(ts *testscript.TestScript, neg bool, args []string) {
+				for _, arg := range args {
+					path := ts.MkAbs(arg)
+					data := ts.ReadFile(path)
+					data = tsExpand(ts, data)
+					ts.Check(os.WriteFile(path, []byte(data), 0o666))
+				}
+			},
+			// cue-mod-wait waits for a CUE module to exist in a registry for up to 20s.
+			// Since this is easily done via an HTTP HEAD request, an OCI client isn't necessary.
+			"cue-mod-wait": func(ts *testscript.TestScript, neg bool, args []string) {
+				manifest := tsExpand(ts, "https://${CUE_REGISTRY}/v2/${MODULE}/manifests/${VERSION}")
+				for i := 0; i < 10; i++ {
+					resp, err := http.Head(manifest)
+					ts.Check(err)
+					if resp.StatusCode == http.StatusOK {
+						break
+					}
+					ts.Logf("waiting for HEAD %s", manifest)
+					time.Sleep(2 * time.Second)
+				}
+			},
+		},
+	}
+	testscript.Run(t, p)
+}

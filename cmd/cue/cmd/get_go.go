b29f6345b019b180771a12587abe8e5b00b36dca
Thomas Way <thomas@6f.io>
2023-11-09 21:17:54 +0000

    cmd/cue: support optional comments in get go
    The Kuberentes ecosystem use comments to denote whether a field is
    optional or required. The comment has a direct effect on the produced
    OpenAPI schema, and often means otherwise correct Go "schemas" will be
    represented incorrectly with CUE.
    
    https://github.com/kubernetes/community/blob/e977e6ea355f26130ca555d1e8704893727ee024/contributors/devel/sig-architecture/api-conventions.md#optional-vs-required
    
    In addition to the heuristics of the "omitempty" struct tag, the
    "// +optional" comment will now also mark a field as optional. The
    behaviour from the Kubernetes project which parses this comment as a
    key/value pair (tag) is also preserved.
    
    https://pkg.go.dev/k8s.io/gengo/types#ExtractCommentTags
    
    Fixes #2679
    
    Change-Id: Ieeb2e7bee61e16ed9910ea46e68cb1a3eaa47197
    Signed-off-by: Thomas Way <thomas@6f.io>

--- cmd/cue/cmd/get_go.go
+++ cmd/cue/cmd/get_go.go
@@ -1291,15 +1291,15 @@ func (e *extractor) addFields(x *types.Struct, st *cueast.StructLit) {
 		if name == "-" {
 			continue
 		}
+
+		doc := docs[i]
+
 		// TODO: check referrers
 		kind := regular
-		if e.isOptional(tag) {
+		if e.isOptional(f, doc, tag) {
 			kind = optional
 		}
-		if _, ok := f.Type().(*types.Pointer); ok {
-			kind = optional
-		}
-		field, cueType := e.makeField(name, kind, f.Type(), docs[i], count > 0)
+		field, cueType := e.makeField(name, kind, f.Type(), doc, count > 0)
 		add(field)
 
 		if s := reflect.StructTag(tag).Get("cue"); s != "" {
@@ -1394,7 +1394,18 @@ func (e *extractor) isInline(tag string) bool {
 		hasFlag(tag, "yaml", "inline", 1)
 }
 
-func (e *extractor) isOptional(tag string) bool {
+func (e *extractor) isOptional(f *types.Var, doc *ast.CommentGroup, tag string) bool {
+	if _, ok := f.Type().(*types.Pointer); ok {
+		return true
+	}
+
+	for _, line := range doc.List {
+		before, _, _ := strings.Cut(strings.TrimSpace(line.Text), "=")
+		if before == "+optional" {
+			return true
+		}
+	}
+
 	// TODO: also when the type is a list or other kind of pointer.
 	return hasFlag(tag, "json", "omitempty", 1) ||
 		hasFlag(tag, "yaml", "omitempty", 1)

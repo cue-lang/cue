b31e83c5859956c182b8fc0bd941ca09618ab9b5
Justen Stall <justenstall@gmail.com>
2023-11-15 11:09:03 -0500

    fix license headers

--- encoding/crd/decode.go
+++ encoding/crd/decode.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2023 Stefan Prodan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package crd
 
 import (
@@ -176,6 +191,7 @@ func convertCRD(crd cue.Value) (*IntermediateCRD, error) {
 
 		// Add attributes for k8s oapi extensions
 		// construct a map of all paths using x-kubernetes-* OpenAPI extensions
+		// TODO: what is the list type?
 		for _, extAttr := range xKubernetesAttributes(defpath.Selectors(), rootosch) {
 			extendedVal := extracted.LookupPath(cue.MakePath(extAttr.path...))
 			node := extendedVal.Source()
--- encoding/crd/unmarshal.go
+++ encoding/crd/unmarshal.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2023 Stefan Prodan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package crd
 
 import (
@@ -10,11 +25,6 @@ import (
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/scheme"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
-)
-
-var (
-	// The "codec factory" that can decode CRDs
-	codecs = serializer.NewCodecFactory(scheme.Scheme, serializer.EnableStrict)
 )
 
 // Unmarshals a YAML file containing one or more CustomResourceDefinitions
@@ -76,7 +86,7 @@ func UnmarshalOne(val cue.Value) (*v1.CustomResourceDefinition, error) {
 
 	// Decode into a v1.CustomResourceDefinition
 	obj := &v1.CustomResourceDefinition{}
-	if err := runtime.DecodeInto(codecs.UniversalDecoder(), d, obj); err != nil {
+	if err = runtime.DecodeInto(serializer.NewCodecFactory(scheme.Scheme).UniversalDecoder(), d, obj); err != nil {
 		return nil, err
 	}
 

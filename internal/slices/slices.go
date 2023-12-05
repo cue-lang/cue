459ff079484be50dc6acf7d10ee3475b569b8dc2
Roger Peppe <rogpeppe@gmail.com>
2023-12-05 10:40:38 +0000

    internal/slices: add Equal
    This comes straight from the standard library. We'll use the
    stdlib version when we can.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I9f7504a98dbcb5f68396b1c89bbc6f312b024db6

--- internal/slices/slices.go
+++ internal/slices/slices.go
@@ -10,3 +10,20 @@ package slices
 func Clip[S ~[]E, E any](s S) S {
 	return s[:len(s):len(s)]
 }
+
+// Equal reports whether two slices are equal: the same length and all
+// elements equal. If the lengths are different, Equal returns false.
+// Otherwise, the elements are compared in increasing index order, and the
+// comparison stops at the first unequal pair.
+// Floating point NaNs are not considered equal.
+func Equal[S ~[]E, E comparable](s1, s2 S) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := range s1 {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+	return true
+}

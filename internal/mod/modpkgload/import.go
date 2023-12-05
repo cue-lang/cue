8716fc67223c5d4a34f8013f049d1d7b517459bd
Roger Peppe <rogpeppe@gmail.com>
2023-12-05 10:48:58 +0000

    internal/mod/modpkgload: new package
    This provides functionality for loading packages and
    their dependencies with respect to a particular module graph.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: Ibc4f08bb3d40ee5f54eee472da36efcfc810b240

---
+++ internal/mod/modpkgload/import.go
@@ -0,0 +1,293 @@
+package modpkgload
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/fs"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"cuelang.org/go/internal/mod/modrequirements"
+	"cuelang.org/go/internal/mod/module"
+	"cuelang.org/go/internal/slices"
+)
+
+// importFromModules finds the module and source location in the dependency graph of
+// pkgs containing the package with the given import path.
+//
+// The answer must be unique: importFromModules returns an error if multiple
+// modules are observed to provide the same package.
+//
+// importFromModules can return a zero module version for packages in
+// the standard library.
+//
+// If the package is not present in any module selected from the requirement
+// graph, importFromModules returns an *ImportMissingError.
+//
+// If the package is present in exactly one module, importFromModules will
+// return the module, its root directory, and a list of other modules that
+// lexically could have provided the package but did not.
+func (pkgs *Packages) importFromModules(ctx context.Context, pkgPath string) (m module.Version, loc SourceLoc, altMods []module.Version, err error) {
+	fail := func(err error) (module.Version, SourceLoc, []module.Version, error) {
+		return module.Version{}, SourceLoc{}, nil, err
+	}
+	failf := func(format string, args ...interface{}) (module.Version, SourceLoc, []module.Version, error) {
+		return fail(fmt.Errorf(format, args...))
+	}
+	// TODO if there's no @ in the import path, take the version from the
+	// user's go.mod file as an initial preference; if not present there, then
+	// take it from requirements; if not present there, search for latest
+	// major version and use that. What should we do when there are
+	// several major versions present in the go.mod file?
+	pkgPathOnly, pkgVersion, ok := module.SplitPathVersion(pkgPath)
+	if !ok {
+		return failf("all imports must currently be qualified with a major version")
+	}
+	if filepath.IsAbs(pkgPathOnly) || path.IsAbs(pkgPathOnly) {
+		return failf("%q is not a package path", pkgPath)
+	}
+	// TODO check that the path isn't relative.
+	// TODO check it's not a meta package name, such as "all".
+
+	// Before any further lookup, check that the path is valid.
+	if err := module.CheckImportPath(pkgPath); err != nil {
+		return fail(err)
+	}
+
+	// Check each module on the build list.
+	var locs []SourceLoc
+	var mods []module.Version
+	var mg *modrequirements.ModuleGraph
+
+	// Iterate over possible modules for the path, not all selected modules.
+	// Iterating over selected modules would make the overall loading time
+	// O(M × P) for M modules providing P imported packages, whereas iterating
+	// over path prefixes is only O(P × k) with maximum path depth k. For
+	// large projects both M and P may be very large (note that M ≤ P), but k
+	// will tend to remain smallish (if for no other reason than filesystem
+	// path limitations).
+	//
+	// We perform this iteration either one or two times.
+	// Firstly we attempt to load the package using only the main module and
+	// its root requirements. If that does not identify the package, then we attempt
+	// to load the package using the full
+	// requirements in mg.
+	for {
+		var altMods []module.Version
+		// TODO we could probably do this loop concurrently.
+		for prefix := pkgPathOnly; prefix != "."; prefix = path.Dir(prefix) {
+			var (
+				v  string
+				ok bool
+			)
+			prefixPath := prefix + "@" + pkgVersion
+			if mg == nil {
+				v, ok = pkgs.requirements.RootSelected(prefixPath)
+			} else {
+				v, ok = mg.Selected(prefixPath), true
+			}
+			if !ok || v == "none" {
+				continue
+			}
+			m, err := module.NewVersion(prefixPath, v)
+			if err != nil {
+				// Not all package paths are valid module versions,
+				// but a parent might be.
+				continue
+			}
+			mloc, isLocal, err := pkgs.fetch(ctx, m)
+			if err != nil {
+				// Report fetch error.
+				// Note that we don't know for sure this module is necessary,
+				// but it certainly _could_ provide the package, and even if we
+				// continue the loop and find the package in some other module,
+				// we need to look at this module to make sure the import is
+				// not ambiguous.
+				return fail(err)
+			}
+			if loc, ok, err := locInModule(pkgPathOnly, prefix, mloc, isLocal); err != nil {
+				return fail(err)
+			} else if ok {
+				mods = append(mods, m)
+				locs = append(locs, loc)
+			} else {
+				altMods = append(altMods, m)
+			}
+		}
+
+		if len(mods) > 1 {
+			// We produce the list of directories from longest to shortest candidate
+			// module path, but the AmbiguousImportError should report them from
+			// shortest to longest. Reverse them now.
+			slices.Reverse(mods)
+			slices.Reverse(locs)
+			return fail(&AmbiguousImportError{ImportPath: pkgPath, Locations: locs, Modules: mods})
+		}
+
+		if len(mods) == 1 {
+			// We've found the unique module containing the package.
+			return mods[0], locs[0], altMods, nil
+		}
+
+		if mg != nil {
+			// We checked the full module graph and still didn't find the
+			// requested package.
+			return fail(&ImportMissingError{Path: pkgPath})
+		}
+
+		// So far we've checked the root dependencies.
+		// Load the full module graph and try again.
+		mg, err = pkgs.requirements.Graph(ctx)
+		if err != nil {
+			// We might be missing one or more transitive (implicit) dependencies from
+			// the module graph, so we can't return an ImportMissingError here — one
+			// of the missing modules might actually contain the package in question,
+			// in which case we shouldn't go looking for it in some new dependency.
+			return fail(err)
+		}
+	}
+}
+
+// locInModule returns the location that would hold the package named by the given path,
+// if it were in the module with module path mpath and root location mloc.
+// If pkgPath is syntactically not within mpath,
+// or if mdir is a local file tree (isLocal == true) and the directory
+// that would hold path is in a sub-module (covered by a go.mod below mdir),
+// locInModule returns "", false, nil.
+//
+// Otherwise, locInModule returns the name of the directory where
+// CUE source files would be expected, along with a boolean indicating
+// whether there are in fact CUE source files in that directory.
+// A non-nil error indicates that the existence of the directory and/or
+// source files could not be determined, for example due to a permission error.
+func locInModule(pkgPath, mpath string, mloc SourceLoc, isLocal bool) (loc SourceLoc, haveCUEFiles bool, err error) {
+	loc.FS = mloc.FS
+
+	// Determine where to expect the package.
+	if pkgPath == mpath {
+		loc = mloc
+	} else if len(pkgPath) > len(mpath) && pkgPath[len(mpath)] == '/' && pkgPath[:len(mpath)] == mpath {
+		loc.Dir = path.Join(mloc.Dir, pkgPath[len(mpath)+1:])
+	} else {
+		return SourceLoc{}, false, nil
+	}
+
+	// Check that there aren't other modules in the way.
+	// This check is unnecessary inside the module cache.
+	// So we only check local module trees
+	// (the main module and, in the future, any directory trees pointed at by replace directives).
+	if isLocal {
+		for d := loc.Dir; d != mloc.Dir && len(d) > len(mloc.Dir); {
+			_, err := fs.Stat(mloc.FS, path.Join(d, "cue.mod/module.cue"))
+			// TODO should we count it as a module file if it's a directory?
+			haveCUEMod := err == nil
+			if haveCUEMod {
+				return SourceLoc{}, false, nil
+			}
+			parent := path.Dir(d)
+			if parent == d {
+				// Break the loop, as otherwise we'd loop
+				// forever if d=="." and mdir=="".
+				break
+			}
+			d = parent
+		}
+	}
+
+	// Are there CUE source files in the directory?
+	// We don't care about build tags, not even "ignore".
+	// We're just looking for a plausible directory.
+	haveCUEFiles, err = isDirWithCUEFiles(loc)
+	if err != nil {
+		return SourceLoc{}, false, err
+	}
+	return loc, haveCUEFiles, err
+}
+
+func isDirWithCUEFiles(loc SourceLoc) (bool, error) {
+	// It would be nice if we could inspect the error returned from
+	// ReadDir to see if it's failing because it's not a directory,
+	// but unfortunately that doesn't seem to be something defined
+	// by the Go fs interface.
+	fi, err := fs.Stat(loc.FS, loc.Dir)
+	if err != nil {
+		if !errors.Is(err, fs.ErrNotExist) {
+			return false, err
+		}
+		return false, nil
+	}
+	if !fi.IsDir() {
+		return false, nil
+	}
+	entries, err := fs.ReadDir(loc.FS, loc.Dir)
+	if err != nil {
+		return false, err
+	}
+	for _, e := range entries {
+		if strings.HasSuffix(e.Name(), ".cue") && e.Type().IsRegular() {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+// fetch downloads the given module (or its replacement)
+// and returns its location.
+//
+// The isLocal return value reports whether the replacement,
+// if any, is within the local main module.
+func (pkgs *Packages) fetch(ctx context.Context, mod module.Version) (loc SourceLoc, isLocal bool, err error) {
+	if mod == pkgs.mainModuleVersion {
+		return pkgs.mainModuleLoc, true, nil
+	}
+
+	loc, err = pkgs.registry.Fetch(ctx, mod)
+	return loc, false, err
+}
+
+// An AmbiguousImportError indicates an import of a package found in multiple
+// modules in the build list, or found in both the main module and its vendor
+// directory.
+type AmbiguousImportError struct {
+	ImportPath string
+	Locations  []SourceLoc
+	Modules    []module.Version // Either empty or 1:1 with Dirs.
+}
+
+func (e *AmbiguousImportError) Error() string {
+	locType := "modules"
+	if len(e.Modules) == 0 {
+		locType = "locations"
+	}
+
+	var buf strings.Builder
+	fmt.Fprintf(&buf, "ambiguous import: found package %s in multiple %s:", e.ImportPath, locType)
+
+	for i, loc := range e.Locations {
+		buf.WriteString("\n\t")
+		if i < len(e.Modules) {
+			m := e.Modules[i]
+			buf.WriteString(m.Path())
+			if m.Version() != "" {
+				fmt.Fprintf(&buf, " %s", m.Version())
+			}
+			// TODO work out how to present source locations in error messages.
+			fmt.Fprintf(&buf, " (%s)", loc.Dir)
+		} else {
+			buf.WriteString(loc.Dir)
+		}
+	}
+
+	return buf.String()
+}
+
+// ImportMissingError is used for errors where an imported package cannot be found.
+type ImportMissingError struct {
+	Path string
+}
+
+func (e *ImportMissingError) Error() string {
+	return "cannot find module providing package " + e.Path
+}
---
+++ internal/mod/modpkgload/pkgload.go
@@ -0,0 +1,343 @@
+package modpkgload
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"runtime"
+	"strings"
+	"sync/atomic"
+
+	"cuelang.org/go/internal/mod/internal/par"
+	"cuelang.org/go/internal/mod/modimports"
+	"cuelang.org/go/internal/mod/modrequirements"
+	"cuelang.org/go/internal/mod/module"
+	"cuelang.org/go/internal/slices"
+)
+
+// Registry represents a module registry, or at least this package's view of it.
+type Registry interface {
+	// Fetch returns the location of the contents for the given module
+	// version, downloading it if necessary.
+	Fetch(ctx context.Context, m module.Version) (SourceLoc, error)
+}
+
+// Flags is a set of flags tracking metadata about a package.
+type Flags int8
+
+const (
+	// PkgInAll indicates that the package is in the "all" package pattern,
+	// regardless of whether we are loading the "all" package pattern.
+	//
+	// When the PkgInAll flag and PkgImportsLoaded flags are both set, the caller
+	// who set the last of those flags must propagate the PkgInAll marking to all
+	// of the imports of the marked package.
+	PkgInAll Flags = 1 << iota
+
+	// PkgIsRoot indicates that the package matches one of the root package
+	// patterns requested by the caller.
+	PkgIsRoot
+
+	// PkgFromRoot indicates that the package is in the transitive closure of
+	// imports starting at the roots. (Note that every package marked as PkgIsRoot
+	// is also trivially marked PkgFromRoot.)
+	PkgFromRoot
+
+	// PkgImportsLoaded indicates that the Imports field of a
+	// Pkg have been populated.
+	PkgImportsLoaded
+)
+
+func (f Flags) String() string {
+	var buf strings.Builder
+	set := func(f1 Flags, s string) {
+		if (f & f1) == 0 {
+			return
+		}
+		if buf.Len() > 0 {
+			buf.WriteString(",")
+		}
+		buf.WriteString(s)
+		f &^= f1
+	}
+	set(PkgInAll, "inAll")
+	set(PkgIsRoot, "isRoot")
+	set(PkgFromRoot, "fromRoot")
+	set(PkgImportsLoaded, "importsLoaded")
+	if f != 0 {
+		set(f, fmt.Sprintf("extra%x", int(f)))
+	}
+	return buf.String()
+}
+
+// has reports whether all of the flags in cond are set in f.
+func (f Flags) has(cond Flags) bool {
+	return f&cond == cond
+}
+
+type Packages struct {
+	mainModuleVersion module.Version
+	mainModuleLoc     SourceLoc
+	pkgCache          par.Cache[string, *Package]
+	pkgs              []*Package
+	rootPkgs          []*Package
+	work              *par.Queue
+	requirements      *modrequirements.Requirements
+	registry          Registry
+}
+
+type Package struct {
+	// Populated at construction time:
+	path string // import path
+
+	// Populated at construction time and updated by [loader.applyPkgFlags]:
+	flags atomicLoadPkgFlags
+
+	// Populated by [loader.load].
+	mod          module.Version // module providing package
+	loc          SourceLoc      // location of source code
+	err          error          // error loading package
+	imports      []*Package     // packages imported by this one
+	inStd        bool
+	fromExternal bool
+	altMods      []module.Version // modules that could have contained the package but did not
+
+	// Populated by postprocessing in [Packages.buildStacks]:
+	stack *Package // package importing this one in minimal import stack for this pkg
+}
+
+// SourceLoc represents the location of some CUE source code.
+type SourceLoc struct {
+	// FS is the filesystem containing the source.
+	FS fs.FS
+	// Dir is the directory within the above filesystem.
+	Dir string
+}
+
+func (pkg *Package) ImportPath() string {
+	return pkg.path
+}
+
+func (pkg *Package) FromExternalModule() bool {
+	return pkg.fromExternal
+}
+
+func (pkg *Package) Location() SourceLoc {
+	return pkg.loc
+}
+
+func (pkg *Package) Error() error {
+	return pkg.err
+}
+
+func (pkg *Package) SetError(err error) {
+	pkg.err = err
+}
+
+func (pkg *Package) HasFlags(flags Flags) bool {
+	return pkg.flags.has(flags)
+}
+
+func (pkg *Package) Imports() []*Package {
+	return pkg.imports
+}
+
+func (pkg *Package) Flags() Flags {
+	return pkg.flags.get()
+}
+
+func (pkg *Package) Mod() module.Version {
+	return pkg.mod
+}
+
+// LoadPackages loads information about all the given packages, using
+// modules from the given requirements to determine which modules
+// they might be obtained from, and reg to download module contents.
+func LoadPackages(
+	ctx context.Context,
+	mainModulePath string,
+	mainModuleLoc SourceLoc,
+	rs *modrequirements.Requirements,
+	reg Registry,
+	rootPkgPaths []string,
+) *Packages {
+	pkgs := &Packages{
+		mainModuleVersion: module.MustNewVersion(mainModulePath, ""),
+		mainModuleLoc:     mainModuleLoc,
+		requirements:      rs,
+		registry:          reg,
+		work:              par.NewQueue(runtime.GOMAXPROCS(0)),
+	}
+	inRoots := map[*Package]bool{}
+	pkgs.rootPkgs = make([]*Package, 0, len(rootPkgPaths))
+	for _, p := range rootPkgPaths {
+		// TODO the original logic didn't add PkgInAll here. Not sure why,
+		// and that might be a lurking problem.
+		if root := pkgs.addPkg(ctx, p, PkgIsRoot|PkgInAll); !inRoots[root] {
+			pkgs.rootPkgs = append(pkgs.rootPkgs, root)
+			inRoots[root] = true
+		}
+	}
+	<-pkgs.work.Idle()
+	pkgs.buildStacks()
+	return pkgs
+}
+
+// buildStacks computes minimal import stacks for each package,
+// for use in error messages. When it completes, packages that
+// are part of the original root set have pkg.stack == nil,
+// and other packages have pkg.stack pointing at the next
+// package up the import stack in their minimal chain.
+// As a side effect, buildStacks also constructs ld.pkgs,
+// the list of all packages loaded.
+func (pkgs *Packages) buildStacks() {
+	for _, pkg := range pkgs.rootPkgs {
+		pkg.stack = pkg // sentinel to avoid processing in next loop
+		pkgs.pkgs = append(pkgs.pkgs, pkg)
+	}
+	for i := 0; i < len(pkgs.pkgs); i++ { // not range: appending to pkgs.pkgs in loop
+		pkg := pkgs.pkgs[i]
+		for _, next := range pkg.imports {
+			if next.stack == nil {
+				next.stack = pkg
+				pkgs.pkgs = append(pkgs.pkgs, next)
+			}
+		}
+	}
+	for _, pkg := range pkgs.rootPkgs {
+		pkg.stack = nil
+	}
+}
+
+func (pkgs *Packages) Roots() []*Package {
+	return slices.Clip(pkgs.rootPkgs)
+}
+
+func (pkgs *Packages) All() []*Package {
+	return slices.Clip(pkgs.pkgs)
+}
+
+func (pkgs *Packages) Pkg(pkgPath string) *Package {
+	pkg, _ := pkgs.pkgCache.Get(pkgPath)
+	return pkg
+}
+
+func (pkgs *Packages) addPkg(ctx context.Context, pkgPath string, flags Flags) *Package {
+	pkg := pkgs.pkgCache.Do(pkgPath, func() *Package {
+		pkg := &Package{
+			path: pkgPath,
+		}
+		pkgs.applyPkgFlags(ctx, pkg, flags)
+
+		pkgs.work.Add(func() { pkgs.load(ctx, pkg) })
+		return pkg
+	})
+
+	// Ensure the flags apply even if the package already existed.
+	pkgs.applyPkgFlags(ctx, pkg, flags)
+	return pkg
+}
+
+func (pkgs *Packages) load(ctx context.Context, pkg *Package) {
+	if IsStdlibPackage(pkg.path) {
+		pkg.inStd = true
+		return
+	}
+	pkg.fromExternal = pkg.mod != pkgs.mainModuleVersion
+	pkg.mod, pkg.loc, pkg.altMods, pkg.err = pkgs.importFromModules(ctx, pkg.path)
+	if pkg.err != nil {
+		return
+	}
+	if pkgs.mainModuleVersion.Path() == pkg.mod.Path() {
+		pkgs.applyPkgFlags(ctx, pkg, PkgInAll)
+	}
+	imports, err := modimports.AllImports(modimports.PackageFiles(pkg.loc.FS, pkg.loc.Dir))
+	if err != nil {
+		pkg.err = err
+		return
+	}
+
+	pkg.imports = make([]*Package, 0, len(imports))
+	var importFlags Flags
+	if pkg.flags.has(PkgInAll) {
+		importFlags = PkgInAll
+	}
+	for _, path := range imports {
+		pkg.imports = append(pkg.imports, pkgs.addPkg(ctx, path, importFlags))
+	}
+	pkgs.applyPkgFlags(ctx, pkg, PkgImportsLoaded)
+}
+
+// applyPkgFlags updates pkg.flags to set the given flags and propagate the
+// (transitive) effects of those flags, possibly loading or enqueueing further
+// packages as a result.
+func (pkgs *Packages) applyPkgFlags(ctx context.Context, pkg *Package, flags Flags) {
+	if flags == 0 {
+		return
+	}
+
+	if flags.has(PkgInAll) {
+		// This package matches a root pattern by virtue of being in "all".
+		flags |= PkgIsRoot
+	}
+	if flags.has(PkgIsRoot) {
+		flags |= PkgFromRoot
+	}
+
+	old := pkg.flags.update(flags)
+	new := old | flags
+	if new == old || !new.has(PkgImportsLoaded) {
+		// We either didn't change the state of pkg, or we don't know anything about
+		// its dependencies yet. Either way, we can't usefully load its test or
+		// update its dependencies.
+		return
+	}
+
+	if new.has(PkgInAll) && !old.has(PkgInAll|PkgImportsLoaded) {
+		// We have just marked pkg with pkgInAll, or we have just loaded its
+		// imports, or both. Now is the time to propagate pkgInAll to the imports.
+		for _, dep := range pkg.imports {
+			pkgs.applyPkgFlags(ctx, dep, PkgInAll)
+		}
+	}
+
+	if new.has(PkgFromRoot) && !old.has(PkgFromRoot|PkgImportsLoaded) {
+		for _, dep := range pkg.imports {
+			pkgs.applyPkgFlags(ctx, dep, PkgFromRoot)
+		}
+	}
+}
+
+// An atomicLoadPkgFlags stores a loadPkgFlags for which individual flags can be
+// added atomically.
+type atomicLoadPkgFlags struct {
+	bits atomic.Int32
+}
+
+// update sets the given flags in af (in addition to any flags already set).
+//
+// update returns the previous flag state so that the caller may determine which
+// flags were newly-set.
+func (af *atomicLoadPkgFlags) update(flags Flags) (old Flags) {
+	for {
+		old := af.bits.Load()
+		new := old | int32(flags)
+		if new == old || af.bits.CompareAndSwap(old, new) {
+			return Flags(old)
+		}
+	}
+}
+
+func (af *atomicLoadPkgFlags) get() Flags {
+	return Flags(af.bits.Load())
+}
+
+// has reports whether all of the flags in cond are set in af.
+func (af *atomicLoadPkgFlags) has(cond Flags) bool {
+	return Flags(af.bits.Load())&cond == cond
+}
+
+func IsStdlibPackage(pkgPath string) bool {
+	firstElem, _, _ := strings.Cut(pkgPath, "/")
+	return !strings.Contains(firstElem, ".")
+}

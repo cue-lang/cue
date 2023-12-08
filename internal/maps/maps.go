1958c325d22e646d0314951fc8503f3c32d64d43
Roger Peppe <rogpeppe@gmail.com>
2023-12-08 16:07:21 +0000

    internal/maps: new package
    This is only here until we can use the stdlib maps package.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: Ie2546826626b932000d026e1f0b172d53479597e
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1173343
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>

---
+++ internal/maps/maps.go
@@ -0,0 +1,20 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package maps defines various functions useful with maps of any type.
+package maps
+
+// Equal reports whether two maps contain the same key/value pairs.
+// Values are compared using ==.
+func Equal[M1, M2 ~map[K]V, K, V comparable](m1 M1, m2 M2) bool {
+	if len(m1) != len(m2) {
+		return false
+	}
+	for k, v1 := range m1 {
+		if v2, ok := m2[k]; !ok || v1 != v2 {
+			return false
+		}
+	}
+	return true
+}

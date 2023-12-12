84485ab788863adbc2b5961037aac199dd786098
Roger Peppe <rogpeppe@gmail.com>
2023-12-12 18:23:15 +0000

    internal/mod/modcache: new package
    This implements module caching - it implements the
    interface required by `internal/mod/modload` in terms
    of `ociregistry.Interface`.
    
    Unarchived module zip files are stored in `$cachedir/$module@$version`.
    Auxilliary files, such as lock files and the contents of the `module.cue`
    file, are stored in `$cachedir/cache/download/$module/@v/$version.*`.
    
    The following methods are substantially the same as their counterparts
    in the original Go code (`cmd/go/internal/modfetch`). All hashing
    and use of the `ziphash` files has been removed.
    
    - `tempFile`
    - `RemoveAll`
    - `makeDirsReadOnly`
    - `downloadZip` (from `modfetch.DownloadZip`)
    - `downloadZip` (from `modfetch.downloadZip`; the actual download logic has changed)
    - `readDiskModFile` (from `modfetch.readDiskGoMod`)
    - `writeDiskModFile` (from `modfetch.writeDiskGoMod`)
    - `readDiskCache`
    - `writeDiskCache`
    - `downloadDir`
    - `cachePath` (modulo differing conventions about cache paths)
    - `lockVersion`
    
    `quoteGlob` is copied from `cmd/go/internal/str/QuoteGlob`.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: Ie8161fd2242becc52806b8b758aecc5659b36c6c

---
+++ internal/mod/modcache/dirfs.go
@@ -0,0 +1,53 @@
+package modcache
+
+import (
+	"io/fs"
+	"os"
+)
+
+// OSRootFS can be implemented by an [fs.FS]
+// implementation to return its root directory as
+// an OS file path,
+type OSRootFS interface {
+	// OSRoot returns the root directory of the FS
+	// as an OS file path, and reports whether that was possible.
+	OSRoot() (string, bool)
+}
+
+func dirFS(p string) fs.FS {
+	return dirFSImpl{
+		augmentedFS: os.DirFS(p).(augmentedFS),
+		osRoot:      p,
+	}
+}
+
+var _ interface {
+	augmentedFS
+	fs.ReadFileFS
+	fs.ReadDirFS
+	OSRootFS
+} = dirFSImpl{}
+
+type augmentedFS interface {
+	fs.FS
+	fs.StatFS
+	// Note: os.DirFS only started implementing ReadFileFS and
+	// ReadDirFS in Go 1.21, so we can't include those here.
+}
+
+type dirFSImpl struct {
+	osRoot string
+	augmentedFS
+}
+
+func (fsys dirFSImpl) OSRoot() (string, bool) {
+	return fsys.osRoot, true
+}
+
+func (fsys dirFSImpl) ReadFile(name string) ([]byte, error) {
+	return fs.ReadFile(fsys.augmentedFS, name)
+}
+
+func (fsys dirFSImpl) ReadDir(name string) ([]fs.DirEntry, error) {
+	return fs.ReadDir(fsys.augmentedFS, name)
+}

0498642b36974a1892d9058783ad0072fa7e2f17
Daniel Martí <mvdan@mvdan.cc>
2023-12-20 08:43:31 +0000

    cue/format: ensure a newline after every comment
    This is similar to the nooverride whiteSpace flag,
    but only enforces the newline whitespace token to be printed.
    
    Fixes #2567.
    
    Signed-off-by: Noam Dolovich <noam.tzvi.dolovich@gmail.com>
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Co-authored-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I83bebcca9aeba14f84cbcf4c4befa0677da3fb0f

--- cue/format/printer.go
+++ cue/format/printer.go
@@ -46,6 +46,12 @@ type printer struct {
 	spaceBefore      bool
 	prevLbraceOnLine bool // true if a '{' has been written on the current line
 
+	// TODO(mvdan): This is similar to nooverride but used only for comments,
+	// to ensure that we always print a newline after them.
+	// We should fix our logic with whiteSpace instead, but for now this ensures
+	// we don't break the syntax by omitting the newline after a comment.
+	printingComment bool
+
 	errs errors.Error
 }
 
@@ -231,6 +237,9 @@ func (p *printer) Print(v interface{}) {
 				case token.NewSection:
 					requested |= newsection
 				}
+				if p.printingComment {
+					requested |= newline
+				}
 				p.writeWhitespace(requested)
 				p.allowed = 0
 				p.requested = 0
@@ -247,6 +256,7 @@ func (p *printer) Print(v interface{}) {
 	p.writeWhitespace(p.allowed)
 	p.allowed = 0
 	p.requested = 0
+	p.printingComment = false
 	p.writeString(data, isLit)
 	p.allowed = nextWS
 	_ = impliedComma // TODO: delay comment printings

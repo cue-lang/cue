7cd3c60897aee947001c4bf01847cecafe28b678
Cedric Charly <cedric.charly@gmail.com>
2023-12-22 22:51:55 -0600

    internal/cli: improve error message on parse issue
    When displaying an error for failing to parse an injected value,
    the error referenced 'environment variable' which is unclear.
    This change instead references 'injection tag' in the message.
    
    Fixes #1011
    
    Signed-off-by: Cedric Charly <cedric.charly@gmail.com>
    Change-Id: I3cdbe763b566c04e3a08647a8cb287c288db15e8

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -32,7 +32,7 @@ func ParseValue(pos token.Pos, name, str string, k cue.Kind) (x ast.Expr, errs e
 		expr, err = parser.ParseExpr(name, str)
 		if err != nil {
 			errs = errors.Wrapf(err, pos,
-				"invalid number for environment variable %s", name)
+				"invalid number for injection tag %q", name)
 		}
 	}
 
@@ -41,7 +41,7 @@ func ParseValue(pos token.Pos, name, str string, k cue.Kind) (x ast.Expr, errs e
 		b, ok := boolValues[str]
 		if !ok {
 			errs = errors.Append(errs, errors.Newf(pos,
-				"invalid boolean value %q for environment variable %s", str, name))
+				"invalid boolean value %q for injection tag %q", str, name))
 		} else if expr != nil || k&cue.StringKind != 0 {
 			// Convert into an expression
 			bl := ast.NewBool(b)
@@ -70,7 +70,7 @@ func ParseValue(pos token.Pos, name, str string, k cue.Kind) (x ast.Expr, errs e
 		return x, nil
 	case errs == nil:
 		return nil, errors.Newf(pos,
-			"invalid type for environment variable %s", name)
+			"invalid type for injection tag %q", name)
 	}
 	return nil, errs
 }

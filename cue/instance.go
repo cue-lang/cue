fe4aa6b769e41c7befef0c2780c7a74cd9e1690e
Daniel Martí <mvdan@mvdan.cc>
2023-12-27 09:35:48 +0000

    cue: replace Instance.value with Instance.Value
    The latter was added for the transition to Value via InstanceOrValue,
    and both have almost identical function bodies.
    It must have been an oversight to end up with both,
    and all tests and code generation still work with the change.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I8fd6723da139dd7e17cb9d2a79549df1d66011b1

--- cue/instance.go
+++ cue/instance.go
@@ -289,10 +289,6 @@ func (inst *hiddenInstance) Build(p *build.Instance) *Instance {
 	return i
 }
 
-func (inst *Instance) value() Value {
-	return newVertexRoot(inst.index, newContext(inst.index), inst.root)
-}
-
 // Lookup reports the value at a path starting from the top level struct. The
 // Exists method of the returned value will report false if the path did not
 // exist. The Err method reports if any error occurred during evaluation. The
@@ -301,7 +297,7 @@ func (inst *Instance) value() Value {
 //
 // Deprecated: use Value.LookupPath
 func (inst *hiddenInstance) Lookup(path ...string) Value {
-	return inst.value().Lookup(path...)
+	return inst.Value().Lookup(path...)
 }
 
 // LookupDef reports the definition with the given name within struct v. The
@@ -310,7 +306,7 @@ func (inst *hiddenInstance) Lookup(path ...string) Value {
 //
 // Deprecated: use Value.LookupPath
 func (inst *hiddenInstance) LookupDef(path string) Value {
-	return inst.value().LookupDef(path)
+	return inst.Value().LookupDef(path)
 }
 
 // LookupField reports a Field at a path starting from v, or an error if the
@@ -323,7 +319,7 @@ func (inst *hiddenInstance) LookupDef(path string) Value {
 //
 // Deprecated: use Value.LookupPath
 func (inst *hiddenInstance) LookupField(path ...string) (f FieldInfo, err error) {
-	v := inst.value()
+	v := inst.Value()
 	for _, k := range path {
 		s, err := v.Struct()
 		if err != nil {

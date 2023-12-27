b78c3316e4dda973a0c056c65e1e19bbb7691965
Paul Jolly <paul@myitcv.io>
2023-12-27 15:28:29 +0000

    cuepls: remove more server implementation
    Now that we have an integration test in place we can more easily remove
    server implementation and see that it doesn't break things.
    
    Of major note, the cache package still needs a major trim down. But that
    is one of many changes that can follow in a later CL.
    
    DO NOT REVIEW
    
    For #142
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: If512615401bb099274d9481ca862288cf62b0a53

---
+++ internal/golangorgx/gopls/hooks/hooks.go
@@ -0,0 +1,15 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package hooks adds all the standard gopls implementations.
+// This can be used in tests without needing to use the gopls main, and is
+// also the place to edit for custom builds of gopls.
+package hooks // import "cuelang.org/go/internal/golangorgx/gopls/hooks"
+
+import (
+	"cuelang.org/go/internal/golangorgx/gopls/settings"
+)
+
+func Options(options *settings.Options) {
+}

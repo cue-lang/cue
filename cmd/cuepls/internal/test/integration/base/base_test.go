31c67e1da477e3695574b101d3b385dc7c409e53
Paul Jolly <paul@myitcv.io>
2023-12-27 17:00:36 +0000

    cuepls: support formatting of CUE files
    Add support for the formatting of CUE files.
    
    While we are here, create a cuelang package alongside the existing
    golang package.
    
    Lots more formatting required in later CLs, but again this is a good
    bootstrap.
    
    For #142
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I13ab83c1251714182e69d06bb53ed08f94bac709

--- cmd/cuepls/internal/test/integration/base/base_test.go
+++ cmd/cuepls/internal/test/integration/base/base_test.go
@@ -7,6 +7,7 @@ import (
 
 	"cuelang.org/go/internal/golangorgx/gopls/hooks"
 	. "cuelang.org/go/internal/golangorgx/gopls/test/integration"
+	"cuelang.org/go/internal/golangorgx/gopls/test/integration/fake"
 	"github.com/go-quicktest/qt"
 )
 
@@ -20,14 +21,17 @@ func TestFormatFile(t *testing.T) {
 module mod.com
 
 go 1.12
--- foo.go --
-	package foo
+-- foo.cue --
+package foo
+
+  // this is a test
 `
 	Run(t, files, func(t *testing.T, env *Env) {
-		env.OpenFile("foo.go")
-		env.FormatBuffer("foo.go")
-		got := env.BufferText("foo.go")
-		want := "package foo\n"
+		env.OpenFile("foo.cue")
+		env.EditBuffer("foo.cue", fake.NewEdit(0, 0, 1, 0, "package bar\n"))
+		env.FormatBuffer("foo.cue")
+		got := env.BufferText("foo.cue")
+		want := "package bar\n\n// this is a test\n"
 		qt.Assert(t, qt.Equals(got, want))
 	})
 }

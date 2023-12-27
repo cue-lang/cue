c07b231519f1a6891b89fff9e734ae39faeaf2ec
Paul Jolly <paul@myitcv.io>
2023-12-27 10:49:40 +0000

    cuepls: run internal/golangorgx/revendorToolsInternal.sh
    This "vendors" parts of golang.org/x/... internally within the CUE code
    base.
    
    This creates some duplicate packages which we will later tidy up. The
    focus for now is on making minimal changes to the gopls code-based to
    create a cuepls equivalent, working end-to-end with integration tests.
    Refactoring, deduplication etc can follow later.
    
    We also run go mod tidy in the CUE main module to leave things in a
    compiling state. Note however that the dependencies added by that tidy
    all get removed in https://cuelang.org/cl/1174165, which removes the
    (third party) hooks, and later CLs which cut down the server
    implementation.
    
    DO NOT REVIEW
    
    For #142
    
    [git-generate]
      ./internal/golangorgx/revendorToolsInternal.sh
      go mod tidy
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I78baa78259a903f08c9fc8296173cc9b5fe44bea

---
+++ internal/golangorgx/gopls/server/implementation.go
@@ -0,0 +1,33 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package server
+
+import (
+	"context"
+
+	"cuelang.org/go/internal/golangorgx/gopls/file"
+	"cuelang.org/go/internal/golangorgx/gopls/lsp/protocol"
+	"cuelang.org/go/internal/golangorgx/gopls/lsp/source"
+	"cuelang.org/go/internal/golangorgx/gopls/telemetry"
+	"cuelang.org/go/internal/golangorgx/tools/event"
+	"cuelang.org/go/internal/golangorgx/tools/event/tag"
+)
+
+func (s *server) Implementation(ctx context.Context, params *protocol.ImplementationParams) (_ []protocol.Location, rerr error) {
+	recordLatency := telemetry.StartLatencyTimer("implementation")
+	defer func() {
+		recordLatency(ctx, rerr)
+	}()
+
+	ctx, done := event.Start(ctx, "lsp.Server.implementation", tag.URI.Of(params.TextDocument.URI))
+	defer done()
+
+	snapshot, fh, ok, release, err := s.beginFileRequest(ctx, params.TextDocument.URI, file.Go)
+	defer release()
+	if !ok {
+		return nil, err
+	}
+	return source.Implementation(ctx, snapshot, fh, params.Position)
+}

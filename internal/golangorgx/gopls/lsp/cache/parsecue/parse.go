1ca0044b2d54838e237615f63aaa85123c7b1b2b
Paul Jolly <paul@myitcv.io>
2023-12-28 05:32:51 +0000

    cuepls: implement parsecue package
    WIP
    DO NOT REVIEW
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Iaa3e7beedecd7d5d7cbf5337e70020556556d45a

---
+++ internal/golangorgx/gopls/lsp/cache/parsecue/parse.go
@@ -0,0 +1,36 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package parsego
+
+import (
+	"context"
+
+	"cuelang.org/go/cue/parser"
+
+	"cuelang.org/go/internal/golangorgx/gopls/lsp/protocol"
+	"cuelang.org/go/internal/golangorgx/tools/event"
+	"cuelang.org/go/internal/golangorgx/tools/event/tag"
+)
+
+// Parse parses a buffer of Go source, repairing the tree if necessary.
+//
+// The provided ctx is used only for logging.
+func Parse(ctx context.Context, uri protocol.DocumentURI, src []byte, options ...parser.Option) (res *File) {
+	ctx, done := event.Start(ctx, "cache.ParseGoSrc", tag.File.Of(uri.Path()))
+	defer done()
+
+	file, parseErr := parser.ParseFile(uri.Path(), src, options...)
+	tok := file.Pos().File()
+
+	return &File{
+		URI:      uri,
+		Options:  options,
+		Src:      src,
+		File:     file,
+		Tok:      tok,
+		Mapper:   protocol.NewMapper(uri, src),
+		ParseErr: parseErr,
+	}
+}

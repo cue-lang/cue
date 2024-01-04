97f9b5078f48adf00788b39022a2f053d2e0d021
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-01-04 21:57:30 +0000

    pkg/path: fix package comment
    This change corrects the package comment in pkg/path so that it refers
    to itself by its current name, and stops mentioning a difference between
    itself and the non-existent "filepath" package.
    
    This correction is required so that CUE (not Go) users referring to the
    automated documentation on pkg.go.dev aren't misled by the package index
    table at https://pkg.go.dev/cuelang.org/go/pkg#section-directories, and
    the path package overview at
    https://pkg.go.dev/cuelang.org/go/pkg/path#pkg-overview.
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: I23a544242da9b6e9a7be0c3591790e5e1ab4063c

--- pkg/path/path.go
+++ pkg/path/path.go
@@ -16,13 +16,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package filepath implements utility routines for manipulating filename paths
-// in a way compatible with the target operating system-defined file paths.
-//
-// The filepath package uses either forward slashes or backslashes,
-// depending on the operating system. To process paths such as URLs
-// that always use forward slashes regardless of the operating
-// system, see the path package.
+// Package path implements utility routines for manipulating filename paths as
+// defined by targetted operating systems, and also paths that always use
+// forward slashes regardless of the operating system, such as URLs.
 package path
 
 import (

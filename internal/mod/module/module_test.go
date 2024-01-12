b4abf557b0f7d7d577d8c57e651767f90b8c3fec
Roger Peppe <rogpeppe@gmail.com>
2024-01-12 13:18:57 +0000

    internal/mod/module: add ParseImportPath
    This provides a canonical implementation that we can use
    for splitting an import path into its component parts.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I06e70a0e55a326f4c9075380c3da3db44470eeef

--- internal/mod/module/module_test.go
+++ internal/mod/module/module_test.go
@@ -197,3 +197,97 @@ func TestEscapePath(t *testing.T) {
 		t.Fatalf("EscapePath(%q) = %q, want %q", path, esc, path)
 	}
 }
+
+var parseImportPathTests = []struct {
+	testName      string
+	path          string
+	want          ImportPath
+	wantCanonical string
+}{{
+	testName: "StdlibLikeWithSlash",
+	path:     "stdlib/path",
+	want: ImportPath{
+		Path:      "stdlib/path",
+		Qualifier: "path",
+	},
+}, {
+	testName: "StdlibLikeNoSlash",
+	path:     "math",
+	want: ImportPath{
+		Path:      "math",
+		Qualifier: "math",
+	},
+}, {
+	testName: "StdlibLikeExplicitQualifier",
+	path:     "stdlib/path:other",
+	want: ImportPath{
+		Path:              "stdlib/path",
+		ExplicitQualifier: true,
+		Qualifier:         "other",
+	},
+}, {
+	testName: "StdlibLikeExplicitQualifierNoSlash",
+	path:     "math:other",
+	want: ImportPath{
+		Path:              "math",
+		ExplicitQualifier: true,
+		Qualifier:         "other",
+	},
+}, {
+	testName: "WithMajorVersion",
+	path:     "foo.com/bar@v0",
+	want: ImportPath{
+		Path:      "foo.com/bar",
+		Version:   "v0",
+		Qualifier: "bar",
+	},
+}, {
+	testName: "WithMajorVersionNoSlash",
+	path:     "main.test@v0",
+	want: ImportPath{
+		Path:      "main.test",
+		Version:   "v0",
+		Qualifier: "main.test",
+	},
+}, {
+	testName: "WithMajorVersionAndExplicitQualifier",
+	path:     "foo.com/bar@v0:other",
+	want: ImportPath{
+		Path:              "foo.com/bar",
+		Version:           "v0",
+		ExplicitQualifier: true,
+		Qualifier:         "other",
+	},
+}, {
+	testName: "WithMajorVersionAndNoQualifier",
+	path:     "foo.com/bar@v0",
+	want: ImportPath{
+		Path:      "foo.com/bar",
+		Version:   "v0",
+		Qualifier: "bar",
+	},
+}, {
+	testName: "WithRedundantQualifier",
+	path:     "foo.com/bar@v0:bar",
+	want: ImportPath{
+		Path:              "foo.com/bar",
+		Version:           "v0",
+		ExplicitQualifier: true,
+		Qualifier:         "bar",
+	},
+	wantCanonical: "foo.com/bar@v0",
+}}
+
+func TestParseImportPath(t *testing.T) {
+	for _, test := range parseImportPathTests {
+		t.Run(test.testName, func(t *testing.T) {
+			parts := ParseImportPath(test.path)
+			qt.Assert(t, qt.DeepEquals(parts, test.want))
+			qt.Assert(t, qt.Equals(parts.String(), test.path))
+			if test.wantCanonical == "" {
+				test.wantCanonical = test.path
+			}
+			qt.Assert(t, qt.Equals(parts.Canonical().String(), test.wantCanonical))
+		})
+	}
+}

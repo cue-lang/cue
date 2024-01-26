5bdad0a9b66ed9673655fe334ec6810c9df26cb0
Daniel Martí <mvdan@mvdan.cc>
2024-01-26 14:06:19 +0000

    cmd/cue: make the config directory configurable
    Otherwise it's impossible to reliably set it across platforms,
    given that UserConfigDir relies on different env vars per platform.
    We have environment variables to configure cache directories already,
    and we will likely make use of other config files in this directory
    in the future, so it makes sense to support an explicit path.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I6c7d90b939d5a5fcc1eaaaf21a41be4e7f61bf28

--- cmd/cue/cmd/login.go
+++ cmd/cue/cmd/login.go
@@ -153,9 +153,13 @@ func registryOAuthConfig(host string) oauth2.Config {
 // changed between reading and writing the file.
 
 func findLoginsPath() (string, error) {
-	configDir, err := os.UserConfigDir()
-	if err != nil {
-		return "", err
+	configDir := os.Getenv("CUE_CONFIG_DIR")
+	if configDir == "" {
+		var err error
+		configDir, err = os.UserConfigDir()
+		if err != nil {
+			return "", err
+		}
 	}
 	return filepath.Join(configDir, "cue", "logins.json"), nil
 }

6ad495d46b07b415a28862bd1cd52dbf166f144f
Roger Peppe <rogpeppe@gmail.com>
2024-03-07 12:42:50 +0000

    internal/cuedebug: new package
    This provides support for a generally useful `CUE_DEBUG`
    environment variable, specified in the same form as `CUE_EXPERIMENT`.
    
    The syntax is similar enough that I've factored out the common
    code into a new package, `internal/envflag`, which will make
    it easy in the future to support enhancements like key-value pairs
    rather than just boolean values.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: Ib5d57639a13566eab66b5d8c3508fafa1fbba048
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1177936
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: CUEcueckoo <cueckoo@gmail.com>

--- internal/cuedebug/debug_test.go
+++ internal/cuedebug/debug_test.go
@@ -1,4 +1,4 @@
-package cueexperiment
+package cuedebug
 
 import (
 	"testing"
@@ -8,8 +8,8 @@ import (
 
 func TestInit(t *testing.T) {
 	// This is just a smoke test to make sure it's all wired up OK.
-	t.Setenv("CUE_EXPERIMENT", "modules")
+	t.Setenv("CUE_DEBUG", "http")
 	err := Init()
 	qt.Assert(t, qt.IsNil(err))
-	qt.Assert(t, qt.IsTrue(Flags.Modules))
+	qt.Assert(t, qt.IsTrue(Flags.HTTP))
 }

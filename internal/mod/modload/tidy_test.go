708885caddefbfed606db8709bd002832d5b0ad3
Roger Peppe <rogpeppe@gmail.com>
2024-03-08 17:03:14 +0000

    internal/mod/modload: split Tidy from UpdateVersions
    As the code is reasonably involved, it seems clearer
    when the two pieces of functionality are in separate files,
    particularly as the complexity of the tidy piece is going
    to increase shortly.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I6ab8f943605000f35cbd21756cd04b7958fa8f61

--- internal/mod/modload/tidy_test.go
+++ internal/mod/modload/tidy_test.go
@@ -55,39 +55,6 @@ func TestTidy(t *testing.T) {
 	}
 }
 
-func TestUpdateVersions(t *testing.T) {
-	files, err := filepath.Glob("testdata/updateversions/*.txtar")
-	qt.Assert(t, qt.IsNil(err))
-	for _, f := range files {
-		t.Run(f, func(t *testing.T) {
-			ar, err := txtar.ParseFile(f)
-			qt.Assert(t, qt.IsNil(err))
-			tfs := txtarfs.FS(ar)
-			reg := newRegistry(t, tfs, "_registry")
-
-			want, err := fs.ReadFile(tfs, "want")
-			qt.Assert(t, qt.IsNil(err))
-
-			versionsData, _ := fs.ReadFile(tfs, "versions")
-			versions := strings.Fields(string(versionsData))
-
-			var out strings.Builder
-			mf, err := UpdateVersions(context.Background(), tfs, ".", reg, versions)
-			if err != nil {
-				fmt.Fprintf(&out, "error: %v\n", err)
-			} else {
-				data, err := mf.Format()
-				qt.Assert(t, qt.IsNil(err))
-				out.Write(data)
-			}
-			if diff := cmp.Diff(string(want), out.String()); diff != "" {
-				t.Log("actual result:\n", out.String())
-				t.Fatalf("unexpected results (-want +got):\n%s", diff)
-			}
-		})
-	}
-}
-
 func newRegistry(t *testing.T, fsys fs.FS, root string) Registry {
 	fsys, err := fs.Sub(fsys, "_registry")
 	qt.Assert(t, qt.IsNil(err))

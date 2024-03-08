12df29fb2b3fff25da3a18f42b3cbc060b08cef4
Marcel van Lohuizen <mpvl@gmail.com>
2024-03-08 19:55:40 +0100

    internal/core/adt: use Vertex.Indirect in more places
    This prepares for several things, including the
    implementation of disjunctions and structure sharing.
    
    Issue #2851
    Issue #2884
    Issue #2854
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I831d5419e6b035667aae6f254f45456b34dfd93c

--- internal/core/adt/unify.go
+++ internal/core/adt/unify.go
@@ -122,6 +122,17 @@ func (v *Vertex) unify(c *OpContext, needs condition, mode runMode) bool {
 
 	nodeOnlyNeeds := needs &^ (subFieldsProcessed)
 	n.process(nodeOnlyNeeds, mode)
+	w := v.Indirect() // Dereference the disjunction result.
+	if w != v {
+		if w.Closed {
+			// Should resolve with dereference.
+			v.Closed = true
+		}
+		v.ArcType = w.ArcType
+		v.ChildErrors = CombineErrors(nil, v.ChildErrors, w.ChildErrors)
+		v.Arcs = nil
+		return w.state.meets(needs)
+	}
 	n.updateScalar()
 
 	// First process all but the subfields.

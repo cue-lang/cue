6653aca123c9c983160d0787581401a70eb5b841
Roger Peppe <rogpeppe@gmail.com>
2024-03-13 09:23:37 +0000

    cmd/cue: mention CUE_DEBUG in cue help environment
    The `CUE_DEBUG` environment variable has recently been added but there
    was no documentation for it. This adds some information about it to `cue
    help environment`.
    
    Also sort all the `cue help environment` entries in alphabetic order for
    consistency.
    
    Also clarify that CUE_EXPERIMENT does not support disabling experiments,
    as one disables an experiment by omitting the experiment name entirely.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I9a16952adde7f8bfb3a82ba05fff0630af5bdec0

--- cmd/cue/cmd/help.go
+++ cmd/cue/cmd/help.go
@@ -161,15 +161,6 @@ var environmentHelp = &cobra.Command{
 If an environment variable is unset or empty, it uses a sensible default
 setting.
 
-	CUE_CONFIG_DIR
-		The directory where the cue command keeps configuration and
-		state files. This defaults to a directory for user-specific
-		configuration data, such as:
-
-			"$XDG_CONFIG_HOME/cue" or "$HOME/.config/cue" on Linux
-			"$HOME/Library/Application Support/cue" on MacOS
-			"%AppData%/cue" on Windows
-
 	CUE_CACHE_DIR
 		The directory where the cue command keeps a cache of files to be
 		reused. This defaults to a directory for user-specific temporary
@@ -179,14 +170,25 @@ setting.
 			"$HOME/Library/Caches/cue" on MacOS
 			"%LocalAppData%/cue" on Windows
 
-	CUE_REGISTRY
+	CUE_CONFIG_DIR
+		The directory where the cue command keeps configuration and
+		state files. This defaults to a directory for user-specific
+		configuration data, such as:
 
-		This variable specifies which registry or registries to use for
-		downloading and publishing modules. See "cue help registryconfig"
-		for details.
+			"$XDG_CONFIG_HOME/cue" or "$HOME/.config/cue" on Linux
+			"$HOME/Library/Application Support/cue" on MacOS
+			"%AppData%/cue" on Windows
+
+	CUE_DEBUG
+		Comma-separated list of debug flags to enable. Current possible
+		values are:
+			http
+				Print log messages in JSON format, one per line showing
+				HTTP requests and responses made when interacting with
+				module registries.
 
 	CUE_EXPERIMENT
-		Comma-separated list of experiments to enable or disable. The
+		Comma-separated list of experiments to enable. The
 		list of available experiments may change arbitrarily over time.
 		See exp.go in package cuelang.org/go/internal/cueexperiment for
 		currently valid values.
@@ -194,6 +196,12 @@ setting.
 		Warning: This variable is provided for the development and
 		testing of the cue commands itself. Use beyond that purpose is
 		unsupported.
+
+	CUE_REGISTRY
+
+		This variable specifies which registry or registries to use for
+		downloading and publishing modules. See "cue help registryconfig"
+		for details.
 `,
 }
 

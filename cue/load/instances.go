13d1528434eb88f0817a2096efec7c140356588d
Roger Peppe <rogpeppe@gmail.com>
2024-03-14 15:56:08 +0000

    cue/load: make packages specified as args work correctly
    Fixes #2945
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I270719dac831b566e621579c8569dd01807cb2f8

--- cue/load/instances.go
+++ cue/load/instances.go
@@ -60,7 +60,21 @@ func Instances(args []string, c *Config) []*build.Instance {
 	}
 	c = newC
 
-	pkgs, err := loadPackages(ctx, c)
+	// TODO: This requires packages to be placed before files. At some point this
+	// could be relaxed.
+	i := 0
+	for ; i < len(args) && filetypes.IsPackage(args[i]); i++ {
+	}
+	pkgArgs := args[:i]
+	otherArgs := args[i:]
+
+	// Pass all arguments that look like packages to loadPackages
+	// so that they'll be available when looking up the packages
+	// that are specified on the command line.
+	// Relative import paths create a package with an associated
+	// error but it turns out that's actually OK because the cue/load
+	// logic resolves such paths without consulting pkgs.
+	pkgs, err := loadPackages(ctx, c, pkgArgs)
 	if err != nil {
 		return []*build.Instance{c.newErrInstance(err)}
 	}
@@ -74,16 +88,9 @@ func Instances(args []string, c *Config) []*build.Instance {
 		)
 	}
 
-	// TODO: require packages to be placed before files. At some point this
-	// could be relaxed.
-	i := 0
-	for ; i < len(args) && filetypes.IsPackage(args[i]); i++ {
-	}
-
 	a := []*build.Instance{}
-
 	if len(args) == 0 || i > 0 {
-		for _, m := range l.importPaths(args[:i]) {
+		for _, m := range l.importPaths(pkgArgs) {
 			if m.Err != nil {
 				inst := c.newErrInstance(m.Err)
 				a = append(a, inst)
@@ -93,8 +100,8 @@ func Instances(args []string, c *Config) []*build.Instance {
 		}
 	}
 
-	if args = args[i:]; len(args) > 0 {
-		files, err := filetypes.ParseArgs(args)
+	if len(otherArgs) > 0 {
+		files, err := filetypes.ParseArgs(otherArgs)
 		if err != nil {
 			return []*build.Instance{c.newErrInstance(err)}
 		}
@@ -137,7 +144,7 @@ func Instances(args []string, c *Config) []*build.Instance {
 	return a
 }
 
-func loadPackages(ctx context.Context, cfg *Config) (*modpkgload.Packages, error) {
+func loadPackages(ctx context.Context, cfg *Config, extraPkgs []string) (*modpkgload.Packages, error) {
 	if cfg.Registry == nil || cfg.modFile == nil || cfg.modFile.Module == "" {
 		return nil, nil
 	}
@@ -155,6 +162,9 @@ func loadPackages(ctx context.Context, cfg *Config) (*modpkgload.Packages, error
 	if err != nil {
 		return nil, fmt.Errorf("cannot enumerate all module imports: %v", err)
 	}
+	// Add any packages specified on the command line so they're always
+	// available.
+	allImports = append(allImports, extraPkgs...)
 	return modpkgload.LoadPackages(
 		ctx,
 		cfg.Module,

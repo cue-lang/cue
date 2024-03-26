5ab228f9adba9796792ac97870dbecedcdee318e
Daniel Martí <mvdan@mvdan.cc>
2024-03-26 23:02:00 +0200

    internal/mod/mvs: avoid using reflect.DeepEqual
    The slices use a comparable type, so use slices.Equal.
    Sent a similar fix to upstream: https://go.dev/cl/574597
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I0952595920dd1a9987606b33d1b531dd81e9efd7

--- internal/mod/mvs/mvs.go
+++ internal/mod/mvs/mvs.go
@@ -8,7 +8,7 @@ package mvs
 
 import (
 	"fmt"
-	"reflect"
+	"slices"
 	"sort"
 	"sync"
 
@@ -170,7 +170,7 @@ func buildList[V comparable](targets []V, reqs Reqs[V], upgrade func(V) (V, erro
 
 	// The final list is the minimum version of each module found in the graph.
 	list := g.BuildList()
-	if vs := list[:len(targets)]; !reflect.DeepEqual(vs, targets) {
+	if vs := list[:len(targets)]; !slices.Equal(vs, targets) {
 		// target.Version will be "" for modload, the main client of MVS.
 		// "" denotes the main module, which has no version. However, MVS treats
 		// version strings as opaque, so "" is not a special value here.

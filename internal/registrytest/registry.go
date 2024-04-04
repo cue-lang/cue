17b89939ae96337a7163f906bbc9cac179790c35
Roger Peppe <rogpeppe@gmail.com>
2024-04-04 14:15:35 +0100

    mod/modregistry: treat "forbidden" as "not found"
    When considering what repositories might contain a module,
    the module resolution logic is careful to distinguish between
    a simple "not found" error (discarded silently) and any other
    kind of error (triggers an error in the whole module
    resolution process).
    
    When a user has authenticated but does not have access to
    a given module, the HTTP standard allows the server to
    return a 403 (Forbidden) error to indicate that the user
    doesn't have permission to access a page.
    
    When considering possible candidates for modules, we
    don't want to fail if there are some modules that exist
    that we can't access, so this CL changes the logic to
    treat any 403 error the same as "not found" in this respect.
    
    We also add tests for this and for token-server-based authentication,
    verifying that with the OCI dependency updated as it
    is in this CL, issue #2955 is actually fixed.
    
    Fixes #2955.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I665b1aa28be255bb2e4a00bfc606b0899575fec1

--- internal/registrytest/registry.go
+++ internal/registrytest/registry.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"regexp"
 	"strings"
 
 	"cuelabs.dev/go/oci/ociregistry"
@@ -28,12 +29,49 @@ import (
 )
 
 // AuthConfig specifies authorization requirements for the server.
-// Currently it only supports basic and bearer auth.
 type AuthConfig struct {
+	// Username and Password hold the basic auth credentials.
+	// If UseTokenServer is true, these apply to the token server
+	// rather than to the registry itself.
 	Username string `json:"username"`
 	Password string `json:"password"`
 
+	// BearerToken holds a bearer token to use as auth.
+	// If UseTokenServer is true, this applies to the token server
+	// rather than to the registry itself.
 	BearerToken string `json:"bearerToken"`
+
+	// UseTokenServer starts a token server and directs client
+	// requests to acquire auth tokens from that server.
+	UseTokenServer bool `json:"useTokenServer"`
+
+	// ACL holds the ACL for an authenticated client.
+	// If it's nil, the user is allowed full access.
+	// Note: there's only one ACL because we only
+	// support a single authenticated user.
+	ACL *ACL `json:"acl,omitempty"`
+
+	// Use401InsteadOf403 causes the server to send a 401
+	// response even when the credentials are present and correct.
+	Use401InsteadOf403 bool `json:"always401"`
+}
+
+// ACL determines what endpoints an authenticated user can accesse
+// Both Allow and Deny hold a list of regular expressions that
+// are matched against an HTTP request formatted as a string:
+//
+//	METHOD URL_PATH
+//
+// For example:
+//
+//	GET /v2/foo/bar
+type ACL struct {
+	// Allow holds the list of allowed paths for a user.
+	// If none match, the user is forbidden.
+	Allow []string
+	// Deny holds the list of denied paths for a user.
+	// If any match, the user is forbidden.
+	Deny []string
 }
 
 // Upload uploads the modules found inside fsys (stored
@@ -67,52 +105,228 @@ func upload(ctx context.Context, r ociregistry.Interface, fsys fs.FS) (authConfi
 //
 // If there's a file named auth.json in the root directory,
 // it will cause access to the server to be gated by the
-// specified authorization. See the AuthConfig type for
+// specified authorization. See the [AuthConfig] type for
 // details.
 //
 // The Registry should be closed after use.
 func New(fsys fs.FS, prefix string) (*Registry, error) {
-	handler, err := NewHandler(fsys, prefix)
-	if err != nil {
-		return nil, err
-	}
-	srv := httptest.NewServer(handler)
-	u, err := url.Parse(srv.URL)
-	if err != nil {
-		return nil, err
-	}
-	return &Registry{
-		srv:  srv,
-		host: u.Host,
-	}, nil
-}
-
-// NewHandler is similar to [New] except that it just returns
-// the HTTP handler for the server instead of actually starting
-// a server.
-func NewHandler(fsys fs.FS, prefix string) (http.Handler, error) {
 	r := ocimem.New()
 
 	authConfigData, err := upload(context.Background(), ocifilter.Sub(r, prefix), fsys)
 	if err != nil {
 		return nil, err
 	}
-	var handler http.Handler = ociserver.New(ocifilter.ReadOnly(r), nil)
+	var authConfig *AuthConfig
 	if authConfigData != nil {
-		var cfg AuthConfig
-		if err := json.Unmarshal(authConfigData, &cfg); err != nil {
+		if err := json.Unmarshal(authConfigData, &authConfig); err != nil {
 			return nil, fmt.Errorf("invalid auth.json: %v", err)
 		}
-		handler = AuthHandler(handler, &cfg)
 	}
-	return handler, nil
+	return NewServer(ocifilter.ReadOnly(r), authConfig)
 }
 
-// AuthHandler wraps the given handler with logic that checks
-// that the incoming requests fulfil the auth requirements defined
+// NewServer is like New except that instead of uploading
+// the contents of a filesystem, it just serves the contents
+// of the given registry guarded by the given auth configuration.
+// If auth is nil, no authentication will be required.
+func NewServer(r ociregistry.Interface, auth *AuthConfig) (*Registry, error) {
+	var tokenSrv *httptest.Server
+	if auth != nil && auth.UseTokenServer {
+		tokenSrv = httptest.NewServer(tokenHandler(auth))
+	}
+	r, err := authzRegistry(auth, r)
+	if err != nil {
+		return nil, err
+	}
+	srv := httptest.NewServer(&registryHandler{
+		auth:     auth,
+		registry: ociserver.New(r, nil),
+		tokenSrv: tokenSrv,
+	})
+	u, err := url.Parse(srv.URL)
+	if err != nil {
+		return nil, err
+	}
+	return &Registry{
+		srv:      srv,
+		host:     u.Host,
+		tokenSrv: tokenSrv,
+	}, nil
+}
+
+// authzRegistry wraps r by checking whether the client has authorization
+// to read any given repository.
+func authzRegistry(auth *AuthConfig, r ociregistry.Interface) (ociregistry.Interface, error) {
+	if auth == nil {
+		return r, nil
+	}
+	allow := func(repoName string) bool {
+		return true
+	}
+	if auth.ACL != nil {
+		allowCheck, err := regexpMatcher(auth.ACL.Allow)
+		if err != nil {
+			return nil, fmt.Errorf("invalid allow list: %v", err)
+		}
+		denyCheck, err := regexpMatcher(auth.ACL.Deny)
+		if err != nil {
+			return nil, fmt.Errorf("invalid deny list: %v", err)
+		}
+		allow = func(repoName string) bool {
+			return allowCheck(repoName) && !denyCheck(repoName)
+		}
+	}
+	return ocifilter.AccessChecker(r, func(repoName string, access ocifilter.AccessKind) (_err error) {
+		if !allow(repoName) {
+			if auth.Use401InsteadOf403 {
+				// TODO this response should be associated with a
+				// Www-Authenticate header, but this won't do that.
+				// Given that the ociauth logic _should_ turn
+				// this back into a 403 error again, perhaps
+				// we're OK.
+				return ociregistry.ErrUnauthorized
+			}
+			// TODO should we be a bit more sophisticated and only
+			// return ErrDenied when the repository doesn't exist?
+			return ociregistry.ErrDenied
+		}
+		return nil
+	}), nil
+}
+
+func regexpMatcher(patStrs []string) (func(string) bool, error) {
+	pats := make([]*regexp.Regexp, len(patStrs))
+	for i, s := range patStrs {
+		pat, err := regexp.Compile(s)
+		if err != nil {
+			return nil, fmt.Errorf("invalid regexp in ACL: %v", err)
+		}
+		pats[i] = pat
+	}
+	return func(name string) bool {
+		for _, pat := range pats {
+			if pat.MatchString(name) {
+				return true
+			}
+		}
+		return false
+	}, nil
+}
+
+type registryHandler struct {
+	auth     *AuthConfig
+	registry http.Handler
+	tokenSrv *httptest.Server
+}
+
+const (
+	registryAuthToken   = "ok-token-for-registrytest"
+	registryUnauthToken = "unauth-token-for-registrytest"
+)
+
+func (h *registryHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if h.auth == nil {
+		h.registry.ServeHTTP(w, req)
+		return
+	}
+	if h.tokenSrv == nil {
+		h.serveDirectAuth(w, req)
+		return
+	}
+
+	// Auth with token server.
+	wwwAuth := fmt.Sprintf("Bearer realm=%q,service=registrytest", h.tokenSrv.URL)
+	authHeader := req.Header.Get("Authorization")
+	if authHeader == "" {
+		w.Header().Set("Www-Authenticate", wwwAuth)
+		writeError(w, ociregistry.ErrUnauthorized)
+		return
+	}
+	kind, token, ok := strings.Cut(authHeader, " ")
+	if !ok || kind != "Bearer" {
+		w.Header().Set("Www-Authenticate", wwwAuth)
+		writeError(w, ociregistry.ErrUnauthorized)
+		return
+	}
+	switch token {
+	case registryAuthToken:
+		// User is authorized.
+	case registryUnauthToken:
+		writeError(w, ociregistry.ErrDenied)
+		return
+	default:
+		// If we don't recognize the token, then presumably
+		// the client isn't authenticated so it's 401 not 403.
+		w.Header().Set("Www-Authenticate", wwwAuth)
+		writeError(w, ociregistry.ErrUnauthorized)
+		return
+	}
+	h.registry.ServeHTTP(w, req)
+}
+
+func (h *registryHandler) serveDirectAuth(w http.ResponseWriter, req *http.Request) {
+	auth := req.Header.Get("Authorization")
+	if auth == "" {
+		if h.auth.BearerToken != "" {
+			// Note that this lacks information like the realm,
+			// but we don't need it for our test cases yet.
+			w.Header().Set("Www-Authenticate", "Bearer service=registry")
+		} else {
+			w.Header().Set("Www-Authenticate", "Basic service=registry")
+		}
+		writeError(w, fmt.Errorf("%w: no credentials", ociregistry.ErrUnauthorized))
+		return
+	}
+	if h.auth.BearerToken != "" {
+		token, ok := strings.CutPrefix(auth, "Bearer ")
+		if !ok || token != h.auth.BearerToken {
+			writeError(w, fmt.Errorf("%w: invalid bearer credentials", ociregistry.ErrUnauthorized))
+			return
+		}
+	} else {
+		username, password, ok := req.BasicAuth()
+		if !ok || username != h.auth.Username || password != h.auth.Password {
+			writeError(w, fmt.Errorf("%w: invalid user-password credentials", ociregistry.ErrUnauthorized))
+			return
+		}
+	}
+	h.registry.ServeHTTP(w, req)
+}
+
+func tokenHandler(*AuthConfig) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != "POST" {
+			http.Error(w, "only POST supported", http.StatusMethodNotAllowed)
+			return
+		}
+		req.ParseForm()
+		if req.Form.Get("service") != "registrytest" {
+			http.Error(w, "invalid service", http.StatusBadRequest)
+			return
+		}
+		if req.Form.Get("grant_type") != "refresh_token" {
+			http.Error(w, "invalid grant type", http.StatusBadRequest)
+			return
+		}
+		refreshToken := req.Form.Get("refresh_token")
+		if refreshToken != "registrytest-refresh" {
+			http.Error(w, fmt.Sprintf("invalid refresh token %q", refreshToken), http.StatusForbidden)
+			return
+		}
+		// See ociauth.wireToken for the full JSON format.
+		data, _ := json.Marshal(map[string]string{
+			"token": registryAuthToken,
+		})
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+	})
+}
+
+// authnHandler wraps the given handler with logic that checks
+// that the incoming requests fulfil the authenticiation requirements defined
 // in cfg. If cfg is nil or there are no auth requirements, it returns handler
 // unchanged.
-func AuthHandler(handler http.Handler, cfg *AuthConfig) http.Handler {
+func authnHandler(cfg *AuthConfig, handler http.Handler) http.Handler {
 	if cfg == nil || (*cfg == AuthConfig{}) {
 		return handler
 	}
@@ -126,24 +340,31 @@ func AuthHandler(handler http.Handler, cfg *AuthConfig) http.Handler {
 			} else {
 				w.Header().Set("Www-Authenticate", "Basic service=registry")
 			}
-			http.Error(w, "no credentials", http.StatusUnauthorized)
+			writeError(w, fmt.Errorf("%w: no credentials", ociregistry.ErrUnauthorized))
 			return
 		}
 		if cfg.BearerToken != "" {
 			token, ok := strings.CutPrefix(auth, "Bearer ")
 			if !ok || token != cfg.BearerToken {
-				http.Error(w, "invalid credentials", http.StatusUnauthorized)
+				writeError(w, fmt.Errorf("%w: invalid bearer credentials", ociregistry.ErrUnauthorized))
 				return
 			}
 		} else {
 			username, password, ok := req.BasicAuth()
 			if !ok || username != cfg.Username || password != cfg.Password {
-				http.Error(w, "invalid credentials", http.StatusUnauthorized)
+				writeError(w, fmt.Errorf("%w: invalid user-password credentials", ociregistry.ErrUnauthorized))
 				return
 			}
 		}
 		handler.ServeHTTP(w, req)
 	})
+}
+
+func writeError(w http.ResponseWriter, err error) {
+	data, httpStatus := ociregistry.MarshalError(err)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(httpStatus)
+	w.Write(data)
 }
 
 func pushContent(ctx context.Context, client *modregistry.Client, mods map[module.Version]*moduleContent) error {
@@ -191,12 +412,16 @@ func visitDepthFirst(mods map[module.Version]*moduleContent, v module.Version, f
 }
 
 type Registry struct {
-	srv  *httptest.Server
-	host string
+	srv      *httptest.Server
+	tokenSrv *httptest.Server
+	host     string
 }
 
 func (r *Registry) Close() {
 	r.srv.Close()
+	if r.tokenSrv != nil {
+		r.tokenSrv.Close()
+	}
 }
 
 // Host returns the hostname for the registry server;

c3b9ce8c468cd75940b34a827b9a69febd58d376
Roger Peppe <rogpeppe@gmail.com>
2024-04-09 16:58:15 +0100

    internal/vcs: new package
    This provides access to the VCS primitives that are required
    when determining what files what go into a module.
    
    For now, git is enough but we plan to support other VCS
    systems in the future.
    
    For #3017.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I9789600d06b3424b22dd08351db6938d2929df7d

---
+++ internal/vcs/copyfs_test.go
@@ -0,0 +1,75 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package vcs
+
+import (
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+// copyFS is copied from Go tip as of 315b6ae682a2a4e7718924a45b8b311a0fe10043.
+// It's slightly adapted to avoid localizing names,
+// because that relies on Go internal functions and we don't need
+// to be that careful for this use case.
+// TODO use [os.CopyFS] when we can.
+
+// copyFS copies the file system fsys into the directory dir,
+// creating dir if necessary.
+//
+// Newly created directories and files have their default modes
+// where any bits from the file in fsys that are not part of the
+// standard read, write, and execute permissions will be zeroed
+// out, and standard read and write permissions are set for owner,
+// group, and others while retaining any existing execute bits from
+// the file in fsys.
+//
+// Symbolic links in fsys are not supported, a *PathError with Err set
+// to ErrInvalid is returned on symlink.
+//
+// Copying stops at and returns the first error encountered.
+func copyFS(dir string, fsys fs.FS) error {
+	return fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// The original version calls safefilepath.Localize here
+		// but we don't have access to that and the context is
+		// limited enough that it shouldn't matter.
+		newPath := filepath.Join(dir, path)
+		if d.IsDir() {
+			return os.MkdirAll(newPath, 0777)
+		}
+
+		// TODO(panjf2000): handle symlinks with the help of fs.ReadLinkFS
+		// 		once https://go.dev/issue/49580 is done.
+		//		we also need safefilepath.IsLocal from https://go.dev/cl/564295.
+		if !d.Type().IsRegular() {
+			return &os.PathError{Op: "CopyFS", Path: path, Err: os.ErrInvalid}
+		}
+
+		r, err := fsys.Open(path)
+		if err != nil {
+			return err
+		}
+		defer r.Close()
+		info, err := r.Stat()
+		if err != nil {
+			return err
+		}
+		w, err := os.OpenFile(newPath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666|info.Mode()&0777)
+		if err != nil {
+			return err
+		}
+
+		if _, err := io.Copy(w, r); err != nil {
+			w.Close()
+			return &os.PathError{Op: "Copy", Path: newPath, Err: err}
+		}
+		return w.Close()
+	})
+}
---
+++ internal/vcs/git.go
@@ -0,0 +1,113 @@
+// Copyright 2024 CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package vcs
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type gitVCS struct {
+	root string
+}
+
+func newGitVCS(dir string) (VCS, error) {
+	root := findRoot(dir, ".git")
+	if root == "" {
+		return nil, &vcsNotFoundError{
+			kind: "git",
+			dir:  dir,
+		}
+	}
+	return gitVCS{
+		root: root,
+	}, nil
+}
+
+// Root implements [VCS.Root].
+func (v gitVCS) Root() string {
+	return v.root
+}
+
+// ListFiles implements [VCS.ListFiles].
+func (v gitVCS) ListFiles(ctx context.Context, dir string) ([]string, error) {
+	rel, err := filepath.Rel(v.root, dir)
+	if err != nil || rel == ".." || strings.HasPrefix(rel, ".."+string(filepath.Separator)) {
+		return nil, fmt.Errorf("cannot list files from %q, outside VCS root %q", dir, v.root)
+	}
+	// TODO should we use --recurse-submodules?
+	out, err := runCmd(ctx, dir, "git", "ls-files", "-z")
+	if err != nil {
+		return nil, err
+	}
+	files := strings.Split(strings.TrimSuffix(out, "\x00"), "\x00")
+	sort.Strings(files)
+	return files, nil
+}
+
+// Status implements [VCS.Status].
+func (v gitVCS) Status(ctx context.Context) (Status, error) {
+	out, err := runCmd(ctx, v.root, "git", "status", "--porcelain")
+	if err != nil {
+		return Status{}, err
+	}
+	uncommitted := len(out) > 0
+
+	// "git status" works for empty repositories, but "git log" does not.
+	// Assume there are no commits in the repo when "git log" fails with
+	// uncommitted files and skip tagging revision / committime.
+	var rev string
+	var commitTime time.Time
+	out, err = runCmd(ctx, v.root, "git",
+		"-c", "log.showsignature=false",
+		"log", "-1", "--format=%H:%ct",
+	)
+	if err != nil && !uncommitted {
+		return Status{}, err
+	}
+	if err == nil {
+		rev, commitTime, err = parseRevTime(out)
+		if err != nil {
+			return Status{}, err
+		}
+	}
+	return Status{
+		Revision:    rev,
+		CommitTime:  commitTime,
+		Uncommitted: uncommitted,
+	}, nil
+}
+
+// parseRevTime parses commit details in "revision:seconds" format.
+func parseRevTime(out string) (string, time.Time, error) {
+	buf := strings.TrimSpace(out)
+
+	rev, t, _ := strings.Cut(buf, ":")
+	if rev == "" {
+		return "", time.Time{}, fmt.Errorf("unrecognized VCS tool output %q", out)
+	}
+
+	secs, err := strconv.ParseInt(t, 10, 64)
+	if err != nil {
+		return "", time.Time{}, fmt.Errorf("unrecognized VCS tool output: %v", err)
+	}
+
+	return rev, time.Unix(secs, 0), nil
+}

e2c748140181948e3d39ece0345d787a6726c879
Roger Peppe <rogpeppe@gmail.com>
2024-04-10 15:33:50 +0100

    cmd/cue/cmd: require source when running cue mod publish
    This is an experimental WIP implementation of the proposal discussed at
    https://cuelang.org/discussion/3017.
    
    Still to do in a followup CL: we need to add the git metadata to the
    module manifest file.
    
    For #3017.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: Ie7d5b0acda4690ee99d2873eed39183ce909a666

--- cmd/cue/cmd/modpublish.go
+++ cmd/cue/cmd/modpublish.go
@@ -16,11 +16,14 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
 
 	"github.com/spf13/cobra"
 
+	"cuelang.org/go/internal/vcs"
 	"cuelang.org/go/mod/modfile"
 	"cuelang.org/go/mod/modregistry"
 	"cuelang.org/go/mod/module"
@@ -50,6 +53,7 @@ for this command to work.
 }
 
 func runModUpload(cmd *Command, args []string) error {
+	ctx := cmd.Context()
 	resolver, err := getRegistryResolver()
 	if err != nil {
 		return err
@@ -75,6 +79,10 @@ func runModUpload(cmd *Command, args []string) error {
 	if err != nil {
 		return fmt.Errorf("cannot form module version: %v", err)
 	}
+	if mf.Source == nil {
+		// TODO print filename relative to current directory
+		return fmt.Errorf("no source field found in cue.mod/module.cue")
+	}
 	zf, err := os.CreateTemp("", "cue-publish-")
 	if err != nil {
 		return err
@@ -83,8 +91,37 @@ func runModUpload(cmd *Command, args []string) error {
 	defer zf.Close()
 
 	// TODO verify that all dependencies exist in the registry.
-	if err := modzip.CreateFromDir(zf, mv, modRoot); err != nil {
-		return err
+
+	var vcsStatus vcs.Status
+
+	switch mf.Source.Kind {
+	case "self":
+		if err := modzip.CreateFromDir(zf, mv, modRoot); err != nil {
+			return err
+		}
+	default:
+		vcsImpl, err := vcs.New(mf.Source.Kind, modRoot)
+		if err != nil {
+			return err
+		}
+		status, err := vcsImpl.Status(ctx)
+		if err != nil {
+			return err
+		}
+		if status.Uncommitted {
+			// TODO implement --force to bypass this check
+			return fmt.Errorf("VCS state is not clean")
+		}
+		files, err := vcsImpl.ListFiles(ctx, modRoot)
+		if err != nil {
+			return err
+		}
+		if err := modzip.Create[string](zf, mv, files, osFileIO{
+			modRoot: modRoot,
+		}); err != nil {
+			return err
+		}
+		vcsStatus = status
 	}
 	info, err := zf.Stat()
 	if err != nil {
@@ -92,9 +129,32 @@ func runModUpload(cmd *Command, args []string) error {
 	}
 
 	rclient := modregistry.NewClientWithResolver(resolver)
+	_ = vcsStatus // TODO attach vcsStatus to PutModule metadata
 	if err := rclient.PutModule(backgroundContext(), mv, zf, info.Size()); err != nil {
 		return fmt.Errorf("cannot put module: %v", err)
 	}
 	fmt.Printf("published %s\n", mv)
 	return nil
+}
+
+// osFileIO implements [modzip.FileIO] for filepath paths relative to
+// the module root directory, as returned by [vcs.VCS.ListFiles].
+type osFileIO struct {
+	modRoot string
+}
+
+func (osFileIO) Path(f string) string {
+	return filepath.ToSlash(f)
+}
+
+func (fio osFileIO) Lstat(f string) (fs.FileInfo, error) {
+	return os.Lstat(fio.absPath(f))
+}
+
+func (fio osFileIO) Open(f string) (io.ReadCloser, error) {
+	return os.Open(fio.absPath(f))
+}
+
+func (fio osFileIO) absPath(f string) string {
+	return filepath.Join(fio.modRoot, f)
 }

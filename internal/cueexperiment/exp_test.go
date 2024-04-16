33630b9afc10839af4ed159e40d31de05c6b7d66
Daniel Martí <mvdan@mvdan.cc>
2024-04-16 11:39:38 +0100

    internal/cueexperiment: enable CUE_EXPERIMENT=yamlv3decoder by default
    Using the field tag feature just added to internal/envflag.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I9f6e4a1e2c7e0fbb1734c9381899174f8275b98c

--- internal/cueexperiment/exp_test.go
+++ internal/cueexperiment/exp_test.go
@@ -8,9 +8,25 @@ import (
 
 func TestInit(t *testing.T) {
 	// This is just a smoke test to make sure it's all wired up OK.
+
+	// Check the default values.
+	t.Setenv("CUE_EXPERIMENT", "")
+	err := initAlways()
+	qt.Assert(t, qt.IsNil(err))
+	qt.Assert(t, qt.IsFalse(Flags.Modules))
+	qt.Assert(t, qt.IsTrue(Flags.YAMLV3Decoder))
+
+	// Check that we can enable all experiments.
 	t.Setenv("CUE_EXPERIMENT", "modules,yamlv3decoder")
-	err := Init()
+	err = initAlways()
 	qt.Assert(t, qt.IsNil(err))
 	qt.Assert(t, qt.IsTrue(Flags.Modules))
 	qt.Assert(t, qt.IsTrue(Flags.YAMLV3Decoder))
+
+	// Check that we can disable all experiments.
+	t.Setenv("CUE_EXPERIMENT", "modules=0,yamlv3decoder=0")
+	err = initAlways()
+	qt.Assert(t, qt.IsNil(err))
+	qt.Assert(t, qt.IsFalse(Flags.Modules))
+	qt.Assert(t, qt.IsFalse(Flags.YAMLV3Decoder))
 }

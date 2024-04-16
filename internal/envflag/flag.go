9f0b92fae3db09d10a62934299a51264e40ed42f
Daniel Martí <mvdan@mvdan.cc>
2024-04-16 11:35:38 +0100

    internal/envflag: add support for key-values and defaults
    That is, SOME_ENV=foo will continue to work like before,
    now being a short-hand for SOME_ENV=foo=1 or SOME_ENV=foo=true.
    This also allows disabling via SOME_ENV=foo=0 or SOME_ENV=foo=false.
    
    Moreover, the default value of each field is no longer always false,
    since we necessarily want to transition some CUE_EXPERIMENT values
    from a default of false to true over time.
    This is implemented via struct field tags, much like encoding/json
    allows attaching options to each field in a Go struct.
    We use the prefix "default:" in the value to be very explicit.
    
    This will be necessary for CUE_EXPERIMENT=yamlv3decoder, which we want
    to start with a default of true, as an opt-out experiment for users.
    
    For #3027.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Ifac2e9f973b3d7f752616de1718ac612fe15119e

--- internal/envflag/flag.go
+++ internal/envflag/flag.go
@@ -4,30 +4,67 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
-// Init initializes the fields in flags from the contents of the given
-// environment variable, which contains a comma-separated
-// list of names representing the boolean fields in the struct type T.
+// Init initializes the fields in flags from the attached struct field tags
+// as well as the contents of the given environment variable.
+//
+// The struct field tag may contain a default value other than the zero value,
+// such as `envflag:"default:true"` to set a boolean field to true by default.
+//
+// The environment variable may contain a comma-separated list of name=value
+// pairs values representing the boolean fields in the struct type T.
+// If the value is omitted entirely, the value is assumed to be name=true.
+//
 // Names are treated case insensitively.
+// Value strings are parsed as Go booleans via [strconv.ParseBool],
+// meaning that they accept "true" and "false" but also the shorter "1" and "0".
 func Init[T any](flags *T, envVar string) error {
+	// Collect the field indices and set the default values.
+	indexByName := make(map[string]int)
+	fv := reflect.ValueOf(flags).Elem()
+	ft := fv.Type()
+	for i := 0; i < ft.NumField(); i++ {
+		field := ft.Field(i)
+		defaultValue := false
+		if tagStr, ok := field.Tag.Lookup("envflag"); ok {
+			defaultStr, ok := strings.CutPrefix(tagStr, "default:")
+			if !ok {
+				return fmt.Errorf("expected tag like `envflag:\"default:true\"`: %s", tagStr)
+			}
+			v, err := strconv.ParseBool(defaultStr)
+			if err != nil {
+				return fmt.Errorf("invalid default bool value for %s: %v", field.Name, err)
+			}
+			defaultValue = v
+		}
+		fv.Field(i).SetBool(defaultValue)
+		indexByName[strings.ToLower(field.Name)] = i
+	}
+
+	// Parse the env value to set the fields.
 	env := os.Getenv(envVar)
 	if env == "" {
 		return nil
 	}
-	names := make(map[string]int)
-	fv := reflect.ValueOf(flags).Elem()
-	ft := fv.Type()
-	for i := 0; i < ft.NumField(); i++ {
-		names[strings.ToLower(ft.Field(i).Name)] = i
-	}
-	for _, name := range strings.Split(env, ",") {
-		index, ok := names[name]
-		if !ok {
-			return fmt.Errorf("unknown %s %s", envVar, name)
+	for _, elem := range strings.Split(env, ",") {
+		name, valueStr, ok := strings.Cut(elem, "=")
+		// "somename" is short for "somename=true" or "somename=1".
+		value := true
+		if ok {
+			v, err := strconv.ParseBool(valueStr)
+			if err != nil {
+				return fmt.Errorf("invalid bool value for %s: %v", name, err)
+			}
+			value = v
 		}
-		fv.Field(index).SetBool(true)
+		index, ok := indexByName[name]
+		if !ok {
+			return fmt.Errorf("unknown %s %s", envVar, elem)
+		}
+		fv.Field(index).SetBool(value)
 	}
 	return nil
 }

e2ba38d47fb28d23b2ed9baa7ac70259e13993a7
Daniel Martí <mvdan@mvdan.cc>
2024-04-19 17:26:56 +0100

    internal/core/adt: avoid creating closures for adt.Config.Format
    We can rather easily use a single static function
    by passing the Runtime around as an extra parameter.
    This saves one allocation per call to NewContext,
    which adds up to the amount of GC work in the evaluator.
    
    For example, for `cue vet -c=false ./...`:
    
                  │    old     │                new                │
                  │   sec/op   │   sec/op    vs base               │
        CueVetAll   2.078 ± 1%   2.030 ± 1%  -2.32% (p=0.000 n=10)
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Id8ab3b198d3c5a9c35c3a7502bb00859024571e7

--- internal/core/adt/context.go
+++ internal/core/adt/context.go
@@ -172,7 +172,7 @@ type Runtime interface {
 
 type Config struct {
 	Runtime
-	Format func(Node) string
+	Format func(Runtime, Node) string
 }
 
 // New creates an operation context.
@@ -205,7 +205,7 @@ func (c *OpContext) isDevVersion() bool {
 // use an OpContext at a time.
 type OpContext struct {
 	Runtime
-	Format func(Node) string
+	Format func(Runtime, Node) string
 
 	cuedebug.Config
 	Version internal.EvaluatorVersion // Copied from Runtime
@@ -1388,7 +1388,7 @@ func (c *OpContext) Str(x Node) string {
 	if c.Format == nil {
 		return fmt.Sprintf("%T", x)
 	}
-	return c.Format(x)
+	return c.Format(c.Runtime, x)
 }
 
 // NewList returns a new list for the given values.

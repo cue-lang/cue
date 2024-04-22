de9fd977446f51e344a01ab21db4f15a7a0ca685
Noam Dolovich <noam.tzvi.dolovich@gmail.com>
2024-04-22 11:09:59 +0300

    cue/format: fix comments formatting in lists
    When list elements are formatted in cue/format, the algorithm prints
    each element followed by a comment. In case an element has associated
    comments, they are printed as part of the element.
    This causes an issue when the comments appear after the element, because
    the comma will be printed at the end of the comment, and will ulimately
    be considered part of the comment, causing syntax errors.
    
    For example, given the following valid example:
    
        list: [
          a.b,                  // selector
          (a),                  // paren
          +1,                   // unary
          a[0],                 // index
          strings.Sort([1, 2]), // call
    
          a.b,
          // under selector
    
          a.b,
          // multiple
          // under
    
          // selector
        ]
    
    The current formatting logic will produce this:
    
        list: [
            a.b                  // selector,
            (a                   // paren),
            +1                   // unary,
            a[0                  // index],
            strings.Sort([1, 2], // call),
    
            a.b
            // under selector,
    
            a.b
            // multiple
            // under
    
            // selector,
        ]
    
    To resolve this, all comments that appear after an element are temporarily
    disassociated from the element, so that the list formatting section can
    insert the comma in the correct location.
    The solution could be cleaner, but a larger refactor is avoided since an
    overhaul of cue/format is planned.
    
    Fixes #2274
    Updates #2567
    
    Signed-off-by: Noam Dolovich <noam.tzvi.dolovich@gmail.com>
    Change-Id: Icbbb10800ed3f04bd5adc658b883fa259844b7e9

--- cue/format/node.go
+++ cue/format/node.go
@@ -188,6 +188,29 @@ func (f *formatter) walkListElems(list []ast.Expr) {
 	f.before(nil)
 	for _, x := range list {
 		f.before(x)
+
+		// This is a hack to ensure that comments are printed correctly in lists.
+		// A comment must be printed after each element in a list, but we can't
+		// print a comma at the end of a comment because it will be considered
+		// part of the comment and ignored.
+		// To fix this we ignore all comments that appear after the element,
+		// and only handle them after it's formatted.
+		originalComments := ast.Comments(x)
+		var commentsBefore, commentsAfter []*ast.CommentGroup
+		splitComments := x.Pos().IsValid()
+		if splitComments {
+			for _, cg := range originalComments {
+				if x.Pos().Before(cg.Pos()) {
+					commentsAfter = append(commentsAfter, cg)
+				} else {
+					commentsBefore = append(commentsBefore, cg)
+				}
+			}
+		}
+
+		if splitComments {
+			f.current.cg = commentsBefore
+		}
 		switch n := x.(type) {
 		case *ast.Comprehension:
 			f.walkClauseList(n.Clauses, blank)
@@ -208,6 +231,10 @@ func (f *formatter) walkListElems(list []ast.Expr) {
 			f.exprRaw(n, token.LowestPrec, 1)
 		}
 		f.print(comma, blank)
+
+		if splitComments {
+			f.current.cg = commentsAfter
+		}
 		f.after(x)
 	}
 	f.after(nil)

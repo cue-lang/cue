050f7b51a1a5b911e24eb50d297260816a7bfecb
Daniel Martí <mvdan@mvdan.cc>
2024-04-24 00:19:32 +0100

    cmd/cue: rip out cue.Instance and cue.Merge from 'cue cmd'
    As can be seen by the cmd_many.txtar test just added,
    we still support the scenario where 'cue cmd' is given multiple packages
    by unifying all of their values together, rather than using cue.Merge.
    
    Other than cue.Merge having been deprecated with DO NOT USE warnings
    since early 2021, it seemed to have had somewhat odd semantics.
    Structs in CUE are open by default, so unifying {} with {b: "b"}
    should result in {b: "b"} without any issue, as we do now.
    
    The merge semantics, which aren't documented and deprecated, disagree.
    However, the passing test mimicking what a few users have reported using
    still passes just as it did before, and the new semantics are now
    well understood - if not documented as such yet - so this seems fine.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Ia3195643612f629ac61a36f8469945ee345e0719

--- cmd/cue/cmd/custom.go
+++ cmd/cue/cmd/custom.go
@@ -43,7 +43,7 @@ import (
 const commandSection = "command"
 
 func lookupString(obj cue.Value, key, def string) string {
-	str, err := obj.Lookup(key).String()
+	str, err := obj.LookupPath(cue.MakePath(cue.Str(key))).String()
 	if err == nil {
 		def = str
 	}
@@ -62,8 +62,8 @@ func splitLine(s string) (line, tail string) {
 // addCustomCommands iterates over all commands defined under field typ
 // and adds them as cobra subcommands to cmd.
 // The func is only used in `cue help cmd`, which doesn't show errors.
-func addCustomCommands(c *Command, cmd *cobra.Command, typ string, tools *cue.Instance) {
-	commands := tools.Lookup(typ)
+func addCustomCommands(c *Command, cmd *cobra.Command, typ string, tools cue.Value) {
+	commands := tools.LookupPath(cue.MakePath(cue.Str(typ)))
 	if !commands.Exists() {
 		return
 	}
@@ -80,11 +80,7 @@ func addCustomCommands(c *Command, cmd *cobra.Command, typ string, tools *cue.In
 }
 
 // customCommand creates a cobra.Command out of a CUE command definition.
-func customCommand(c *Command, typ, name string, tools *cue.Instance) (*cobra.Command, error) {
-	if tools == nil {
-		return nil, errors.New("no commands defined")
-	}
-
+func customCommand(c *Command, typ, name string, tools cue.Value) (*cobra.Command, error) {
 	// TODO: validate allowing incomplete.
 	o := tools.Lookup(typ, name)
 	if !o.Exists() {
@@ -149,7 +145,7 @@ func customCommand(c *Command, typ, name string, tools *cue.Instance) (*cobra.Co
 	return sub, nil
 }
 
-func doTasks(cmd *Command, typ, command string, root *cue.Instance) error {
+func doTasks(cmd *Command, typ, command string, root cue.Value) error {
 	cfg := &flow.Config{
 		Root:           cue.MakePath(cue.Str(commandSection), cue.Str(command)),
 		InferTasks:     true,

99b4a05f92f0ed1af4830df84fd0187a11085eee
Noam Dolovich <noam.tzvi.dolovich@gmail.com>
2024-04-24 21:50:07 +0300

    cue/cmd: add shorthands for --check and --diff
    This adds shorthand flags for the `cue fmt` and `cue mod tidy`
    commands, so that we can now run:
    
        - cue fmt -c      // cue fmt --check
        - cue fmt -d      // cue fmt --diff
        - cue mod tidy -c // cue mod tidy --check
    
    Signed-off-by: Noam Dolovich <noam.tzvi.dolovich@gmail.com>
    Change-Id: Ieb05b2f606f98e046bf837989fa02e64c12df8af

--- cmd/cue/cmd/fmt.go
+++ cmd/cue/cmd/fmt.go
@@ -162,8 +162,8 @@ func newFmtCmd(c *Command) *cobra.Command {
 		}),
 	}
 
-	cmd.Flags().Bool(string(flagCheck), false, "exits with non-zero status if any files are not formatted")
-	cmd.Flags().Bool(string(flagDiff), false, "display diffs instead of rewriting files")
+	cmd.Flags().BoolP(string(flagCheck), "c", false, "exits with non-zero status if any files are not formatted")
+	cmd.Flags().BoolP(string(flagDiff), "d", false, "display diffs instead of rewriting files")
 
 	return cmd
 }

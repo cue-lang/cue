67ac31f11611dabfcfd07062bf78444b6c0818e4
Marcel van Lohuizen <mpvl@gmail.com>
2024-04-29 15:52:31 +0200

    internal/core/adt: fix matchPattern
    matchPattern did not handle the case where Feature f
    was `AnyString`. It now does. This also means that
    more places in the code should check for label != nil,
    as it is now possible for it to be nil even if kind is string.
    
    Issue #3060
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I3a814b92b0d28dd8408a95beff329042938f7192

--- internal/core/adt/constraints.go
+++ internal/core/adt/constraints.go
@@ -121,9 +121,7 @@ func matchPattern(ctx *OpContext, pattern Value, f Feature) bool {
 	// avoid many bound checks), which we probably should. Especially when we
 	// allow list constraints, like [<10]: T.
 	var label Value
-	if int64(f.Index()) == MaxIndex {
-		f = 0
-	} else if f.IsString() {
+	if f.IsString() && int64(f.Index()) != MaxIndex {
 		label = f.ToValue(ctx)
 	}
 
@@ -144,8 +142,8 @@ func matchPatternValue(ctx *OpContext, pattern Value, f Feature, label Value) (r
 	}
 
 	k := IntKind
-	if label != nil {
-		k = label.Kind()
+	if f.IsString() {
+		k = StringKind
 	}
 	if !k.IsAnyOf(pattern.Kind()) {
 		return false
@@ -173,7 +171,6 @@ func matchPatternValue(ctx *OpContext, pattern Value, f Feature, label Value) (r
 		return true
 
 	case *BasicType:
-		k := label.Kind()
 		return x.K&k == k
 
 	case *BoundValue:
@@ -198,6 +195,9 @@ func matchPatternValue(ctx *OpContext, pattern Value, f Feature, label Value) (r
 		return err == nil && xi == yi
 
 	case *String:
+		if label == nil {
+			return false
+		}
 		y, ok := label.(*String)
 		return ok && x.Str == y.Str
 

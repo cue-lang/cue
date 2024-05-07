1e6a77951304a97ba0a67edf7605c7a99f93f92d
Noam Dolovich <noam.tzvi.dolovich@gmail.com>
2024-05-07 20:19:15 +0300

    internal/core/adt: allow unresolved disjunctions in builtin calls
    Signed-off-by: Noam Dolovich <noam.tzvi.dolovich@gmail.com>
    Change-Id: Id34b5ada4704df0e43ff9ea33ad47d631af9ba74

--- pkg/encoding/yaml/pkg.go
+++ pkg/encoding/yaml/pkg.go
@@ -70,7 +70,7 @@ var p = &pkg.Package{
 		},
 		Result: adt.BoolKind,
 		Func: func(c *pkg.CallCtxt) {
-			b, v := c.Bytes(0), c.Value(1)
+			b, v := c.Bytes(0), c.Top(1)
 			if c.Do() {
 				c.Ret, c.Err = Validate(b, v)
 			}
---
+++ pkg/encoding/yaml/test.cue
@@ -0,0 +1,3 @@
+_#disjunct: {a!: int} | {b!: int}
+t10: yaml.Validate("a: 2", _#disjunct)
+

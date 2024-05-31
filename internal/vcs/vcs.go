d7e6ebbe9b01ea625a7d0dbc6500a5098260343b
Paul Jolly <paul@myitcv.io>
2024-05-31 09:11:03 +0100

    internal/vcs: refactor to prepare for LICENSE file inclusion
    Including LICENSE files from the root of a VCS repo in the case where no
    LICENSE file is present in a subdirectory module requires that we
    perform all operations from the module root.
    
    We prepare for this by making ListFiles and Status take arguments
    instead of implicitly operating on the module subdirectory. This allows
    us to perform separate checks on the root LICENSE file if one exists.
    Indeed the concept of a VCS implementation keeping track of a subdir is
    eliminated entirely.
    
    We also fix a bug in passing where git ListFiles implementation did not
    correctly handle an empty result, i.e. the situation where no files
    match args.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Id669490a447a4f07ced611f3323868b4519b1919

--- internal/vcs/vcs.go
+++ internal/vcs/vcs.go
@@ -31,17 +31,24 @@ type VCS interface {
 	// the VCS (e.g. the directory containing .git).
 	Root() string
 
-	// ListFiles returns a list of all the files tracked by the VCS
-	// under the given directory, relative to that directory, as
-	// filepaths, in lexical order. It does not include directory
-	// names.
+	// ListFiles returns a list of files tracked by VCS, rooted at dir. The
+	// optional args determine what should be listed. If no args are provided,
+	// then all of the files under VCS control under dir are returned. It us up
+	// to the caller to ensure that dir and args are contained by the VCS root
+	// (some VCS implementations might return an error if an arg is provided
+	// that is outside the VCS root). Filepaths are relative to dir and returned
+	// in lexical order. They do not include directory names.
 	//
-	// The directory should be within the VCS root.
-	ListFiles(ctx context.Context, dir string) ([]string, error)
+	// Note that ListFiles is generally silent in the case an arg is provided
+	// that does correspond to a VCS-controlled file. For example, calling
+	// with an arg of "BANANA" where no such file is controlled by VCS will
+	// result in no filepaths being returned.
+	ListFiles(ctx context.Context, dir string, args ...string) ([]string, error)
 
-	// Status returns the current state of the repository holding
-	// the given directory.
-	Status(ctx context.Context) (Status, error)
+	// Status returns the current state of the repository holding the given args
+	// (files or directories). If args is not provided it implies the state of
+	// the VCS repository in its entirety, including untracked files.
+	Status(ctx context.Context, args ...string) (Status, error)
 }
 
 // Status is the current state of a local repository.
@@ -57,10 +64,7 @@ var vcsTypes = map[string]func(dir string) (VCS, error){
 
 // New returns a new VCS value representing the
 // version control system of the given type that
-// controls the given directory.
-//
-// Status checks apply only to the given directory; other
-// directories controlled by the VCS will not be considered.
+// controls the given directory dir.
 //
 // It returns an error if a VCS of the specified type
 // cannot be found.

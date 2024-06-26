d71724ae34c35010cf9584aae6252f129c65a24c
Marcel van Lohuizen <mpvl@gmail.com>
2024-06-26 09:51:02 +0200

    cue/interpreter/embed: don't allow hidden files in glob
    This could later be allowed as an option.
    
    This is a security feature. We also disallow hidden files on
    Windows for the same reason, which is a bit more involved.
    
    Note that this results in potentially slightly different
    behavior under Windows and Unix. This is already the case.
    For instance, the set of valid filenames is different on
    the different supported OSes. So we accept this discrepancy
    in favor of added security.
    
    Verified that without adding the new logic, the hidden file
    that was added to embed.txtar gets included in the output.
    
    Issue #2031
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: Iacff803f4c388d1f2792665ed5adb32f68f00ffa

---
+++ cue/interpreter/embed/hidden_unix.go
@@ -0,0 +1,26 @@
+// Copyright 2024 CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build unix
+
+package embed
+
+import "path"
+
+// isHidden checks if a file is hidden on Windows. We do not return an error
+// if the file does not exist and will check that elsewhere.
+func isHidden(file string) bool {
+	base := path.Base(file) // guaranteed to be non-empty
+	return base[0] == '.'
+}
---
+++ cue/interpreter/embed/hidden_windows.go
@@ -0,0 +1,46 @@
+// Copyright 2024 CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build windows
+
+package embed
+
+import (
+	"path/filepath"
+	"syscall"
+)
+
+// isHidden checks if a file is hidden on Windows. We do not return an error
+// if the file does not exist and will check that elsewhere.
+func isHidden(path string) bool {
+	path = filepath.FromSlash(path)
+
+	// To be safe, we also include dot files on Windows.
+	base := filepath.Base(path)
+	if base[0] == '.' {
+		return true
+	}
+
+	pointer, err := syscall.UTF16PtrFromString(path)
+	if err != nil {
+		return false
+	}
+
+	attributes, err := syscall.GetFileAttributes(pointer)
+	if err != nil {
+		return false
+	}
+
+	return attributes&syscall.FILE_ATTRIBUTE_HIDDEN != 0
+}

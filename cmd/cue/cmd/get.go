514e8191601b221eda7246a02f971ec2fdc68084
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-07-26 09:26:44 +0100

    cmd/cue/get: point help text at CUE-specific cmds
    This updates the "cue help get" text to make it clear that "cue get"
    doesn't interact with CUE dependencies. For CUE modules, it updates two
    help texts by pointing the reader onwards to "cue help modules", and
    from there to "cue help mod".
    
    Also: fix a typo (which was the initial motivation for this edit)
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: I2a980be042fe544c95a71c8b46a20616cb7c4bd6

--- cmd/cue/cmd/get.go
+++ cmd/cue/cmd/get.go
@@ -23,16 +23,19 @@ import (
 func newGetCmd(c *Command) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "get <language> [packages]",
-		Short: "add dependencies to the current module",
-		Long: `Get downloads packages or modules for CUE or another language
+		Short: "add non-CUE dependencies to the current module",
+		Long: `Get downloads packages or modules for non-CUE languages
 to include them in the module's pkg directory.
 
 Get requires an additional language field to determine for which
-language definitions should be fetched. If get fetches definitions
-for a language other than CUE, the definitions are extracted from
-the source of the respective language and stored.
-The specifics on how dependencies are fechted and converted vary
+language definitions should be fetched. Definitions are extracted
+from the source of the respective language and stored.
+The specifics on how dependencies are fetched and converted vary
 per language and are documented in the respective subcommands.
+
+For information on native CUE modules:
+
+    cue help modules
 `,
 		RunE: mkRunE(c, func(cmd *Command, args []string) error {
 			stderr := cmd.Stderr()

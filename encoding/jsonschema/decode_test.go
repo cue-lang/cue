dc8877542422310900767c24f4bb941b02835bce
Daniel Martí <mvdan@mvdan.cc>
2024-08-01 12:44:06 +0100

    encoding/jsonschema: port txtar tests to cuetxtar
    This saves about 60 lines of code to deal with loading the txtar files
    as well as updating output files following CUE_UPDATE and so on,
    but most importantly, it unlocks the ability to test with evalv3 too.
    
    For #3351.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I3f9a368391c4510d2f5a389da3ac7babb376e036

--- encoding/jsonschema/decode_test.go
+++ encoding/jsonschema/decode_test.go
@@ -12,19 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package jsonschema
+package jsonschema_test
 
 import (
 	"bytes"
-	"io/fs"
-	"os"
 	"path"
-	"path/filepath"
-	"strings"
 	"testing"
 
-	"github.com/go-quicktest/qt"
-	"github.com/google/go-cmp/cmp"
 	"golang.org/x/tools/txtar"
 
 	"cuelang.org/go/cue"
@@ -34,9 +28,10 @@ import (
 	"cuelang.org/go/cue/format"
 	"cuelang.org/go/cue/token"
 	"cuelang.org/go/encoding/json"
+	"cuelang.org/go/encoding/jsonschema"
 	"cuelang.org/go/encoding/yaml"
 	"cuelang.org/go/internal/astinternal"
-	"cuelang.org/go/internal/cuetest"
+	"cuelang.org/go/internal/cuetxtar"
 	_ "cuelang.org/go/pkg"
 )
 
@@ -45,121 +40,66 @@ import (
 //
 // Set CUE_UPDATE=1 to update test files with the corresponding output.
 func TestDecode(t *testing.T) {
-	err := filepath.WalkDir("testdata", func(fullpath string, entry fs.DirEntry, err error) error {
-		if err != nil {
-			return err
+	test := cuetxtar.TxTarTest{
+		Root: "./testdata",
+		Name: "decode",
+	}
+	test.Run(t, func(t *cuetxtar.Test) {
+		cfg := &jsonschema.Config{ID: ""}
+
+		if t.HasTag("openapi") {
+			cfg.Root = "#/components/schemas/"
+			cfg.Map = func(p token.Pos, a []string) ([]ast.Label, error) {
+				// Just for testing: does not validate the path.
+				return []ast.Label{ast.NewIdent("#" + a[len(a)-1])}, nil
+			}
 		}
-		if !strings.HasSuffix(fullpath, ".txtar") {
-			return nil
-		}
 
-		t.Run(fullpath, func(t *testing.T) {
-			a, err := txtar.ParseFile(fullpath)
-			if err != nil {
-				t.Fatal(err)
-			}
+		ctx := t.Context()
+		var v cue.Value
 
-			cfg := &Config{ID: fullpath}
-
-			if bytes.Contains(a.Comment, []byte("openapi")) {
-				cfg.Root = "#/components/schemas/"
-				cfg.Map = func(p token.Pos, a []string) ([]ast.Label, error) {
-					// Just for testing: does not validate the path.
-					return []ast.Label{ast.NewIdent("#" + a[len(a)-1])}, nil
-				}
-			}
-
-			ctx := cuecontext.New()
-			var v cue.Value
-			var out, errout []byte
-			outIndex := -1
-			errIndex := -1
-
-			for i, f := range a.Files {
-				switch path.Ext(f.Name) {
-				case ".json":
-					expr, err := json.Extract(f.Name, f.Data)
-					if err != nil {
-						t.Fatal(err)
-					}
-					v = ctx.BuildExpr(expr)
-				case ".yaml":
-					file, err := yaml.Extract(f.Name, f.Data)
-					if err != nil {
-						t.Fatal(err)
-					}
-					v = ctx.BuildFile(file)
-				case ".cue":
-					out = f.Data
-					outIndex = i
-				case ".err":
-					errout = f.Data
-					errIndex = i
-				}
-			}
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			updated := false
-
-			expr, err := Extract(v, cfg)
-			if err != nil {
-				got := []byte(errors.Details(err, nil))
-
-				got = bytes.TrimSpace(got)
-				errout = bytes.TrimSpace(errout)
-
-				switch {
-				case !cmp.Equal(errout, got):
-					if cuetest.UpdateGoldenFiles {
-						a.Files[errIndex].Data = got
-						updated = true
-						break
-					}
-					t.Error(cmp.Diff(string(got), string(errout)))
-				}
-			}
-
-			if expr != nil {
-				b, err := format.Node(expr, format.Simplify())
-				if err != nil {
-					t.Fatal(errors.Details(err, nil))
-				}
-
-				// verify the generated CUE.
-				if !bytes.Contains(a.Comment, []byte("#noverify")) {
-					v := ctx.CompileBytes(b, cue.Filename(fullpath))
-					if err := v.Err(); err != nil {
-						t.Fatal(errors.Details(err, nil))
-					}
-				}
-
-				b = bytes.TrimSpace(b)
-				out = bytes.TrimSpace(out)
-
-				switch {
-				case !cmp.Equal(b, out):
-					if cuetest.UpdateGoldenFiles {
-						updated = true
-						a.Files[outIndex].Data = b
-						break
-					}
-					t.Error(cmp.Diff(string(out), string(b)))
-				}
-			}
-
-			if updated {
-				b := txtar.Format(a)
-				err = os.WriteFile(fullpath, b, 0644)
+		for _, f := range t.Archive.Files {
+			switch path.Ext(f.Name) {
+			case ".json":
+				expr, err := json.Extract(f.Name, f.Data)
 				if err != nil {
 					t.Fatal(err)
 				}
+				v = ctx.BuildExpr(expr)
+			case ".yaml":
+				file, err := yaml.Extract(f.Name, f.Data)
+				if err != nil {
+					t.Fatal(err)
+				}
+				v = ctx.BuildFile(file)
 			}
-		})
-		return nil
+		}
+
+		expr, err := jsonschema.Extract(v, cfg)
+		if err != nil {
+			got := []byte(errors.Details(err, nil))
+			got = append(bytes.TrimSpace(got), '\n')
+			t.Writer("err").Write(got)
+		}
+
+		if expr != nil {
+			b, err := format.Node(expr, format.Simplify())
+			if err != nil {
+				t.Fatal(errors.Details(err, nil))
+			}
+
+			// verify the generated CUE.
+			if !t.HasTag("noverify") {
+				v := ctx.CompileBytes(b, cue.Filename("generated.cue"))
+				if err := v.Err(); err != nil {
+					t.Fatal(errors.Details(err, nil))
+				}
+			}
+
+			b = append(bytes.TrimSpace(b), '\n')
+			t.Writer("cue").Write(b)
+		}
 	})
-	qt.Assert(t, qt.IsNil(err))
 }
 
 func TestX(t *testing.T) {
@@ -190,8 +130,8 @@ func TestX(t *testing.T) {
 		}
 	}
 
-	cfg := &Config{ID: "test"}
-	expr, err := Extract(v, cfg)
+	cfg := &jsonschema.Config{ID: "test"}
+	expr, err := jsonschema.Extract(v, cfg)
 	if err != nil {
 		t.Fatal(err)
 	}

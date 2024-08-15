ea43f81b63ca2c22e54adc2b1e3fdc67f521c2d3
Daniel Martí <mvdan@mvdan.cc>
2024-08-15 13:59:10 +0100

    all: remove two deprecated APIs
    semver.Max was an internal deprecated API that was only used in tests.
    It doesn't need to stay around for even a single release.
    
    modfile.LatestKnownSchemaVersion was marked for deletion in v0.11;
    now that we've tagged v0.10.0, it can go as well.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Ie5f50d848e56a5a7876fe91617bad1f1742160ab
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1199545
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    Reviewed-by: Matthew Sackman <matthew@cue.works>
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>

--- internal/mod/semver/semver.go
+++ internal/mod/semver/semver.go
@@ -140,20 +140,6 @@ func Compare(v, w string) int {
 	return comparePrerelease(pv.prerelease, pw.prerelease)
 }
 
-// Max canonicalizes its arguments and then returns the version string
-// that compares greater.
-//
-// Deprecated: use Compare instead. In most cases, returning a canonicalized
-// version is not expected or desired.
-func Max(v, w string) string {
-	v = Canonical(v)
-	w = Canonical(w)
-	if Compare(v, w) > 0 {
-		return v
-	}
-	return w
-}
-
 // Sort sorts a list of semantic version strings.
 func Sort(list []string) {
 	slices.SortFunc(list, func(a, b string) int {

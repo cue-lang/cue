e525f29af6fb0c4a53e99443f00f207b40c49f18
Daniel Martí <mvdan@mvdan.cc>
2024-08-22 13:10:53 +0100

    pkg/uuid: use google/uuid.Validate in Valid
    So that any UUID string accepted by uuid.Parse also validates correctly
    with uuid.Valid, for the sake of consistency.
    
    Note that this means that uuid.Valid error messages no longer contain
    the input string, but that's for the best, as that often led
    to rendered errors repeating themselves:
    
        valid.err.a: invalid value "052ef62d_7223_58b6_a551_c1deee46d401" (does not satisfy uuid.Valid): invalid UUID "052ef62d_7223_58b6_a551_c1deee46d401":
            ./in.cue:16:5
    
    The new rendered error is better:
    
        valid.err.a: invalid value "052ef62d_7223_58b6_a551_c1deee46d401" (does not satisfy uuid.Valid): invalid UUID format:
            ./in.cue:16:5
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Ib7b44247b1ff2efe3fe90b916211b268ad8d856a
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1199881
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Reviewed-by: Matthew Sackman <matthew@cue.works>
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>

--- pkg/uuid/uuid.go
+++ pkg/uuid/uuid.go
@@ -18,26 +18,14 @@
 package uuid
 
 import (
-	"fmt"
 	"math/big"
-	"regexp"
-	"sync"
 
 	"github.com/google/uuid"
 )
 
-var valid = sync.OnceValue(func() *regexp.Regexp {
-	return regexp.MustCompile("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$")
-})
-
-// TODO(mvdan): should Valid not use [uuid.Validate]?
-
-// Valid can be used to define a valid Valid.
+// Valid ensures that s is a valid UUID which would be accepted by Parse.
 func Valid(s string) error {
-	if !valid().MatchString(string(s)) {
-		return fmt.Errorf("invalid UUID %q", s)
-	}
-	return nil
+	return uuid.Validate(s)
 }
 
 // Parse decodes s into a UUID or returns an error. Both the standard UUID forms
@@ -49,6 +37,8 @@ func Parse(s string) (string, error) {
 	x, err := uuid.Parse(s)
 	return string(x.String()), err
 }
+
+// TODO(mvdan): what is ToString meant to do? it appears like a no-op?
 
 // String represents a 128-bit UUID value as a string.
 func ToString(x string) string {

90d1dd4272a02877be40251095ae0305fc907d98
Daniel Martí <mvdan@mvdan.cc>
2024-08-24 10:55:19 +0100

    cmd/cue: add mod registry example to help text
    A user on Slack was getting confused about how to use this command.
    Add a short example to start the registry and then use it.
    
    While here, don't start the help text with an empty line.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I66815a4791232fc27a6ccba0988cf037ef6dae28

--- cmd/cue/cmd/modregistry.go
+++ cmd/cue/cmd/modregistry.go
@@ -45,8 +45,16 @@ This command starts an OCI-compliant server that stores all its
 contents in memory. It can serve as a scratch CUE modules registry
 for use in testing.
 
+For example, start a local registry with:
+
+	cue mod registry localhost:8080
+
+and point CUE_REGISTRY to it to publish a module version:
+
+	CUE_REGISTRY=localhost:8080 cue mod publish v0.0.1
+
 Note: this command might be removed or changed significantly in the future.
-`,
+`[1:],
 		RunE: mkRunE(c, runModRegistry),
 		Args: cobra.MaximumNArgs(1),
 	}

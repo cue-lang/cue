e0f798e0477093871b76780ead1a3547ab318ff2
Daniel Martí <mvdan@mvdan.cc>
2024-09-17 16:19:02 +0100

    pkg: drop cue/build and pkg/internal from gen.go
    The code just needs to parse CUE files to extract the declarations.
    There is no need to build instances and values to extract the syntax;
    we can parse the syntax directly.
    
    This gives us better results in any case,
    as the resulting syntax directly follows the source.
    For example, the original source uses octal integer literals
    and uses some inline structs for readability, and we keep those now.
    
    As a bonus, as we no longer use the CUE evaluator, `go generate`
    drops from ~0.35s to ~0.25s on my laptop.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Id6c03c49421c4b2ead467b523bcd4f25b41c002b

--- pkg/tool/http/pkg.go
+++ pkg/tool/http/pkg.go
@@ -65,18 +65,10 @@ var _ = adt.TopKind // in case the adt package isn't used
 var p = &pkg.Package{
 	Native: []*pkg.Builtin{},
 	CUE: `{
-	Get: Do & {
-		method: "GET"
-	}
-	Post: Do & {
-		method: "POST"
-	}
-	Put: Do & {
-		method: "PUT"
-	}
-	Delete: Do & {
-		method: "DELETE"
-	}
+	Get: Do & {method: "GET"}
+	Post: Do & {method: "POST"}
+	Put: Do & {method: "PUT"}
+	Delete: Do & {method: "DELETE"}
 	Do: {
 		$id:    *"tool/http.Do" | "http"
 		method: string
@@ -87,23 +79,15 @@ var p = &pkg.Package{
 		}
 		request: {
 			body?: bytes | string
-			header: {
-				[string]: string | [...string]
-			}
-			trailer: {
-				[string]: string | [...string]
-			}
+			header: [string]: string | [...string]
+			trailer: [string]: string | [...string]
 		}
 		response: {
 			status:     string
 			statusCode: int
 			body:       *bytes | string
-			header: {
-				[string]: string | [...string]
-			}
-			trailer: {
-				[string]: string | [...string]
-			}
+			header: [string]: string | [...string]
+			trailer: [string]: string | [...string]
 		}
 	}
 }`,

297aead9d0e42208e03c2a05f1d2b0a785cf86d0
Daniel Martí <mvdan@mvdan.cc>
2024-09-20 10:40:56 +0100

    cmd/cue/cmd: use the new goproxytest.NewTestServer
    Which avoids chatty tests logging directly to stderr:
    
        $ go test
        go proxy: no archive externalmod.test v1.0.0
        go proxy: no archive externalmod.test/imports/root v1.0.0
        go proxy: no archive externalmod.test v1.0.0
        PASS
        ok  	cuelang.org/go/cmd/cue/cmd	3.314s
    
    Now these log lines are part of TestScript, and shown only with -v:
    
        $ go test -run TestScript/get_go_non_local -v
        [...]
        === NAME  TestScript
            proxy.go:225: go proxy: no archive externalmod.test v1.0.0
            proxy.go:225: go proxy: no archive externalmod.test v1.0.0
            proxy.go:225: go proxy: no archive externalmod.test/imports/root v1.0.0
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I073b9b284694900634f9bc315813c6579bafa03d

--- cmd/cue/cmd/script_test.go
+++ cmd/cue/cmd/script_test.go
@@ -96,11 +96,7 @@ func TestLatest(t *testing.T) {
 }
 
 func TestScript(t *testing.T) {
-	srv, err := goproxytest.NewServer(filepath.Join("testdata", "mod"), "")
-	if err != nil {
-		t.Fatalf("cannot start proxy: %v", err)
-	}
-	t.Cleanup(srv.Close)
+	srv := goproxytest.NewTestServer(t, filepath.Join("testdata", "mod"), "")
 	p := testscript.Params{
 		Dir:                 filepath.Join("testdata", "script"),
 		UpdateScripts:       cuetest.UpdateGoldenFiles,

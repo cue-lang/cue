888d1d079c58b8063c2108b420155fa7a37f422c
Marcel van Lohuizen <mpvl@gmail.com>
2024-09-27 13:55:00 +0200

    internal/core/adt: bail on empty node
    Some resolvers return 'emptyNode' as a value upon error,
    others nil. Treat both cases equally.
    
    Issue #2850
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I295cc58cd3836d95ecc4a459d6a56f5ac1393b66

--- internal/core/adt/tasks.go
+++ internal/core/adt/tasks.go
@@ -97,7 +97,7 @@ func processResolver(ctx *OpContext, t *task, mode runMode) {
 	// would be a pretty significant rework, though.
 
 	arc := r.resolve(ctx, oldOnly(0))
-	if arc == nil {
+	if arc == nil || arc == emptyNode {
 		// TODO: yield instead?
 		return
 	}

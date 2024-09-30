9b5cf3be5b49fed68ba925f4d8791e778365e020
Roger Peppe <rogpeppe@gmail.com>
2024-09-30 15:59:03 +0100

    internal/core/adt: export ErrorGraphs as a method not a field
    Similar to the previous CL in the stack, it makes sense
    to export this read-only data via a method rather than a field.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: Ia53d45bee54c9ee0a3877d917c06cdf31f6d3285

--- internal/core/adt/eval_test.go
+++ internal/core/adt/eval_test.go
@@ -179,7 +179,7 @@ func runEvalTest(t *cuetxtar.Test, version internal.EvaluatorVersion, flags cued
 	v.Finalize(ctx)
 
 	// Print discrepancies in dependencies.
-	m := ctx.ErrorGraphs
+	m := ctx.ErrorGraphs()
 	name := t.T.Name()[len("TestEvalAlpha/"):]
 	expectErrs := skipDebugDepErrors[name]
 
@@ -304,7 +304,7 @@ language: version: "v0.9.0"
 
 	out := debug.NodeString(r, v, nil)
 	if adt.OpenGraphs {
-		for p, g := range ctx.ErrorGraphs {
+		for p, g := range ctx.ErrorGraphs() {
 			path := filepath.Join(".debug/TestX", p)
 			adt.OpenNodeGraph("TestX", path, in, out, g)
 		}

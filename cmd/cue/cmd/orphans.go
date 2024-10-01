4a308b5981809ccd5eb983bb385a4ebdcef65045
haoqixu <hq.xu0o0@gmail.com>
2024-10-01 22:46:59 +0800

    cmd/cue: skip over null when importing with --path
    YAML format allows multiple documents to be embedded in a single file,
    and it is common that a multiple documents file contains empty
    documents whose value is null.
    
    This change skips over null when it encounters an error during
    importing with the --path flag.
    
    Fixes #3464
    
    Change-Id: I5f1756a109938d405682ece2f67437e7b831a6aa
    Signed-off-by: haoqixu <hq.xu0o0@gmail.com>

--- cmd/cue/cmd/orphans.go
+++ cmd/cue/cmd/orphans.go
@@ -189,6 +189,8 @@ func placeOrphans(b *buildPlan, d *encoding.Decoder, pkg string, objs ...*ast.Fi
 	filename := d.Filename()
 
 	index := newIndex()
+
+OBJS_LOOP:
 	for i, file := range objs {
 		if i == 0 {
 			astutil.CopyMeta(f, file)
@@ -200,6 +202,10 @@ func placeOrphans(b *buildPlan, d *encoding.Decoder, pkg string, objs ...*ast.Fi
 
 		switch {
 		case len(b.path) > 0:
+			var isNull bool
+			if lit, ok := expr.(*ast.BasicLit); ok && lit.Value == "null" {
+				isNull = true
+			}
 			expr := expr
 			if b.useContext {
 				expr = ast.NewStruct(
@@ -245,6 +251,9 @@ func placeOrphans(b *buildPlan, d *encoding.Decoder, pkg string, objs ...*ast.Fi
 					default:
 						var arg interface{} = l
 						if err := l.Err(); err != nil {
+							if isNull {
+								continue OBJS_LOOP
+							}
 							arg = err
 						}
 						return nil, fmt.Errorf(

f2ad5996330d8221da2a37823ae043c33b36565f
Matthew Sackman <matthew@cue.works>
2024-10-09 14:47:41 +0100

    internal/core: sort graph topologically
    Using the Strongly Connected Components, and the cycles within, flatten
    the graph into a single slice of nodes. This is fully deterministic and
    associative: it doesn't matter in what order the nodes or edges with-in
    the graph are created. This means that whenever two structs are unified
    together, the order of fields in the output won't depend on the order of
    the arguments to unification.
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I614d6b80029acf65240760e65e1330e90a41b163

--- internal/core/toposort/graph.go
+++ internal/core/toposort/graph.go
@@ -15,11 +15,16 @@
 package toposort
 
 import (
+	"cmp"
+	"math"
+	"slices"
+
 	"cuelang.org/go/internal/core/adt"
 )
 
 const (
-	NodeUnsorted = -1
+	NodeUnsorted     = -1
+	NodeInCurrentScc = -2
 )
 
 type Graph struct {
@@ -41,6 +46,12 @@ type Node struct {
 
 func (n *Node) IsSorted() bool {
 	return n.position >= 0
+}
+
+func (n *Node) Name(index adt.StringIndexer) string {
+	// TODO: two different fields like "#foo" and #foo, can have the same raw
+	// string
+	return n.Feature.RawString(index)
 }
 
 type Nodes []*Node
@@ -105,4 +116,257 @@ func (builder *GraphBuilder) Build() *Graph {
 		nodes = append(nodes, node)
 	}
 	return &Graph{nodes: nodes}
+}
+
+type indexComparison struct{ adt.StringIndexer }
+
+func (index *indexComparison) compareNodeByName(a, b *Node) int {
+	return cmp.Compare(a.Name(index), b.Name(index))
+}
+
+func (index *indexComparison) compareNodesByNames(a, b Nodes) int {
+	lim := min(len(a), len(b))
+	for i := 0; i < lim; i++ {
+		if comparison := index.compareNodeByName(a[i], b[i]); comparison != 0 {
+			return comparison
+		}
+	}
+	return cmp.Compare(len(a), len(b))
+}
+
+func (index *indexComparison) compareCyclesByNames(a, b *Cycle) int {
+	return index.compareNodesByNames(a.Nodes, b.Nodes)
+}
+
+func (index *indexComparison) compareComponentsByNodes(a, b *StronglyConnectedComponent) int {
+	return index.compareNodesByNames(a.Nodes, b.Nodes)
+}
+
+func chooseCycleEntryNode(cycle *Cycle) (entryNode *Node, enabledSince, brokenEdgeCount int) {
+	enabledSince = math.MaxInt
+
+	for _, cycleNode := range cycle.Nodes {
+		if cycleNode.IsSorted() {
+			// this node is already in the sorted result
+			continue
+		}
+	NextNodeIncoming:
+		for _, incoming := range cycleNode.Incoming {
+			position := incoming.position
+
+			if position < 0 {
+				// this predecessor node has not yet been added to the sorted
+				// result.
+				for _, cycleNode1 := range cycle.Nodes {
+					// ignore this predecessor node if it is part of this cycle.
+					if cycleNode1 == incoming {
+						continue NextNodeIncoming
+					}
+				}
+				brokenEdgeCount++
+				continue NextNodeIncoming
+			}
+
+			// this predecessor node must already be in the sorted output.
+			if position < enabledSince {
+				enabledSince = position
+				entryNode = cycleNode
+			}
+		}
+	}
+	return entryNode, enabledSince, brokenEdgeCount
+}
+
+func chooseCycle(indexCmp *indexComparison, unusedCycles []*Cycle) *Cycle {
+	chosenCycleIdx := -1
+	chosenCycleBrokenEdgeCount := math.MaxInt
+	chosenCycleEnabledSince := math.MaxInt
+	var chosenCycleEntryNode *Node
+
+	for i, cycle := range unusedCycles {
+		if cycle == nil {
+			continue
+		}
+		debug("cycle %d: %v\n", i, cycle)
+		entryNode, enabledSince, brokenEdgeCount := chooseCycleEntryNode(cycle)
+
+		if entryNode == nil {
+			entryNode = slices.MinFunc(
+				cycle.Nodes, indexCmp.compareNodeByName)
+		}
+
+		debug("cycle %v; edgeCount %v; enabledSince %v; entryNode %v\n",
+			cycle, brokenEdgeCount, enabledSince,
+			entryNode.Name(indexCmp))
+
+		cycleIsBetter := chosenCycleIdx == -1
+		// this is written out long-form for ease of readability
+		switch {
+		case cycleIsBetter:
+			// noop
+		case brokenEdgeCount < chosenCycleBrokenEdgeCount:
+			cycleIsBetter = true
+		case brokenEdgeCount > chosenCycleBrokenEdgeCount:
+			// noop - only continue if ==
+
+		case enabledSince < chosenCycleEnabledSince:
+			cycleIsBetter = true
+		case enabledSince > chosenCycleEnabledSince:
+			// noop - only continue if ==
+
+		case entryNode.Name(indexCmp) < chosenCycleEntryNode.Name(indexCmp):
+			cycleIsBetter = true
+		case entryNode == chosenCycleEntryNode:
+			cycleIsBetter =
+				indexCmp.compareCyclesByNames(cycle, unusedCycles[chosenCycleIdx]) < 0
+		}
+
+		if cycleIsBetter {
+			chosenCycleIdx = i
+			chosenCycleBrokenEdgeCount = brokenEdgeCount
+			chosenCycleEnabledSince = enabledSince
+			chosenCycleEntryNode = entryNode
+		}
+	}
+
+	if chosenCycleEntryNode == nil {
+		return nil
+	}
+
+	debug("Chose cycle: %v; entering at node: %s\n",
+		unusedCycles[chosenCycleIdx], chosenCycleEntryNode.Name(indexCmp))
+	cycle := unusedCycles[chosenCycleIdx]
+	unusedCycles[chosenCycleIdx] = nil
+	cycle.RotateToStartAt(chosenCycleEntryNode)
+	return cycle
+}
+
+// Sort the features of the graph into a single slice.
+//
+// As far as possible, a topological sort is used.
+//
+// Whenever there is choice as to which feature should occur next, a
+// lexicographical comparison is done, and minimum feature chosen.
+//
+// Whenever progress cannot be made due to needing to enter into
+// cycles, the cycle to enter into, and the node of that cycle with
+// which to start, is selected based on:
+//
+//  1. minimising the number of incoming edges that are violated
+//  2. chosing a node which was reachable as early as possible
+//  3. chosing a node with a smaller feature name (lexicographical)
+func (self *Graph) Sort(index adt.StringIndexer) []adt.Feature {
+	indexCmp := &indexComparison{index}
+
+	nodesSorted := make(Nodes, 0, len(self.nodes))
+
+	scc := self.StronglyConnectedComponents()
+	var sccReady []*StronglyConnectedComponent
+	for _, component := range scc {
+		component.visited = false
+		slices.SortFunc(component.Nodes, indexCmp.compareNodeByName)
+		if len(component.Incoming) == 0 {
+			sccReady = append(sccReady, component)
+		}
+	}
+	slices.SortFunc(sccReady, indexCmp.compareComponentsByNodes)
+
+	sccVisitedCount := 0
+	for sccVisitedCount != len(scc) {
+		sccCurrent := sccReady[0]
+		sccReady = sccReady[1:]
+		if sccCurrent.visited {
+			continue
+		}
+		sccCurrent.visited = true
+		sccVisitedCount++
+		debug("scc current: %p %v\n", sccCurrent, sccCurrent)
+		var cyclesCurrent []*Cycle
+
+		var nodesReady Nodes
+	NextNode:
+		for _, node := range sccCurrent.Nodes {
+			node.position = NodeInCurrentScc
+			for _, required := range node.Incoming {
+				if !required.IsSorted() {
+					continue NextNode
+				}
+			}
+			nodesReady = append(nodesReady, node)
+		}
+		slices.SortFunc(nodesReady, indexCmp.compareNodeByName)
+
+		requiredLen := len(nodesSorted) + len(sccCurrent.Nodes)
+		for requiredLen != len(nodesSorted) {
+			if len(nodesReady) == 0 {
+				debug("Stuck after: %v\n", nodesSorted)
+				if cyclesCurrent == nil {
+					cyclesCurrent = sccCurrent.ElementaryCycles()
+					debug("cycles current: %v\n", cyclesCurrent)
+				}
+				cycle := chooseCycle(indexCmp, cyclesCurrent)
+				if cycle == nil {
+					panic("No cycle found.")
+				}
+				nodesSorted, nodesReady = appendNodes(
+					indexCmp, nodesSorted, cycle.Nodes, nodesReady)
+
+			} else {
+				nodesSorted, nodesReady = appendNodes(
+					indexCmp, nodesSorted, nodesReady[:1], nodesReady[1:])
+			}
+		}
+
+		sccReadyNeedsSorting := false
+	SccNextOutgoing:
+		for _, next := range sccCurrent.Outgoing {
+			for _, required := range next.Incoming {
+				if !required.visited {
+					continue SccNextOutgoing
+				}
+			}
+			sccReady = append(sccReady, next)
+			sccReadyNeedsSorting = true
+		}
+		if sccReadyNeedsSorting {
+			slices.SortFunc(sccReady, indexCmp.compareComponentsByNodes)
+		}
+	}
+
+	return nodesSorted.Features()
+}
+
+func appendNodes(indexCmp *indexComparison, nodesSorted, nodesReady, nodesEnabled Nodes) (nodesSortedOut, nodesEnabledOut Nodes) {
+	nodesReadyNeedsSorting := false
+	for _, node := range nodesReady {
+		if node.IsSorted() {
+			continue
+		}
+		node.position = len(nodesSorted)
+		nodesSorted = append(nodesSorted, node)
+
+	NextOutgoing:
+		for _, next := range node.Outgoing {
+			if next.position != NodeInCurrentScc {
+				continue
+			}
+			for _, required := range next.Incoming {
+				if !required.IsSorted() {
+					continue NextOutgoing
+				}
+			}
+			debug("After %v, found new ready: %s\n",
+				nodesSorted, next.Name(indexCmp))
+			nodesEnabled = append(nodesEnabled, next)
+			nodesReadyNeedsSorting = true
+		}
+	}
+	if nodesReadyNeedsSorting {
+		slices.SortFunc(nodesEnabled, indexCmp.compareNodeByName)
+	}
+	return nodesSorted, nodesEnabled
+}
+
+func debug(formatting string, args ...any) {
+	//	fmt.Printf(formatting, args...)
 }

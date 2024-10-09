05c9a53738b6eb93cdd1c21cdf6055333abeed39
Matthew Sackman <matthew@cue.works>
2024-10-09 14:21:03 +0100

    internal/core: build graphs of Features
    We construct a graph where each node is an adt.Feature, and the edges
    indicate the desired ordering of nodes in the output. The graph is
    directed, and can model cycles. Cycles are caused by the unification of
    two or more structs that share at least two fields, but in opposite
    orders. E.g.
    
    ```
    x: {
      a: _
      b: _
      c: _
    }
    
    y: {
      c: _
      a: _
    }
    
    z: x & y
    ```
    
    From the graph, we can construct the Strongly Connected Components. Each
    component contains one or more nodes (every node in the graph exists in
    exactly one component). Nodes i and j are grouped into the same component
    when there is a path from i to j and also a path from j to i. Thus a
    component contains more than one node if-and-only-if there are cycles
    involving every node in the component.
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I5082c0273e883b69f854943994542067f35e8afc

---
+++ internal/core/toposort/graph.go
@@ -0,0 +1,103 @@
+// Copyright 2024 CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package toposort
+
+import (
+	"cuelang.org/go/internal/core/adt"
+)
+
+const (
+	NODE_UNSORTED = -1
+)
+
+type Graph struct {
+	nodes Nodes
+}
+
+type Node struct {
+	Feature      adt.Feature
+	Outgoing     Nodes
+	Incoming     Nodes
+	sccNodeState *sccNodeState
+	position     int
+}
+
+func (n *Node) IsSorted() bool {
+	return n.position >= 0
+}
+
+type Nodes []*Node
+
+func (nodes Nodes) Features() []adt.Feature {
+	features := make([]adt.Feature, len(nodes))
+	for idx, node := range nodes {
+		features[idx] = node.Feature
+	}
+	return features
+}
+
+type edge struct {
+	from adt.Feature
+	to   adt.Feature
+}
+
+type GraphBuilder struct {
+	edgesSet       map[edge]struct{}
+	nodesByFeature map[adt.Feature]*Node
+}
+
+func NewGraphBuilder() *GraphBuilder {
+	return &GraphBuilder{
+		edgesSet:       make(map[edge]struct{}),
+		nodesByFeature: make(map[adt.Feature]*Node),
+	}
+}
+
+// Adds an edge between the two features. Nodes for the features will
+// be created if they don't already exist. This method is idempotent:
+// multiple calls with the same arguments will not create multiple
+// edges, nor error.
+func (builder *GraphBuilder) AddEdge(from, to adt.Feature) {
+	edge := edge{from: from, to: to}
+	if _, found := builder.edgesSet[edge]; found {
+		return
+	}
+
+	builder.edgesSet[edge] = struct{}{}
+	fromNode := builder.EnsureNode(from)
+	toNode := builder.EnsureNode(to)
+	fromNode.Outgoing = append(fromNode.Outgoing, toNode)
+	toNode.Incoming = append(toNode.Incoming, fromNode)
+}
+
+// Ensure that a node for this feature exists. This is necessary for
+// features that are not necessarily connected to any other feature.
+func (builder *GraphBuilder) EnsureNode(feature adt.Feature) *Node {
+	node, found := builder.nodesByFeature[feature]
+	if !found {
+		node = &Node{Feature: feature, position: NODE_UNSORTED}
+		builder.nodesByFeature[feature] = node
+	}
+	return node
+}
+
+func (builder *GraphBuilder) Build() *Graph {
+	nodesByFeature := builder.nodesByFeature
+	nodes := make(Nodes, 0, len(nodesByFeature))
+	for _, node := range nodesByFeature {
+		nodes = append(nodes, node)
+	}
+	return &Graph{nodes: nodes}
+}

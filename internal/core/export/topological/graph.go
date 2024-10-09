a911f17edfd611d0207c1ef290b61bf0ca61fc13
Matthew Sackman <matthew@cue.works>
2024-10-09 14:50:57 +0100

    export/topological: extract sorted features from Vertex
    Given:
    
    ```
    a: {
      z: 4
      y: 3
    }
    b: {
      x: 2
      w: 1
    }
    ```
    
    I want the following behaviour:
    
    ```
    c1: a & b
    c2: b & a
    ```
    
    c1 and c2 should have fields in the same order. I.e. order of arguments
    to unification does not matter (essential for associativity).
    
    ```
    d1: {
      a
      b
    }
    d2: {
      b
      a
    }
    ```
    
    d1 and d2 should not have fields in the same order. I.e. order of
    embedding dictates order of fields in output.
    
    ```
    e1: {
      zz: 14
      a&b
      t: 13
    }
    e2: {
      zz: 14
      b&a
      t: 13
    }
    ```
    
    e1 and e2 should have fields in the same order, both with zz first and t
    last. This means we need to understand the embedding of expressions, We
    also need to be able to add edges between nodes from plain fields and
    nodes from embedded structs.
    
    ```
    f: {
      g: b | a
      h: a | b
    }
    ```
    
    The expansion of the values of g and h should match the order of the
    branches in the disjunctions.
    
    ```
    z: d: _
    z: c: _
    z: {
      b: _
      f: _
    }
    ```
    
    Within z, the output fields should be ordered d, c, b, f.
    
    In order to do this, I need to modify StructInfo so that it contains a
    pointer back to the declaration from which it came. This is sufficient
    to be able to construct the graph correctly, by traversing the vertex
    structs themselves in a topological order.
    
    This change only supports evalv3. Support for evalv2 is added in the
    next commit.
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: Ic47d380f25b74f7633475091c34ff193dc547e01

--- internal/core/export/topological/graph.go
+++ internal/core/export/topological/graph.go
@@ -4,14 +4,13 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package topological
 
 import (
@@ -35,6 +34,7 @@ type Node struct {
 	Feature      adt.Feature
 	Outgoing     Nodes
 	Incoming     Nodes
+	structMeta   *structMeta
 	sccNodeState *sccNodeState
 	ecNodeState  *ecNodeState
 	name         string
@@ -335,5 +335,5 @@ func appendNodes(indexerCmp *indexerComparison, nodesSorted, nodesReady, nodesEn
 }
 
 func debug(formatting string, args ...any) {
-	//	fmt.Printf(formatting, args...)
+	// fmt.Printf(formatting, args...)
 }

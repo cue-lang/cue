d9fda8ceea8c850fd4778d62db8aa33103df93c5
Daniel Martí <mvdan@mvdan.cc>
2024-10-15 13:57:24 +0100

    cue/interpreter: move test helpers to the right file
    So that the test helper funcs are not unused in the default build,
    as they are only used by exe_test.go with -tags=cuewasm.
    
    Updates #3335.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Iae13afa9164afa331801a9df5d76fe7a6dbfc6e7
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1202601
    Reviewed-by: Roger Peppe <rogpeppe@gmail.com>
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>

--- cue/interpreter/wasm/exe_test.go
+++ cue/interpreter/wasm/exe_test.go
@@ -20,11 +20,20 @@
 package wasm_test
 
 import (
+	"io/fs"
 	"os"
 	"path/filepath"
+	stdruntime "runtime"
+	"strings"
 	"testing"
 
 	"cuelang.org/go/cmd/cue/cmd"
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/ast"
+	"cuelang.org/go/cue/build"
+	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/cue/interpreter/wasm"
+	"cuelang.org/go/cue/parser"
 	"cuelang.org/go/internal/cuetest"
 
 	"github.com/rogpeppe/go-internal/gotooltest"
@@ -59,4 +68,82 @@ func TestExe(t *testing.T) {
 		t.Fatal(err)
 	}
 	testscript.Run(t, p)
+}
+
+func copyWasmFiles(t *testing.T, dstDir, srcDir string) {
+	filepath.WalkDir(dstDir, func(path string, d fs.DirEntry, err error) error {
+		if filepath.Ext(path) != ".wasm" {
+			return nil
+		}
+		relPath := must(filepath.Rel(dstDir, path))(t)
+		from := filepath.Join(srcDir, relPath)
+		copyFile(t, path, from)
+		return nil
+	})
+}
+
+func copyFile(t *testing.T, dst, src string) {
+	buf := must(os.ReadFile(src))(t)
+	err := os.WriteFile(dst, buf, 0664)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func check(t *testing.T, dir string, got string) {
+	golden := filepath.Join("testdata", dir) + ".golden"
+
+	if cuetest.UpdateGoldenFiles {
+		os.WriteFile(golden, []byte(got), 0666)
+	}
+
+	want := string(must(os.ReadFile(golden))(t))
+	if got != want {
+		t.Errorf("want %v, got %v", want, got)
+	}
+}
+
+func loadDir(t *testing.T, name string) cue.Value {
+	ctx := cuecontext.New(cuecontext.Interpreter(wasm.New()))
+	bi := dirInstance(t, name)
+	return ctx.BuildInstance(bi)
+}
+
+func dirInstance(t *testing.T, name string) *build.Instance {
+	ctx := build.NewContext(build.ParseFile(loadFile))
+	inst := ctx.NewInstance(name, nil)
+
+	files := must(os.ReadDir(name))(t)
+	for _, f := range files {
+		if strings.HasSuffix(f.Name(), "cue") {
+			inst.AddFile(filepath.Join(name, f.Name()), nil)
+		}
+		if strings.HasSuffix(f.Name(), "wasm") {
+			f := &build.File{
+				Filename: f.Name(),
+			}
+			inst.UnknownFiles = append(inst.UnknownFiles, f)
+		}
+	}
+	inst.Complete()
+	return inst
+}
+
+func loadFile(filename string, src any) (*ast.File, error) {
+	return parser.ParseFile(filename, src, parser.ParseFuncs)
+}
+
+func must[T any](v T, err error) func(t *testing.T) T {
+	fail := false
+	if err != nil {
+		fail = true
+	}
+	return func(t *testing.T) T {
+		if fail {
+			_, file, line, _ := stdruntime.Caller(1)
+			file = filepath.Base(file)
+			t.Fatalf("unexpected error at %v:%v: %v", file, line, err)
+		}
+		return v
+	}
 }

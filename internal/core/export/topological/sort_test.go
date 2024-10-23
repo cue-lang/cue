4cecd80c597de5656602f9c37d0e7f417371ce8d
Matthew Sackman <matthew@cue.works>
2024-10-23 16:16:21 +0100

    export/topological: add explicit txtar test
    This explicitly tests field ordering behaviour.
    
    Change-Id: I62874dca7410565a0b4b3ab4668a420169f97508
    Signed-off-by: Matthew Sackman <matthew@cue.works>

---
+++ internal/core/export/topological/sort_test.go
@@ -0,0 +1,53 @@
+package topological_test
+
+import (
+	"fmt"
+	"testing"
+
+	"cuelang.org/go/cue/format"
+	"cuelang.org/go/internal/core/eval"
+	"cuelang.org/go/internal/core/export"
+	"cuelang.org/go/internal/cuetdtest"
+	"cuelang.org/go/internal/cuetxtar"
+)
+
+func TestTopologicalSort(t *testing.T) {
+	test := cuetxtar.TxTarTest{
+		Root:   "testdata",
+		Name:   "topological",
+		Matrix: cuetdtest.SmallMatrix,
+	}
+
+	test.Run(t, func(t *cuetxtar.Test) {
+		run := t.Runtime()
+		inst := t.Instance()
+
+		v, err := run.Build(nil, inst)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		v.Finalize(eval.NewContext(run, v))
+
+		evalWithOptions := export.Profile{
+			TakeDefaults:    true,
+			ShowOptional:    true,
+			ShowDefinitions: true,
+			ShowAttributes:  true,
+		}
+
+		expr, err := evalWithOptions.Value(run, inst.ID(), v)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		{
+			b, err := format.Node(expr)
+			if err != nil {
+				t.Fatal(err)
+			}
+			_, _ = t.Write(b)
+			fmt.Fprintln(t)
+		}
+	})
+}

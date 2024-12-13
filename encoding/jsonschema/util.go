d16119fc7c6659cf9045754f09f6113d77356925
Roger Peppe <rogpeppe@gmail.com>
2024-12-13 14:13:59 +0000

    encoding/jsonschema: refactor reference handling
    This refactors references to use a general system
    that maps references through a new `MapRef` function
    that supercedes the existing `MapURL` and `Map` functions.
    To support this, we use the new `structBuilder` type
    to build up the final syntax.
    
    This fixes a bunch of existing reported and unreported issues, including:
    - json pointer escaping: JSON pointers were not previously
    escaped and unescaped correctly.
    - references into internal structure: `$ref` references can now
    reference arbitrary internal structure inside the schema
    being extracted, including nodes that aren't actually schemas.
    - better doc comments: the outer-level doc comment is
    now correctly preserved in all circumstances
    
    There are inevitably some changes in the form of the generated schemas:
    - field ordering of definitions is now always lexical
    - some comments move to new (better) locations
    - attribute placement also moves to a (better) location
    - by default, only top level `$defs` members are exported
    as public definitions.
    
    The last issue could be considered a backward incompatible
    change, but in practice
    - nested definitions are rare
    - the nested definitions were not easily accessible anyway
    in most cases (e.g. when inside a property or other expression)
    - the new `MapRef` feature can be used to change the
    location of any schema, including these.
    
    As yet, the `MapRef` functionality as provided to the API
    is not tested other than with `DefaultMapRef`, and the
    `DefineSchemas` callback is not wired up. This will
    land in a subsequent CL: in the meantime, what we've
    got here seems sufficient as an intermediate step.
    
    Fixes #3593
    Fixes #3548
    Updates #2699
    Fixes #2287
    Fixes #390
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: Icfcff6e3d9f1d09f0418ddd493e01beb78045d59

--- encoding/jsonschema/util.go
+++ encoding/jsonschema/util.go
@@ -17,6 +17,7 @@ package jsonschema
 import (
 	"fmt"
 	"slices"
+	"strconv"
 	"strings"
 
 	"cuelang.org/go/cue"
@@ -158,6 +159,40 @@ func labelForSelector(sel cue.Selector) (ast.Label, error) {
 	default:
 		return nil, fmt.Errorf("cannot form label for selector %q with type %v", sel, sel.LabelType())
 	}
+}
+
+func cuePathToJSONPointer(p cue.Path) string {
+	var buf strings.Builder
+	for _, sel := range p.Selectors() {
+		buf.WriteByte('/')
+		switch sel.Type() {
+		case cue.StringLabel:
+			buf.WriteString(jsonPtrEsc.Replace(sel.Unquoted()))
+		case cue.IndexLabel:
+			buf.WriteString(strconv.Itoa(sel.Index()))
+		default:
+			panic(fmt.Errorf("cannot convert selector %v to JSON pointer", sel))
+		}
+	}
+	return buf.String()
+}
+
+// relPath returns the path to v relative to root,
+// which must be a direct ancestor of v.
+func relPath(v, root cue.Value) cue.Path {
+	rootPath := root.Path().Selectors()
+	vPath := v.Path().Selectors()
+	if !sliceHasPrefix(vPath, rootPath) {
+		panic("value is not inside root")
+	}
+	return cue.MakePath(vPath[len(rootPath):]...)
+}
+
+func sliceHasPrefix[E comparable](s1, s2 []E) bool {
+	if len(s2) > len(s1) {
+		return false
+	}
+	return slices.Equal(s1[:len(s2)], s2)
 }
 
 // TODO remove this when we can use [slices.SortedFunc] and [maps.Keys].

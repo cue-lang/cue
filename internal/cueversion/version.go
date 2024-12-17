be16614429babe85ff343aee3df46ecfadde5181
Daniel Martí <mvdan@mvdan.cc>
2024-12-17 17:09:11 +0000

    internal/cueversion: bump for v0.11.1
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I35f61eb262d44c00b00ceb92a81a82f7da12276a
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1205929
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    Reviewed-by: Paul Jolly <paul@myitcv.io>

--- internal/cueversion/version.go
+++ internal/cueversion/version.go
@@ -17,7 +17,7 @@ import (
 // This determines the latest version of CUE that
 // is accepted by the module.
 func LanguageVersion() string {
-	return "v0.11.0"
+	return "v0.11.1"
 }
 
 // ModuleVersion returns the version of the cuelang.org/go module as best as can

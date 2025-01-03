8f6db97982ab1de9010eb683b75cda4bd3cb9d80
Paul Jolly <paul@myitcv.io>
2025-01-03 12:31:59 +0000

    lsp: add simple workspace-based test for basic CUE module
    This is in preparation for later CLs which enforce that a workspace
    folder must correspond to the root of a CUE module.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Id3cad53160f4de6360c9d9866a2291dca567bc42

--- cmd/cue/cmd/integration/workspace/workspace_folders_test.go
+++ cmd/cue/cmd/integration/workspace/workspace_folders_test.go
@@ -5,10 +5,38 @@ import (
 
 	"cuelang.org/go/internal/golangorgx/gopls/hooks"
 	. "cuelang.org/go/internal/golangorgx/gopls/test/integration"
+	"cuelang.org/go/internal/golangorgx/gopls/test/integration/fake"
+	"github.com/go-quicktest/qt"
 )
 
 func TestMain(m *testing.M) {
 	Main(m, hooks.Options)
+}
+
+// TestWorkingSimpleModule ensures that we have a successful package load for a
+// simple module rooted in the workspace folder with a single CUE file at the
+// root.
+func TestWorkingSimpleModule(t *testing.T) {
+	const files = `
+-- cue.mod/module.cue --
+module: "mod.example"
+language: {
+        version: "v0.11.0"
+}
+-- a.cue --
+package a
+-- b/b.cue --
+package c
+`
+	WithOptions().Run(t, files, func(t *testing.T, env *Env) {
+		// Simulate a change and ensure we get diagnostics back
+		env.OpenFile("a.cue")
+		env.EditBuffer("a.cue", fake.NewEdit(1, 0, 1, 0, "\nx: 5\n"))
+		got := env.BufferText("a.cue")
+		want := "package a\n\nx: 5\n"
+		qt.Assert(t, qt.Equals(got, want))
+		env.Await(env.DoneWithChange())
+	})
 }
 
 // TestMultipleWorkspaceFolders verifies the behaviour of starting 'cue lsp'

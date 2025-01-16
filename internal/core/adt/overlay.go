1029b1df1c9b391511ba77ac51953a6acff55297
Marcel van Lohuizen <mpvl@gmail.com>
2025-01-16 14:06:23 +0100

    internal/core/adt: split closeContext.arcs
    Right now, closeContext.arcs is used to store both
    arcs and notifications. Although they share a lot
    of the same logic, it is confusing and prone to bugs.
    It also makes changing either mechanism hard and
    makes it harder to understand how everything works.
    
    This CL does a "dumb" split of the functionality.
    Further cleaning up can be done down the line.
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: Ib73254166e33210192c19b9cc4a3d092ffae1bfc

--- internal/core/adt/overlay.go
+++ internal/core/adt/overlay.go
@@ -318,9 +318,7 @@ func (ctx *overlayContext) allocCC(cc *closeContext) *closeContext {
 	// We only explicitly tag dependencies of type ARC. Notifications that
 	// point within the disjunct overlay will be tagged elsewhere.
 	for _, a := range cc.arcs {
-		if a.kind == ARC {
-			ctx.allocCC(a.cc)
-		}
+		ctx.allocCC(a.cc)
 	}
 
 	return o
@@ -448,6 +446,19 @@ func (ctx *overlayContext) initCloneCC(x *closeContext) {
 		}
 		a.cc = a.cc.overlay
 		o.arcs = append(o.arcs, a)
+	}
+
+	for _, a := range x.notify {
+		// If an arc does not have an overlay, we should not decrement the
+		// dependency counter. We simply remove the dependency in that case.
+		if a.cc.overlay == nil {
+			continue
+		}
+		if a.key.overlay != nil {
+			a.key = a.key.overlay // TODO: is this necessary?
+		}
+		a.cc = a.cc.overlay
+		o.notify = append(o.notify, a)
 	}
 
 	// NOTE: copying externalDeps is hard and seems unnecessary, as it needs to

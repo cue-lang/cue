07473ea0b8d9a99ec1faf9187f85d95c6280ce57
Marcel van Lohuizen <mpvl@gmail.com>
2025-02-16 12:37:46 +0100

    internal/core/adt: improve reference cycle behavior
    Sometimes, when a node recursively processes itself,
    it may be finalized when midway inside a unification.
    As a result, some disjunctions and tasks may still
    complete after finalization.
    
    Ideally we should have "frozen" mode that will allow
    all future task completions, but check for idemptotency.
    For now, though, we just do an additional round of
    filtering of disjunctions to eliminate disjuncts that
    fail for a preliminary freeze.
    
    Additionally, we check that if a disjunction was
    referenced within a reference cycle, that it may not
    discard its default value. This allows us to safely
    use default values in an order independent way.
    See comments for more detail.
    
    Unfortunately, this triggers quite a few, assumably
    related, node errors. We will address this in
    another CL.
    
    Explanation of txtar differences:
    
    018_self...: fixes and improvments over V2.
      See diff/explanation
    
    051...: now correctly errors where V2 did as well.
    
    052...: now correctly resolves where V2 did as well.
    
    issue241: better errors and more in line with V2.
    
    issue429: some tests that used to fail now "incorrectly"
      pass. However, this is due to the current semantics of
      how boundaries may simplify. As such, V3 is correct.
      We should very likely change the semantics, though.
      TODO: create and refer to issue.
    
    Fixes #3737
    Fixes #3669
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I53e7018b4ae6620d6d249642ab413cfab5e7a3fa

--- internal/core/adt/eval.go
+++ internal/core/adt/eval.go
@@ -1171,6 +1171,51 @@ type nodeContextState struct {
 	// improve the error messages.
 	hasFieldValue bool
 
+	// defaultAttemptInCycle indicates that a value relies on the default value
+	// and that it will be an error to remove the default value from the
+	// disjunction. It is set to the referring Vertex. Consider for instance:
+	//
+	//      a: 1 - b
+	//      b: 1 - a
+	//      a: *0 | 1
+	//      b: *0 | 1
+	//
+	// versus
+	//
+	//      a: 1 - b
+	//      b: 1 - a
+	//      a: *1 | 0
+	//      b: *0 | 1
+	//
+	// In both cases there are multiple solutions to the configuration. In the
+	// first case there is an ambiguity: if we start with evaluating 'a' and
+	// pick the default for 'b', we end up with a value of '1' for 'a'. If,
+	// conversely, we start evaluating 'b' and pick the default for 'a', we end
+	// up with {a: 0, b: 0}. In the seconds case, however, we do _will_ get the
+	// same answer regardless of order.
+	//
+	// In general, we will allow expressions on cyclic paths to be resolved if
+	// in all cases the default value is taken. In order to do that, we do not
+	// allow a default value to be removed from a disjunction if such value is
+	// depended on.
+	//
+	// For completeness, note that CUE will NOT solve a solution, even if there
+	// is only one solution. Consider for instance:
+	//
+	//      a: 0 | 1
+	//      a: b + 1
+	//      b: c - 1
+	//      c: a - 1
+	//      c: 1 | 2
+	//
+	// There the only consistent solution is {a: 1, b: 0, c: 1}. CUE, however,
+	// will not attempt this solve this as, in general, such solving would be NP
+	// complete.
+	//
+	// NOTE(evalv4): note that this would be easier if we got rid of default
+	// values and had pre-selected overridable values instead.
+	defaultAttemptInCycle *Vertex
+
 	// Value info
 
 	kind     Kind

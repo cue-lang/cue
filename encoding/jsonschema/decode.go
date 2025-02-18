09effdb2ae3d8d478c56f1850ed7c9025e098555
Roger Peppe <rogpeppe@gmail.com>
2025-02-18 13:07:05 +0000

    encoding/jsonschema: better placement of jsonschema attribute
    It makes more sense to put the attribute at the top level when possible.
    It's still not perfect when the `_schema` value is embedded at the top
    level, but definitely better than before.
    
    In passing, remove txtar files that don't appear to be used.
    Specifically, removing them does not affect the test passing or failing,
    and at least one of the files (`out/decode/extract`) was appearing in
    the `diff/-out/decode-v3/extract<==>+out/decode/extract` file as if the
    former was meaningful.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I9863bf642b0816c9b5b0008ba4eba1de16917515
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1208969
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>

--- encoding/jsonschema/decode.go
+++ encoding/jsonschema/decode.go
@@ -527,10 +527,6 @@ func (s *state) object(n cue.Value) *ast.StructLit {
 	if s.obj == nil {
 		s.obj = &ast.StructLit{}
 		s.objN = n
-
-		if s.id != nil {
-			s.obj.Elts = append(s.obj.Elts, s.idTag())
-		}
 	}
 	return s.obj
 }
@@ -694,8 +690,8 @@ func (s *state) finalize() (e ast.Expr) {
 		}
 	}
 
-	// If an "$id" exists and has not been included in any object constraints
-	if s.id != nil && s.obj == nil {
+	// If an "$id" exists, make sure it's present in the output.
+	if s.id != nil {
 		if st, ok := e.(*ast.StructLit); ok {
 			st.Elts = append([]ast.Decl{s.idTag()}, st.Elts...)
 		} else {

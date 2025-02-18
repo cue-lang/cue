72bb636ae64d74d050c8199ea1e2fb10ba877576
Marcel van Lohuizen <mpvl@gmail.com>
2025-02-18 16:03:49 +0100

    WIP: internal/core/adt: improve structure sharing for {} and _
    We allow {} and _, which itself do not add more
    information than just the type, to not break
    structure sharing, as long as it is consistent.
    
    TODO: issue3571: structural cycle
    
    Issue #2854
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I20184761a39f0a26a8f5a0e38e6c03a636da90e0

--- internal/core/adt/conjunct.go
+++ internal/core/adt/conjunct.go
@@ -173,8 +173,6 @@ func (n *nodeContext) scheduleConjunct(c Conjunct, id CloseInfo) {
 		}
 
 	case Value:
-		// TODO: perhaps some values could be shared.
-		n.unshare()
 		n.insertValueConjunct(env, x, id)
 
 	case *BinaryExpr:
@@ -653,14 +651,15 @@ func (n *nodeContext) insertValueConjunct(env *Environment, v Value, id CloseInf
 		return
 
 	case *Bottom:
+		n.unshare()
 		if x == NoShareSentinel {
-			n.unshare()
 			return
 		}
 		n.addBottom(x)
 		return
 
 	case *Builtin:
+		n.unshare()
 		if v := x.BareValidator(); v != nil {
 			n.insertValueConjunct(env, v, id)
 			return
@@ -674,6 +673,7 @@ func (n *nodeContext) insertValueConjunct(env *Environment, v Value, id CloseInf
 	switch x := v.(type) {
 	case *Disjunction:
 		n.updateCyclicStatusV3(id)
+		n.unshare()
 
 		// TODO(perf): reuse envDisjunct values so that we can also reuse the
 		// disjunct slice.
@@ -712,9 +712,11 @@ func (n *nodeContext) insertValueConjunct(env *Environment, v Value, id CloseInf
 		id.cc.hasTop = true
 
 	case *BasicType:
+		n.unshare()
 		n.updateCyclicStatusV3(id)
 
 	case *BoundValue:
+		n.unshare()
 		n.updateCyclicStatusV3(id)
 
 		switch x.Op {
@@ -770,6 +772,7 @@ func (n *nodeContext) insertValueConjunct(env *Environment, v Value, id CloseInf
 		}
 
 	case Validator:
+		n.unshare()
 		// This check serves as simplifier, but also to remove duplicates.
 		cx := MakeConjunct(env, x, id)
 		kind := x.Kind()
@@ -814,6 +817,7 @@ func (n *nodeContext) insertValueConjunct(env *Environment, v Value, id CloseInf
 	// handled above.
 
 	case Value: // *NullLit, *BoolLit, *NumLit, *StringLit, *BytesLit, *Builtin
+		n.unshare()
 		n.updateCyclicStatusV3(id)
 
 		if y := n.scalar; y != nil {

ff680206df20639983b18bdf91ac34ff5c18d55b
Marcel van Lohuizen <mpvl@gmail.com>
2025-02-23 17:05:21 +0100

    internal/core/adt: fix FromDef
    This CL does some magic with FromDef in order
    to make the tests pass again after the reduction
    in closeContext graph depth.
    
    In general, FromDef should be taken from the
    ConjunctGroup leafs. However, sometimes FromDef
    is passed down too aggressively. We therefore erase
    the incoming FromDef in adt.Unify.
    
    At the same time, when adt.Unify is called from
    the API and passes a Vertex, we still need to
    honor the ClosedRecursive flag. We therefore keep
    setting this flag for adt.Unify (in addConjuncts).
    
    Another issue is that in insertSkipConjuncts, we
    now need to clear the FromDef not only in
    ConjunctGroups, but any conjunct with a lower depth,
    because there is no longer a conjunct group
    corresponding to each increase in depth of the
    graph.
    
    Issue #2850
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I89d53ff14e374350c1fff317d2f1baf20268aaec

--- internal/core/adt/composite.go
+++ internal/core/adt/composite.go
@@ -1022,6 +1022,7 @@ func Unify(c *OpContext, a, b Value) *Vertex {
 
 func addConjuncts(ctx *OpContext, dst *Vertex, src Value) {
 	closeInfo := ctx.CloseInfo()
+	closeInfo.FromDef = false
 	c := MakeConjunct(nil, src, closeInfo)
 
 	if v, ok := src.(*Vertex); ok && v.ClosedRecursive {

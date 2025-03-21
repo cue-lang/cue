052b192feca2899ea6a0c008830a5847de69604f
Roger Peppe <rogpeppe@gmail.com>
2025-03-21 09:53:41 +0000

    mod/module: deprecate ParseImportPath
    We prefer people to use `ast.ParseImportPath` instead.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I6b646e61bd849c467cf31bd5378952c0dc208bee

--- mod/module/path.go
+++ mod/module/path.go
@@ -394,12 +394,16 @@ func SplitPathVersion(path string) (prefix, version string, ok bool) {
 }
 
 // ImportPath holds the various components of an import path.
+//
+// Deprecated: use [ast.ImportPath] instead.
 type ImportPath = ast.ImportPath
 
 // ParseImportPath returns the various components of an import path.
 // It does not check the result for validity.
+//
+// Deprecated: use [ast.SplitPackageVersion] instead.
 func ParseImportPath(p string) ImportPath {
-	return ast.ParseImportPath(p)
+	return ast.SplitPackageVersion(p)
 }
 
 // CheckPathMajor returns a non-nil error if the semantic version v

96e0ebcec56fa9cefc4de12f01828ca44a7e1f29
Marcel van Lohuizen <mpvl@gmail.com>
2025-04-16 09:44:17 +0200

    internal/core/adt: fix Vertex dereferencing for disjunctions
    When creating disjuncts, the Vertex values created
    so far are copied for each disjunct. We must take
    this into account when copying over Conjuncts as
    well: their Environments may still point to original
    nodes, rather than the copied ones, which may lead
    to incomplete data.
    
    We originally replaced Environments only
    superficially. This mostly worked. However, we
    forgot to also update the Environment for tasks.
    A straightforward update, however, seems to fix
    the outstanding bugs, but introduces some new
    ones.
    
    We now take a more complete approach where every
    Environment is recursively updated with any new
    Vertex.
    
    Here are a few key properties of the new approach:
    - We track all Vertex values that are copied and
      make no assumptions that some of them will never
      appear in Environment values.
    - When unifying multiple disjunctions, we carry
      over the full Vertex map, to be sure we have a
      complete dereference graph.
    - We replace the Vertex values only after all
      Vertex values have been cloned. This prevents us
      from accidentally missing some replacements.
    
    Fixes #3850
    Fixes #3875
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I20102183c4ed1e903157909a739d164a8a64ce9b
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1213570
    Reviewed-by: Matthew Sackman <matthew@cue.works>
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>

--- internal/core/adt/eval.go
+++ internal/core/adt/eval.go
@@ -1038,6 +1038,9 @@ type nodeContext struct {
 
 	arcMap []arcKey // not copied for cloning
 
+	// vertexMap is used to map vertices in disjunctions.
+	vertexMap vertexMap
+
 	// notify is used to communicate errors in cyclic dependencies.
 	// TODO: also use this to communicate increasingly more concrete values.
 	notify []receiver

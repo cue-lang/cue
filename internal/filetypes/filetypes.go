3dab6a289a1b3c0a8a413dce6d02ab10fb52ea56
Roger Peppe <rogpeppe@gmail.com>
2025-04-29 13:12:17 +0100

    internal/filetypes: generate toFile implementation
    We generate a version of filetypes.toFile that does not use
    the CUE evaluator. We use a hybrid approach for this:
    - for tags, we explore the entire tag namespace, relying on the
    fact that CUE is commutative so if we arrive at a point in the
    space from one route, we know that we'd get to the same point
    by any other route, and in particular an error will always be
    an error, so we can prune early which results in a much
    smaller parameter space (reduced from the potential 33 million
    down to 10k or so).
    - for subsidiary tags, we generated Go functions that simulate
    the (very) simple CUE logic for all the possible subsidiary tags
    logic.
    
    The tag exploration is encapsulated in a binary table, `tofile.dat`
    which holds an entry for all possible invocations that don't
    result in a tag conflict, consisting of the parameters for the `toFile`
    call immediately followed by its results. We use a simple
    compact fixed-length representation for the data, enabling us
    to do a simple binary search to find the results. This also
    means that the init time overhead is minimal: we can
    use the data exactly as it's embedded.
    
    This all means that `types.cue` remains the primary source of
    the filetypes logic. For now, we leave the old implementation
    in place so that we'll be warned of any discrepancies, but
    the old implementation will be removed from the filetypes package
    itself in a subsequent CL.
    
    For #3280.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I458bb2b4a2c82f82f805a2fa00e72fbee2f2fd88

--- internal/filetypes/filetypes.go
+++ internal/filetypes/filetypes.go
@@ -34,6 +34,7 @@ const (
 	Export
 	Def
 	Eval
+	NumModes
 )
 
 func (m Mode) String() string {
@@ -220,12 +221,12 @@ func parseScope(scopeStr string) (*scope, error) {
 	for _, tag := range strings.Split(scopeStr, "+") {
 		tagName, tagVal, hasValue := strings.Cut(tag, "=")
 		switch tagTypeOf(tagName) {
-		case tagTopLevel:
+		case TagTopLevel:
 			if hasValue {
 				return nil, errors.Newf(token.NoPos, "cannot specify value for tag %q", tagName)
 			}
 			sc.topLevel[tagName] = true
-		case tagSubsidiaryBool:
+		case TagSubsidiaryBool:
 			if hasValue {
 				t, err := strconv.ParseBool(tagVal)
 				if err != nil {
@@ -235,7 +236,7 @@ func parseScope(scopeStr string) (*scope, error) {
 			} else {
 				sc.subsidiaryBool[tagName] = true
 			}
-		case tagSubsidiaryString:
+		case TagSubsidiaryString:
 			if !hasValue {
 				return nil, errors.Newf(token.NoPos, "tag %q must have value (%s=<value>)", tagName, tagName)
 			}

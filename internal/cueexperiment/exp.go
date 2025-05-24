9a4c1d377746aad138259ddb289446dcb6d88f27
Daniel Martí <mvdan@mvdan.cc>
2025-05-24 12:45:37 +0100

    internal/cueexperiment: tweak the docs for all experiment flags
    Expand the docs for the active ones to match what is shown in
    `cue help environment`, and expand the deprecated ones too
    so that we can see what they did without having to look at the git log.
    Accumulating comments doesn't add any binary size or maintenance burden.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: If872db15e3f49b0bee32e673f1a27c1d53a58ca2

--- internal/cueexperiment/exp.go
+++ internal/cueexperiment/exp.go
@@ -11,20 +11,22 @@ import (
 // When adding, deleting, or modifying entries below,
 // update cmd/cue/cmd/help.go as well for `cue help environment`.
 var Flags struct {
-	// EvalV3 enables the new evaluator. The new evaluator addresses various
-	// performance concerns.
+	// EvalV3 enables the new CUE evaluator, addressing performance issues
+	// and bringing better algorithms for disjunctions, closedness, and cycles.
 	EvalV3 bool `envflag:"default:true"`
 
-	// Embed enables file embedding.
+	// Embed enables support for embedded data files as described in
+	// https://cuelang.org/discussion/3264.
 	// TODO(v0.14): deprecate this flag to forbid disabling this feature.
 	Embed bool `envflag:"default:true"`
 
-	// Enable topological sorting of struct fields.
+	// TopoSort enables topological sorting of struct fields.
+	// Provide feedback via https://cuelang.org/issue/3558.
 	// TODO(v0.14): deprecate this flag to forbid disabling this feature.
 	TopoSort bool `envflag:"default:true"`
 
 	// CmdReferencePkg requires referencing an imported tool package to declare tasks.
-	// Otherwise, declaring tasks by setting "$id" or "kind" string fields is allowed.
+	// Otherwise, declaring tasks via "$id" or "kind" string fields is allowed.
 	CmdReferencePkg bool
 
 	// The flags below describe completed experiments; they can still be set
@@ -32,13 +34,20 @@ var Flags struct {
 	// Breaking users who set such a flag seems unnecessary,
 	// and it simplifies using the same experiment flags across a range of CUE versions.
 
-	// Modules was an experiment which ran from early 2023 to late 2024.
+	// Modules enables support for the modules and package management proposal
+	// as described in https://cuelang.org/discussion/2939.
+	// This experiment ran from early 2023 to late 2024.
 	Modules bool `envflag:"deprecated,default:true"`
 
-	// YAMLV3Decoder was an experiment which ran from early 2024 to late 2024.
+	// YAMLV3Decoder swaps the old internal/third_party/yaml decoder with the new
+	// decoder implemented in internal/encoding/yaml on top of yaml.v3.
+	// This experiment ran from early 2024 to late 2024.
 	YAMLV3Decoder bool `envflag:"deprecated,default:true"`
 
-	// DecodeInt64 was an experiment which ran from late 2024 to mid 2025.
+	// DecodeInt64 changes [cuelang.org/go/cue.Value.Decode] to choose
+	// `int64` rather than `int` as the default type for CUE integer values
+	// to ensure consistency with 32-bit platforms.
+	// This experiment ran from late 2024 to mid 2025.
 	DecodeInt64 bool `envflag:"deprecated,default:true"`
 }
 

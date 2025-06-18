9e6bfdc79b4944696ffb68852e8d088b58a1f8e8
Marcel van Lohuizen <mpvl@gmail.com>
2025-06-18 14:55:58 +0200

    cue/parser: enable cueexperiment.File throughout
    This threads the usage of a cueexperiment.File
    from Parser to Compiler to Evaluator. It parses
    the @experiment() attribute in file preambles.
    
    Add a "testing" experiment that changes behavior
    of a __test_experiment builtin.
    
    In the future we could repurpose this flag if
    we need testing-specific builtins etc. for
    cue test.
    
    Issue #2583
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I1eba2b8792c1bb692ad34a958036bf52da58dfda

---
+++ internal/cueexperiment/file.go
@@ -0,0 +1,58 @@
+// Copyright 2025 CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cueexperiment
+
+// This contains experiments that are configured per file.
+
+// File defines the experiments that can be set per file. Users can activate
+// experiments by setting them using a file-based attribute @experiment() in
+// a CUE file. When an experiment is first introduced, it is disabled by
+// default.
+//
+//	since:       the version from when the experiment was introduced.
+//	accepted:    the version from when it is permanently set to true.
+//	rejected:    results in an error if the user attempts to use the flag.
+type File struct {
+	// version is the module version of the file that was compiled.
+	version string
+
+	// experiments is a comma-separated list of experiments that are enabled
+	// for this file. This is for documentation purposes only, as the
+	// experiments are already set in the struct fields.
+	experiments string
+
+	// Testing is used to enable experiments for testing.
+	//
+	// TODO: we could later use it for enabling testing features, such as
+	// testing-specific builtins.
+	Testing bool `experiment:"since:v0.13.0"`
+}
+
+// NewFile parses the given comma-separated list of experiments for
+// the given version and returns a PerFile struct with the experiments enabled.
+// A empty version indicates the default version.
+func NewFile(version string, experiments ...string) (*File, error) {
+	// TODO: cash versions for a given version where there is no experiment
+	// string.
+	f := &File{version: version}
+	var m expMap
+	m.add(experiments...)
+	f.experiments = m.key()
+
+	if err := parseConfig(f, version, m); err != nil {
+		return nil, err
+	}
+	return f, nil
+}

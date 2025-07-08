653f0b1fb9be30b35aba68a028e1fa467f07d247
Matthew Sackman <matthew@cue.works>
2025-07-08 14:08:57 +0100

    internal/gopls: rewrite gopls/cuelsp
    Implement the foundation of LSP for CUE.
    
    This foundation is written using terms that refer to the LSP spec
    itself, such as "Workspace" and "WorkspaceFolder".
    https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/
    
    The gopls terms such as "session", "view", "snapshot" are all gone.
    
    This foundation achieves the following:
    
    - workspaces can be created, containing one or more workspace folders
    - workspaces can be reconfigured
    - workspace folders can be configured, and reconfigured
    - the server understands text modification messages from the client
    - it can search for and load cue modules
    - it can search for and load cue packages within modules
    - it understands when modules and packages need to be reloaded,
      including:
        - a file changes package
        - a package vanishes
        - one package is imported by another and the upstream is dirtied
    - it can apply cue canonical formatting to a file
    
    This currently does not cope at all with cue files which are not part of
    any package and module.
    
    This does not perform any analysis of cue files beyond extracting their
    package and imports.
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I28e66d8ee294aeed50f0dfae05bf5f508886c9d1

--- internal/golangorgx/gopls/server/server.go
+++ internal/golangorgx/gopls/server/server.go
@@ -10,27 +10,36 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"sync"
+	"strconv"
+	"sync/atomic"
 
+	"cuelang.org/go/internal/golangorgx/gopls/cache"
 	"cuelang.org/go/internal/golangorgx/gopls/progress"
 	"cuelang.org/go/internal/golangorgx/gopls/protocol"
 	"cuelang.org/go/internal/golangorgx/gopls/settings"
 	"cuelang.org/go/internal/golangorgx/tools/event"
 )
 
+var serverIDCounter int64
+
+type ServerWithID interface {
+	protocol.Server
+
+	ID() string
+}
+
 // New creates an LSP server and binds it to handle incoming client
 // messages on the supplied stream.
-func New(client protocol.ClientCloser, options *settings.Options) protocol.Server {
-	const concurrentAnalyses = 1
-	// If this assignment fails to compile after a protocol
-	// upgrade, it means that one or more new methods need new
-	// stub declarations in unimplemented.go.
+func New(cache *cache.Cache, client protocol.ClientCloser, options *settings.Options) ServerWithID {
+	counter := atomic.AddInt64(&serverIDCounter, 1)
+
 	return &server{
-		watchedGlobPatterns: nil, // empty
-		changedFiles:        make(map[protocol.DocumentURI]struct{}),
-		client:              client,
-		progress:            progress.NewTracker(client),
-		options:             options,
+		id:     strconv.FormatInt(counter, 10),
+		client: client,
+		cache:  cache,
+
+		state:   serverCreated,
+		options: options,
 	}
 }
 
@@ -57,57 +66,93 @@ func (s serverState) String() string {
 	return fmt.Sprintf("(unknown state: %d)", int(s))
 }
 
-// server implements the protocol.server interface.
+// server implements the protocol.server interface. The server is
+// mainly concerned with the connection to the editor/client's
+// lifecycle start and end. It does the initial [server.Initialize] /
+// [server.Initialized] dance, then creating and configuring the
+// workspace. Once the workspace is up and running, most messages from
+// the editor/client (e.g. text modification notifications) go
+// straight to the workspace. The server also takes care of shutdown,
+// handling the [server.Shutdown] and [server.Exit] messages.
 type server struct {
-	client protocol.ClientCloser
+	id string
 
-	stateMu sync.Mutex
+	client    protocol.ClientCloser
+	cache     *cache.Cache
+	workspace *cache.Workspace
+
 	state   serverState
-	// notifications generated before serverInitialized
-	notifications []*protocol.ShowMessageParams
+	options *settings.Options
 
-	tempDir string
+	progress *progress.Tracker
 
-	// changedFiles tracks files for which there has been a textDocument/didChange.
-	changedFilesMu sync.Mutex
-	changedFiles   map[protocol.DocumentURI]struct{}
-
-	// folders is only valid between initialize and initialized, and holds the
-	// set of folders to build views for when we are ready.
-	// Each has a valid, non-empty 'file'-scheme URI.
-	//
-	// TODO(myitcv): it doesn't feel clean having this state at the "same level"
-	// as other server state. This field is only relevant for the time between
-	// the call to Initialize and notification of Initialized.
-	pendingFolders []protocol.WorkspaceFolder
+	pendingMessages []*protocol.ShowMessageParams
+	pendingFolders  []protocol.WorkspaceFolder
 
 	// watchedGlobPatterns is the set of glob patterns that we have requested
 	// the client watch on disk. It will be updated as the set of directories
 	// that the server should watch changes.
-	// The map field may be reassigned but the map is immutable.
-	watchedGlobPatternsMu  sync.Mutex
-	watchedGlobPatterns    map[protocol.RelativePattern]struct{}
-	watchRegistrationCount int
-
-	progress *progress.Tracker
-
-	// When the workspace fails to load, we show its status through a progress
-	// report with an error message.
-	criticalErrorStatusMu sync.Mutex
-	criticalErrorStatus   *progress.WorkDone
-
-	// Track an ongoing CPU profile created with the StartProfile command and
-	// terminated with the StopProfile command.
-	ongoingProfileMu sync.Mutex
-	ongoingProfile   *os.File // if non-nil, an ongoing profile is writing to this file
-
-	// Track most recently requested options.
-	optionsMu sync.Mutex
-	options   *settings.Options
-
-	lastModificationID uint64 // incrementing clock
+	// The map field may be reassigned but the map itself is immutable.
+	watchedGlobPatterns map[protocol.RelativePattern]struct{}
+	watchingIDCounter   int
 }
 
+var _ ServerWithID = (*server)(nil)
+
+func (s *server) ID() string { return s.id }
+
+// Shutdown implements the 'shutdown' LSP handler. It releases resources
+// associated with the server and waits for all ongoing work to complete.
+//
+// https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#shutdown
+//
+// Shutdown is a request from the client, to the server, and it gets a
+// response. The server should not exit after the shutdown response is
+// sent, instead it should wait for an exit message, which is
+// asynchronous.
+func (s *server) Shutdown(ctx context.Context) error {
+	ctx, done := event.Start(ctx, "lsp.Server.shutdown")
+	defer done()
+
+	switch s.state {
+	case serverInitialized:
+		s.state = serverShutDown
+		// It's very likely that eventually we'll need to tell the
+		// workspace we're shutting down, and maybe wait for it to
+		// finish making changes.
+
+	case serverShutDown:
+		return nil
+
+	default:
+		event.Log(ctx, "server shutdown without initialization")
+	}
+
+	return nil
+}
+
+// Exit implements the 'exit' LSP handler.
+//
+// https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#exit
+//
+// This is asynchronous - it does not get a response.
+func (s *server) Exit(ctx context.Context) error {
+	ctx, done := event.Start(ctx, "lsp.Server.exit")
+	defer done()
+
+	s.client.Close()
+
+	if s.state != serverShutDown {
+		// TODO: We should be able to do better than this.
+		os.Exit(1)
+	}
+	// We don't terminate the process on a normal exit, we just allow it to
+	// close naturally if needed after the connection is closed.
+	return nil
+}
+
+// WorkDoneProgressCancel is a message from the editor/client
+// requesting the cancellation of a long-running process.
 func (s *server) WorkDoneProgressCancel(ctx context.Context, params *protocol.WorkDoneProgressCancelParams) error {
 	ctx, done := event.Start(ctx, "lsp.Server.workDoneProgressCancel")
 	defer done()
@@ -115,11 +160,66 @@ func (s *server) WorkDoneProgressCancel(ctx context.Context, params *protocol.Wo
 	return s.progress.Cancel(params.Token)
 }
 
-func (s *server) Shutdown(context.Context) error { return nil }
-func (s *server) Exit(context.Context) error     { return nil }
-func (s *server) Initialize(context.Context, *protocol.ParamInitialize) (*protocol.InitializeResult, error) {
-	return nil, nil
-}
-func (s *server) Initialized(context.Context, *protocol.InitializedParams) error {
+// eventuallyShowMessage (eventually) calls showMessage in the
+// client. This can only be called once the server has completed
+// initialization. So if the server is not yet initialized, then the
+// message is buffered, and will be sent to the client later (when
+// [server.MaybeShowPendingMessages] is called and the server
+// completes initialization).
+func (s *server) eventuallyShowMessage(ctx context.Context, msg *protocol.ShowMessageParams) error {
+	if s.state == serverInitialized {
+		return s.client.ShowMessage(ctx, msg)
+	}
+	s.pendingMessages = append(s.pendingMessages, msg)
 	return nil
+}
+
+// maybeShowPendingMessages sends any pending showMessage messages to
+// the client if the server has completed initialization. If the
+// server has not completed initialization, this is a noop.
+func (s *server) maybeShowPendingMessages(ctx context.Context) error {
+	if s.state != serverInitialized {
+		return nil
+	}
+	messages := s.pendingMessages
+	s.pendingMessages = nil
+	for _, msg := range messages {
+		err := s.client.ShowMessage(ctx, msg)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// debugLog sends a log message to the client, with type=debug. This
+// is used extensively for testing, and debugging.
+func (s *server) debugLog(msg string) {
+	ctx := context.Background()
+	s.client.LogMessage(ctx, &protocol.LogMessageParams{Type: protocol.Debug, Message: msg})
+}
+
+// eventuallyUseWorkspaceFolders records the folders that the server
+// has received as part of the initialize message. Those folders
+// aren't used until after initialization is complete.
+func (s *server) eventuallyUseWorkspaceFolders(folders []protocol.WorkspaceFolder) {
+	if s.state != serverInitializing {
+		panic("Must not be called except when server is initializing")
+	}
+
+	s.pendingFolders = folders
+}
+
+// maybeUseWorkspaceFolders is used once server initialization is
+// complete. It adds any workspace folders stashed in the server
+// during initialization to the workspace.
+func (s *server) maybeUseWorkspaceFolders(ctx context.Context) error {
+	if s.state != serverInitialized {
+		panic("Must not be called except once server is initialized")
+	}
+
+	folders := s.pendingFolders
+	s.pendingFolders = nil
+
+	return s.AddFolders(ctx, folders)
 }

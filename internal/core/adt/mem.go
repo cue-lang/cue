a3979742984fe641b043343441722cbd3ce0ce0b
Marcel van Lohuizen <mpvl@gmail.com>
2025-07-09 13:33:44 +0200

    internal/core/adt: clean up orphaned arcs
    When a node is redirected to a disjunction,
    for instance, or when a node becomes
    erroneous, its arcs become irrelevant.
    The evaluator clears the arc list in these
    cases to avoid bugs resulting from
    descening into the wrong set of arcs.
    
    We now ensure these arcs are freed.
    
    Issue #3334
    Issue #2850
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: Ief2d59accc66cce28327b6ad31b347cbafce3d85
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1218190
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>

--- internal/core/adt/mem.go
+++ internal/core/adt/mem.go
@@ -207,3 +207,10 @@ func (r reclaimer) reclaimBaseValueBuffers(v *Vertex) {
 		}
 	}
 }
+
+func (v *Vertex) clearArcs(c *OpContext) {
+	for _, arc := range v.Arcs {
+		c.reclaimRecursive(arc)
+	}
+	v.Arcs = v.Arcs[:0]
+}

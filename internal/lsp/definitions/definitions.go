5a5cbdfd7616939979c970459a2ccc4d6b3dfa3a
Matthew Sackman <matthew@cue.works>
2025-07-24 16:57:11 +0100

    lsp/definitions: add analysis to determine resolution of idents
    WIP
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I3b2d62884db8e3d6e247da4193b7291595e96543

---
+++ internal/lsp/definitions/definitions.go
@@ -0,0 +1,400 @@
+package definitions
+
+import (
+	"fmt"
+	"slices"
+
+	"cuelang.org/go/cue/ast"
+	"cuelang.org/go/cue/token"
+)
+
+type Definitions struct {
+	byFile map[string][][]ast.Node
+	root   *scope
+}
+
+type scope struct {
+	dfns   *Definitions
+	parent *scope
+	// unprocessed holds the nodes that make up this scope. Once a call
+	// to [scope.eval] has returned, unprocessed must never be altered.
+	unprocessed []ast.Node
+	// keyPositions holds the positions that are considered to define
+	// this scope. For example, if a scope represents `a: {}` then
+	// keyPositions will hold the location of the `a`. Due to implicit
+	// unification, keyPositions may contain several positions.
+	keys []ast.Node
+	// resolvesTo points to the scopes reachable from nodes which are
+	// embedded within this scope.
+	resolvesTo []*scope
+	aliases    []string
+	allScopes  []*scope
+	fields     map[string]*scope
+}
+
+func Analyse(files ...*ast.File) *Definitions {
+	dfns := &Definitions{
+		byFile: make(map[string][][]ast.Node),
+	}
+	root := dfns.newScope(nil, nil)
+	dfns.root = root
+
+	for _, file := range files {
+		dfns.byFile[file.Filename] = make([][]ast.Node, file.End().Offset())
+		root.unprocessed = append(root.unprocessed, file)
+	}
+
+	root.EvalAll()
+	return dfns
+}
+
+func (dfns *Definitions) ForFile(file string) [][]ast.Node {
+	return dfns.byFile[file]
+}
+
+func (dfns *Definitions) newScope(parent *scope, key ast.Node, unprocessed ...ast.Node) *scope {
+	keys := []ast.Node{}
+	if key != nil {
+		keys = []ast.Node{key}
+	}
+	return &scope{
+		dfns:        dfns,
+		parent:      parent,
+		unprocessed: unprocessed,
+		keys:        keys,
+		fields:      make(map[string]*scope),
+	}
+}
+
+func (s *scope) newScope(key ast.Node, unprocessed ...ast.Node) *scope {
+	r := s.dfns.newScope(s, key, unprocessed...)
+	s.allScopes = append(s.allScopes, r)
+	return r
+}
+
+func (s *scope) Dump(depth int) {
+	fmt.Printf("%*sScope %p\n", depth*3, "", s)
+	fmt.Printf("%*s Positions", depth*3, "")
+	for _, key := range s.keys {
+		fmt.Printf(" %v", key.Pos().Position())
+	}
+	fmt.Println()
+
+	if len(s.aliases) != 0 {
+		fmt.Printf("%*s Aliases: %v\n", depth*3, "", s.aliases)
+	}
+
+	if len(s.fields) != 0 {
+		fmt.Printf("%*s Fields:\n", depth*3, "")
+		for name, r := range s.fields {
+			fmt.Printf("%*s  %s:\n", depth*3, "", name)
+			r.Dump(depth + 1)
+		}
+	}
+
+	if len(s.allScopes) != 0 {
+		fmt.Printf("%*s All scopes:\n", depth*3, "")
+		for _, r := range s.allScopes {
+			r.Dump(depth + 1)
+		}
+	}
+}
+
+func (s *scope) EvalAll() {
+	s.eval()
+	for _, r := range s.allScopes {
+		r.EvalAll()
+	}
+}
+
+func (s *scope) eval() {
+	if s.unprocessed == nil {
+		return
+	}
+
+	unprocessed := s.unprocessed
+	s.unprocessed = nil
+
+	var embeddedResolvable, resolvable []ast.Expr
+
+	for len(unprocessed) != 0 {
+		n := unprocessed[0]
+		unprocessed = unprocessed[1:]
+
+		switch n := n.(type) {
+		case *ast.File:
+			for _, decl := range n.Decls {
+				unprocessed = append(unprocessed, decl)
+			}
+
+		case *ast.StructLit:
+			for _, elt := range n.Elts {
+				unprocessed = append(unprocessed, elt)
+			}
+
+		case *ast.ListLit:
+			for i, elt := range n.Elts {
+				unprocessed = append(unprocessed, &ast.Field{
+					Label:      ast.NewIdent(fmt.Sprint(i)),
+					Constraint: token.ILLEGAL,
+					TokenPos:   elt.Pos(),
+					Token:      token.COLON,
+					Value:      elt,
+				})
+			}
+
+		case *ast.Interpolation:
+			resolvable = append(resolvable, n.Elts...)
+
+		case *ast.EmbedDecl:
+			unprocessed = append(unprocessed, n.Expr)
+
+		case *ast.ParenExpr:
+			unprocessed = append(unprocessed, n.X)
+
+		case *ast.UnaryExpr:
+			resolvable = append(resolvable, n.X)
+
+		case *ast.BinaryExpr:
+			switch n.Op {
+			case token.AND:
+				unprocessed = append(unprocessed, n.X, n.Y)
+			case token.OR:
+				lhs := s.newScope(nil, n.X)
+				rhs := s.newScope(nil, n.Y)
+				s.resolvesTo = append(s.resolvesTo, lhs, rhs)
+			default:
+				resolvable = append(resolvable, n.X, n.Y)
+			}
+
+		case *ast.Alias:
+			// X=e
+			s.aliases = append(s.aliases, n.Ident.Name)
+			unprocessed = append(unprocessed, n.Expr)
+
+		case *ast.Ellipsis:
+			unprocessed = append(unprocessed, n.Type)
+
+		case *ast.CallExpr:
+			resolvable = append(resolvable, n.Args...)
+
+		case *ast.Ident, *ast.SelectorExpr, *ast.IndexExpr:
+			embeddedResolvable = append(embeddedResolvable, n.(ast.Expr))
+
+		case *ast.Comprehension:
+			parent := s
+			for _, clause := range n.Clauses {
+				cur := parent.newScope(nil, clause)
+				parent = cur
+			}
+			if parent != s {
+				parent.newScope(nil, n.Value)
+			}
+
+		case *ast.IfClause:
+			unprocessed = append(unprocessed, n.Condition)
+
+		case *ast.LetClause:
+			s.ensureField(n.Ident.Name, n.Ident, n.Expr)
+
+		case *ast.ForClause:
+			if n.Key != nil {
+				s.ensureField(n.Key.Name, n.Key)
+			}
+			if n.Value != nil {
+				s.ensureField(n.Value.Name, n.Value)
+			}
+			resolvable = append(resolvable, n.Source)
+
+		case *ast.Field:
+			l := n.Label
+
+			alias, isAlias := l.(*ast.Alias)
+			if isAlias {
+				if expr, ok := alias.Expr.(ast.Label); ok {
+					l = expr
+				}
+			}
+
+			var field *scope
+			switch l := l.(type) {
+			case *ast.Ident:
+				field = s.ensureField(l.Name, l, n.Value)
+			case *ast.BasicLit:
+				field = s.ensureField(l.Value, l, n.Value)
+			default:
+				field = s.newScope(l, n.Value)
+			}
+
+			if isAlias {
+				switch alias.Expr.(type) {
+				case *ast.ListLit:
+					// X=[e]: field
+					// X is only visible within field
+					field.aliases = append(field.aliases, alias.Ident.Name)
+				case ast.Label:
+					// X=ident: field
+					// X="basic": field
+					// X="\(e)": field
+					// X=(e): field
+					// X is visible within s
+					s.fields[alias.Ident.Name] = field
+				}
+			}
+
+			switch l := l.(type) {
+			case *ast.Interpolation:
+				resolvable = append(resolvable, l.Elts...)
+			case *ast.ParenExpr:
+				// Although the spec supports this, the parser doesn't seem to.
+				// if alias, ok := l.X.(*ast.Alias); ok {
+				// 	// (X=e): field
+				// 	// X is visible within s
+				// 	s.ensureField(alias.Ident.Name, alias.Ident, alias.Expr)
+				// } else {
+				resolvable = append(resolvable, l.X)
+				// }
+			case *ast.ListLit:
+				for _, elt := range l.Elts {
+					if alias, ok := elt.(*ast.Alias); ok {
+						// [X=e]: field
+						// X is only visible within field. Given that X
+						// refers to the field's key and not the field's
+						// value, we can't treat X as an alias for the
+						// field, and so we inject a wrapping scope instead:
+						wrapper := s.newScope(nil)
+						wrapper.ensureField(alias.Ident.Name, alias.Ident, alias.Expr)
+						field.parent = wrapper
+					} else {
+						resolvable = append(resolvable, elt)
+					}
+				}
+			}
+		}
+	}
+
+	for _, expr := range embeddedResolvable {
+		scopes := s.resolve(expr)
+		s.resolvesTo = append(s.resolvesTo, scopes...)
+	}
+	for _, expr := range resolvable {
+		s.allScopes = append(s.allScopes, s.resolve(expr)...)
+	}
+}
+
+func (dfns *Definitions) addResolution(start token.Pos, length int, targets []ast.Node) {
+	startPosition := start.Position()
+	filename := startPosition.Filename
+	offsets := dfns.byFile[filename]
+	startOffset := startPosition.Offset
+	for i := range length {
+		offsets[startOffset+i] = append(offsets[startOffset+i], targets...)
+	}
+}
+
+func (s *scope) resolve(e ast.Expr) (scopes []*scope) {
+	switch e := e.(type) {
+	case *ast.Ident:
+		resolved := s.resolvePathRoot(e)
+		if resolved == nil {
+			return nil
+		} else {
+			s.dfns.addResolution(e.NamePos, len(e.Name), resolved.keys)
+			return []*scope{resolved}
+		}
+
+	case *ast.SelectorExpr:
+		resolved := s.resolve(e.X)
+		if len(resolved) == 0 {
+			return nil
+		}
+		scopesSet := make(map[*scope]struct{})
+		for len(resolved) > 0 {
+			r := resolved[0]
+			resolved = resolved[1:]
+			if _, seen := scopesSet[r]; seen {
+				continue
+			}
+			scopesSet[r] = struct{}{}
+			r.eval()
+			resolved = append(resolved, r.resolvesTo...)
+		}
+		name := ""
+		switch l := e.Sel.(type) {
+		case *ast.Ident:
+			name = l.Name
+		case *ast.BasicLit:
+			name = l.Value
+		default:
+			return nil
+		}
+
+		for r := range scopesSet {
+			if field, found := r.fields[name]; found {
+				scopes = append(scopes, field)
+				s.dfns.addResolution(e.Sel.Pos(), len(name), field.keys)
+			}
+		}
+		if len(scopes) == 0 {
+			return nil
+		}
+		return scopes
+
+	case *ast.IndexExpr:
+		return append(s.resolve(e.X), s.resolve(e.Index)...)
+
+	case *ast.StructLit, *ast.ListLit:
+		return []*scope{s.newScope(nil, e)}
+
+	case *ast.ParenExpr:
+		return s.resolve(e.X)
+
+	case *ast.BinaryExpr:
+		switch e.Op {
+		case token.AND, token.OR:
+			return append(s.resolve(e.X), s.resolve(e.Y)...)
+		}
+	}
+
+	return nil
+}
+
+func (s *scope) resolvePathRoot(ident *ast.Ident) *scope {
+	name := ident.Name
+	for ancestor := s; ancestor != nil; ancestor = ancestor.parent {
+		if slices.Contains(ancestor.aliases, name) {
+			return ancestor
+		}
+		if field, found := ancestor.fields[name]; found {
+			return field
+		}
+	}
+	return nil
+}
+
+func (s *scope) ensureField(name string, key ast.Node, unprocessed ...ast.Node) *scope {
+	field, found := s.fields[name]
+	if found {
+		field.unprocessed = append(field.unprocessed, unprocessed...)
+
+		if key != nil {
+			if !slices.Contains(field.keys, key) {
+				field.keys = append(field.keys, key)
+			}
+		}
+
+	} else {
+		field = s.newScope(key, unprocessed...)
+		s.fields[name] = field
+	}
+	return field
+}
+
+func (s *scope) isAncestorOf(other *scope) bool {
+	for other = other.parent; other != nil; other = other.parent {
+		if other == s {
+			return true
+		}
+	}
+	return false
+}
---
+++ internal/lsp/definitions/definitions_test.go
@@ -0,0 +1,599 @@
+package definitions_test
+
+import (
+	"cmp"
+	"slices"
+	"strings"
+	"testing"
+
+	"cuelang.org/go/cue/parser"
+	"cuelang.org/go/cue/token"
+	"cuelang.org/go/internal/lsp/definitions"
+	"github.com/go-quicktest/qt"
+)
+
+type testCase struct {
+	name         string
+	prog         string
+	expectations map[*position][]*position
+}
+
+type testCases []testCase
+
+func (tcs testCases) run(t *testing.T) {
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			ast, err := parser.ParseFile(tc.name, tc.prog, parser.ParseComments)
+			qt.Assert(t, qt.IsNil(err))
+			dfns := definitions.Analyse(ast)
+			resolutions := dfns.ForFile(tc.name)
+			t.Log(resolutions)
+
+			allPositions := make(map[*position]struct{})
+			for from, tos := range tc.expectations {
+				allPositions[from] = struct{}{}
+				for _, to := range tos {
+					allPositions[to] = struct{}{}
+				}
+			}
+			file := ast.Pos().File()
+			for pos := range allPositions {
+				pos.determineOffset(file, tc.prog)
+			}
+
+			// It's deliberate that we allow the expectations to be a
+			// subset of what is resolved. However, for each expectation,
+			// the resolutions must match exactly (reordering permitted).
+
+			for posFrom, positionsWant := range tc.expectations {
+				if positionsWant == nil {
+					continue
+				}
+				offset := posFrom.offset
+				qt.Assert(t, qt.IsTrue(len(resolutions) > offset))
+				for _, pos := range positionsWant {
+					if pos.filename == "" {
+						pos.filename = tc.name
+					}
+				}
+
+				nodesGot := resolutions[offset]
+				positionsGot := make([]token.Position, len(nodesGot))
+				for i, node := range nodesGot {
+					positionsGot[i] = node.Pos().Position()
+				}
+				qt.Assert(t, qt.Equals(len(positionsGot), len(positionsWant)))
+				slices.SortFunc(positionsWant, cmpPositions)
+				slices.SortFunc(positionsGot, cmpTokenPositions)
+				for i, want := range positionsWant {
+					got := positionsGot[i]
+					if !(got.Filename == want.filename && got.Offset == want.offset) {
+						t.Fatalf("Got %v; Want %s:%d", got, want.filename, want.offset)
+					}
+				}
+			}
+		})
+	}
+}
+
+func cmpTokenPositions(a, b token.Position) int {
+	// Deliberately avoid using the offset field as it may not be specified
+	if c := cmp.Compare(a.Filename, b.Filename); c != 0 {
+		return c
+	}
+	return cmp.Compare(a.Offset, b.Offset)
+}
+
+func cmpPositions(a, b *position) int {
+	// Deliberately avoid using the offset field as it may not be specified
+	if c := cmp.Compare(a.filename, b.filename); c != 0 {
+		return c
+	}
+	return cmp.Compare(a.offset, b.offset)
+}
+
+type position struct {
+	filename string
+	line     int
+	n        int
+	str      string
+	offset   int
+}
+
+// Convenience constructor to make a new [position] with the given
+// line number (1-based), for the n-th (1-based) occurrence of str.
+func ln(i, n int, str string) *position {
+	return &position{
+		line: i,
+		n:    n,
+		str:  str,
+	}
+}
+
+func (p *position) determineOffset(file *token.File, prog string) {
+	// lines is the cumulative offset of the start of each line
+	lines := file.Lines()
+	startOffset := lines[p.line-1]
+	endOffset := file.Size()
+	if len(lines) > p.line {
+		endOffset = lines[p.line]
+	}
+	line := prog[startOffset:endOffset]
+	n := p.n
+	for i := range line {
+		if strings.HasPrefix(line[i:], p.str) {
+			n--
+			if n == 0 {
+				p.offset = startOffset + i
+				return
+			}
+		}
+	}
+	panic("Failed to determine offset")
+}
+
+func TestSimple(t *testing.T) {
+	testCases{
+		{
+			name: "pointer chasing - implicit",
+			prog: `x1: f: 3
+x2: f: 4
+y: x1
+y: x2
+z: y
+out1: z
+out2: z.f
+`,
+			expectations: map[*position][]*position{
+				ln(3, 1, "x1"): {ln(1, 1, "x1")},
+				ln(4, 1, "x2"): {ln(2, 1, "x2")},
+				ln(5, 1, "y"):  {ln(3, 1, "y"), ln(4, 1, "y")},
+				ln(6, 1, "z"):  {ln(5, 1, "z")},
+				ln(7, 1, "z"):  {ln(5, 1, "z")},
+				ln(7, 1, "f"):  {ln(1, 1, "f"), ln(2, 1, "f")},
+			},
+		},
+
+		{
+			name: "pointer chasing - explicit",
+			prog: `x1: f: 3
+x2: f: 4
+y: x1 & x2
+z: y
+out1: z
+out2: z.f
+`,
+			expectations: map[*position][]*position{
+				ln(3, 1, "x1"): {ln(1, 1, "x1")},
+				ln(3, 1, "x2"): {ln(2, 1, "x2")},
+				ln(4, 1, "y"):  {ln(3, 1, "y")},
+				ln(5, 1, "z"):  {ln(4, 1, "z")},
+				ln(6, 1, "z"):  {ln(4, 1, "z")},
+				ln(6, 1, "f"):  {ln(1, 1, "f"), ln(2, 1, "f")},
+			},
+		},
+
+		{
+			name: "embedding",
+			prog: `x: y: z: 3
+o: { p: 4, x.y }
+`,
+			expectations: map[*position][]*position{
+				ln(2, 1, "x"): {ln(1, 1, "x")},
+				ln(2, 1, "y"): {ln(1, 1, "y")},
+			},
+		},
+	}.run(t)
+}
+
+func TestInline(t *testing.T) {
+	testCases{
+		{
+			name: "struct selector",
+			prog: `a: {in: {x: 5}, out: in}.out.x`,
+			expectations: map[*position][]*position{
+				ln(1, 2, "in"):  {ln(1, 1, "in")},
+				ln(1, 2, "out"): {ln(1, 1, "out")},
+				ln(1, 2, "x"):   {ln(1, 1, "x")},
+			},
+		},
+		{
+			name: "list index",
+			prog: `a: [7, {b: 3}, true][1].b`,
+			// We do not attempt any sort of resolution via dynamic
+			// indices.
+			expectations: map[*position][]*position{
+				ln(1, 1, "1"): {},
+				ln(1, 2, "b"): {},
+			},
+		},
+		{
+			name: "disjunction internal",
+			prog: `a: ({b: c, c: 3} | {c: 4}).c`,
+			expectations: map[*position][]*position{
+				ln(1, 1, "c"): {ln(1, 2, "c")},
+				ln(1, 4, "c"): {ln(1, 2, "c"), ln(1, 3, "c")},
+			},
+		},
+	}.run(t)
+}
+
+func TestCycles(t *testing.T) {
+	testCases{
+		{
+			name: "cycle - simple 2",
+			prog: `a: b
+b: a`,
+			expectations: map[*position][]*position{
+				ln(1, 1, "b"): {ln(2, 1, "b")},
+				ln(2, 1, "a"): {ln(1, 1, "a")},
+			},
+		},
+		{
+			name: "cycle - simple 3",
+			prog: `a: b
+b: c
+c: a`,
+			expectations: map[*position][]*position{
+				ln(1, 1, "b"): {ln(2, 1, "b")},
+				ln(2, 1, "c"): {ln(3, 1, "c")},
+				ln(3, 1, "a"): {ln(1, 1, "a")},
+			},
+		},
+		// These "structural" cycles are errors in the evaluator. But
+		// there's no reason we can't resolve them.
+		{
+			name: "structural - simple",
+			prog: `a: b: c: a`,
+			expectations: map[*position][]*position{
+				ln(1, 2, "a"): {ln(1, 1, "a")},
+			},
+		},
+		{
+			name: "structural - simple - selector",
+			prog: `a: b: c: a.b`,
+			expectations: map[*position][]*position{
+				ln(1, 2, "a"): {ln(1, 1, "a")},
+				ln(1, 2, "b"): {ln(1, 1, "b")},
+			},
+		},
+		{
+			name: "structural - complex",
+			prog: `y: [string]: b: y
+x: y
+x: c: x
+`,
+			expectations: map[*position][]*position{
+				ln(1, 2, "y"): {ln(1, 1, "y")},
+				ln(2, 1, "y"): {ln(1, 1, "y")},
+				ln(3, 2, "x"): {ln(2, 1, "x"), ln(3, 1, "x")},
+			},
+		},
+	}.run(t)
+}
+
+func TestAliases(t *testing.T) {
+	testCases{
+		{
+			name: "plain label - internal",
+			prog: `l=a: {b: 3, c: l.b}`,
+			expectations: map[*position][]*position{
+				ln(1, 2, "l"): {ln(1, 1, "a")},
+				ln(1, 2, "b"): {ln(1, 1, "b")},
+			},
+		},
+		{
+			name: "plain label - internal - implicit",
+			prog: `l=a: b: 3
+a: c: l.b`,
+			expectations: map[*position][]*position{
+				ln(2, 1, "l"): {ln(1, 1, "a"), ln(2, 1, "a")},
+				ln(2, 1, "b"): {ln(1, 1, "b")},
+			},
+		},
+		{
+			name: "plain label - internal - implicit - reversed",
+			prog: `a: b: 3
+l=a: c: l.b`,
+			expectations: map[*position][]*position{
+				ln(2, 2, "l"): {ln(1, 1, "a"), ln(2, 1, "a")},
+				ln(2, 1, "b"): {ln(1, 1, "b")},
+			},
+		},
+		{
+			name: "plain label - external",
+			prog: `l=a: b: 3
+c: l.b`,
+			expectations: map[*position][]*position{
+				ln(2, 1, "l"): {ln(1, 1, "a")},
+				ln(2, 1, "b"): {ln(1, 1, "b")},
+			},
+		},
+
+		{
+			name: "dynamic label - internal",
+			prog: `l=(a): {b: 3, c: l.b}`,
+			expectations: map[*position][]*position{
+				ln(1, 2, "l"): {ln(1, 1, "(")},
+				ln(1, 2, "b"): {ln(1, 1, "b")},
+			},
+		},
+		{
+			name: "dynamic label - internal - implicit",
+			prog: `l=(a): b: 3
+(a): c: l.b`,
+			// We do not attempt to compute equivalence of
+			// expressions. Therefore we don't consider the two `(a)`
+			// keys to be the same.
+			expectations: map[*position][]*position{
+				ln(2, 1, "l"): {ln(1, 1, "(")},
+				ln(2, 1, "b"): {ln(1, 1, "b")},
+			},
+		},
+		{
+			name: "dynamic label - internal - implicit - reversed",
+			prog: `(a): b: 3
+l=(a): c: l.b`,
+			// Because we don't compute equivalence of expressions, we do
+			// not link the two `(a)` keys, and so we cannot resolve the
+			// b in l.b.
+			expectations: map[*position][]*position{
+				ln(2, 2, "l"): {ln(2, 1, "(")},
+				ln(2, 1, "b"): {},
+			},
+		},
+		{
+			name: "dynamic label - external",
+			prog: `l=(a): b: 3
+c: l.b`,
+			expectations: map[*position][]*position{
+				ln(2, 1, "l"): {ln(1, 1, ("("))},
+				ln(2, 1, "b"): {ln(1, 1, ("b"))},
+			},
+		},
+
+		{
+			name: "pattern label - internal",
+			prog: `l=[a]: {b: 3, c: l.b}`,
+			expectations: map[*position][]*position{
+				ln(1, 2, "l"): {ln(1, 1, "[")},
+				ln(1, 2, "b"): {ln(1, 1, "b")},
+			},
+		},
+		{
+			name: "pattern label - internal - implicit",
+			prog: `l=[a]: b: 3
+[a]: c: l.b`,
+			// We do not attempt to compute equivalence of
+			// patterns. Therefore we don't consider the two `[a]`
+			// patterns to be the same. Because this style of alias is
+			// only visible within the key's value, no part of l.b can be
+			// resolved.
+			expectations: map[*position][]*position{
+				ln(2, 1, "l"): {},
+				ln(2, 1, "b"): {},
+			},
+		},
+		{
+			name: "pattern label - internal - implicit - reversed",
+			prog: `[a]: b: 3
+l=[a]: c: l.b`,
+			// Again, the two [a] patterns are not merged. The l of l.b
+			// can be resolved, but not the b.
+			expectations: map[*position][]*position{
+				ln(2, 2, "l"): {ln(2, 1, "[")},
+				ln(2, 1, "b"): {},
+			},
+		},
+		{
+			name: "pattern label - external",
+			prog: `l=[a]: b: 3
+c: l.b`,
+			// This type of alias is only visible within the key's
+			// value. So the use of l.b in c does not resolve.
+			expectations: map[*position][]*position{
+				ln(2, 1, "l"): {},
+				ln(2, 1, "b"): {},
+			},
+		},
+
+		{
+			name: "pattern expr - internal",
+			prog: `[l=a]: {b: 3, c: l, d: l.b}`,
+			// This type of alias binds l to the key. So c: l will work,
+			// but for the b in d: l.b there is no resolution.
+			expectations: map[*position][]*position{
+				ln(1, 2, "l"): {ln(1, 1, "l")},
+				ln(1, 3, "l"): {ln(1, 1, "l")},
+				ln(1, 2, "b"): {},
+			},
+		},
+		{
+			name: "pattern expr - internal - implicit",
+			prog: `[l=a]: b: 3
+[a]: c: l`,
+			// We do not attempt to compute equivalence of
+			// patterns. Therefore we don't consider the two `[a]`
+			// patterns to be the same. Because this style of alias is
+			// only visible within the key's value, no part of l.b can be
+			// resolved.
+			expectations: map[*position][]*position{
+				ln(2, 1, "l"): {},
+			},
+		},
+		{
+			name: "pattern expr - external",
+			prog: `[l=a]: b: 3
+c: l`,
+			// This type of alias is only visible within the key's
+			// value. So the use of l.b in c does not resolve.
+			expectations: map[*position][]*position{
+				ln(2, 1, "l"): {},
+			},
+		},
+
+		{
+			name: "expr - internal",
+			prog: `a: l={b: 3, c: l.b}`,
+			expectations: map[*position][]*position{
+				ln(1, 2, "l"): {ln(1, 1, "a")},
+				ln(1, 2, "b"): {ln(1, 1, "b")},
+			},
+		},
+		{
+			name: "expr - internal - explicit",
+			prog: `a: l={b: 3} & {c: l.b}`,
+			expectations: map[*position][]*position{
+				ln(1, 2, "l"): {ln(1, 1, "a")},
+				ln(1, 2, "b"): {ln(1, 1, "b")},
+			},
+		},
+		{
+			name: "expr - internal - explicit - paren",
+			// The previous test case works because it's parsed like
+			// this:
+			prog: `a: l=({b: 3} & {c: l.b})`,
+			expectations: map[*position][]*position{
+				ln(1, 2, "l"): {ln(1, 1, "a")},
+				ln(1, 2, "b"): {ln(1, 1, "b")},
+			},
+		},
+		{
+			name: "expr - external",
+			prog: `a: l={b: 3}
+c: l.b`,
+			// This type of alias is only visible within the value.
+			expectations: map[*position][]*position{
+				ln(2, 1, "l"): {},
+				ln(2, 1, "b"): {},
+			},
+		},
+	}.run(t)
+}
+
+func TestDisjunctions(t *testing.T) {
+	testCases{
+		{
+			name: "simple",
+			prog: `d: {a: b: 3} | {a: b: 4, c: 5}
+o: d.a.b
+p: d.c
+`,
+			expectations: map[*position][]*position{
+				ln(2, 1, "d"): {ln(1, 1, "d")},
+				ln(2, 1, "a"): {ln(1, 1, "a"), ln(1, 2, "a")},
+				ln(2, 1, "b"): {ln(1, 1, "b"), ln(1, 2, "b")},
+				ln(3, 1, "d"): {ln(1, 1, "d")},
+				ln(3, 1, "c"): {ln(1, 1, "c")},
+			},
+		},
+		{
+			name: "inline",
+			prog: `d: ({a: b: 3} | {a: b: 4}) & {c: 5}
+o: d.a.b
+p: d.c
+`,
+			expectations: map[*position][]*position{
+				ln(2, 1, "d"): {ln(1, 1, "d")},
+				ln(2, 1, "a"): {ln(1, 1, "a"), ln(1, 2, "a")},
+				ln(2, 1, "b"): {ln(1, 1, "b"), ln(1, 2, "b")},
+				ln(3, 1, "d"): {ln(1, 1, "d")},
+				ln(3, 1, "c"): {ln(1, 1, "c")},
+			},
+		},
+		{
+			name: "chained",
+			prog: `d1: {a: 1} | {a: 2}
+d2: {a: 3} | {a: 4}
+o: (d1 & d2).a
+`,
+			expectations: map[*position][]*position{
+				ln(3, 1, "a"): {ln(1, 1, "a"), ln(1, 2, "a"), ln(2, 1, "a"), ln(2, 2, "a")},
+			},
+		},
+		{
+			name: "selected",
+			prog: `d: {x: 17} | string
+r: d & {x: int}
+out: r.x
+`,
+			expectations: map[*position][]*position{
+				ln(2, 1, "d"): {ln(1, 1, "d")},
+				ln(3, 1, "r"): {ln(2, 1, "r")},
+				ln(3, 1, "x"): {ln(1, 1, "x"), ln(2, 1, "x")},
+			},
+		},
+		{
+			name: "scopes",
+			prog: `c: {a: b} | {b: 3}
+b: 7
+d: c.b
+`,
+			expectations: map[*position][]*position{
+				ln(1, 1, "b"): {ln(2, 1, "b")},
+				ln(3, 1, "c"): {ln(1, 1, "c")},
+				ln(3, 1, "b"): {ln(1, 2, "b")},
+			},
+		},
+		{
+			name: "looping",
+			prog: `a: {b: c.d, d: 3} | {d: 4}
+c: a
+`,
+			expectations: map[*position][]*position{
+				ln(1, 1, "c"): {ln(2, 1, "c")},
+				ln(1, 1, "d"): {ln(1, 2, "d"), ln(1, 3, "d")},
+				ln(2, 1, "a"): {ln(1, 1, "a")},
+			},
+		},
+	}.run(t)
+}
+
+func TestComprehensions(t *testing.T) {
+	testCases{
+		{
+			name: "if",
+			prog: `a: 17
+b: 3
+if a < 10 {
+	c: b
+}`,
+			expectations: map[*position][]*position{
+				ln(3, 1, "a"): {ln(1, 1, "a")},
+				ln(4, 1, "b"): {ln(2, 1, "b")},
+			},
+		},
+		{
+			name: "let",
+			prog: `a: b: c: 17
+let x=a.b
+y: x.c
+`,
+			expectations: map[*position][]*position{
+				ln(2, 1, "a"): {ln(1, 1, "a")},
+				ln(2, 1, "b"): {ln(1, 1, "b")},
+				ln(3, 1, "x"): {ln(2, 1, "x")},
+				ln(3, 1, "c"): {ln(1, 1, "c")},
+			},
+		},
+		{
+			name: "for",
+			prog: `a: { x: 1, y: 2, z: 3}
+b: { x: 4, y: 5, z: 6}
+o: {
+	for k, v in a {
+		(k): v * b[k]
+	}
+}`,
+			expectations: map[*position][]*position{
+				ln(4, 1, "k"): {},
+				ln(4, 1, "v"): {},
+				ln(4, 1, "a"): {ln(1, 1, "a")},
+				ln(5, 1, "k"): {ln(4, 1, "k")},
+				ln(5, 1, "v"): {ln(4, 1, "v")},
+				ln(5, 1, "b"): {ln(2, 1, "b")},
+				ln(5, 2, "k"): {ln(4, 1, "k")},
+			},
+		},
+	}.run(t)
+}

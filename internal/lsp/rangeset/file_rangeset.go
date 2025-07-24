5abc98ba706bd91ee83c2c73c94798b342a84e9b
Matthew Sackman <matthew@cue.works>
2025-07-24 16:57:11 +0100

    lsp/definitions: add analysis to determine resolution of idents
    We wish to implement LSP features such as "jump to definition" and
    auto-complete. For this to be possible we need to be able to resolve
    paths (e.g. x.y.z) and find the definitions to which they refer.
    
    The ast/astutil package only resolves the first element of a path. We
    wish to resolve all elements of a path, as far as is possible without
    fully evaluating the CUE.
    
    Using the full evaluator is potentially too slow, and any value in the
    source that does not have a path to it from the outermost scope, cannot
    be resolved. Furthermore, in the event that the CUE source contains
    errors, it may prove difficult or impossible to resolve paths by using
    the full evaluator.
    
    Consequently, a custom evaluator is implemented.
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I3b2d62884db8e3d6e247da4193b7291595e96543

---
+++ internal/lsp/rangeset/file_rangeset.go
@@ -0,0 +1,69 @@
+// Copyright 2025 CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rangeset
+
+import (
+	"cmp"
+	"slices"
+)
+
+type FilenameRangeSet struct {
+	pairs []filenameRangeSetPair
+}
+
+func NewFilenameRangeSet() *FilenameRangeSet {
+	return &FilenameRangeSet{}
+}
+
+type filenameRangeSetPair struct {
+	filename string
+	ranges   *RangeSet
+}
+
+func (frs *FilenameRangeSet) Add(filename string, start, end int) {
+	pairs := frs.pairs
+	i, found := slices.BinarySearchFunc(pairs, filename, filenameRangeSetPairCmp)
+
+	if found {
+		pairs[i].ranges.Add(start, end)
+
+	} else {
+		prefix := pairs[:i]
+		suffix := pairs[i:]
+
+		pair := filenameRangeSetPair{
+			filename: filename,
+			ranges:   NewRangeSet(),
+		}
+		pair.ranges.Add(start, end)
+
+		newPairs := make([]filenameRangeSetPair, 0, len(pairs)+1)
+		newPairs = append(newPairs, prefix...)
+		newPairs = append(newPairs, pair)
+		newPairs = append(newPairs, suffix...)
+
+		frs.pairs = newPairs
+	}
+}
+
+func (frs *FilenameRangeSet) Contains(filename string, offset int) bool {
+	pairs := frs.pairs
+	i, found := slices.BinarySearchFunc(pairs, filename, filenameRangeSetPairCmp)
+	return found && pairs[i].ranges.Contains(offset)
+}
+
+func filenameRangeSetPairCmp(pair filenameRangeSetPair, filename string) int {
+	return cmp.Compare(pair.filename, filename)
+}

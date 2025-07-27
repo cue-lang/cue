48b38420a84c6d70f183e717987e929477de42bd
Stefan Nuxoll <stefan@nuxoll.me>
2025-07-27 02:19:44 -0700

    feat(cue): add support for custom CUE marshaling via Marshaler interface
    This is the counterpoint to +1219262, adding support for custom Marshaler
    implementations for Go types. If context.Decode() sees a type implements
    cue.Marshaler then it will use that immediately.
    
    Signed-off-by: Stefan Nuxoll <stefan@nuxoll.me>
    Change-Id: I1779c0a33eb63a7170d701afd524492817f23d10

--- cue/marshal.go
+++ cue/marshal.go
@@ -31,6 +31,10 @@ import (
 	"cuelang.org/go/internal/core/export"
 )
 
+type Marshaler interface {
+	MarshalCUE(context *Context) (Value, error)
+}
+
 // root.
 type instanceData struct {
 	Root  bool

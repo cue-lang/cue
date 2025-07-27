21698d5edb0a9a0ad851d75900d7416eccfe04de
Stefan Nuxoll <stefan@nuxoll.me>
2025-07-27 12:45:38 -0700

    cue: refactor how Decode handles custom unmarshalers
    The indirect function returning multiple different types of unmarshalers
    added more conditional branches to Decode than ideal. This wraps up the
    logic unto an unmarshalFunc interface that indirect() returns instead,
    so now Decode only needs to check if it was given custom unmarshalling
    logic or not.
    
    This also removes an unnecessary use of reflection to set the value of
    *big.Float values, to avoid needing to also pass a reflect.Value into
    the new unmarshalFunc implementations. Updating the pointer itself
    should be sufficient in all cases, including setting struct fields, and
    a test has been added for this case as a precaution.
    
    Updates #4008.
    
    Signed-off-by: Stefan Nuxoll <stefan@nuxoll.me>
    Change-Id: I08780120cbe62b3a211a6ee0d64d1b146e37b875

--- cue/decode.go
+++ cue/decode.go
@@ -89,6 +89,45 @@ func (d *decoder) clear(x reflect.Value) {
 
 var valueType = reflect.TypeFor[Value]()
 
+type unmarshalFunc func(d *decoder, v Value) error
+
+func unmarshalCUE(x Unmarshaler) unmarshalFunc {
+	return func(d *decoder, v Value) error {
+		return x.UnmarshalCUE(v)
+	}
+}
+
+func unmarshalJSON(x json.Unmarshaler) unmarshalFunc {
+	return func(d *decoder, v Value) error {
+		b, err := v.MarshalJSON()
+		d.addErr(err)
+		return x.UnmarshalJSON(b)
+	}
+}
+
+func unmarshalText(u encoding.TextUnmarshaler) unmarshalFunc {
+	return func(d *decoder, v Value) error {
+		switch x := u.(type) {
+		case *big.Float:
+			if f, err := v.Float(nil); err != nil {
+				d.addErr(errors.Wrapf(err, v.Pos(), "Decode"))
+				return nil
+			} else {
+				*x = *f
+				return nil
+			}
+		default:
+			if b, err := v.Bytes(); err != nil {
+				d.addErr(errors.Wrapf(err, v.Pos(), "Decode"))
+				return nil
+			} else {
+				d.addErr(u.UnmarshalText(b))
+				return nil
+			}
+		}
+	}
+}
+
 func (d *decoder) decode(x reflect.Value, v Value, isPtr bool) {
 	if !x.IsValid() {
 		d.addErr(errors.Newf(v.Pos(), "cannot decode into invalid value"))
@@ -110,37 +149,9 @@ func (d *decoder) decode(x reflect.Value, v Value, isPtr bool) {
 		return
 	}
 
-	ic, ij, it, x := indirect(x, v.IsNull())
-	if ic != nil {
-		d.addErr(ic.UnmarshalCUE(v))
-		return
-	}
-	if ij != nil {
-		b, err := v.MarshalJSON()
-		d.addErr(err)
-		d.addErr(ij.UnmarshalJSON(b))
-		return
-	}
-	if it != nil {
-		if _, ok := it.(*big.Float); ok {
-			f, err := v.Float(nil)
-			if err != nil {
-				err = errors.Wrapf(err, v.Pos(), "Decode")
-				d.addErr(err)
-				return
-			}
-			x.Elem().Set(reflect.ValueOf(*f))
-			return
-		}
-
-		b, err := v.Bytes()
-		if err != nil {
-			err = errors.Wrapf(err, v.Pos(), "Decode")
-			d.addErr(err)
-			return
-		}
-		d.addErr(it.UnmarshalText(b))
-		return
+	unmarshal, x := indirect(x, v.IsNull())
+	if unmarshal != nil {
+		d.addErr(unmarshal(d, v))
 	}
 
 	kind := x.Kind()
@@ -867,7 +878,7 @@ func simpleLetterEqualFold(s, t []byte) bool {
 // If it encounters an Unmarshaler, indirect stops and returns that.
 // If decodingNull is true, indirect stops at the first settable pointer so it
 // can be set to nil.
-func indirect(v reflect.Value, decodingNull bool) (Unmarshaler, json.Unmarshaler, encoding.TextUnmarshaler, reflect.Value) {
+func indirect(v reflect.Value, decodingNull bool) (unmarshalFunc, reflect.Value) {
 	// Issue #24153 indicates that it is generally not a guaranteed property
 	// that you may round-trip a reflect.Value by calling Value.Addr().Elem()
 	// and expect the value to still be settable for values derived from
@@ -920,15 +931,14 @@ func indirect(v reflect.Value, decodingNull bool) (Unmarshaler, json.Unmarshaler
 			v.Set(reflect.New(v.Type().Elem()))
 		}
 		if v.Type().NumMethod() > 0 && v.CanInterface() {
-			if u, ok := v.Interface().(Unmarshaler); ok {
-				return u, nil, nil, v
-			}
-			if u, ok := v.Interface().(json.Unmarshaler); ok {
-				return nil, u, nil, v
-			}
-			if !decodingNull {
-				if u, ok := v.Interface().(encoding.TextUnmarshaler); ok {
-					return nil, nil, u, v
+			switch u := v.Interface().(type) {
+			case Unmarshaler:
+				return unmarshalCUE(u), v
+			case json.Unmarshaler:
+				return unmarshalJSON(u), v
+			case encoding.TextUnmarshaler:
+				if !decodingNull {
+					return unmarshalText(u), v
 				}
 			}
 		}
@@ -940,5 +950,5 @@ func indirect(v reflect.Value, decodingNull bool) (Unmarshaler, json.Unmarshaler
 			v = v.Elem()
 		}
 	}
-	return nil, nil, nil, v
+	return nil, v
 }

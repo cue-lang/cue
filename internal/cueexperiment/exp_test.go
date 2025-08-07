40e1595d5554dbdc8b5bbfdfca2c7c5eeadd6950
Daniel Martí <mvdan@mvdan.cc>
2025-08-07 13:11:18 +0100

    internal/cueexperiment: deprecate the evalv3 flag
    Turning it off now is a no-op, given that the entire evalv2 is removed.
    Don't give the impression to users that it's still possible.
    
    Note that we must update TestInit, as it can no longer use the evalv3
    flag to smoke test that we can disable an experiment.
    TestEvalVersion is also pointless now, given that the evalv3
    experiment flag no longer does anything.
    
    Finally, expand the CUE_EXPERIMENT env var test to cover what happens
    when trying to set deprecated flags. Note that the envflag package
    already covered this, but we had no end-to-end tests with cmd/cue.
    
    Given we now test deprecated experiments in experiment_error.txtar,
    the two "modules mode not enabled" tests seem redundant now,
    especially given how non-modules mode was removed years ago.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Ieca498018c3f81a5be82c2b734a5efb5ae5c5685
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1220045
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Reviewed-by: Paul Jolly <paul@myitcv.io>
    Reviewed-by: Marcel van Lohuizen <mpvl@gmail.com>

--- internal/cueexperiment/exp_test.go
+++ internal/cueexperiment/exp_test.go
@@ -8,9 +8,9 @@ import (
 
 func TestInit(t *testing.T) {
 	// This is just a smoke test to make sure it's all wired up OK.
-	t.Setenv("CUE_EXPERIMENT", "evalv3=0,cmdreferencepkg")
+	t.Setenv("CUE_EXPERIMENT", "cmdreferencepkg,keepvalidators=0")
 	err := Init()
 	qt.Assert(t, qt.IsNil(err))
-	qt.Assert(t, qt.IsFalse(Flags.EvalV3))
 	qt.Assert(t, qt.IsTrue(Flags.CmdReferencePkg))
+	qt.Assert(t, qt.IsFalse(Flags.KeepValidators))
 }

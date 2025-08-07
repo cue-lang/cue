9f8df88538c3169ddc2d6d1ddcff5ba5b0bc3fb5
Marcel van Lohuizen <mpvl@gmail.com>
2025-08-07 09:03:56 +0200

    internal/core/adt: add more error positions
    Deleting evalv2 pointed out some error position
    logic that did not exist in evalv3 yet.
    The containment slice recently introduced in
    OpContext is a good place to keep track of
    containment positions.
    
    We implement this, following the same approach
    as for evalv2.
    
    Note that containment only works, at the moment,
    for positions derived within the same OpContext.
    It does not solve the position reported in Issue
    3992, possibly for that reason.
    
    Issue #3992
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I882769188321c5662707049ba08adbcc79699d16

--- cue/load/loader_test.go
+++ cue/load/loader_test.go
@@ -74,6 +74,7 @@ module: conflicting values 123 and "" (mismatched types int and string):
     cuelang.org/go/mod/modfile/schema.cue:56:22
 module: conflicting values 123 and string (mismatched types int and string):
     $CWD/testdata/badmod/cue.mod/module.cue:2:9
+    cuelang.org/go/mod/modfile/schema.cue:56:12
     cuelang.org/go/mod/modfile/schema.cue:98:12
 path:   ""
 module: ""

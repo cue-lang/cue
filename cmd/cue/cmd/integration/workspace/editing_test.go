2ddc6de07ab443a2701401a2ed90b05486e5f6bf
Matthew Sackman <matthew@cue.works>
2025-08-11 15:21:48 +0100

    lsp/definitions: implement ability for jump-to-dfn to switch package and module
    Both the LSP and definitions code have the package as the basic unit for
    loading and analysing. So a new argument to definitions.Analyse is added
    to allow the definitions code to resolve an import to a different set of
    definitions.
    
    This design means that even if the definition (in some other package)
    changes, after the call-site (reference-site?) is loaded and setup,
    there is no need to invalidate and rebuild any state surrounding the
    call-site package.
    
    Change-Id: Ic2f98c066d092fb4271d4069821d1aa29636cda8
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Reviewed-on: https://cue.gerrithub.io/c/cue-lang/cue/+/1220258
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    Reviewed-by: Roger Peppe <rogpeppe@gmail.com>

--- cmd/cue/cmd/integration/workspace/editing_test.go
+++ cmd/cue/cmd/integration/workspace/editing_test.go
@@ -246,4 +246,32 @@ v4: v2
 			}))
 		})
 	})
+
+	t.Run("jump to definition - inter package", func(t *testing.T) {
+		WithOptions(RootURIAsDefaultFolder()).Run(t, files, func(t *testing.T, env *Env) {
+			rootURI := env.Sandbox.Workdir.RootURI()
+			env.Await(
+				LogExactf(protocol.Debug, 1, false, "Workspace folder added: %v", rootURI),
+			)
+			env.OpenFile("b/c/c.cue")
+			env.Await(
+				env.DoneWithOpen(),
+			)
+			locs := env.Definition(protocol.Location{
+				URI: rootURI + "/b/c/c.cue",
+				Range: protocol.Range{
+					Start: protocol.Position{Line: 5, Character: 6},
+				},
+			})
+			qt.Assert(t, qt.ContentEquals(locs, []protocol.Location{
+				{
+					URI: rootURI + "/a/a.cue",
+					Range: protocol.Range{
+						Start: protocol.Position{Line: 4, Character: 0},
+						End:   protocol.Position{Line: 4, Character: 2},
+					},
+				},
+			}))
+		})
+	})
 }

8893db51d946b2fa0ac03a4601bb2a9df4ecafa6
Daniel Martí <mvdan@mvdan.cc>
2025-08-12 22:44:48 +0100

    internal/core/adt: remove duplicate package docs
    The adt package already has a top-level package godoc in doc.go.
    These seem to be small and outdated remnants from a time where
    the core evaluator was split in multiple packages.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I09787d596b779681fc781f8ad167ad212f26d142
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1220344
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    Reviewed-by: Marcel van Lohuizen <mpvl@gmail.com>

--- internal/core/adt/validate.go
+++ internal/core/adt/validate.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package validate collects errors from an evaluated Vertex.
 package adt
 
 type ValidateConfig struct {

ff4e2a98239f90335e79624103b4cb70a2c84eaa
Jonathan Matthews <github@hello.jonathanmatthews.com>
2025-08-13 18:44:00 +0100

    cmd/cue: add experimental datacmp command
    This adds an experimental command to assert the equivalence of 2 or more
    data files.
    
    It uses a technique I feel vaguely justified in claiming to have
    "invented" a couple of years ago[0], which was later evolved into a
    cmd/cue one-liner mentioned in a commit message[1] for the cuelang.org
    site, and is used elsewhere in non-public repos.
    
    The command is hidden (as well as experimental) for a few reasons:
    - the simplistic implementation is exec.Command-based, mirroring its
      ancestors
    - it can't handle .cue files (because "cue import" can't), which could
      seem odd if someone stumbled across the command without context
    - it's tested only with script-based tests, not unit tests
    
    Nevertheless, the technique is one which we use semi-regularly, so
    perhaps having the command available (but not advertised) will allow us
    to use it in its unrefined state in order to inform use cases for a more
    complete "cue diff", or similar.
    
    [0] https://github.com/jpluscplusm/cuetils/blob/main/utils/eCue
    [1] https://cuelang.org/cl/1213993
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: Ic2118cf574aef26051c23eb993118c2a6730ff89

--- cmd/cue/cmd/exp.go
+++ cmd/cue/cmd/exp.go
@@ -15,6 +15,14 @@
 package cmd
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
 	"cuelang.org/go/cue/load"
 	"cuelang.org/go/internal/encoding/gotypes"
 
@@ -37,6 +45,7 @@ as the objective is to gain experience and then move the feature elsewhere.
 	})
 
 	cmd.AddCommand(newExpGenGoTypesCmd(c))
+	cmd.AddCommand(newExpDatacmpCmd(c))
 	return cmd
 }
 
@@ -102,4 +111,119 @@ The default is "zero", representing a missing field as the zero value.
 func runExpGenGoTypes(cmd *Command, args []string) error {
 	insts := load.Instances(args, &load.Config{})
 	return gotypes.Generate(cmd.ctx, insts...)
+}
+
+func newExpDatacmpCmd(c *Command) *cobra.Command {
+	cmd := &cobra.Command{
+		Hidden:        true,
+		SilenceErrors: true, // TODO: This doesn't appear to prevent Cobra's "exit status 1".
+		SilenceUsage:  true, // TODO: Ditto.
+		Use:           "datacmp <file1> <file2> ... <fileN>",
+		Args:          cobra.MinimumNArgs(2),
+		Short:         "assert that data files contain the same data",
+		Long: `
+WARNING: THIS COMMAND IS EXPERIMENTAL. It may be changed or removed at any time.
+
+datacmp checks that any number of data files all contain exactly the same data.
+
+It operates only on data files that have these filename extensions:
+
+  .json .jsonl .ndjson   Files must contain JSON data
+  .yaml .yml             Files must contain YAML data
+  .toml                  Files must contain TOML data			
+  .txt                   Files must contain text data
+
+See "cue help filetypes" for more information on these specific encodings.
+
+CUE files are not supported. The "cue vet" command uses unification to help
+check that multiple CUE files contain the same data.
+`[1:],
+		RunE: mkRunE(c, runExpDatacmpCmd),
+	}
+
+	return cmd
+}
+
+// runExpDatacmpCmd runs "cue import" on each data file input parameter,
+// placing each in its own definition path in a temporary file, and then runs
+// "cue eval -e '#def1 & #def2 & #def3' tmp1.cue tmp2.cue tmp3.cue" to assert
+// that each file contains exactly (and only) the same data.
+func runExpDatacmpCmd(cmd *Command, args []string) error {
+	tmpDir, err := os.MkdirTemp("", "cue-exp-datacmp-*")
+	if err != nil {
+		return fmt.Errorf("failed to create temporary directory: %w", err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	for i, arg := range args {
+		if stderr, err := datacmpImportDatafile(arg, tmpDir, i+1); err != nil {
+			return errors.New(stderr)
+		}
+	}
+	if stderr, err := datacmpCueEval(tmpDir, len(args)); err != nil {
+		return errors.New(stderr)
+	}
+	return nil
+}
+
+const (
+	// Used as both a .cue filename prefix and a CUE definition identifier prefix
+	datacmpFilePrefix = "file"
+)
+
+func datacmpImportDatafile(dataFile, dstDir string, idx int) (stderr string, err error) {
+	cueFileName := fmt.Sprintf("%s%d.cue", datacmpFilePrefix, idx)
+	cueFilePath := filepath.Join(dstDir, cueFileName)
+	importParam := fmt.Sprintf("#%s%d:", datacmpFilePrefix, idx)
+
+	var b bytes.Buffer
+	cmd := exec.Command("cue", "import",
+		"-o", cueFilePath,
+		"-l", importParam,
+		dataFile)
+	cmd.Stderr = &b
+
+	err = cmd.Run()
+	stderr = b.String()
+
+	return
+}
+
+func datacmpCueEval(dir string, fileCount int) (stderr string, err error) {
+	var b bytes.Buffer
+	cmdArgs := append([]string{
+		"eval",
+		"-e", datacmpUnifiedExpression(fileCount),
+		"cue:"}, datacmpFilePaths(dir, fileCount)...)
+	cmd := exec.Command("cue", cmdArgs...)
+	cmd.Stderr = &b
+
+	err = cmd.Run()
+	stderr = b.String()
+
+	return
+}
+
+// datacmpUnifiedExpression returns a string like "#file1 & #file2 & ... & #filen".
+func datacmpUnifiedExpression(n int) string {
+	if n <= 0 {
+		return ""
+	}
+	files := make([]string, n)
+	for i := 1; i <= n; i++ {
+		files[i-1] = fmt.Sprintf("#%s%d", datacmpFilePrefix, i)
+	}
+	return strings.Join(files, " & ")
+}
+
+// datacmpFilePaths returns a slice of file paths like "dstDir/file1", ..., "dstDir/fileN".
+func datacmpFilePaths(dstDir string, fileCount int) []string {
+	if fileCount <= 0 {
+		return nil
+	}
+	paths := make([]string, fileCount)
+	for i := 1; i <= fileCount; i++ {
+		paths[i-1] = filepath.Join(dstDir, fmt.Sprintf("%s%d.cue", datacmpFilePrefix, i))
+	}
+	return paths
 }

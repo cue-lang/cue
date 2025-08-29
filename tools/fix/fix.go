af6c567a7bf7d241e9ce57d913dbb20cd1f0902b
Marcel van Lohuizen <mpvl@gmail.com>
2025-08-29 20:13:35 +0200

    tools/fix: add experiment and upgrade support
    Adds experiment validation, upgrade version
    handling, and fixExplicitOpen transformation
    to automatically convert embeddings to postfix
    ellipsis syntax.
    
    Discussion #4032
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I638220c344f19fd96f6284dc1c1abf042a0d6c63
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1221570
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>

--- tools/fix/fix.go
+++ tools/fix/fix.go
@@ -20,15 +20,22 @@
 package fix
 
 import (
+	"fmt"
+	"slices"
+
 	"cuelang.org/go/cue/ast"
 	"cuelang.org/go/cue/ast/astutil"
+	"cuelang.org/go/cue/errors"
 	"cuelang.org/go/cue/token"
+	"cuelang.org/go/internal/cueexperiment"
 )
 
 type Option func(*options)
 
 type options struct {
-	simplify bool
+	simplify       bool
+	exps           []string
+	upgradeVersion string
 }
 
 // Simplify enables fixes that simplify the code, but are not strictly
@@ -37,11 +44,89 @@ func Simplify() Option {
 	return func(o *options) { o.simplify = true }
 }
 
+// Experiments enables fixes for specific experiments. Depending on the version
+// of the module, this may result in adding an @experiment attribute to the
+// top of the file for the given experiment.
+func Experiments(experiment ...string) Option {
+	return func(o *options) {
+		// Deduplicate experiments to avoid running the same fix twice
+		for _, exp := range experiment {
+			if !slices.Contains(o.exps, exp) {
+				o.exps = append(o.exps, exp)
+			}
+		}
+	}
+}
+
+// UpgradeVersion enables upgrade fixes to the specified language version,
+// applying all accepted experiments up to that version.
+func UpgradeVersion(version string) Option {
+	return func(o *options) {
+		o.upgradeVersion = version
+	}
+}
+
 // File applies fixes to f and returns it. It alters the original f.
 func File(f *ast.File, o ...Option) *ast.File {
-	var options options
+	f, err := file(f, "", o...)
+	// TODO: this File method is public, and its signature was fixed
+	// before we started calling Sanitize. Ideally, we want to return
+	// this error, but that would require deprecating this File method,
+	// and creating a new one, which might happen in due course if we
+	// also discover that we need to be a bit more flexible than just
+	// accepting a File.
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func file(f *ast.File, version string, o ...Option) (*ast.File, errors.Error) {
+	existingExps := f.Pos().Experiment()
+	if version == "" {
+		version = existingExps.LanguageVersion()
+	}
+
+	options := options{}
 	for _, f := range o {
 		f(&options)
+	}
+
+	// Handle upgrade version logic
+	targetVersion := version
+	if options.upgradeVersion != "" {
+		targetVersion = options.upgradeVersion
+
+		// Add accepted experiments for the target version to the experiment list
+		acceptedExps := cueexperiment.GetUpgradable(version, targetVersion)
+		for _, exp := range acceptedExps {
+			// Only add if not already in the list and if it would make changes
+			if !slices.Contains(options.exps, exp) {
+				options.exps = append(options.exps, exp)
+			}
+		}
+	}
+
+	// Handle --exp=all (only valid as a lone argument)
+	if slices.Equal(options.exps, []string{"all"}) {
+		activeExps := cueexperiment.GetActive(version, targetVersion)
+		options.exps = activeExps
+	}
+
+	for _, exp := range options.exps {
+		if err := cueexperiment.CanApplyFix(exp, version, targetVersion); err != nil {
+			return nil, errors.Newf(token.NoPos, "fix: %v", err)
+		}
+	}
+
+	// TODO: should this error be wrapped better to not panic?
+	wantExps, err := cueexperiment.NewFile(targetVersion, options.exps...)
+	if err != nil {
+		return nil, errors.Wrapf(err, token.NoPos, "fix: invalid experiment")
+	}
+
+	if wantExps.ExplicitOpen && !existingExps.ExplicitOpen {
+		f = fixExperiment(fixExplicitOpen, f, "explicitopen", targetVersion)
 	}
 
 	// Make sure we use the "after" function, and not the "before",
@@ -111,17 +196,11 @@ func File(f *ast.File, o ...Option) *ast.File {
 		f = simplify(f)
 	}
 
-	err := astutil.Sanitize(f)
-	// TODO: this File method is public, and its signature was fixed
-	// before we started calling Sanitize. Ideally, we want to return
-	// this error, but that would require deprecating this File method,
-	// and creating a new one, which might happen in due course if we
-	// also discover that we need to be a bit more flexible than just
-	// accepting a File.
+	err = astutil.Sanitize(f)
 	if err != nil {
-		panic(err)
+		return nil, errors.Wrapf(err, token.NoPos, "fix: sanitize failed")
 	}
-	return f
+	return f, nil
 }
 
 func expandConcats(exprs ...ast.Expr) (result []ast.Expr) {
@@ -161,4 +240,51 @@ func concatCallArgs(expr ast.Expr) (*ast.ListLit, bool) {
 		return nil, false
 	}
 	return list, true
+}
+
+func fixExperiment(fn func(*ast.File) (*ast.File, bool), f *ast.File, exp, version string) *ast.File {
+	result, hasChanges := fn(f)
+
+	// If we made any changes, add the @experiment(explicitopen) attribute at the top
+	if hasChanges {
+		// there must be at least one decl for there to be any changes.
+		ast.SetRelPos(result.Decls[0], token.NewSection)
+
+		if !cueexperiment.IsStable(exp, version) {
+			expAttr := &ast.Attribute{Text: fmt.Sprintf("@experiment(%s)", exp)}
+
+			// Insert the attribute as the first declaration
+			result.Decls = append([]ast.Decl{expAttr}, result.Decls...)
+		}
+	}
+
+	return result
+}
+
+func fixExplicitOpen(f *ast.File) (result *ast.File, hasChanges bool) {
+	result = astutil.Apply(f, func(c astutil.Cursor) bool {
+		n := c.Node()
+		switch n := n.(type) {
+		case *ast.EmbedDecl:
+			// Check if the embedded expression needs to be "opened" with ellipsis
+			if _, ok := n.Expr.(*ast.PostfixExpr); ok {
+				break
+			}
+
+			// Transform the embedding to use postfix ellipsis
+			postfixExpr := &ast.PostfixExpr{
+				X:     n.Expr,
+				Op:    token.ELLIPSIS,
+				OpPos: n.Expr.End(),
+			}
+
+			c.Replace(&ast.EmbedDecl{
+				Expr: postfixExpr,
+			})
+			hasChanges = true
+		}
+		return true
+	}, nil).(*ast.File)
+
+	return result, hasChanges
 }

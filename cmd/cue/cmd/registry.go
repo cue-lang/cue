6e94bf921aad12f4b3d3b8d0b1242a06449a3d77
Daniel Martí <mvdan@mvdan.cc>
2025-09-10 09:44:08 +0100

    mod/modconfig: lazily initialize registry clients
    TODO write a fuller commit message
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Ia3583ec0717f13a376f861b4528f2af9b5d552b8

--- cmd/cue/cmd/registry.go
+++ cmd/cue/cmd/registry.go
@@ -1,14 +1,16 @@
 package cmd
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 	"os"
+	"sync"
 
 	"cuelang.org/go/internal/cuedebug"
 	"cuelang.org/go/internal/httplog"
-	"cuelang.org/go/internal/mod/modload"
 	"cuelang.org/go/mod/modconfig"
+	"cuelang.org/go/mod/module"
 )
 
 // getRegistryResolver returns an implementation of [modregistry.Resolver]
@@ -17,8 +19,12 @@ func getRegistryResolver() (*modconfig.Resolver, error) {
 	return modconfig.NewResolver(newModConfig(""))
 }
 
-func getCachedRegistry() (modload.Registry, error) {
+func getRegistry() (modconfig.CachedRegistry, error) {
 	return modconfig.NewRegistry(newModConfig(""))
+}
+
+func getLazyRegistry() modconfig.CachedRegistry {
+	return modconfig.NewLazyRegistry(newModConfig(""))
 }
 
 func newModConfig(registry string) *modconfig.Config {
@@ -27,6 +33,56 @@ func newModConfig(registry string) *modconfig.Config {
 		ClientType:  "cmd/cue",
 		CUERegistry: registry,
 	}
+}
+
+var _ modconfig.CachedRegistry = (*lazyRegistry)(nil)
+
+// lazyRegistry implements [modconfig.CachedRegistry] by returning err from all methods.
+type lazyRegistry struct {
+	fn func() (modconfig.CachedRegistry, error)
+
+	once    sync.Once
+	onceReg modconfig.CachedRegistry
+	onceErr error
+}
+
+func (r *lazyRegistry) registry() (modconfig.CachedRegistry, error) {
+	r.once.Do(func() {
+		r.onceReg, r.onceErr = r.fn()
+	})
+	return r.onceReg, r.onceErr
+}
+
+func (r *lazyRegistry) Requirements(ctx context.Context, m module.Version) ([]module.Version, error) {
+	reg, err := r.registry()
+	if err != nil {
+		return nil, err
+	}
+	return reg.Requirements(ctx, m)
+}
+
+func (r *lazyRegistry) Fetch(ctx context.Context, m module.Version) (module.SourceLoc, error) {
+	reg, err := r.registry()
+	if err != nil {
+		return module.SourceLoc{}, err
+	}
+	return reg.Fetch(ctx, m)
+}
+
+func (r *lazyRegistry) FetchFromCache(m module.Version) (module.SourceLoc, error) {
+	reg, err := r.registry()
+	if err != nil {
+		return module.SourceLoc{}, err
+	}
+	return reg.FetchFromCache(m)
+}
+
+func (r *lazyRegistry) ModuleVersions(ctx context.Context, mpath string) ([]string, error) {
+	reg, err := r.registry()
+	if err != nil {
+		return nil, err
+	}
+	return reg.ModuleVersions(ctx, mpath)
 }
 
 func httpTransport() http.RoundTripper {

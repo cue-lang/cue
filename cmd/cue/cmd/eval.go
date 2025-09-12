26efa72fb0cf17ef3f5a4909f90ac70ffacbadb8
Marcel van Lohuizen <mpvl@gmail.com>
2025-09-12 09:24:41 +0200

    cmd/cue: treat top-level incomplete errors consistently
    When outputting partial CUE evaluations, top-level
    incomplete errors were treated differently from
    other incomplete errors. This was a regression
    introduced to fix issue #3651 in https://cuelang.org/cl/1206950.
    
    However, this change was partly done to support
    matchN and reverting it breaks matchN in many
    places. We therefore introduce an option for
    matchN to keep considering incomplete errors,
    even if there is no "final" option, by means of the
    ReportIncomplete option.
    
    get_go_non_local.txtar: this test was broken in
    CL 1206950 and now has been reverted to its
    original to verify it is working again.
    
    Fixes #4029
    Issue #4079
    Issue #3651
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: I901137bec0c840b5dca8e0b195c4f9c521360496
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1222444
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>

--- cmd/cue/cmd/eval.go
+++ cmd/cue/cmd/eval.go
@@ -151,8 +151,10 @@ func runEval(cmd *Command, args []string) error {
 
 		if !flagIgnore.Bool(cmd) {
 			if err := v.Err(); err != nil {
-				errHeader()
-				return v.Validate(syn...)
+				if err = v.Validate(syn...); err != nil {
+					errHeader()
+					return err
+				}
 			}
 
 			// TODO(#553): this can be removed once v.Syntax() below retains line

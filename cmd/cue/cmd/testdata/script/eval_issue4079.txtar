cue-lang/cue

commit 3251b5c607e5650ebaeae1be24cc1bd432b193c6

internal/core/validate: disallow incomplete errors in "Final" mode
Previously "Final" was only used for disallowing
unsatisfied required fields. However, this issue is
more general. We now disallow any incomplete
errors in final mode that are not within definitions.

Disabled test in get_no_non_local.txtar which breaks
as a result of this change.

Fixes #3651

Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
Change-Id: I13eb9126c03c7e62b37068c74aa8db6f97c496d8
Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1206950
Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
Reviewed-by: Roger Peppe <rogpeppe@gmail.com>

cmd/cue/cmd/testdata/script/get_go_non_local.txtar
@@ -19,7 +19,8 @@ cmp go.mod go.mod.golden
 # If any are missing, the evaluation would fail to load them.
 go get externalmod.test/imports/root@v1.0.0
 exec cue get go externalmod.test/imports/root
-exec cue eval uses_imports_root.cue
+
+exec cue vet -c uses_imports_root.cue
 
 -- cue.mod/module.cue --
 module: "mod.test/local"
@@ -29,8 +30,9 @@ package p
 
 import "externalmod.test/imports/root"
 
-root.Type
-
+root.#Type & {
+    Sub1: Sub2: Leaf: 1
+}
 -- go.mod --
 module mod.test/local
 

internal/core/validate/validate.go
@@ -57,6 +57,10 @@ func (v *validator) checkConcrete() bool {
 	return v.Concrete && v.inDefinition == 0
 }
 
+func (v *validator) checkFinal() bool {
+	return (v.Concrete || v.Final) && v.inDefinition == 0
+}
+
 func (v *validator) add(b *adt.Bottom) {
 	if !v.AllErrors {
 		v.err = adt.CombineErrors(nil, v.err, b)
@@ -77,12 +81,12 @@ func (v *validator) validate(x *adt.Vertex) {
 	if b := x.Bottom(); b != nil {
 		switch b.Code {
 		case adt.CycleError:
-			if v.checkConcrete() || v.DisallowCycles {
+			if v.checkFinal() || v.DisallowCycles {
 				v.add(b)
 			}
 
 		case adt.IncompleteError:
-			if v.checkConcrete() {
+			if v.checkFinal() {
 				v.add(b)
 			}
 

internal/core/validate/validate_test.go
@@ -237,6 +237,21 @@ y: conflicting values 4 and 2:
 				x: string | {foo!: string}
 			`,
 		out: "incomplete\nx.foo: field is required but not present:\n    test:3:18",
+	}, {
+		name: "disallow incomplete error with final",
+		cfg:  &validate.Config{Final: true},
+		in: `
+			x: y + 1
+			y: int
+				`,
+		out: "incomplete\nx: non-concrete value int in operand to +:\n    test:2:7\n    test:3:7",
+	}, {
+		name: "allow incomplete error with final while in definition",
+		cfg:  &validate.Config{Final: true},
+		in: `
+			#D: x: #D.y + 1
+			#D: y: int
+				`,
 	}}
 
 	cuetdtest.Run(t, testCases, func(t *cuetdtest.T, tc *testCase) {

cmd/cue/cmd/testdata/script/dev.txtar
@@ -1,5 +1,6 @@
-exec cue eval
-cmp stdout outv2
+env CUE_EXPERIMENT=evalv3=0
+! exec cue eval
+cmp stderr outv2
 
 env CUE_EXPERIMENT=evalv3
 exec cue eval
@@ -15,3 +16,9 @@ y
 -- outv3 --
 y: 1
 -- outv2 --
+2 errors in empty disjunction:
+conflicting values 1 and {y:(1|{y:1}),y} (mismatched types int and struct):
+    ./test.cue:1:1
+    ./test.cue:4:4
+cannot add field y: was already used:
+    ./test.cue:4:9

cue/testdata/cycle/builtins.txtar
@@ -359,7 +359,11 @@ Result:
   }
   issue3633: (struct){
     final: (struct){
-      data: (#struct){
+      data: (_|_){
+        // [incomplete] issue3633.final.data: invalid value {} (does not satisfy matchN): 0 matched, expected 1:
+        //     ./matchn.cue:63:6
+        //     ./matchn.cue:62:8
+        //     ./matchn.cue:63:13
       }
       #s: (_){ matchN(1, (#list){
           0: (_|_){// &[matchN(1, [
@@ -497,12 +501,8 @@ diff old new
            n: (struct){
              n: (_){ _ }
            }
-@@ -167,14 +179,14 @@
-   }
-   issue3633: (struct){
-     final: (struct){
--      data: (struct){
-+      data: (#struct){
+@@ -174,11 +186,11 @@
+         //     ./matchn.cue:63:13
        }
        #s: (_){ matchN(1, (#list){
 -          0: (_|_){// matchN(1, [
@@ -687,7 +687,11 @@ Result:
   }
   issue3633: (struct){
     final: (struct){
-      data: (struct){
+      data: (_|_){
+        // [incomplete] issue3633.final.data: invalid value {} (does not satisfy matchN): 0 matched, expected 1:
+        //     ./matchn.cue:63:6
+        //     ./matchn.cue:62:8
+        //     ./matchn.cue:63:13
       }
       #s: (_){ matchN(1, (#list){
           0: (_|_){// matchN(1, [

commit 83bb5d31822c354dffb8e0d3b86d55f7c9ded124

cmd/cue: treat top-level incomplete errors consistently
When outputting partial CUE evaluations, top-level
incomplete errors were treated differently from
other incomplete errors. This was a regression
introduced to fix issue #3651 in https://cuelang.org/cl/1206950.

However, this change was partly done to support
matchN and reverting it breaks matchN in many
places. We therefore introduce an option for
matchN to keep considering incomplete errors,
even if there is no "final" option, by means of the
ReportIncomplete option.

get_go_non_local.txtar: this test was broken in
CL 1206950 and now has been reverted to its
original to verify it is working again.

Issue #4079
Issue #3651

Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
Change-Id: I901137bec0c840b5dca8e0b195c4f9c521360496

cmd/cue/cmd/eval.go
@@ -151,8 +151,11 @@ func runEval(cmd *Command, args []string) error {
 
 		if !flagIgnore.Bool(cmd) {
 			if err := v.Err(); err != nil {
-				errHeader()
-				return v.Validate(syn...)
+				err = v.Validate(syn...)
+				if err != nil {
+					errHeader()
+					return err
+				}
 			}
 
 			// TODO(#553): this can be removed once v.Syntax() below retains line

internal/core/adt/validate.go
@@ -24,6 +24,10 @@ type ValidateConfig struct {
 	// DisallowCycles indicates that there may not be cycles.
 	DisallowCycles bool
 
+	// ReportIncomplete reports an incomplete error even when concrete is not
+	// requested.
+	ReportIncomplete bool
+
 	// AllErrors continues descending into a Vertex, even if errors are found.
 	AllErrors bool
 
@@ -149,7 +153,7 @@ func (v *validator) validate(x *Vertex) {
 			}
 
 		case IncompleteError:
-			if v.checkFinal() {
+			if v.ReportIncomplete || v.checkConcrete() {
 				v.add(b)
 			}
 

internal/core/compile/validator.go
@@ -122,7 +122,8 @@ var matchIfBuiltin = &adt.Builtin{
 	},
 }
 
-var finalCfg = &adt.ValidateConfig{Final: true}
+// Explicitly disabllow incomplete errors.
+var finalCfg = &adt.ValidateConfig{ReportIncomplete: true, Final: true}
 
 // finalizeSelf ensures a value is fully evaluated and then strips it of any
 // of its validators or default values.

cmd/cue/cmd/testdata/script/eval_issue4079.txtar
@@ -1,9 +1,8 @@
-# TODO: none of these should fail
-! exec cue eval -e '#c' x.cue
-# cmp stdout out/stdout-c
+exec cue eval -e '#c' x.cue
+cmp stdout out/stdout-c
 
-! exec cue eval -e 'd' x.cue
-# cmp stdout out/stdout-d
+exec cue eval -e 'd' x.cue
+cmp stdout out/stdout-d
 
 exec cue eval x.cue
 cmp stdout out/stdout
@@ -25,8 +24,30 @@ d: struct.MinFields(3) & {
 	x!: 4
 	...
 }
+issue3633: {
+	// With nested matchNs as below, the schema will become an incomplete error
+	// as a value of the root vertex. Make sure this edge case is properly
+	// handled.
+	data: {} & #s
+	#s: matchN(1, [matchN(1, [{a!: _}])])
+}
 -- out/stdout-c --
+matchN(1, [{
+    a!: _
+    c?: _
+}, {
+    b!: _
+    c?: _
+}]) & {
+    c!: _
+}
 -- out/stdout-d --
+import "struct"
+
+struct.MinFields(3) & {
+    x!: 4
+    ...
+}
 -- out/stdout --
 import "struct"
 
@@ -42,4 +63,10 @@ import "struct"
 d: struct.MinFields(3) & {
     x!: 4
     ...
+}
+issue3633: {
+    data: #s & {}
+    #s: matchN(1, [matchN(1, [{
+        a!: _
+    }])])
 }

cmd/cue/cmd/testdata/script/get_go_non_local.txtar
@@ -22,7 +22,7 @@ cmp go.mod go.mod.golden
 go get externalmod.test/imports/root@v1.0.0
 exec cue get go externalmod.test/imports/root
 
-exec cue vet -c uses_imports_root.cue
+exec cue eval uses_imports_root.cue
 
 -- cue.mod/module.cue --
 module: "mod.test/local"
@@ -32,9 +32,7 @@ package p
 
 import "externalmod.test/imports/root"
 
-root.#Type & {
-    Sub1: Sub2: Leaf: 1
-}
+root.Type
 -- go.mod --
 module mod.test/local
 

internal/core/adt/validate_test.go
@@ -267,7 +267,7 @@ func TestValidate(t *testing.T) {
 				    test:3:18`,
 	}, {
 		name: "disallow incomplete error with final",
-		cfg:  &adt.ValidateConfig{Final: true},
+		cfg:  &adt.ValidateConfig{ReportIncomplete: true},
 		in: `
 			x: y + 1
 			y: int
@@ -276,6 +276,14 @@ func TestValidate(t *testing.T) {
 				x: non-concrete value int in operand to +:
 				    test:2:7
 				    test:3:7`,
+	}, {
+		name: "disallow incomplete error with final",
+		cfg:  &adt.ValidateConfig{Final: true},
+		in: `
+			x: y + 1
+			y: int
+				`,
+		out: "",
 	}, {
 		name: "allow incomplete error with final while in definition",
 		cfg:  &adt.ValidateConfig{Final: true},
763403ad8a651ce77e7fbe0c220db2c8527ac5ec
Daniel Martí <mvdan@mvdan.cc>
2025-09-30 14:46:38 +0100

    cue/ast: introduce NewLabel helper for regular fields
    Needing to create a cue/ast regular field with a specific string
    as a lbel is a fairly common scenario, particularly when decoding
    data into CUE where incoming keys like `_foo` or `bar baz` need to be
    quoted to ensure we produce valid regular fields.
    
    The logic to do this was scattered across the codebase, which was
    verbose but also led to mistakes. For example, the textproto decoder
    only checked for valid identifiers, so it would quote `bar baz`
    but not `_foo`, which is incorrect.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I00fee1790c3871263084cc604ff6c6d8571ec592

--- cue/ast/ast.go
+++ cue/ast/ast.go
@@ -421,9 +421,19 @@ type BasicLit struct {
 	label
 }
 
-// TODO: introduce and use NewLabel and NewBytes and perhaps NewText (in the
+// TODO: introduce and use NewBytes and perhaps NewText (in the
 // later case NewString would return a string or bytes type) to distinguish from
 // NewString. Consider how to pass indentation information.
+
+// NewLabel creates a new Label which describes a regular field with a given name,
+// which may need to be quoted as a string literal.
+// To create labels for definition or hidden fields, use [NewIdent].
+func NewLabel(name string) Label {
+	if strings.HasPrefix(name, "#") || strings.HasPrefix(name, "_") || !IsValidIdent(name) {
+		return NewString(name)
+	}
+	return NewIdent(name)
+}
 
 // NewString creates a new BasicLit with a string value without position.
 // It quotes the given string.

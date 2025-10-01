dd78b05e019a302ca1b9150c384928ce342d479e
Daniel Martí <mvdan@mvdan.cc>
2025-10-01 16:53:12 +0100

    cue/ast: add StringLabelNeedsQuoting
    This is the boolean form of the recently added NewLabel;
    it returns true if the given string must be quoted
    for it to be used as a string label representing its own value.
    
    Logic identical to it was sprinkled across a number of packages;
    switch those to the new API. Other bits of code which should use the
    exact same logic, but don't actually yet, will be switched
    in separate commits for the sake of easier reviewing and bisecting.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I4699b10a424f06e973093ac87b7a20bb9015b33e

--- internal/core/adt/feature.go
+++ internal/core/adt/feature.go
@@ -23,7 +23,6 @@ import (
 	"cuelang.org/go/cue/errors"
 	"cuelang.org/go/cue/literal"
 	"cuelang.org/go/cue/token"
-	"cuelang.org/go/internal"
 )
 
 // A Feature is an encoded form of a label which comprises a compact
@@ -76,14 +75,14 @@ func (f Feature) SelectorString(index StringIndexer) string {
 		}
 		return strconv.Itoa(int(x))
 	case StringLabel:
-		s := index.IndexToString(x)
-		if ast.IsValidIdent(s) && !internal.IsDefOrHidden(s) {
-			return s
-		}
 		if f == AnyString {
 			return "_"
 		}
-		return literal.Label.Quote(s)
+		s := index.IndexToString(x)
+		if ast.StringLabelNeedsQuoting(s) {
+			return literal.Label.Quote(s)
+		}
+		return s
 	default:
 		return f.IdentString(index)
 	}

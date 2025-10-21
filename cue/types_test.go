e13e39d1f5465855f9f760b32fed6efeab0ecd3f
Marcel van Lohuizen <mpvl@gmail.com>
2025-10-21 15:31:14 +0200

    cue: support ... operator in Expr
    And added some tests.
    
    Closes #4143
    
    Signed-off-by: Marcel van Lohuizen <mpvl@gmail.com>
    Change-Id: Ia636440f3daecd3c60995b25e5178f0ff527ca46

--- cue/types_test.go
+++ cue/types_test.go
@@ -4037,6 +4037,12 @@ func TestExpr(t *testing.T) {
 		input: "v: 2 mod 5",
 		want:  "mod(2 5)",
 	}, {
+		input: "@experiment(explicitopen)\n v: #Y..., #Y: {b: 2}",
+		want:  `...(.(〈〉 "#Y"))`,
+	}, {
+		input: "@experiment(explicitopen)\n v: {#Y..., a: 1}, #Y: {b: 2}",
+		want:  `&(...(.(〈〉 "#Y")) {a:1})`,
+	}, {
 		input: "v: a.b, a: b: 4",
 		want:  `.(.(〈〉 "a") "b")`,
 	}, {

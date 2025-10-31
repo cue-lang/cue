0126671d46e6d1ae5712af5d9a711f766d66897d
Matthew Sackman <matthew@cue.works>
2025-10-31 14:41:47 +0000

    internal/lsp: implement support for LSP DocumentSymbols
    So far, we have only had to manage state on behalf of modules and
    packages, and standalone files. Within a package, it has not been
    necessary to associate much state with individual files, with the one
    exception of the workspace "mappers" which, for each file, performs
    coordinate conversions from LSP's utf16 encoding to plain byte offsets.
    
    Various LSP features, such as DocumentSymbols and Diagnostics (to
    follow) demand however that we keep more state per file. Here we
    introduce that state, and ensure it is created and maintained for each
    file which is used by one or more packages, or standalone, and is also
    open (i.e. loaded within the editor).
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I62330b1eabf018d900d77cd733c98758c8532468

--- internal/lsp/cache/workspace.go
+++ internal/lsp/cache/workspace.go
@@ -59,6 +59,11 @@ type Workspace struct {
 	activeFiles map[protocol.DocumentURI][]packageOrModule
 	activeDirs  map[protocol.DocumentURI]struct{}
 
+	// files holds the general state for each loaded file. This state
+	// is used whether the file is part of one or more packages, or is
+	// with standalone.
+	files map[protocol.DocumentURI]*File
+
 	standalone *Standalone
 }
 
@@ -74,6 +79,7 @@ func NewWorkspace(cache *Cache, debugLog func(string)) *Workspace {
 		debugLog:  debugLog,
 		modules:   make(map[protocol.DocumentURI]*Module),
 		mappers:   make(map[*token.File]*protocol.Mapper),
+		files:     make(map[protocol.DocumentURI]*File),
 	}
 	w.standalone = NewStandalone(w)
 	return w
@@ -274,6 +280,12 @@ func (w *Workspace) DidModifyFiles(ctx context.Context, modifications []file.Mod
 
 	activeFiles, activeDirs := w.activeFilesAndDirs()
 	for uri, fh := range updatedFiles {
+		if fh == nil {
+			w.closeFile(uri)
+		} else {
+			w.ensureFile(uri).open()
+		}
+
 		// If it is in activeFiles then we know it's a file we have
 		// loaded in the past.
 		//

3f58bba2542388f0f827f98db620f57ea70329c3
Daniel Martí <mvdan@mvdan.cc>
2025-11-19 17:45:36 +0000

    internal/encoding: do not grow byte slices when reading source files
    When one loads a CUE file such as in `cue export foo.cue`,
    that file gets eventually passed to internal/encoding.NewDecoder,
    which then uses internal/source.Open to get an io.Reader.
    
    Unfortunately, most encodings like CUE itself via cue/parser
    read the entire input bytes before they start decoding a single byte.
    This is typically done by io.ReadAll or our own internal/source.ReadAll,
    which is a more generic version accepting more input types.
    
    These work fine, but they are wasteful; when presented with a reader,
    the only general approach they can use is to create an initial small
    buffer to read into, and grow it as needed if the input is larger.
    This leads to repeated byte slice growth, which gets more expensive
    as input files get into the multiple megabytes in size.
    
    This is particularly unfortunate given that cmd/cue typically deals
    with regular files on disk, and it's trivial to get their size upfront.
    Instead, teach internal/source.Open to return a size if one is known,
    and introduce internal/source.ReadAllSized which uses it as a hint
    to initialize the capacity for a buffer.
    
    Note that, for non-binary (utf-8) inputs such as CUE or JSON,
    we layer a Unicode transform reader, which deals with BOM headers
    as well as replacing invalid UTF-8 characters.
    Given that the vast majority of input files are plain valid UTF-8,
    and given that the size is only used as a hint and doesn't need to be
    exactly right all the time, this is not a problem.
    
    This approach passes the size around as a separate integer;
    a different approach was considered where internal/source.Open
    would return an io.Reader with an optional `Size() int` method.
    This method was scrapped because it adds another abstraction layer,
    and it's already hard enough to follow what reader we're using
    with the indirections through internal/source and x/text/transform.
    
    Given that Roman Mazur's project has many CUE files weighing
    multiple megabytes, avoiding the buffer growth and directly allocating
    just enough memory without any excess leads to saving about 350MiB.
    
    This figure may seem way off, but io.ReadAll begins with a generally
    conservative buffer size of just 512 bytes, and only grows the capacity
    by 1.5x via append as needed. It takes 33 steps to reach a capacity
    of over 5MiB, and the sum of all grown stacks takes up over 31MiB.
    So it's not at all surprising that just a dozen large CUE files
    can lead to allocating hundreds of megabytes unnecessarily.
    
    Note that this didn't really cause an issue in terms of maximum memory
    usage given that a lot of these allocations were intermediary buffers
    which were thrown away at the next buffer growth step.
    However, allocating large buffers unnecessarily will still cause extra
    GC runs as we're quickly generating megabytes of garbage to clean up.
    
              │     old      │                new                 │
              │     B/op     │     B/op      vs base              │
        Roman   9.646Gi ± 0%   9.276Gi ± 0%  -3.84% (p=0.000 n=8)
    
              │     old     │                new                │
              │  allocs/op  │  allocs/op   vs base              │
        Roman   91.40M ± 0%   91.39M ± 0%  -0.00% (p=0.000 n=8)
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I69983aea91a522f02cbd52585435fd66457edaf2

--- internal/source/source.go
+++ internal/source/source.go
@@ -20,6 +20,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"strings"
 )
@@ -46,22 +47,56 @@ func ReadAll(filename string, src any) ([]byte, error) {
 	return os.ReadFile(filename)
 }
 
+// ReadAllSize is like [io.ReadAll] while taking advantage of a size hint for the input reader,
+// much like [os.ReadFile] does when reading regular files with a known size.
+// When the size hint is negative, it simply uses [io.ReadAll].
+func ReadAllSize(r io.Reader, size int) ([]byte, error) {
+	if size >= 0 {
+		// We use a [bytes.Buffer] here, because the given size is a hint,
+		// and not actually a guarantee to be exactly correct.
+		//
+		// Before each read, [bytes.Buffer] ensures that the internal buffer
+		// has enough available capacity to read at least [bytes.MinRead] bytes.
+		// Many readers tend to signal EOF via a final (0, EOF) read,
+		// which then triggers growing the slice to accomodate [bytes.MinRead].
+		buf := bytes.NewBuffer(make([]byte, 0, size+bytes.MinRead))
+		_, err := buf.ReadFrom(r)
+		return buf.Bytes(), err
+	}
+	return io.ReadAll(r)
+}
+
 // Open creates a source reader for the given arguments.
 // If src != nil, Open converts src to an [io.Reader] if possible; otherwise it returns an error.
 // If src == nil, Open returns the result of opening the file specified by filename.
 //
 // The caller must check if the result is an [io.Closer], and if so, close it when done.
-func Open(filename string, src any) (io.Reader, error) {
+// The size of the opened reader is returned if possible, or -1 otherwise.
+func Open(filename string, src any) (io.Reader, int, error) {
 	if src != nil {
 		switch src := src.(type) {
 		case string:
-			return strings.NewReader(src), nil
+			return strings.NewReader(src), len(src), nil
 		case []byte:
-			return bytes.NewReader(src), nil
+			return bytes.NewReader(src), len(src), nil
 		case io.Reader:
-			return src, nil
+			return src, -1, nil
 		}
-		return nil, fmt.Errorf("invalid source type %T", src)
+		return nil, -1, fmt.Errorf("invalid source type %T", src)
 	}
-	return os.Open(filename)
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, -1, err
+	}
+	// If we just opened a regular file, return its size too.
+	stat, err := f.Stat()
+	if err != nil || !stat.Mode().IsRegular() {
+		return nil, -1, err
+	}
+	size := stat.Size()
+	// If the size would overflow an int, it won't fit in memory anyway.
+	if size > math.MaxInt {
+		return nil, -1, err
+	}
+	return f, int(size), nil
 }

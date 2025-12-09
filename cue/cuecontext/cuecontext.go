86ce3764feacc48fcc672e04e20575cd149376ba
Daniel Martí <mvdan@mvdan.cc>
2025-12-09 23:21:17 +0000

    cue/cuecontext: write a short package godoc
    Fixes #1132.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I19ad7c34995bda36749f1d2e9c9fb2934590a88b

--- cue/cuecontext/cuecontext.go
+++ cue/cuecontext/cuecontext.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package cuecontext creates [cue.Context] values,
+// which are needed for creating [cue.Value] values
+// and using the core API in the [cue] package.
 package cuecontext
 
 import (

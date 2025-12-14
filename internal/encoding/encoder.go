389f539efde2ed58b4d447dd3f55def2d6075989
Daniel Martí <mvdan@mvdan.cc>
2025-12-14 00:38:26 +0000

    cmd/cue: allow named pipes and devices with --outfile
    The CLI should be able to write to these without any file overwriting.
    
    Fixes #3624.
    Fixes #3631.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I0d26dedd0a36a97a690eb833c02ccec8dd5325b0

--- internal/encoding/encoder.go
+++ internal/encoding/encoder.go
@@ -311,11 +311,20 @@ func writer(f *build.File, cfg *Config) (_ io.Writer, close func() error) {
 			// Swap O_EXCL for O_TRUNC to allow replacing an entire existing file.
 			mode = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
 		}
+	retryOpen:
 		f, err := os.OpenFile(path, mode, 0o666)
-		if err != nil {
-			if errors.Is(err, fs.ErrExist) {
-				return errors.Wrapf(fs.ErrExist, token.NoPos, "error writing %q", path)
+		if errors.Is(err, fs.ErrExist) {
+			// If we failed because the file already existed,
+			// but the file in question is not regular, allow writing to it.
+			// This is done as a retry to avoid an Lstat before every OpenFile.
+			stat, err := os.Stat(path)
+			if err == nil && !stat.Mode().IsRegular() {
+				mode = os.O_WRONLY
+				goto retryOpen
 			}
+			return errors.Wrapf(fs.ErrExist, token.NoPos, "error writing %q", path)
+		}
+		if err != nil {
 			return err
 		}
 		_, err = f.Write(b.Bytes())
@@ -326,3 +335,7 @@ func writer(f *build.File, cfg *Config) (_ io.Writer, close func() error) {
 	}
 	return b, fn
 }
+
+// modePipe includes all the file mode types where --outfile can write to
+// without overwriting the entire file with a new regular file.
+var modePipe = fs.ModeNamedPipe | fs.ModeDevice

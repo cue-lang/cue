a59a2f23444fbc165e7f6a2f174af5d9bfa63844
Daniel Martí <mvdan@mvdan.cc>
2026-01-26 22:51:00 +0000

    pkg/net: add ParseCIDR to extract CIDR information
    For example, net.ParseCIDR("10.20.30.40/24") returns:
    
        {
            prefix_mask:    "255.255.255.0"
            prefix_len:     24
            prefix_addr:    "10.20.30.0"
            broadcast_addr: "10.20.30.255"
        }
    
    The names are chosen to loosely follow the API in Go's net/netip
    as well as the basic terminology used on Wikipedia.
    
    We might add more fields to this struct in the future,
    as long as it's information that is useful and cheap to compute.
    
    Fixes #4236.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I072c85e62976f7ce1041b00d406938b04638f779

--- pkg/net/pkg.go
+++ pkg/net/pkg.go
@@ -277,6 +277,18 @@ var p = &pkg.Package{
 			}
 		},
 	}, {
+		Name: "ParseCIDR",
+		Params: []pkg.Param{
+			{Kind: adt.StringKind},
+		},
+		Result: adt.StructKind,
+		Func: func(c *pkg.CallCtxt) {
+			s := c.String(0)
+			if c.Do() {
+				c.Ret, c.Err = ParseCIDR(s)
+			}
+		},
+	}, {
 		Name: "PathEscape",
 		Params: []pkg.Param{
 			{Kind: adt.StringKind},

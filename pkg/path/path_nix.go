0e8d704d88c1c237786592fc046ab506ca1cc495
Daniel Martí <mvdan@mvdan.cc>
2026-01-29 11:24:17 +0000

    pkg/path: remove osInfo.HasPrefix
    We seem to have imported this deprecated API from Go back in 2020,
    when that was entirely unnecessary at the time.
    We don't have any tests nor documentation for it.
    
    The good news is that we never exported any of the OS types in Go,
    nor did we expose a HasPrefix func for CUE importers
    like we did with IsAbs and others.
    
    Hence, I assume this is an entirely backwards compatible change.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I2a23d4aa7d88678ae922d579b9f68a21c6d60f72

--- pkg/path/path_nix.go
+++ pkg/path/path_nix.go
@@ -44,14 +44,6 @@ func (o unixInfo) volumeNameLen(path string) int {
 	return 0
 }
 
-// HasPrefix exists for historical compatibility and should not be used.
-//
-// Deprecated: HasPrefix does not respect path boundaries and
-// does not ignore case when required.
-func (o unixInfo) HasPrefix(p, prefix string) bool {
-	return strings.HasPrefix(p, prefix)
-}
-
 func (o unixInfo) splitList(path string) []string {
 	if path == "" {
 		return []string{}

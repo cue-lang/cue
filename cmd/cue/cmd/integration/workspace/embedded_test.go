07cd9ff8ffabda79579a9ba2f39385510c93272c
Matthew Sackman <matthew@cue.works>
2026-02-02 12:25:27 +0000

    internal/lsp: model embedded files as packages
    Now that we can read a yaml or json file as if it were a CUE file, the
    next step is to allow the lsp to model such files as packages.
    
    The existing modpkgload machinery does not attempt to discover the
    import graph for embeddings: it focuses purely on the import graph of
    package imports. So the LSP must build and maintain the graph for
    embeddings. Like with imported packages, we focus only on the upstream
    relationship. So just as loading a .cue file will load its required
    imports, now, loading a .cue file will load its embedded files
    (packages).
    
    This CL does not add any features to the LSP evaluator - that comes
    next. This CL is only about modelling embedded files as packages,
    and building and maintaining the import graph.
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I6dde6584f406850b696298467d1c46b1ae1ebcfe

---
+++ cmd/cue/cmd/integration/workspace/embedded_test.go
@@ -0,0 +1,266 @@
+package workspace
+
+import (
+	"fmt"
+	"testing"
+
+	"cuelang.org/go/internal/golangorgx/gopls/protocol"
+	. "cuelang.org/go/internal/golangorgx/gopls/test/integration"
+	"github.com/go-quicktest/qt"
+)
+
+func TestEmbedSimple(t *testing.T) {
+	const files = `
+-- cue.mod/module.cue --
+module: "mod.example/x"
+language: version: "v0.16.0"
+
+-- a.cue --
+@extern(embed)
+package a
+
+out: _ @embed(file=data/data.json)
+-- data/data.json --
+{"field": true}
+`
+	WithOptions(RootURIAsDefaultFolder()).Run(t, files, func(t *testing.T, env *Env) {
+		rootURI := env.Sandbox.Workdir.RootURI()
+
+		env.OpenFile("a.cue")
+		env.Await(
+			env.DoneWithOpen(),
+			LogExactf(protocol.Debug, 1, false, "Module dir=%v module=mod.example/x@v0 Loading packages [mod.example/x@v0:a]", rootURI),
+			LogExactf(protocol.Debug, 1, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Created`, rootURI), 1, false),
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Reloaded`, rootURI), 1, false),
+		)
+	})
+}
+
+func TestEmbedMissingExtern(t *testing.T) {
+	const files = `
+-- cue.mod/module.cue --
+module: "mod.example/x"
+language: version: "v0.16.0"
+
+-- a.cue --
+package a
+
+out: _ @embed(file=data/data.json)
+-- data/data.json --
+{"field": true}
+`
+	WithOptions(RootURIAsDefaultFolder()).Run(t, files, func(t *testing.T, env *Env) {
+		rootURI := env.Sandbox.Workdir.RootURI()
+
+		env.OpenFile("a.cue")
+		env.Await(
+			env.DoneWithOpen(),
+			LogExactf(protocol.Debug, 1, false, "Module dir=%v module=mod.example/x@v0 Loading packages [mod.example/x@v0:a]", rootURI),
+			LogExactf(protocol.Debug, 1, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+			NoLogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Created`, rootURI)),
+		)
+	})
+}
+
+func TestEmbedMissingFile(t *testing.T) {
+	const files = `
+-- cue.mod/module.cue --
+module: "mod.example/x"
+language: version: "v0.16.0"
+
+-- a.cue --
+@extern(embed)
+package a
+
+out: _ @embed(file=data/missing.json)
+`
+	WithOptions(RootURIAsDefaultFolder()).Run(t, files, func(t *testing.T, env *Env) {
+		rootURI := env.Sandbox.Workdir.RootURI()
+
+		env.OpenFile("a.cue")
+		env.Await(
+			env.DoneWithOpen(),
+			LogExactf(protocol.Debug, 1, false, "Module dir=%v module=mod.example/x@v0 Loading packages [mod.example/x@v0:a]", rootURI),
+			LogExactf(protocol.Debug, 1, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+			NoLogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Created`, rootURI)),
+		)
+	})
+}
+
+func TestEmbedLateFile(t *testing.T) {
+	const files = `
+-- cue.mod/module.cue --
+module: "mod.example/x"
+language: version: "v0.16.0"
+
+-- a.cue --
+@extern(embed)
+package a
+
+out: _ @embed(file=data/late.json)
+`
+	WithOptions(RootURIAsDefaultFolder()).Run(t, files, func(t *testing.T, env *Env) {
+		rootURI := env.Sandbox.Workdir.RootURI()
+
+		env.OpenFile("a.cue")
+		env.Await(
+			env.DoneWithOpen(),
+			LogExactf(protocol.Debug, 1, false, "Module dir=%v module=mod.example/x@v0 Loading packages [mod.example/x@v0:a]", rootURI),
+			LogExactf(protocol.Debug, 1, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+			NoLogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Created`, rootURI)),
+		)
+		env.CreateBuffer("data/late.json", `{"field": 5}`)
+		// embeds are "upstream", so we do reload the downstream a.cue -
+		// it's the same as if one of its imports had changed.
+		env.Await(
+			env.DoneWithOpen(),
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Reloaded`, rootURI), 1, false),
+			LogExactf(protocol.Debug, 2, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+		)
+	})
+}
+
+func TestEmbedDeleteFile(t *testing.T) {
+	const files = `
+-- cue.mod/module.cue --
+module: "mod.example/x"
+language: version: "v0.16.0"
+
+-- a.cue --
+@extern(embed)
+package a
+
+out: _ @embed(file=data/data.json)
+-- data/data.json --
+{"field": true}
+`
+	WithOptions(RootURIAsDefaultFolder()).Run(t, files, func(t *testing.T, env *Env) {
+		rootURI := env.Sandbox.Workdir.RootURI()
+
+		env.OpenFile("a.cue")
+		env.Await(
+			env.DoneWithOpen(),
+			LogExactf(protocol.Debug, 1, false, "Module dir=%v module=mod.example/x@v0 Loading packages [mod.example/x@v0:a]", rootURI),
+			LogExactf(protocol.Debug, 1, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Created`, rootURI), 1, false),
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Reloaded`, rootURI), 1, false),
+		)
+		err := env.Sandbox.Workdir.RemoveFile(env.Ctx, "data/data.json")
+		qt.Assert(t, qt.IsNil(err))
+		env.CheckForFileChanges()
+		// We should see a delete of the embed pkg, and a reload of the x@v0:a
+		env.Await(
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Deleted`, rootURI), 1, false),
+			LogExactf(protocol.Debug, 2, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+		)
+	})
+}
+
+func TestEmbedMissingGlob(t *testing.T) {
+	const files = `
+-- cue.mod/module.cue --
+module: "mod.example/x"
+language: version: "v0.16.0"
+
+-- a.cue --
+@extern(embed)
+package a
+
+out: _ @embed(glob=data/*.json)
+`
+	WithOptions(RootURIAsDefaultFolder()).Run(t, files, func(t *testing.T, env *Env) {
+		rootURI := env.Sandbox.Workdir.RootURI()
+
+		env.OpenFile("a.cue")
+		env.Await(
+			env.DoneWithOpen(),
+			LogExactf(protocol.Debug, 1, false, "Module dir=%v module=mod.example/x@v0 Loading packages [mod.example/x@v0:a]", rootURI),
+			LogExactf(protocol.Debug, 1, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+			NoLogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Created`, rootURI)),
+		)
+	})
+}
+
+func TestEmbedLateGlob(t *testing.T) {
+	const files = `
+-- cue.mod/module.cue --
+module: "mod.example/x"
+language: version: "v0.16.0"
+
+-- a.cue --
+@extern(embed)
+package a
+
+out: _ @embed(glob=data/*.json)
+`
+	WithOptions(RootURIAsDefaultFolder()).Run(t, files, func(t *testing.T, env *Env) {
+		rootURI := env.Sandbox.Workdir.RootURI()
+
+		env.OpenFile("a.cue")
+		env.Await(
+			env.DoneWithOpen(),
+			LogExactf(protocol.Debug, 1, false, "Module dir=%v module=mod.example/x@v0 Loading packages [mod.example/x@v0:a]", rootURI),
+			LogExactf(protocol.Debug, 1, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+			NoLogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Created`, rootURI)),
+		)
+		env.CreateBuffer("data/late1.json", `{"field": 5}`)
+		// embeds are "upstream", so we do reload the downstream a.cue -
+		// it's the same as if one of its imports had changed.
+		env.Await(
+			env.DoneWithOpen(),
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Reloaded`, rootURI), 1, false),
+			LogExactf(protocol.Debug, 2, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+		)
+
+		env.CreateBuffer("data/late2.json", `{"field": 6}`)
+		// We will create+load the late2.json package, and reload a.cue, but we won't reload late1.json.
+		env.Await(
+			env.DoneWithOpen(),
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Reloaded`, rootURI), 2, false),
+			LogExactf(protocol.Debug, 3, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+		)
+	})
+}
+
+func TestEmbedDeleteGlob(t *testing.T) {
+	const files = `
+-- cue.mod/module.cue --
+module: "mod.example/x"
+language: version: "v0.16.0"
+
+-- a.cue --
+@extern(embed)
+package a
+
+out: _ @embed(glob=data/*.json)
+-- data/data1.json --
+{"field": true}
+-- data/data2.json --
+{"field": true}
+`
+	WithOptions(RootURIAsDefaultFolder()).Run(t, files, func(t *testing.T, env *Env) {
+		rootURI := env.Sandbox.Workdir.RootURI()
+
+		env.OpenFile("a.cue")
+		env.Await(
+			env.DoneWithOpen(),
+			LogExactf(protocol.Debug, 1, false, "Module dir=%v module=mod.example/x@v0 Loading packages [mod.example/x@v0:a]", rootURI),
+			LogExactf(protocol.Debug, 1, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+			// Both embedded files/packages get loaded
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Created`, rootURI), 2, false),
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Reloaded`, rootURI), 2, false),
+		)
+		err := env.Sandbox.Workdir.RemoveFile(env.Ctx, "data/data1.json")
+		qt.Assert(t, qt.IsNil(err))
+		env.CheckForFileChanges()
+		env.Await(
+			// One embedded file gets deleted.
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Deleted`, rootURI), 1, false),
+			// The survivor does *not* get reloaded (no need).
+			LogMatching(protocol.Debug, fmt.Sprintf(`Package dirs=\[%v/data\] importPath=mod\.example/x/data@v0:_.+ Reloaded`, rootURI), 2, false),
+			// And the downstream will get reloaded.
+			LogExactf(protocol.Debug, 2, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
+		)
+	})
+}

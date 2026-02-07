da908a1df64595c9d7d46c5b5872c9c811fee72e
Matthew Sackman <matthew@cue.works>
2026-02-07 11:43:29 +0000

    internal/lsp: implement ConvertToStruct code action
    With the cursor on `b`, we want to transform:
    
        a: b: whatever
    
    into
    
        a: {
        	b: whatever
        }
    
    Fun complications:
    * `whatever` can be multi-line
    * field names can be nested. For example:
    
        a: [{x: y: z: _, w: "foo"}.w]: {}
    
    with the cursor on `y`, we want to transform the inner-most field name
    (i.e. `y`). So this gets transformed into:
    
        a: [{x: {
        	y: z: _
        }
        w: "foo"}.w]: {}
    
    * in some circumstances, we need to remove commas and spaces. E.g.
    
        a: b: _, x: y: _
    
    becomes (with cursor on `b`):
    
        a: {
        	b: _
        }
        x: y: _
    
    Note the comma and space before `x` has gone away.
    
    It turns out that parsing CUE into an AST is lossy: i.e. you can't
    reconstruct the original []byte from the AST. Consequently, we can't
    make this transformation by adjusting the parsed AST because doing so
    would lead to canonically formatting the entire AST which is not what
    the user expects. so we are forced into byte splicing.
    
    Fixes #4073
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I1d56d5154993e7b28fbdc907672b97827e5ec9d4

---
+++ internal/lsp/cache/codeaction.go
@@ -0,0 +1,166 @@
+// Copyright 2026 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cache
+
+import (
+	"bytes"
+	"context"
+	"slices"
+	"strings"
+
+	"cuelang.org/go/cue/ast"
+	"cuelang.org/go/internal/golangorgx/gopls/protocol"
+	"cuelang.org/go/internal/golangorgx/tools/diff"
+)
+
+func (w *Workspace) CodeActionConvertToStruct(ctx context.Context, params *protocol.CodeActionParams) (*protocol.WorkspaceEdit, error) {
+	f := w.GetFile(params.TextDocument.URI)
+	if f == nil || f.syntax == nil || f.mapper == nil || f.tokFile == nil {
+		return nil, nil
+	}
+
+	offset, _, err := f.mapper.RangeOffsets(params.Range)
+	if err != nil {
+		return nil, err
+	}
+
+	var structLit *ast.StructLit
+	var field *ast.Field
+	var label ast.Label
+
+	ast.Walk(f.syntax,
+		// Walk over the AST, traversing into whatever contains the cursor position.
+		func(node ast.Node) bool {
+			start := node.Pos()
+			end := node.End()
+
+			if !start.HasAbsPos() || !end.HasAbsPos() {
+				return false
+			}
+			if !(start.Offset() <= offset && offset <= end.Offset()) {
+				return false
+			}
+			return true
+		},
+
+		// We want the inner-most matching field, so on the way back
+		// out, capture the first suitable structlit+field+label.
+		func(node ast.Node) {
+			if structLit != nil {
+				return
+			}
+
+			sl, ok := node.(*ast.StructLit)
+			if !ok || sl.Lbrace.HasAbsPos() || sl.Rbrace.HasAbsPos() || len(sl.Elts) != 1 {
+				return
+			}
+
+			f, ok := sl.Elts[0].(*ast.Field)
+			if !ok {
+				return
+			}
+			l := f.Label
+			start := l.Pos()
+			end := l.End()
+
+			if !start.HasAbsPos() || !end.HasAbsPos() {
+				return
+			}
+			if !(start.Offset() <= offset && offset <= end.Offset() && start.Line() == sl.Pos().Line()) {
+				return
+			}
+
+			structLit = sl
+			field = f
+			label = l
+		})
+
+	if structLit == nil {
+		return nil, nil
+	}
+
+	content := f.tokFile.Content()
+	lineStartOffsets := f.tokFile.Lines() // Lines is 0-based
+	numLines := len(lineStartOffsets)
+	labelStart := label.Pos()
+	lineNo := labelStart.Line() - 1 // Line is 1-based, hence the -1
+	if lineNo < 0 || lineNo >= len(lineStartOffsets) {
+		return nil, nil
+	}
+
+	lineEnding := "\n"
+	if numLines > 1 {
+		if content[lineStartOffsets[1]-2] == '\r' {
+			lineEnding = "\r\n"
+		}
+	}
+
+	var indent []byte
+	lineStartOffset := lineStartOffsets[lineNo]
+	for i := lineStartOffset; i < len(content); i++ {
+		c := content[i]
+		if c != ' ' && c != '\t' {
+			indent = content[lineStartOffset:i]
+			break
+		}
+	}
+
+	var contentBuilder strings.Builder
+	contentBuilder.Write(content[:labelStart.Offset()])
+
+	contentBuilder.WriteString("{")
+	contentBuilder.WriteString(lineEnding)
+
+	fieldEndOffset := field.End().Offset()
+	fieldLines := slices.Collect(bytes.Lines(content[field.Pos().Offset():fieldEndOffset]))
+	for i, fieldLine := range fieldLines {
+		contentBuilder.Write(indent)
+		contentBuilder.WriteString("\t")
+		isLastLine := i+1 == len(fieldLines)
+		if isLastLine {
+			// The last field line may or may not have a trailing line
+			// ending (because it might be the last line in the file).
+			fieldLine = bytes.TrimRight(fieldLine, "\r\n")
+			fieldLine = bytes.TrimRight(fieldLine, ", \t")
+		}
+		contentBuilder.Write(fieldLine)
+		if isLastLine {
+			contentBuilder.WriteString(lineEnding)
+		}
+	}
+
+	contentBuilder.Write(indent)
+	contentBuilder.WriteString("}")
+
+	remaining := content[fieldEndOffset:]
+	remaining = bytes.TrimLeft(remaining, ", \t")
+
+	if bytes.HasPrefix(remaining, []byte(lineEnding)) {
+		contentBuilder.Write(remaining)
+	} else if len(remaining) > 0 {
+		contentBuilder.WriteString(lineEnding)
+		contentBuilder.Write(indent)
+		contentBuilder.Write(remaining)
+	}
+
+	diffEdits := diff.Strings(string(content), contentBuilder.String())
+	edits, err := protocol.EditsFromDiffEdits(f.mapper, diffEdits)
+	if err != nil {
+		return nil, nil
+	}
+
+	docChanges := protocol.TextEditsToDocumentChanges(params.TextDocument.URI, f.tokFile.Revision(), edits)
+	return &protocol.WorkspaceEdit{DocumentChanges: docChanges}, nil
+}

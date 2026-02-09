4b44280d90954ad8ae7ea1d3f28b430b92968021
Matthew Sackman <matthew@cue.works>
2026-02-09 17:13:54 +0000

    internal/lsp: implement ConvertFromStruct code action
    This is the opposite of https://cue.gerrithub.io/c/cue-lang/cue/+/1230699
    
    Rather than converting
    
        a: b: whatever
    
    to
    
        a: {
        	b: whatever
        }
    
    we now support the opposite: for a structlit with exactly 1 field where
    the struct has braces, the new code action removes the braces, and
    shuffles the field up so it starts at the location of the original {
    
    Fixes #4073
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: Ieda3aade7df48bef4dcde5763e60e856a60530bb

--- cmd/cue/cmd/integration/workspace/codeaction_test.go
+++ cmd/cue/cmd/integration/workspace/codeaction_test.go
@@ -27,6 +27,7 @@ foo: {
 }
 `[1:],
 		},
+
 		{
 			name: "multiline_field",
 			input: `
@@ -47,6 +48,7 @@ before: _
   after: _ // weird indent
 `[1:],
 		},
+
 		{
 			name:     "nested_fields",
 			input:    `a: [{x: y: z: _, w: "foo"}.w]: {}`,
@@ -58,6 +60,7 @@ a: [{x: {
 w: "foo"}.w]: {}
 `[1:],
 		},
+
 		{
 			name:     "comma_separated",
 			input:    `  a: b: _, x: y: _`,
@@ -68,6 +71,21 @@ w: "foo"}.w]: {}
   	b: _
   }
   x: y: _
+`[1:],
+		},
+
+		{
+			name: "multiline_labels",
+			input: `
+a:
+	b: _
+`[1:],
+			position: protocol.Position{Line: 1, Character: 1}, // b
+			expected: `
+a:
+	{
+		b: _
+	}
 `[1:],
 		},
 	}
@@ -101,6 +119,132 @@ w: "foo"}.w]: {}
 				})
 				if !found {
 					t.Fatal("Failed to find ConvertToStruct code action")
+				}
+
+				env.ApplyCodeAction(action)
+				after := env.BufferText("input.cue")
+				qt.Check(t, qt.Equals(after, tc.expected))
+			})
+		})
+	}
+}
+
+func TestCodeActionConvertFromStruct(t *testing.T) {
+	type testCase struct {
+		name     string
+		input    string
+		position protocol.Position
+		expected string
+	}
+	testCases := []testCase{
+		{
+			name: "simple",
+			input: `
+foo: {
+	bar: "baz"
+}
+`[1:],
+			position: protocol.Position{Line: 1, Character: 1}, // bar
+			expected: `
+foo: bar: "baz"
+`[1:],
+		},
+
+		{
+			name: "multiline_field",
+			input: `
+before: _
+          foo: {
+          	bar: {
+          		"baz"
+          	}
+          }
+  after: _ // weird indent
+`[1:],
+			position: protocol.Position{Line: 2, Character: 11}, // bar
+			expected: `
+before: _
+          foo: bar: {
+          			"baz"
+          		}
+  after: _ // weird indent
+`[1:],
+		},
+
+		{
+			name: "nested_fields",
+			input: `
+a: [{x: {
+	y: z: _
+}
+w: "foo"}.w]: {}
+`[1:],
+			position: protocol.Position{Line: 1, Character: 1}, // y
+			expected: `
+a: [{x: y: z: _
+w: "foo"}.w]: {}
+`[1:],
+		},
+
+		{
+			name: "comma_separated",
+			input: `
+  a: {
+  	b: _
+  }, x: y: _
+`[1:],
+			position: protocol.Position{Line: 1, Character: 3}, // b
+			expected: `
+  a: b: _
+  x: y: _
+`[1:],
+		},
+
+		{
+			name: "multiline_labels",
+			input: `
+a:
+	{
+		b: _
+	}
+`[1:],
+			position: protocol.Position{Line: 2, Character: 2}, // b
+			expected: `
+a:
+	b: _
+`[1:],
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			WithOptions(RootURIAsDefaultFolder()).Run(t, "-- input.cue --\n"+tc.input, func(t *testing.T, env *Env) {
+				env.OpenFile("input.cue")
+				env.Await(env.DoneWithOpen())
+				rootURI := env.Sandbox.Workdir.RootURI()
+
+				cursor := protocol.Location{
+					URI: rootURI + "/input.cue",
+					Range: protocol.Range{
+						Start: tc.position,
+					},
+				}
+
+				actions, err := env.Editor.CodeAction(env.Ctx, cursor, nil)
+				if err != nil {
+					qt.Assert(t, qt.IsNil(err))
+				}
+
+				var action protocol.CodeAction
+				found := slices.ContainsFunc(actions, func(a protocol.CodeAction) bool {
+					if a.Title == "Unwrap field from struct" {
+						action = a
+						return true
+					}
+					return false
+				})
+				if !found {
+					t.Fatal("Failed to find ConvertFromStruct code action")
 				}
 
 				env.ApplyCodeAction(action)

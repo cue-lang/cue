3a8b676e80524284060b4770fa94f978538d071c
Matthew Sackman <matthew@cue.works>
2026-02-09 13:14:14 +0000

    integration/workspace: add test showing bad behaviour
    This test shows that spaces in filenames cause problems: the edits to
    the file are not properly applied to the overlay, and consequently the
    wrong Hover results are returned.
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I4c70e34d7826c4775fecd1e1b4d17695ff8796b9

--- cmd/cue/cmd/integration/workspace/symlink_test.go
+++ cmd/cue/cmd/integration/workspace/symlink_test.go
@@ -3,11 +3,13 @@ package workspace
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
 	"cuelang.org/go/internal/golangorgx/gopls/protocol"
 	. "cuelang.org/go/internal/golangorgx/gopls/test/integration"
+	"cuelang.org/go/internal/golangorgx/gopls/test/integration/fake"
 	"github.com/go-quicktest/qt"
 )
 
@@ -60,5 +62,50 @@ package a
 			env.DoneWithClose(),
 			LogExactf(protocol.Debug, 3, false, "Package dirs=[%v] importPath=mod.example/x@v0:a Reloaded", rootURI),
 		)
+	})
+}
+
+func TestSymlinksAncestor(t *testing.T) {
+	const files = `
+-- real/cue.mod/module.cue --
+module: "mod.example/x"
+language: version: "v0.11.0"
+
+-- real/a.cue --
+package a
+
+y: x
+
+// docs for x
+x: _
+`
+	WithOptions(RootURIAsDefaultFolder()).Run(t, files, func(t *testing.T, env *Env) {
+		rootURI := env.Sandbox.Workdir.RootURI()
+		rootFilePath := rootURI.Path()
+
+		err := os.Symlink(filepath.Join(rootFilePath, "real"), filepath.Join(rootFilePath, "sym link"))
+		qt.Assert(t, qt.IsNil(err))
+
+		env.OpenFile("sym link/a.cue")
+		env.Await(
+			env.DoneWithOpen(),
+			LogExactf(protocol.Debug, 1, false, "Package dirs=[%v/sym%%20link] importPath=mod.example/x@v0:a Reloaded", rootURI),
+		)
+
+		got, _ := env.Hover(protocol.Location{
+			URI:   rootURI + "/sym%20link/a.cue",
+			Range: protocol.Range{Start: protocol.Position{Line: 2, Character: 3}},
+		})
+		qt.Assert(t, qt.IsTrue(strings.HasPrefix(got.Value, "docs for x")))
+
+		// add a newline (and comment so that the hover coords exist), before line 2
+		env.EditBuffer("sym link/a.cue", fake.NewEdit(2, 0, 2, 0, "// hi\n"))
+		env.Await(env.DoneWithChange())
+
+		got, _ = env.Hover(protocol.Location{
+			URI:   rootURI + "/sym%20link/a.cue",
+			Range: protocol.Range{Start: protocol.Position{Line: 2, Character: 3}},
+		})
+		qt.Assert(t, qt.IsNotNil(got)) // This is WRONG: it *should* be nil.
 	})
 }

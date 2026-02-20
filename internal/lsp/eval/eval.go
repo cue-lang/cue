6b02ade122a2e7e5ed1667bda7fc1f72510064cd
Matthew Sackman <matthew@cue.works>
2026-02-20 17:12:16 +0000

    lsp/eval: fixes for comprehensions
    It woz wrong.
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I58e2527cf63d2159453267f89e32866e42ccaf7e

--- internal/lsp/eval/eval.go
+++ internal/lsp/eval/eval.go
@@ -1651,6 +1651,8 @@ type frame struct {
 	// have explicit cases within [frame.eval]. A good example is
 	// BasicLit: within a BasicLit, no completions should be offered.
 	unknownRanges *rangeset.RangeSet
+
+	comprehensionTails []ast.Node
 }
 
 // isFileFrame reports whether f is the top level package frame or a
@@ -1993,6 +1995,17 @@ func (f *frame) eval() {
 		case *ast.Comprehension:
 			clause := node.Clauses[0]
 			unprocessed = append(unprocessed, clause)
+			value := (ast.Node)(node.Value)
+			found := false
+			for parent := f.parent; parent != nil; parent = parent.parent {
+				if slices.Contains(parent.comprehensionTails, value) {
+					found = true
+					break
+				}
+			}
+			if !found {
+				f.comprehensionTails = append(f.comprehensionTails, value)
+			}
 			// We don't know how many child frames we'll need to
 			// process clause. So we stash whatever remains of this
 			// comprehension and can later find it once we've finished
@@ -2005,7 +2018,7 @@ func (f *frame) eval() {
 				// clause gets processed in this frame, and we make sure we
 				// can later use that last clause to find the body (value)
 				// of this comprehension.
-				comprehensionsStash[clause] = node.Value
+				comprehensionsStash[clause] = value
 			} else {
 				// Non-base-case: we're processing the first clause in
 				// this frame, and all that remain go into a copy of the
@@ -2020,12 +2033,26 @@ func (f *frame) eval() {
 			f.newFrame(node.Condition, nil)
 
 			comprehensionTail := comprehensionsStash[node]
-			f.newFrame(comprehensionTail, f.navigable)
+			tailFr := f.newFrame(comprehensionTail, nil)
+			f.navigable.ensureResolvesTo([]*navigable{tailFr.navigable})
+
+			// TODO: factor this out
+			if _, isCompr := comprehensionTail.(*ast.Comprehension); !isCompr {
+				for fr := f; fr != nil; fr = fr.parent {
+					if !slices.Contains(fr.comprehensionTails, comprehensionTail) {
+						continue
+					}
+					tailFr.navigable.ensureResolvesTo([]*navigable{fr.navigable})
+					break
+				}
+			}
 
 		case *ast.ForClause:
 			f.newFrame(node.Source, nil)
 
-			stack := frameStack{f.newFrame(nil, nil)}
+			childFr := f.newFrame(nil, nil)
+			f.navigable.ensureResolvesTo([]*navigable{childFr.navigable})
+			stack := frameStack{childFr}
 
 			if key := node.Key; key != nil {
 				stack.push(key, stack.peek().newBinding(key, nil))
@@ -2035,7 +2062,18 @@ func (f *frame) eval() {
 			}
 
 			comprehensionTail := comprehensionsStash[node]
-			stack.push(comprehensionTail, stack.peek().newFrame(comprehensionTail, f.navigable))
+			tailFr := stack.peek().newFrame(comprehensionTail, nil)
+			stack.push(comprehensionTail, tailFr)
+
+			if _, isCompr := comprehensionTail.(*ast.Comprehension); !isCompr {
+				for fr := f; fr != nil; fr = fr.parent {
+					if !slices.Contains(fr.comprehensionTails, comprehensionTail) {
+						continue
+					}
+					tailFr.navigable.ensureResolvesTo([]*navigable{fr.navigable})
+					break
+				}
+			}
 
 		case *ast.LetClause:
 			ident := node.Ident
@@ -2044,9 +2082,22 @@ func (f *frame) eval() {
 				// We're within a wider comprehension.
 				f.newFrame(node.Expr, nil)
 
-				stack := frameStack{f.newFrame(nil, nil)}
+				childFr := f.newFrame(nil, nil)
+				f.navigable.ensureResolvesTo([]*navigable{childFr.navigable})
+				stack := frameStack{childFr}
 				stack.push(ident, stack.peek().newBinding(ident, nil))
-				stack.push(comprehensionTail, stack.peek().newFrame(comprehensionTail, f.navigable))
+				tailFr := stack.peek().newFrame(comprehensionTail, nil)
+				stack.push(comprehensionTail, tailFr)
+
+				if _, isCompr := comprehensionTail.(*ast.Comprehension); !isCompr {
+					for fr := f; fr != nil; fr = fr.parent {
+						if !slices.Contains(fr.comprehensionTails, comprehensionTail) {
+							continue
+						}
+						tailFr.navigable.ensureResolvesTo([]*navigable{fr.navigable})
+						break
+					}
+				}
 
 			} else {
 				// We're not within a wider comprehension: the binding
@@ -2057,16 +2108,31 @@ func (f *frame) eval() {
 
 		case *ast.TryClause:
 			comprehensionTail := comprehensionsStash[node]
+			var tailFr *frame
 			if ident := node.Ident; ident != nil {
 				// Assignment form: try x = expr { ... }
 				f.newFrame(node.Expr, nil)
 
-				stack := frameStack{f.newFrame(nil, nil)}
+				childFr := f.newFrame(nil, nil)
+				f.navigable.ensureResolvesTo([]*navigable{childFr.navigable})
+				stack := frameStack{childFr}
 				stack.push(ident, stack.peek().newBinding(ident, nil))
-				stack.push(comprehensionTail, stack.peek().newFrame(comprehensionTail, f.navigable))
+				tailFr = stack.peek().newFrame(comprehensionTail, nil)
+				stack.push(comprehensionTail, tailFr)
 			} else {
 				// Struct form: try { ... }
-				f.newFrame(comprehensionTail, f.navigable)
+				tailFr = f.newFrame(comprehensionTail, nil)
+				f.navigable.ensureResolvesTo([]*navigable{tailFr.navigable})
+			}
+
+			if _, isCompr := comprehensionTail.(*ast.Comprehension); !isCompr {
+				for fr := f; fr != nil; fr = fr.parent {
+					if !slices.Contains(fr.comprehensionTails, comprehensionTail) {
+						continue
+					}
+					tailFr.navigable.ensureResolvesTo([]*navigable{fr.navigable})
+					break
+				}
 			}
 
 		case *ast.Field:
@@ -2888,18 +2954,23 @@ func (p *path) definitionsForOffset(offset int) (int, []*navigable) {
 // stack (stacks grow upwards).
 type frameStack []*frame
 
-func (stack *frameStack) push(n ast.Node, node *frame) {
-	nodes := *stack
-	for _, node := range nodes {
-		node.addRange(n)
+func (stack *frameStack) push(n ast.Node, childFr *frame) {
+	frames := *stack
+	var lastFr *frame
+	for _, fr := range frames {
+		fr.addRange(n)
+		lastFr = fr
 	}
-	*stack = append(nodes, node)
+	if lastFr != nil {
+		lastFr.navigable.ensureResolvesTo([]*navigable{childFr.navigable})
+	}
+	*stack = append(frames, childFr)
 }
 
 func (stack *frameStack) peek() *frame {
-	nodes := *stack
-	if len(nodes) == 0 {
+	frames := *stack
+	if len(frames) == 0 {
 		return nil
 	}
-	return nodes[len(nodes)-1]
+	return frames[len(frames)-1]
 }

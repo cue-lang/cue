392ad8f8bd6310670f8882a48e697c9ca2dd7990
Roger Peppe <rogpeppe@gmail.com>
2026-03-04 17:38:52 +0000

    cue: implement user-provided function support
    This implements initial support for user-provided functions,
    as proposed and discussed here: https://github.com/cue-lang/cue/discussions/4293
    It adds an adaptor function for each argument count
    to create a Value that can be called within CUE.
    
    Only pure functions are supported for now, and passing
    on non-concrete values is not allowed, although that
    restriction is not implemented yet.
    
    Open question: what should we do about exporting such values?
    For now we've made exporting a func an error.
    
    Signed-off-by: Roger Peppe <rogpeppe@gmail.com>
    Change-Id: I95f0a6449fdda0808613ee67b54c10bf0b95df54

---
+++ cue/func.go
@@ -0,0 +1,70 @@
+// Copyright 2025 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cue
+
+//go:generate go run generate_func.go
+
+import (
+	"reflect"
+
+	"cuelang.org/go/internal/core/adt"
+	"cuelang.org/go/internal/core/convert"
+	"cuelang.org/go/internal/core/runtime"
+)
+
+// FuncOption configures a function created by [NewPureFunc1], [NewPureFunc2], etc.
+type FuncOption func(*funcConfig)
+
+type funcConfig struct {
+	name string
+}
+
+// Name sets the name of the function for error messages.
+func Name(name string) FuncOption {
+	return func(cfg *funcConfig) {
+		cfg.name = name
+	}
+}
+
+func newPureFunc[Args, R any](f func(Args) (R, error), opts []FuncOption) Value {
+	var cfg funcConfig
+	for _, o := range opts {
+		o(&cfg)
+	}
+	ctx := (*Context)(runtime.New()) // Share the context between all values.
+	argT := reflect.TypeFor[Args]()
+	return newValueRoot(ctx.runtime(), ctx.ctx(), &adt.Func{
+		Name: cfg.name,
+		Func: func(opCtx *adt.OpContext, argValues []adt.Value) adt.Expr {
+			numArgs := argT.NumField()
+			if len(argValues) != numArgs {
+				return opCtx.NewErrf("expected %d argument(s), got %d", numArgs, len(argValues))
+			}
+			var args Args
+			dstArgs := reflect.ValueOf(&args).Elem()
+			for i := range numArgs {
+				argVal := newValueRoot(ctx.runtime(), opCtx, argValues[i])
+				if err := argVal.Decode(dstArgs.Field(i).Addr().Interface()); err != nil {
+					return opCtx.NewErrf("decoding argument %d: %v", i, err)
+				}
+			}
+			result, err := f(args)
+			if err != nil {
+				return opCtx.NewErrf("%v", err)
+			}
+			return convert.FromGoValue(opCtx, result, false)
+		},
+	})
+}

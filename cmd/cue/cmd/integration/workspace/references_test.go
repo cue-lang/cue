a5e0ef561d37d2a9d769fceecffee26c9192a50c
Matthew Sackman <matthew@cue.works>
2026-03-16 15:13:31 +0000

    integration/workspace: add test showing bad behaviour
    This test shows renamings are being applied to aliases, which is
    incorrect. Fixed in the next CL.
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I454c226f0708291599adfad4735c1f41e335814a
    Reviewed-on: https://cue.gerrithub.io/c/cue-lang/cue/+/1233637
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1233873
    Reviewed-by: Roger Peppe <rogpeppe@gmail.com>

--- cmd/cue/cmd/integration/workspace/references_test.go
+++ cmd/cue/cmd/integration/workspace/references_test.go
@@ -1,17 +1,18 @@
 package workspace
 
 import (
-	"strings"
 	"testing"
 
 	"cuelang.org/go/internal/golangorgx/gopls/protocol"
 	. "cuelang.org/go/internal/golangorgx/gopls/test/integration"
 
 	"github.com/go-quicktest/qt"
-	"golang.org/x/tools/txtar"
 )
 
-const referencesFiles = `
+// TestReferences checks that querying for references will load
+// packages within the current module and search them for references.
+func TestReferences(t *testing.T) {
+	const files = `
 -- cue.mod/module.cue --
 module: "example.com/bar"
 language: version: "v0.14.0"
@@ -61,45 +62,9 @@ package dontload
 // packages are loaded.
 
 greeting: "hello"
--- want/a/a.cue --
-package a
-
-result: 3
-a1: a2: result
--- want/b/b1.cue --
-package b
-
-import "example.com/bar/a"
-
-b1: a.result
--- want/b/b2.cue --
-package b
-
-import "example.com/bar/a:a"
-
-b2: a.result
--- want/b/b3.cue --
-package b
-
-import mya "example.com/bar/a"
-
-b3: mya.result
--- want/c/c.cue --
-package c
-
-import p1 "example.com/bar/a"
-import p2 "example.com/bar/a"
-import p3 "example.com/bar/a"
-
-c1: p1.result
-c2: p2.result
-c3: p3.result
 `
 
-// TestReferences checks that querying for references will load
-// packages within the current module and search them for references.
-func TestReferences(t *testing.T) {
-	WithOptions(RootURIAsDefaultFolder()).Run(t, referencesFiles, func(t *testing.T, env *Env) {
+	WithOptions(RootURIAsDefaultFolder()).Run(t, files, func(t *testing.T, env *Env) {
 		rootURI := env.Sandbox.Workdir.RootURI()
 		env.OpenFile("a/a.cue")
 		env.Await(
@@ -181,45 +146,5 @@ func TestReferences(t *testing.T) {
 			// only belongs to the nested module.
 			NoLogExactf(protocol.Debug, "Package dirs=[%v/nested] importPath=example.com/bar/nested@v0:dontload", rootURI),
 		)
-	})
-}
-
-func TestRename(t *testing.T) {
-	WithOptions(RootURIAsDefaultFolder()).Run(t, referencesFiles, func(t *testing.T, env *Env) {
-		rootURI := env.Sandbox.Workdir.RootURI()
-		env.OpenFile("c/c.cue")
-		env.Await(
-			env.DoneWithOpen(),
-			LogExactf(protocol.Debug, 1, false, "Package dirs=[%v/a] importPath=example.com/bar/a@v0 Reloaded", rootURI),
-		)
-
-		result, err := env.Editor.Server.PrepareRename(env.Ctx, &protocol.PrepareRenameParams{
-			TextDocumentPositionParams: protocol.TextDocumentPositionParams{
-				TextDocument: protocol.TextDocumentIdentifier{
-					URI: rootURI + "/c/c.cue",
-				},
-				Position: protocol.Position{Line: 6, Character: 8}, // the u of out
-			},
-		})
-		qt.Assert(t, qt.IsNil(err))
-		// PrepareRename should return the range which is the whole of out
-		qt.Assert(t, qt.DeepEquals(result.Range, protocol.Range{
-			Start: protocol.Position{Line: 6, Character: 7},
-			End:   protocol.Position{Line: 6, Character: 10},
-		}))
-		qt.Assert(t, qt.Equals(result.Placeholder, "out"))
-
-		// Now do the rename for real
-		env.Rename(protocol.Location{
-			URI:   rootURI + "/c/c.cue",
-			Range: protocol.Range{Start: protocol.Position{Line: 6, Character: 8}}, // the u of out
-		}, "result")
-
-		for _, file := range txtar.Parse([]byte(referencesFiles)).Files {
-			if filename, ok := strings.CutPrefix(file.Name, "want/"); ok {
-				content, _ := env.Editor.BufferText(filename)
-				qt.Check(t, qt.Equals(content, string(file.Data)), qt.Commentf("%s", filename))
-			}
-		}
 	})
 }

70a104f80e20ed70ec7801ae26a0ed468d075182
Matthew Sackman <matthew@cue.works>
2026-04-10 13:31:36 +0100

    internal/lsp: implement organise imports code action
    For now, this is limited to sorting blocks of imports, and removing
    unused imports.
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I8810191c56bdb4df00b5e674ac364051bc689b5a
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cue/+/1235340
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>

--- internal/lsp/eval/eval.go
+++ internal/lsp/eval/eval.go
@@ -2640,7 +2640,11 @@ func (f *frame) resolvePathRoot(name string, requireIdent bool) (*navigable, str
 				return parentNav, ""
 			}
 			// Finally, inspect the globals
-			return fNav.parent.bindings[name], ""
+			if fNav.parent == nil {
+				return nil, ""
+			} else {
+				return fNav.parent.bindings[name], ""
+			}
 		}
 	}
 	return nil, ""

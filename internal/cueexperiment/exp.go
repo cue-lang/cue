afc9cab6502afe052f16eb3cb0319bd520e3866d
Matthew Sackman <matthew@cue.works>
2026-06-16 17:21:18 +0100

    internal/cueexperiment: rename FormatterV2 to FormatV2
    There's a preference for shorter names.
    
    Signed-off-by: Matthew Sackman <matthew@cue.works>
    Change-Id: I4f940125e4cba5c94068295c64c65c0264067706
    Reviewed-on: https://cue.gerrithub.io/c/cue-lang/cue/+/1239620
    Unity-Result: CUE porcuepine <cue.porcuepine@gmail.com>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>

--- internal/cueexperiment/exp.go
+++ internal/cueexperiment/exp.go
@@ -17,9 +17,9 @@ type Config struct {
 	// The flags in this first section describe active experiments.
 	// Sort from oldest to newest based on when they were introduced as a `preview`.
 
-	// FormatterV2 selects the Wadler-Lindig pretty-printer as the
+	// FormatV2 selects the Wadler-Lindig pretty-printer as the
 	// implementation behind "cue fmt" and "cue/format".
-	FormatterV2 bool `experiment:"default:v0.18.0"`
+	FormatV2 bool `experiment:"default:v0.18.0"`
 
 	// The flags in this second section describe completed experiments; they can still be set
 	// as long as the value aligns with the final behavior once the experiment finished.

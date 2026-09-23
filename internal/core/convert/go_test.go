776e2594d0e68b190b95e36e484db52a58706acf
Daniel Martí <mvdan@mvdan.cc>
2026-09-23 14:20:34 +0100

    internal/core/convert: test struct field names from protobuf tags
    Converting a Go struct field to CUE takes the text before the first
    comma of its protobuf tag as the field name, which is the wire type
    such as "bytes" rather than the name given by the "name=" option.
    
    Moreover, a json tag which gives no name, such as `json:",omitempty"`,
    does not stop the conversion from taking a name from a yaml or protobuf
    tag, whereas encoding/json would use the Go field name.
    
    Add test cases showing both problems.
    
    For #2261
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I0c1be00f2c59803ab38c9197fca2936b43a1d9cb
    GitOrigin-RevId: b113c072497621e536ebdeb34bb02b2f80a43538

--- internal/core/convert/go_test.go
+++ internal/core/convert/go_test.go
@@ -287,12 +287,22 @@ func TestConvert(t *testing.T) {
 }`,
 	}, {
 		struct {
-			A int `json:"" yaml:"" protobuf:"aa"`
-			B int `yaml:"cc" json:"bb" protobuf:"aa"`
-		}{3, 4},
+			A int    `json:"" yaml:"aa" protobuf:"bytes,1,opt,name=aa"`
+			B int    `yaml:"cc" json:"bb" protobuf:"bytes,2,opt,name=aa"`
+			C string `json:",omitempty" protobuf:"bytes,3,opt,name=foo"`
+			D int    `yaml:"dd" protobuf:"varint,4,opt,name=foo"`
+			E int    `protobuf:"varint,5,opt,name=ee,proto3"`
+			F int    `protobuf:"fixed64,6,opt"`
+		}{3, 4, "hello", 5, 6, 7},
+		// TODO: A should keep its Go name, as its json tag gives no name,
+		// and C, E, and F should not take protobuf's wire type as a name.
 		`(struct){
   aa: (int){ 3 }
   bb: (int){ 4 }
+  bytes: (string){ "hello" }
+  dd: (int){ 5 }
+  varint: (int){ 6 }
+  fixed64: (int){ 7 }
 }`,
 	}, {
 		&struct{ A int }{3}, `(struct){
@@ -424,6 +434,16 @@ func TestConvertType(t *testing.T) {
   A?: (int){ &(>=-9223372036854775808, <=9223372036854775807, int) }
   B?: (int){ &(>=-9223372036854775808, <=9223372036854775807, int) }
   c?: (int){ &(>=-9223372036854775808, <=9223372036854775807, int) }
+}`,
+	}, {
+		goTyp: struct {
+			A string `json:",omitempty" protobuf:"bytes,1,opt,name=foo"`
+			B int    `protobuf:"varint,2,opt,name=bb"`
+		}{},
+		// TODO: A and B should not take protobuf's wire type as a name.
+		want: `(struct){
+  bytes?: (string){ string }
+  varint: (int){ &(>=-9223372036854775808, <=9223372036854775807, int) }
 }`,
 	}, {
 		goTyp: struct {
